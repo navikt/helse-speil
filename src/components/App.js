24408ada092fcbe16a98d11613822ee16c8735a1
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2019-03-17 13:16:30 +0100

    linting

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import "./App.css";
-import HeaderBar from "./HeaderBar/HeaderBar";
+import React from 'react';
+import './App.css';
+import HeaderBar from './HeaderBar/HeaderBar';
 
 export default class App extends React.Component {
    render() {
       return (
          <div>
-            <HeaderBar displayname="Ukjent Ukjentsen" />
-            <div className="main">main content</div>
+            <HeaderBar displayname='S. Aksbehandler' />
+            <div className='main'>main content</div>
          </div>
       );
    }
