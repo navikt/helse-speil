fdc7d68d5b8f15f81e16c782d2e063337b04828a
Erlend Vige <erlend.vige@gmail.com>
2019-03-20 13:31:19 +0100

    Made <LabelWithIcon> component, rename Appplicant to Applicant.

--- src/components/Applicant/Applicant.js
+++ src/components/Applicant/Applicant.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Applicant.css'
 import fnr from './img/id.png'
 import phone from './img/phone.png'
+import LabelWithIcon from '../LabelWithIcon/LabelWithIcon';
 
 export default class Applicant extends React.Component {
    render() {
@@ -9,8 +10,8 @@ export default class Applicant extends React.Component {
          <div className='applicant'>
             <h3 className='personName'>Søker Søkersen</h3>
             <div className='labels'>
-               <div className='labelWithIcon'><img className='icon' src={phone} /><span className='label'>98765432</span></div>
-               <div className='labelWithIcon'><img className='icon' src={fnr} /><span className='label'>12345678910</span></div>
+               <LabelWithIcon iconSrc={phone} label='98765432' />
+               <LabelWithIcon iconSrc={fnr} label='12345678910' />
             </div>
          </div>
       )
