90a37dc7687da904a5a2cffecd981de0473b2ad4
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2019-03-20 10:29:42 +0100

    start of Applicant component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import HeaderBar from './HeaderBar/HeaderBar'
+import Applicant from './Applicant/Appplicant'
 
 export default class App extends React.Component {
    render() {
       return (
          <div>
             <HeaderBar displayname='S. Aksbehandler' />
+            <Applicant />
             <div className='main'>main content</div>
          </div>
       )
