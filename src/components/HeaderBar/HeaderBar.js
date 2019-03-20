25f3380e3253859b18f99ebdc9de0e53b7bc6be3
Erlend Vige <erlend.vige@gmail.com>
2019-03-20 13:50:45 +0100

    Prettier formatting

--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -1,56 +1,55 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './HeaderBar.css'
-import navLogoSrc from './img/navlogo.svg'
-import rettskildeneSrc from './img/rettskildene.svg'
-import systemrutineSrc from './img/systemrutine.svg'
-import navAnsattSrc from './img/navansatt.svg'
+import React from "react";
+import PropTypes from "prop-types";
+import "./HeaderBar.css";
+import navLogoSrc from "./img/navlogo.svg";
+import rettskildeneSrc from "./img/rettskildene.svg";
+import systemrutineSrc from "./img/systemrutine.svg";
+import navAnsattSrc from "./img/navansatt.svg";
 
 export default class HeaderBar extends React.Component {
-
    render() {
       return (
-         <header className='topbar'>
-            <div className='leftElements'>
-               <div className='imgitem'>
-                  <a href='/'>
-                     <img src={navLogoSrc} className='header_img' />
+         <header className="topbar">
+            <div className="leftElements">
+               <div className="imgitem">
+                  <a href="/">
+                     <img src={navLogoSrc} className="header_img" />
                   </a>
                </div>
-               <div className='header_divider' />
-               <div className='header_title item'>
+               <div className="header_divider" />
+               <div className="header_title item">
                   <h2>Sykepenger</h2>
                </div>
             </div>
 
-            <div className='rightElements'>
-               <div className='imgitem'>
-                  <a href='https://www.nav.no'>
-                     <img src={systemrutineSrc} className='header_img' />
+            <div className="rightElements">
+               <div className="imgitem">
+                  <a href="https://www.nav.no">
+                     <img src={systemrutineSrc} className="header_img" />
                   </a>
                </div>
-               <div className='imgitem'>
-                  <a href='https://www.nav.no'>
-                     <img src={rettskildeneSrc} className='header_img' />
+               <div className="imgitem">
+                  <a href="https://www.nav.no">
+                     <img src={rettskildeneSrc} className="header_img" />
                   </a>
                </div>
-               <div className='header_divider' />
-               <div className='imgitem'>
-                  <img src={navAnsattSrc} className='header_img' />
+               <div className="header_divider" />
+               <div className="imgitem">
+                  <img src={navAnsattSrc} className="header_img" />
                </div>
-               <div id='bruker' className='brukernavn'>
+               <div id="bruker" className="brukernavn">
                   {this.props.displayname}
                </div>
             </div>
          </header>
-      )
+      );
    }
 }
 
 HeaderBar.propTypes = {
    displayname: PropTypes.string
-}
+};
 
 HeaderBar.defaultProps = {
    displayname: "Ukjent Ukjentsen"
-}
+};
--- tests/HeaderBar.test.js
+++ tests/HeaderBar.test.js
@@ -1,18 +1,31 @@
-import React from "react"
-import { render, fireEvent, cleanup, waitForElement } from "react-testing-library"
-import HeaderBar from "../src/components/HeaderBar/HeaderBar"
-import 'jest-dom/extend-expect'
+import React from "react";
+import {
+   render,
+   fireEvent,
+   cleanup,
+   waitForElement
+} from "react-testing-library";
+import HeaderBar from "../src/components/HeaderBar/HeaderBar";
+import "jest-dom/extend-expect";
 
-afterEach(cleanup)
+afterEach(cleanup);
 
 test("Default value is used for rukernavn if none provided", async () => {
-   const { getByText, getByTestId, container, asFragment } = render(<HeaderBar />)
-   const brukerTextNode = await waitForElement(() => container.querySelector('#bruker'))
-   expect(brukerTextNode).toHaveTextContent('Ukjent Ukjentsen')
-})
+   const { getByText, getByTestId, container, asFragment } = render(
+      <HeaderBar />
+   );
+   const brukerTextNode = await waitForElement(() =>
+      container.querySelector("#bruker")
+   );
+   expect(brukerTextNode).toHaveTextContent("Ukjent Ukjentsen");
+});
 
 test("Provided value is used as brukernavn", async () => {
-   const { getByText, getByTestId, container, asFragment } = render(<HeaderBar displayname="john doe" />)
-   const brukerTextNode = await waitForElement(() => container.querySelector('#bruker'))
-   expect(brukerTextNode).toHaveTextContent('john doe')
-})
+   const { getByText, getByTestId, container, asFragment } = render(
+      <HeaderBar displayname="john doe" />
+   );
+   const brukerTextNode = await waitForElement(() =>
+      container.querySelector("#bruker")
+   );
+   expect(brukerTextNode).toHaveTextContent("john doe");
+});
