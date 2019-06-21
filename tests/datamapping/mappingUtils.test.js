dabf7364dac3448723c28edaa4ad9ed4c780d0bc
Stephen Ramthun <stephen.ramthun@bekk.no>
2019-06-21 08:53:02 +0200

    Add tests

---
+++ tests/datamapping/mappingUtils.test.js
@@ -0,0 +1,14 @@
+import { toDate, capitalize } from '../../src/datamapping/mappingUtils';
+import 'jest-dom/extend-expect';
+
+test('toDate', () => {
+    expect(toDate('notadate')).toEqual('Invalid Date');
+    expect(toDate('12.4.2019')).toEqual('12/4/2019');
+    expect(toDate('2019-05-09T00:00:00.000Z')).toEqual('5/9/2019');
+});
+
+test('capitalize', () => {
+    expect(capitalize('abCDef')).toEqual('Abcdef');
+    expect(capitalize('123abcDEF')).toEqual('123abcdef');
+    expect(capitalize('ABCDEF')).toEqual('Abcdef');
+});
