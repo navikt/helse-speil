6112d6c65b2e71925fae550fcc1a7db94ccd6dfd
Stephen Ramthun <stephen.ramthun@bekk.no>
2019-06-24 08:07:32 +0200

    Add page components (#18)
    * Add page-components
    
    * Add HOC for getting and validating behandlinger-context
    
    * Use withBehandlingContext
    
    * Add routes for pages

---
+++ src/components/pages/Inngangsvilkår/Inngangsvilkår.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Bolk from '../../widgets/Bolk';
+import ItemMapper from '../../../datamapping/inngangsvilkårMapper';
+import { Panel } from 'nav-frontend-paneler';
+import { Undertittel } from 'nav-frontend-typografi';
+import { inngangsvilkårtekster as tekster } from '../../../tekster';
+import { withBehandlingContext } from '../../../context/withBehandlingContext';
+
+const Inngangsvilkår = withBehandlingContext(({ behandling }) => (
+    <Panel border>
+        <Undertittel className="panel-tittel">
+            {tekster(`tittel`)}
+        </Undertittel>
+        <Bolk title="Inngangsvilkår oppfylt" />
+        <Bolk
+            title="Medlemskap"
+            items={ItemMapper.medlemskap(behandling.inngangsvilkår.medlemskap)}
+        />
+        <Bolk
+            title="Opptjening"
+            items={ItemMapper.opptjening(behandling.inngangsvilkår.opptjening)}
+        />
+        <Bolk
+            title="Søknadsfrist"
+            items={ItemMapper.søknadsfrist(
+                behandling.inngangsvilkår.søknadsfrist
+            )}
+        />
+        <Bolk
+            title="Dager igjen"
+            items={ItemMapper.dagerIgjen(behandling.inngangsvilkår.dagerIgjen)}
+        />
+    </Panel>
+));
+
+export default Inngangsvilkår;
