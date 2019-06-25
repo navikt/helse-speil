923dc959fbeb084e140fe094552fb0eacd922ce6
Stephen Ramthun <stephen.ramthun@bekk.no>
2019-06-25 10:55:21 +0200

    Refactor

--- src/datamapping/mappingUtils.js
+++ src/datamapping/mappingUtils.js
@@ -1,7 +1,1 @@
 export const item = (label, value) => ({ label, value });
-
-export const toDate = dateString =>
-    new Date(dateString).toLocaleDateString('nb-NO');
-
-export const capitalize = string =>
-    string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
