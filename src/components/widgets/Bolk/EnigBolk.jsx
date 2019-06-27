e3fa15248ccd23283d8fe860105163f8d31d4b36
Stephen Ramthun <stephen.ramthun@bekk.no>
2019-06-27 10:42:31 +0200

    Add text input field to utbetaling

--- src/components/widgets/Bolk/EnigBolk.jsx
+++ src/components/widgets/Bolk/EnigBolk.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import Bolk from './Bolk';
 import ListeItem from '../ListeItem';
 import EnigBoks from '../EnigBoks';
+import PropTypes from 'prop-types';
 import './EnigBolk.css';
 
-const EnigBolk = ({ label, value }) => (
+const EnigBolk = ({ label, value, hasInputField }) => (
     <span className="EnigBolk">
         <ListeItem label={label} value={value} />
-        <EnigBoks hasInputField={false} />
+        <EnigBoks hasInputField={hasInputField} />
     </span>
 );
 
 EnigBolk.propTypes = {
-    ...Bolk.propTypes
+    ...Bolk.propTypes,
+    hasInputField: PropTypes.bool
+};
+
+EnigBolk.defaultProps = {
+    hasInputField: true
 };
 
 export default EnigBolk;
