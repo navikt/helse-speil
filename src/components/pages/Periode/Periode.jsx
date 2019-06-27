8c26e6b8fc0094bd0d491983edfbc8bd1a9e8454
Stephen Ramthun <stephen.ramthun@bekk.no>
2019-06-27 12:47:36 +0200

    Add Periode

--- src/components/pages/Periode/Periode.jsx
+++ src/components/pages/Periode/Periode.jsx
@@ -1,17 +1,81 @@
 import React from 'react';
 import { Undertittel } from 'nav-frontend-typografi';
 import { Panel } from 'nav-frontend-paneler';
+import Ikon from 'nav-frontend-ikoner-assets';
 import Navigasjonsknapper from '../../widgets/Navigasjonsknapper';
+import './Periode.css';
+import { withBehandlingContext } from '../../../context/withBehandlingContext';
+import ListeItemBolk from '../../widgets/Bolk/ListeItemBolk';
+import ListeSeparator from '../../widgets/ListeSeparator';
 
-const Periode = () => {
+const Periode = withBehandlingContext(({ behandling }) => {
+    console.log(behandling.periode);
+    const {
+        antallKalenderdager,
+        arbeidsgiverperiodeKalenderdager,
+        antallVirkedager,
+        antallFeriedager,
+        antallDager,
+        sykmeldingsgrad
+    } = behandling.periode;
     return (
-        <Panel border>
+        <Panel className="Periode" border>
             <Undertittel className="panel-tittel">
-                Beregning av sykepengeperiode
+                Sykepengeperiode
             </Undertittel>
+
+            <ListeItemBolk
+                label="Antall kalenderdager"
+                value={antallKalenderdager}
+            />
+            <ListeItemBolk
+                label="Arb.giverperiode, kalenderdager"
+                value={arbeidsgiverperiodeKalenderdager}
+            />
+
+            <ListeSeparator type="dotted" />
+
+            <ListeItemBolk
+                label="Antall virkedager"
+                value={antallVirkedager}
+            />
+            <ListeItemBolk
+                label="Ferie"
+                value={antallFeriedager ? antallFeriedager : '-'}
+            />
+
+            <ListeSeparator type="dotted" />
+
+            <ListeItemBolk
+                label="Antall dager"
+                value={antallDager}
+            />
+
+            <ListeSeparator type="dotted" />
+
+            <ListeItemBolk
+                label="Sykmeldingsgrad"
+                value={`${sykmeldingsgrad}%`}
+            />
+
+            <ListeSeparator />
+
+            <Undertittel>
+                Filter for MVP
+            </Undertittel>
+
+            <ListeItemBolk
+                label="Ingen friskmelding"
+                value={<Ikon kind="ok-sirkel-fyll" size={24} />}
+            />
+            <ListeItemBolk
+                label="Ingen gradering"
+                value={<Ikon kind="ok-sirkel-fyll" size={24} />}
+            />
+
             <Navigasjonsknapper previous="/beregning" next="/utbetaling" />
         </Panel>
     );
-};
+});
 
 export default Periode;
