10964498c052447f5110225afafc54bcdb7e5341
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2019-07-08 12:54:05 +0200

    fqdn for service discovery to avoid the proxy

--- src/server/config.js
+++ src/server/config.js
@@ -14,7 +14,7 @@ exports.oidc = {
 };
 
 exports.s3 = {
-    s3url: process.env.S3_URL || 'http://s3.nais-rook/',
+    s3url: process.env.S3_URL || 'http://s3.nais-rook.svc.nais.local',
     s3AccessKey: process.env.S3_ACCESS_KEY,
     s3SecretKey: process.env.S3_SECRET_KEY
 };
