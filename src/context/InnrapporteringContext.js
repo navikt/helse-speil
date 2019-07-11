9200defe8b255c0a977471586c5a05e9e06be8be
Stephen Ramthun <stephenramthun@gmail.com>
2019-07-11 10:47:10 +0200

    Remove unused imports

--- src/context/InnrapporteringContext.js
+++ src/context/InnrapporteringContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext } from 'react';
 import PropTypes from 'prop-types';
 import { useSessionStorage } from '../hooks/useSessionStorage';
 
