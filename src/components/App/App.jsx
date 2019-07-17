6f5b3bfd87233f41a41773a68594f0811905b621
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-07-17 15:37:30 +0200

    Gjeninnføre AuthContext for håndtering av inn-/utlogget og visning av navn

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HeaderBar from '../HeaderBar/HeaderBar';
 import MainContentWrapper from '../MainContentWrapper/MainContentWrapper';
 import { BehandlingerProvider } from '../../context/BehandlingerContext';
 import { InnrapporteringProvider } from '../../context/InnrapporteringContext';
+import { AuthProvider, AuthContext } from '../../context/AuthContext';
 import { withContextProviders } from '../../context/withContextProviders';
 import './App.css';
 import 'reset-css';
+import fetchIntercept from 'fetch-intercept';
 
-const App = withContextProviders(
-    () => (
+const App = withContextProviders(() => {
+    const { setUserLoggedOut } = useContext(AuthContext);
+
+    useEffect(() => {
+        fetchIntercept.register({
+            responseError: error => {
+                setUserLoggedOut();
+                return Promise.reject(error);
+            }
+        });
+    }, []);
+
+    return (
         <Router>
             <HeaderBar />
             <MainContentWrapper />
         </Router>
-    ),
-    [InnrapporteringProvider, BehandlingerProvider]
-);
+    );
+}, [InnrapporteringProvider, BehandlingerProvider, AuthProvider]);
 
 export default App;
--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,17 +1,5 @@
 'use strict';
 
-import { useState, useEffect } from 'react';
-
-const useLoggedInUser = defaultName => {
-    const [userNameFromCookie, setUserNameFromCookie] = useState(extractName());
-
-    useEffect(() => {
-        setUserNameFromCookie(extractName());
-    }, []);
-
-    return { name: userNameFromCookie || defaultName };
-};
-
 const extractName = () => {
     return document.cookie
         .split(';')
@@ -23,11 +11,15 @@ const extractName = () => {
 const decode = cookie => {
     const jwt = cookie.split('=')[1];
     try {
-        return JSON.parse(atob(jwt.split('.')[1]))['name'];
+        const tokenBody = jwt
+            .split('.')[1]
+            .replace(/%3D/g, '=')
+            .replace(/%3d/g, '=');
+        return JSON.parse(atob(tokenBody))['name'];
     } catch (err) {
         console.log(`error while extracting name: ${err}`); // eslint-disable-line no-console
         return null;
     }
 };
 
-export default useLoggedInUser;
+export default extractName;
