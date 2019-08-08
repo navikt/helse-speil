ff1bb373a0cd1d6fa638030b37d4042518c3f224
Stephen Ramthun <stephenramthun@gmail.com>
2019-08-08 12:44:01 +0200

    Refactor

--- src/components/widgets/Uenigboks.jsx
+++ src/components/widgets/Uenigboks.jsx
@@ -1,53 +1,48 @@
-import React, { useContext, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import DynamicTextarea from './DynamicTextarea';
 import { Checkbox } from 'nav-frontend-skjema';
-import { InnrapporteringContext } from '../../context/InnrapporteringContext';
 import { useFocusRef } from '../../hooks/useFocusRef';
 import './Uenigboks.css';
+import { useUenighet } from '../../hooks/useUenighet';
 
 const errorMessage = 'Du må oppgi årsak';
 
 const Uenigboks = ({ label }) => {
     const id = label + decodeURIComponent(window.location.pathname);
-    const innrapportering = useContext(InnrapporteringContext);
 
-    const uenighet = useMemo(
-        () => innrapportering.uenigheter.find(uenighet => uenighet.id === id),
-        [innrapportering.uenigheter]
-    );
+    const uenighet = useUenighet(id);
+    const [checked, setChecked] = useState(!!uenighet.value);
+    const [inputValue, setInputValue] = useState(uenighet.value || '');
 
-    const [checked, setChecked] = useState(!!uenighet);
-    const [inputValue, setInputValue] = useState(
-        uenighet ? uenighet.value : ''
-    );
+    const showValidationError = checked && !inputValue;
+    const ref = useFocusRef(showValidationError);
     const [error, setError] = useState(
-        checked && (!inputValue || inputValue === '') ? errorMessage : undefined
+        showValidationError ? errorMessage : undefined
     );
-    const ref = useFocusRef(checked && inputValue === '');
 
     useEffect(() => {
-        if (uenighet) {
+        setInputValue(uenighet.value || '');
+        if (uenighet.value) {
             setChecked(true);
-            setInputValue(uenighet.value || '');
         } else {
             setError(undefined);
         }
-    }, [uenighet]);
+    }, [uenighet.value]);
 
     const onCheckboxChange = e => {
         setChecked(e.target.checked);
         setInputValue('');
         if (e.target.checked) {
-            innrapportering.addUenighet(id, label);
+            uenighet.create(label);
         } else {
-            innrapportering.removeUenighet(id);
+            uenighet.remove();
         }
     };
 
     const onInputChange = e => {
         setInputValue(e.target.value);
-        innrapportering.updateUenighet(id, e.target.value);
+        uenighet.update(e.target.value);
     };
 
     const onInputBlur = e => {
