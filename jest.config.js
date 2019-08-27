98f93b3f2d0101c74da48005bc26001e7719b4b3
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-08-27 16:40:54 +0200

    Prettier jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-   moduleFileExtensions: ["js", "jsx", "json"],
-   transform: {
-      ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "babel-jest",
-      "^.+\\.js$": "babel-jest",
-      "^.+\\.jsx$": "babel-jest"
-   },
-   moduleNameMapper: {
-      "^@/(.*)$": "<rootDir>/src/$1",
-      "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
-      "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
-   },
-   transformIgnorePatterns: ["<rootDir>/node_modules/"]
-}
-
+    moduleFileExtensions: ['js', 'jsx', 'json'],
+    transform: {
+        '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'babel-jest',
+        '^.+\\.js$': 'babel-jest',
+        '^.+\\.jsx$': 'babel-jest'
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+        '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js'
+    },
+    transformIgnorePatterns: ['<rootDir>/node_modules/']
+};
