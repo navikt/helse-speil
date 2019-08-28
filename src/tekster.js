8512670dfddf993a6cce94020745ad1360f378e6
Stephen Ramthun <stephen.ramthun@bekk.no>
2019-08-28 08:34:19 +0200

    Store comments/general feedback from users (#152)
    * Store comments/general feedback from users
    
    * Add 'kommentarer' to context initialization

--- src/tekster.js
+++ src/tekster.js
@@ -80,7 +80,7 @@ bokmål.oppsummering['sykmeldingsgrad'] = 'Sykmeldingsgrad';
 bokmål.oppsummering['utbetalesFom'] = 'Utbetales fra og med';
 bokmål.oppsummering['utbetalesTom'] = 'Utbetales til og med';
 bokmål.oppsummering['utbetaling'] = 'Utbetaling';
-bokmål.oppsummering['innrapportert'] = 'Innrapportert';
+bokmål.oppsummering['innrapportert'] = 'Innrapportering';
 bokmål.oppsummering['ingen_uenigheter'] = 'Ingen uenigheter';
 bokmål.oppsummering['felter_uten_årsak'] =
     'Felter uten oppgitt årsak blir ikke sendt inn.';
