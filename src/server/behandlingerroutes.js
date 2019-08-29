2541d1f20541165532153a7bde219c01e1acd5d3
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-08-29 08:42:52 +0200

    Add second set of test data
    This one is fetched from preprod, using network log in Chrome.
    Identifying data has been masked.

--- src/server/behandlingerroutes.js
+++ src/server/behandlingerroutes.js
@@ -7,17 +7,25 @@ const api = require('./behandlingerlookup');
 const setup = app => {
     app.get('/behandlinger/:aktorId', (req, res) => {
         if (process.env.NODE_ENV === 'development') {
-            fs.readFile('__mock-data__/behandlinger.json', (err, data) => {
+            const filename =
+                req.params.aktorId.charAt(0) < 5
+                    ? 'behandlinger.json'
+                    : 'behandlinger_mapped.json';
+            fs.readFile(`__mock-data__/${filename}`, (err, data) => {
                 if (err) {
                     console.log(err);
                     res.sendStatus(500);
                 }
-                res.header('Content-type', 'application/json; charset=utf-8');
-                res.send(
-                    JSON.parse(data).behandlinger.map(behandling =>
-                        mapping.alle(behandling)
-                    )
-                );
+                res.header('Content-Type', 'application/json; charset=utf-8');
+                if (filename.indexOf('mapped') > -1) {
+                    res.send(data);
+                } else {
+                    res.send(
+                        JSON.parse(data).behandlinger.map(behandling =>
+                            mapping.alle(behandling)
+                        )
+                    );
+                }
             });
             return;
         }
