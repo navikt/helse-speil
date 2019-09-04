b1b8979bf67cc5f859acbdf8d9cd7644802e1e8c
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-09-04 14:06:45 +0200

    Error handling for person lookup

--- src/components/widgets/Personinfo/Personinfo.jsx
+++ src/components/widgets/Personinfo/Personinfo.jsx
@@ -5,37 +5,38 @@ import { withBehandlingContext } from '../../../context/BehandlingerContext';
 import { toDate } from '../../../utils/date';
 import { getPerson } from '../../../io/http';
 
-const Kjønn = {
-    MANN: 'mann',
-    KVINNE: 'kvinne',
-    NØYTRAL: 'kjønnsnøytral'
-};
-
 const Personinfo = withBehandlingContext(({ behandling }) => {
     const { aktorId, arbeidsgiver, fom, tom } = behandling.originalSøknad;
     const sykmeldingsgrad = behandling.periode.sykmeldingsgrad;
-    const [person, setPerson] = useState({
-        navn: 'Navn Navnesen',
-        kjønn: Kjønn.NØYTRAL
-    });
+    const [person, setPerson] = useState();
 
     useEffect(() => {
-        getPerson(aktorId).then(response => {
-            response.data && setPerson(response.data);
-        });
+        getPerson(aktorId)
+            .then(response => {
+                response.data && setPerson(response.data);
+            })
+            .catch(err => {
+                console.error('Feil ved henting av person.', err);
+                setPerson({
+                    navn: 'Navn ikke tilgjengelig',
+                    kjønn: 'Ikke tilgjengelig'
+                });
+            });
     }, [behandling]);
 
     return (
         <>
-            <div className="personalia-linje">
-                <figure
-                    id="personinfo-kjønn"
-                    aria-label={`Kjønn: ${person.kjønn}`}
-                    className={person.kjønn.toLowerCase()}
-                />
-                <Element>{person.navn}</Element>/
-                <Undertekst>Aktør-ID: {aktorId}</Undertekst>
-            </div>
+            {person && (
+                <div className="personalia-linje">
+                    <figure
+                        id="personinfo-kjønn"
+                        aria-label={`Kjønn: ${person.kjønn}`}
+                        className={person.kjønn.toLowerCase()}
+                    />
+                    <Element>{person.navn}</Element>
+                    <Undertekst>Aktør-ID: {aktorId}</Undertekst>
+                </div>
+            )}
             <div className="behandling-hovedinfo">
                 <Undertekst className="arbeidsgivernavn">
                     {arbeidsgiver.navn}
