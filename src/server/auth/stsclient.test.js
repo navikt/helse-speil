6cafa1d3595bed289051d14e2622438e729458e4
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-09-05 14:07:00 +0200

    Move STS token fetching to separate module
    It will be needed for aktør-ID lookup as well as person lookup.

---
+++ src/server/auth/stsclient.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+jest.mock('request-promise-native');
+
+const stsclient = require('./stsclient');
+
+const validCreds = {
+    serviceUserName: 'valid',
+    serviceUserPassword: 'creds'
+};
+
+const bogusCreds = {
+    serviceUserName: 'bogus',
+    serviceUserPassword: 'creds'
+};
+
+test('valid credentials resolves with valid token', () => {
+    stsclient.init({
+        stsUrl: 'http://localhost',
+        ...validCreds
+    });
+
+    expect(stsclient.hentAccessToken()).resolves.toMatch('bogussignature');
+});
+
+test('invalid credentials rejects with error message', () => {
+    stsclient.init({
+        stsUrl: 'http://localhost',
+        ...bogusCreds
+    });
+
+    expect(stsclient.hentAccessToken()).rejects.toMatch('wrong creds');
+});
