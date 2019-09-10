356d8be810d784102cada9f68ab703000afcbb1e
Stephen Ramthun <stephenramthun@gmail.com>
2019-09-10 12:35:27 +0200

    Use a single route for fetching all feedback

--- src/server/feedback/feedbackroutes.js
+++ src/server/feedback/feedbackroutes.js
@@ -1,10 +1,9 @@
 'use strict';
 
 const storage = require('./storage');
-const { ipAddressFromRequest } = require('../requestData.js');
 const { log } = require('../logging');
-const moment = require('moment');
-const { csvMapper } = require('./feedback-csv-mapper');
+const { ipAddressFromRequest } = require('../requestData.js');
+const { excludeOlderFeedback, isInvalid, parseDate, prepareCsvFeedback } = require('./utils');
 
 const setup = (app, config) => {
     return new Promise((resolve, reject) => {
@@ -37,52 +36,26 @@ const routes = app => {
             });
     });
 
-    app.get('/feedback-json', async (req, res) => {
+    app.get('/feedback', async (req, res) => {
+        const date = parseDate(req.query.fraogmed);
+
         storage
             .loadAll()
-            .then(objects => {
-                res.send(objects);
+            .then(response => {
+                const feedback = date ? excludeOlderFeedback(date, response) : response;
+                log(`Will return ${feedback.length} feedbacks out of ${response.length}`);
+                if (res.accepts('csv')) {
+                    const csvResponse = prepareCsvFeedback(feedback, res);
+                    res.send(csvResponse);
+                } else {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(feedback);
+                }
             })
             .catch(err => {
                 console.warn(err);
                 res.sendStatus(err.statusCode || 500);
             });
-    });
-
-    app.get('/feedback', async (req, res) => {
-        let fom;
-        const fomFromQuery = req.query.fraogmed;
-        if (fomFromQuery !== undefined) {
-            try {
-                fom = convertToMoment(fomFromQuery);
-                log(`Parsed feedback search params '${fomFromQuery}' to ${fom}.`);
-            } catch (e) {
-                console.warn(e.message);
-                res.send(`Det lot seg ikke gjøre å tolke '${fomFromQuery}' som en dato`);
-                return;
-            }
-        }
-
-        const feedback = await storage.loadAll().catch(err => {
-            console.warn(err);
-            res.sendStatus(err.statusCode || 500);
-            return;
-        });
-
-        const response = fom !== undefined ? excludeOlderFeedback(fom, feedback) : feedback;
-        log(
-            `Will return ${response.length} feedbacks to client, out of ${feedback.length} fetched from storage`
-        );
-
-        const timestamp = moment().format('YYYY-MM-DDTHH.mm.ss');
-        res.setHeader(
-            'Content-Disposition',
-            `attachment; filename=tilbakemeldinger_${timestamp}.csv`
-        );
-        res.setHeader('Content-Type', 'text/csv');
-        const csvResponse = csvMapper(response);
-        log(`Mapped CSV file is ${csvResponse.length} characters long.`);
-        res.send(csvResponse);
     });
 
     app.put('/feedback', (req, res) => {
@@ -104,33 +77,6 @@ const routes = app => {
     });
 };
 
-const convertToMoment = dateString => {
-    const norwegianMoment = moment(dateString, 'DD.MM.YYYY');
-    if (norwegianMoment.isValid()) {
-        return norwegianMoment;
-    }
-    const isoMoment = moment(dateString, 'YYYY-MM-DD');
-    if (isoMoment.isValid()) {
-        return isoMoment;
-    }
-    throw new Error('not a parseable date: ' + dateString);
-};
-
-const excludeOlderFeedback = (fom, feedback) =>
-    feedback.filter(f => moment(f.value.submittedDate || -1).isAfter(fom));
-
-const isInvalid = req => {
-    return (
-        !req.body.id ||
-        !req.body.txt ||
-        req.body.id.length === 0 ||
-        req.body.id.length > 50 ||
-        req.body.txt.length === 0 ||
-        req.body.txt.length > 20000
-    );
-};
-
 module.exports = {
-    setup: setup,
-    _convertToMoment: convertToMoment
+    setup
 };
---
+++ src/server/feedback/utils.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const moment = require('moment');
+const { log } = require('../logging');
+const { csvMapper } = require('./feedback-csv-mapper');
+
+const prepareCsvFeedback = (feedback, res) => {
+    const csvResponse = csvMapper(feedback);
+    const timestamp = moment().format('YYYY-MM-DDTHH.mm.ss');
+    res.setHeader('Content-Disposition', `attachment; filename=tilbakemeldinger_${timestamp}.csv`);
+    res.setHeader('Content-Type', 'text/csv');
+    log(`Mapped CSV file is ${csvResponse.length} characters long.`);
+    return csvResponse;
+};
+
+const parseDate = dateString => {
+    let date = moment(dateString, 'DD.MM.YYYY');
+    if (!date.isValid()) {
+        date = moment(dateString, 'YYYY-MM-DD');
+    }
+    if (date.isValid()) {
+        log(`Parsed feedback search params '${dateString}' to ${date}.`);
+        return date;
+    } else {
+        console.warn('not a parseable date: ' + dateString);
+        return;
+    }
+};
+
+const excludeOlderFeedback = (fom, feedback) =>
+    feedback.filter(f => moment(f.value.submittedDate || -1).isAfter(fom));
+
+const isInvalid = req => {
+    return (
+        !req.body.id ||
+        !req.body.txt ||
+        req.body.id.length === 0 ||
+        req.body.id.length > 50 ||
+        req.body.txt.length === 0 ||
+        req.body.txt.length > 20000
+    );
+};
+
+module.exports = {
+    excludeOlderFeedback,
+    isInvalid,
+    parseDate,
+    prepareCsvFeedback
+};
