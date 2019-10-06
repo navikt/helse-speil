0267f00af421cd1d0c5b27e9e1347fd84c4321a0
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2019-10-06 12:11:36 +0200

    replace wait-for with curl and shellscripting

--- e2e-tests/run.sh
+++ e2e-tests/run.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-PORT=1234
-npm run start-no-browser >/dev/null 2>&1&
-$(npm bin)/wait-on -t 15000 "http-get://localhost:$PORT"
-NODE_PID=$(lsof -i :$PORT -t)
+set -e
+
+npm run dev-express >/dev/null 2>&1&
+echo "Started Node, waiting for it to accept connections..."
+timeout 30 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:3000)" != "200" ]]; do sleep 5; done' || false
+
+NODE_PID=$(lsof -i :3000 -t)
 if [ -z "NODE_PID" ]
 then
     echo "Node ain't running, nothing to do"
     exit 1
 else
-    echo "Node is running as $NODE_PID, executing tests"
+    echo "Node is running as pid $NODE_PID, executing tests"
     $(npm bin)/cypress run --project e2e-tests
     CYPRESS_EXIT_STATUS=$(echo $?)
     echo "Stopping Node"
