d552a74bd0d41265e394e03235e59d687c6099a7
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-10-15 13:19:19 +0200

    Error handling for e2e test runner

--- e2e-tests/run.sh
+++ e2e-tests/run.sh
@@ -2,11 +2,27 @@
 
 set -e
 
-npm run dev-express &
-echo "Started Node, waiting for it to accept connections..."
-timeout 10 bash -c 'while [[ "$(curl -L -s -o /dev/null -w ''%{http_code}'' localhost:3000)" != "200" ]]; do sleep 5; done' || false
+declare NPM_PID
 
-NODE_PID=$(lsof -i :3000 -t)
+function node_pid {
+    echo $(lsof -i :3000 -t)
+}
+
+function cleanup {
+    local -r exit_code=$?
+    echo "Something errored, with exit code $exit_code. Stopping processes"
+    kill $NPM_PID $(node_pid)
+    exit $exit_code
+}
+trap cleanup ERR
+
+npm run dev-express &
+NPM_PID=$!
+
+echo "Started Node, waiting for it to accept connections..."
+timeout 10 bash -c 'while [[ "$(curl -L -s -o /dev/null -w ''%{http_code}'' localhost:3000)" != "200" ]]; do sleep 1; done' || false
+
+NODE_PID=$(node_pid)
 if [ -z "NODE_PID" ]
 then
     echo "Node ain't running, nothing to do"
@@ -14,8 +30,6 @@ then
 else
     echo "Node is running as pid $NODE_PID, executing tests"
     $(npm bin)/cypress run --project e2e-tests
-    CYPRESS_EXIT_STATUS=$(echo $?)
-    echo "Stopping Node"
-    kill $NODE_PID
-    exit $CYPRESS_EXIT_STATUS
+    echo "Done, stopping processes"
+    kill $NODE_PID $NPM_PID
 fi
