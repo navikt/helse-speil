0fd06424d53ac7f5b430b38e1c167dd6365c984f
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-10-16 09:43:56 +0200

    console.log -> logger.info

--- src/server/tildeling/tildelingroutes.js
+++ src/server/tildeling/tildelingroutes.js
@@ -9,8 +9,6 @@ const setup = redisclient => {
     return router;
 };
 
-routes;
-
 const routes = ({ router }) => {
     router.post('/list', (req, res) => {
         const behandlingsIdList = req.body;
@@ -35,7 +33,7 @@ const routes = ({ router }) => {
                 res.send(JSON.stringify({ behandlingsId: behandlingsId, userId: result }));
             })
             .catch(err => {
-                console.log(`Error while retrieving value from Redis. Error: ${err}`);
+                logger.info(`Error while retrieving value from Redis. Error: ${err}`);
                 res.sendStatus(err.statusCode || 500);
             });
     });
@@ -76,18 +74,18 @@ const routes = ({ router }) => {
         const behandlingsId = req.params.behandlingsId;
         if (behandlingsId === undefined) {
             const errorMessage = `behandlingsId '${behandlingsId}' is not valid.`;
-            console.log(`Unassign case: ${errorMessage}`);
+            logger.info(`Unassign case: ${errorMessage}`);
             res.status(400).send(errorMessage);
             return;
         }
         storage
             .unassignCase(behandlingsId)
             .then(() => {
-                console.log(`The case ${behandlingsId} is no longer assigned.`);
+                logger.info(`The case ${behandlingsId} is no longer assigned.`);
                 res.sendStatus(204);
             })
             .catch(err => {
-                console.log(`Error while deleting key in Redis: ${err}`);
+                logger.info(`Error while deleting key in Redis: ${err}`);
                 res.status(500).send({
                     message: `Sletting av tildeling feilet.`
                 });
