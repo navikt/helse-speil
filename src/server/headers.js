792e7cc57db0b94c7a02f3b3cd88f87fe28580d9
Stephen Ramthun <stephenramthun@gmail.com>
2019-10-21 14:12:16 +0200

    Fix CSP for real this time

--- src/server/headers.js
+++ src/server/headers.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const styleSource = 'https://fonts.googleapis.com';
+const fontSource = 'https://fonts.gstatic.com';
+const cspString = `default-src 'self' data:; style-src 'self' ${styleSource} data: 'unsafe-inline'; connect-src 'self'; font-src ${fontSource} 'self' data:`;
+
 const setup = app => {
     app.disable('x-powered-by');
     app.use((req, res, next) => {
@@ -8,18 +12,9 @@ const setup = app => {
         res.header('X-Content-Type-Options', 'nosniff');
         res.header('Referrer-Policy', 'no-referrer');
 
-        res.header(
-            'Content-Security-Policy',
-            "default-src 'self' data:; style-src 'self' https://font.googleapis.com data: 'unsafe-inline'; connect-src 'self'; font-src https://fonts.gstatic.com 'self' data:"
-        );
-        res.header(
-            'X-WebKit-CSP',
-            "default-src 'self' data:; style-src 'self' https://font.googleapis.com data: 'unsafe-inline'; connect-src 'self'; font-src https://fonts.gstatic.com 'self' data:"
-        );
-        res.header(
-            'X-Content-Security-Policy',
-            "default-src 'self' data:; style-src 'self' https://font.googleapis.com data: 'unsafe-inline'; connect-src 'self'; font-src https://fonts.gstatic.com 'self' data:"
-        );
+        res.header('Content-Security-Policy', cspString);
+        res.header('X-WebKit-CSP', cspString);
+        res.header('X-Content-Security-Policy', cspString);
 
         res.header('Feature-Policy', "geolocation 'none'; microphone 'none'; camera 'none'");
         if (process.env.NODE_ENV === 'development') {
