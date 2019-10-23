2f47b06ef7aef28e5935e304226d7ade27303a84
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-10-23 11:53:49 +0200

    Fix end-to-end test

--- e2e-tests/cypress/integration/search-spec.js
+++ e2e-tests/cypress/integration/search-spec.js
@@ -5,14 +5,10 @@ describe('Search for cases for a person', function() {
         cy.get('#root > header > div.search > div > input[type=text]')
             .type('12345')
             .type('{enter}');
-
-        cy.get(
-            'body > div.ReactModalPortal > div > div > section > div > ul > li:nth-child(1) > button'
-        ).click();
     });
 
     it('sets the active tab to "sykdomsvilkår"', function() {
-        cy.get('#nav-link-sykdomsvilkår').should('have.class', 'active');
+        cy.get('#nav-link-sykmeldingsperiode').should('have.class', 'active');
     });
 
     it('shows the person bar', function() {
