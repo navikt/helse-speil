5a19bfffc9cb2e57c88e4aaa868499d572d337f8
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-10-30 14:28:37 +0100

    Vise riktig tekst og farge for alle dagtyper
    Co-authored-by: Stefan Engelien <stefan.andreas.engelien@nav.no>

--- src/client/components/Timeline/TimelineRow.jsx
+++ src/client/components/Timeline/TimelineRow.jsx
@@ -4,14 +4,62 @@ import SourceLink from './SourceLink';
 
 const capitalize = text => text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 
+const dayTypes = {
+    ARBEIDSDAG: {
+        cssName: 'arbeidsdag',
+        text: 'Arbeidsdag'
+    },
+    EGENMELDINGSDAG: {
+        cssName: 'egenmeldingsdag',
+        text: 'Egenmeldingsdag'
+    },
+    FERIEDAG: {
+        cssName: 'feriedag',
+        text: 'Feriedag'
+    },
+    PERMISJONSDAG: {
+        cssName: 'permisjonsdag',
+        text: 'Permisjonsdag'
+    },
+    STUDIEDAG: {
+        cssName: 'studiedag',
+        text: 'Studiedag'
+    },
+    SYKEDAG: {
+        cssName: 'sykedag',
+        text: 'Sykedag'
+    },
+    SYK_HELGEDAG: {
+        cssName: 'sykHelgedag',
+        text: 'Syk helgedag'
+    },
+    UBESTEMTDAG: {
+        cssName: 'ubestemtdag',
+        text: 'Ubestemt dag'
+    },
+    UTENLANDSDAG: {
+        cssName: 'utenlandsdag',
+        text: 'Utenlandsdag'
+    }
+};
+
+const dayType = type => {
+    const dayType = dayTypes[type];
+    return (
+        dayType || {
+            cssName: 'default',
+            text: type
+        }
+    );
+};
 const TimelineRow = ({ date, type, source, showType }) => {
     return (
         <tr>
             <td>
                 <div className="TimelineRow__date">{date}</div>
                 {type && (
-                    <div className={`TimelineRow__type ${type}`}>
-                        <span>{showType && capitalize(type)}</span>
+                    <div className={`TimelineRow__type ${dayType(type).cssName}`}>
+                        <span>{showType && dayType(type).text}</span>
                         {source && <SourceLink label={source} />}
                     </div>
                 )}
