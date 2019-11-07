5de4f2e3cfb88db2b88cea0e30b40328db92952f
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2019-11-07 13:11:15 +0100

    mulighet for å hnte onBehalfOf token til bruk mot andre apps, ikke tatt i bruk noen steder enda

---
+++ src/server/auth/onbehalfof.js
@@ -0,0 +1,32 @@
+const request = require('request-promise-native');
+
+let config;
+
+const init = oidcConfig => {
+    config = oidcConfig;
+    return {};
+};
+
+const hentFor = async (clientId, speilAccessToken) => {
+    const options = {
+        uri: `${config.providerBaseUrl}/token`,
+        json: true,
+        postData: {
+            mimeType: 'application/x-www-form-urlencoded',
+            params: [
+                { name: 'grant_type', value: 'urn:ietf:params:oauth:grant-type:jwt-bearer' },
+                { name: 'client_id', value: config.clientID }, // our own
+                { name: 'client_secret', value: config.clientSecret },
+                { name: 'assertion', value: speilAccessToken },
+                { name: 'scope', value: `${clientId}/.default` }, // the app we're reaching out to
+                { name: 'requested_token_use', value: 'on_behalf_of' }
+            ]
+        }
+    };
+    const response = await request.post(options);
+    return response.access_token;
+};
+
+module.exports = {
+    init
+};
