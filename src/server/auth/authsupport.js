1892e78317f01f75351ae7f5e37b06578296338f
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-11-12 10:57:42 +0100

    Logg token til securelog slik at vi kan inspisere det

--- src/server/auth/authsupport.js
+++ src/server/auth/authsupport.js
@@ -41,6 +41,7 @@ const validateOidcCallback = (req, azureClient, config) => {
         azureClient
             .callback(config.redirectUrl, params, { nonce, state })
             .then(tokenSet => {
+                logger.audit(tokenSet);
                 const accessTokenKey = 'access_token';
                 const idTokenKey = 'id_token';
                 const errorMessages = checkAzureResponseContainsTokens(
