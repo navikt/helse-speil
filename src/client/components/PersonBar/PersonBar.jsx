26fe4a6cfd65f7b844394b7fc70ba15f94fb02de
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-11-13 14:39:18 +0100

    Forenkling av utfisking av data i PersonBar
    Trenger ikke bruke en hook og state for data som kommer inn via
    PersonContext.
    
    Også fint med early return for å slippe en del conditionals.

--- src/client/components/PersonBar/PersonBar.jsx
+++ src/client/components/PersonBar/PersonBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useMemo, useState } from 'react';
+import React, { useContext } from 'react';
 import Clipboard from '../Clipboard';
 import { Element, Normaltekst } from 'nav-frontend-typografi';
 import { PersonContext } from '../../context/PersonContext';
@@ -13,56 +13,49 @@ const finnSøknad = person =>
 
 const finnSykmeldingsgrad = person => finnSøknad(person).soknadsperioder[0].sykmeldingsgrad;
 
+const render = content => <div className="PersonBar">{content}</div>;
+
 const PersonBar = () => {
     const { personTilBehandling } = useContext(PersonContext);
-    const aktørId = personTilBehandling?.aktørId;
-    const [personinfo, setPersoninfo] = useState(undefined);
 
-    const data = useMemo(() => {
-        if (personTilBehandling && !personinfo) {
-            setPersoninfo(personTilBehandling.personinfo);
-        }
-        if (!personTilBehandling || !personinfo) {
-            return null;
-        }
-        const arbeidsgiver = finnSøknad(personTilBehandling)?.arbeidsgiver || {};
-        return {
-            navn: personinfo.navn ?? 'Navn ikke tilgjengelig',
-            fnr: personinfo.fnr,
-            arbeidsgivernavn: arbeidsgiver?.navn || arbeidsgiver?.orgnummer,
-            sykmeldingsgrad: `${finnSykmeldingsgrad(personTilBehandling)}%`,
-            ariaLabelKjønn: `Kjønn: ${personinfo.kjønn ?? 'Ikke tilgjengelig'}`,
-            classNameKjønn: personinfo.kjønn?.toLowerCase() ?? 'kjønnsnøytral'
-        };
-    }, [personTilBehandling, personinfo]);
+    if (!personTilBehandling) return render(null);
 
-    return (
-        <div className="PersonBar">
-            {data && (
-                <>
-                    <Normaltekst>{data.arbeidsgivernavn}</Normaltekst>
-                    <Normaltekst>{' / '}</Normaltekst>
-                    <Normaltekst>{data.sykmeldingsgrad}</Normaltekst>
-                    <span className="PersonBar__separator" />
-                    <figure
-                        id="PersonBar__gender"
-                        aria-label={data.ariaLabelKjønn}
-                        className={data.classNameKjønn}
-                    />
-                    <Element>{data.navn}</Element>
-                    <Normaltekst>/</Normaltekst>
-                    {data.fnr ? (
-                        <Clipboard>
-                            <Normaltekst>{formatFnr(data.fnr)}</Normaltekst>
-                        </Clipboard>
-                    ) : (
-                        <Normaltekst>Fødselsnummer ikke tilgjengelig</Normaltekst>
-                    )}
-                    <Normaltekst>/</Normaltekst>
-                    <Normaltekst>Aktør-ID: {aktørId}</Normaltekst>
-                </>
+    const { aktørId } = personTilBehandling;
+    const personinfo = personTilBehandling.personinfo ?? {};
+    const arbeidsgiver = finnSøknad(personTilBehandling)?.arbeidsgiver ?? {};
+
+    const data = {
+        navn: personinfo.navn ?? 'Navn ikke tilgjengelig',
+        fnr: personinfo.fnr,
+        arbeidsgivernavn: arbeidsgiver.navn || arbeidsgiver.orgnummer,
+        sykmeldingsgrad: `${finnSykmeldingsgrad(personTilBehandling)}%`,
+        ariaLabelKjønn: `Kjønn: ${personinfo.kjønn ?? 'Ikke tilgjengelig'}`,
+        classNameKjønn: personinfo.kjønn?.toLowerCase() ?? 'kjønnsnøytral'
+    };
+
+    return render(
+        <>
+            <Normaltekst>{data.arbeidsgivernavn}</Normaltekst>
+            <Normaltekst>{' / '}</Normaltekst>
+            <Normaltekst>{data.sykmeldingsgrad}</Normaltekst>
+            <span className="PersonBar__separator" />
+            <figure
+                id="PersonBar__gender"
+                aria-label={data.ariaLabelKjønn}
+                className={data.classNameKjønn}
+            />
+            <Element>{data.navn}</Element>
+            <Normaltekst>/</Normaltekst>
+            {data.fnr ? (
+                <Clipboard>
+                    <Normaltekst>{formatFnr(data.fnr)}</Normaltekst>
+                </Clipboard>
+            ) : (
+                <Normaltekst>Fødselsnummer ikke tilgjengelig</Normaltekst>
             )}
-        </div>
+            <Normaltekst>/</Normaltekst>
+            <Normaltekst>Aktør-ID: {aktørId}</Normaltekst>
+        </>
     );
 };
 
