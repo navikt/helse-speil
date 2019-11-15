3ec7106dca80d506d3cf6a2890c7bf44586dee2d
Stephen Ramthun <stephenramthun@gmail.com>
2019-11-15 09:57:54 +0100

    Fjern console.log og ubrukt jsx

--- src/client/routes/Oppsummering/Oppsummering.jsx
+++ src/client/routes/Oppsummering/Oppsummering.jsx
@@ -12,104 +12,12 @@ import ListSeparator from '../../components/ListSeparator';
 import { toLocaleFixedNumberString } from '../../utils/locale';
 
 const Oppsummering = () => {
-    // const { personTilBehandling } = useContext(PersonContext);
     const simuleringContext = useContext(SimuleringContext);
-    console.log({ simuleringContext });
 
     return (
         <div className="Oppsummering">
             <Panel>
                 <Undertittel>{oppsummeringstekster('tittel')}</Undertittel>
-                {/*{personTilBehandling.oppsummering ? (*/}
-                {/*    <>*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('sykdomsvilkår')}*/}
-                {/*            value={personTilBehandling.oppsummering.sykdomsvilkårErOppfylt}*/}
-                {/*        />*/}
-                {/*        <ListSeparator type="dotted" />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('inngangsvilkår')}*/}
-                {/*            value={personTilBehandling.oppsummering.inngangsvilkårErOppfylt}*/}
-                {/*        />*/}
-                {/*        <ListSeparator type="dotted" />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('arbeidsgiver')}*/}
-                {/*            value={personTilBehandling.oppsummering.arbeidsgiver.navn}*/}
-                {/*            bold*/}
-                {/*        />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('orgnr')}*/}
-                {/*            value={personTilBehandling.oppsummering.arbeidsgiver.orgnummer}*/}
-                {/*            bold*/}
-                {/*        />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('refusjon')}*/}
-                {/*            value={tekster('informasjon ikke tilgjengelig')}*/}
-                {/*            bold*/}
-                {/*        />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('betaler')}*/}
-                {/*            value={*/}
-                {/*                personTilBehandling.oppsummering.betalerArbeidsgiverperiode*/}
-                {/*                    ? 'Nei'*/}
-                {/*                    : 'Ja'*/}
-                {/*            }*/}
-                {/*        />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('fordeling')}*/}
-                {/*            value={`${personTilBehandling.oppsummering.fordeling}%`}*/}
-                {/*        />*/}
-                {/*        <ListSeparator type="dotted" />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('sykepengegrunnlag')}*/}
-                {/*            value={`${toKroner(*/}
-                {/*                personTilBehandling.oppsummering.sykepengegrunnlag*/}
-                {/*            )} kr`}*/}
-                {/*        />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('månedsbeløp')}*/}
-                {/*            value={`${toKroner(personTilBehandling.oppsummering.månedsbeløp)} kr`}*/}
-                {/*            bold*/}
-                {/*        />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('dagsats')}*/}
-                {/*            value={`${toKroner(personTilBehandling.oppsummering.dagsats)} kr`}*/}
-                {/*        />*/}
-                {/*        <ListSeparator type="dotted" />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('antall_utbetalingsdager')}*/}
-                {/*            value={personTilBehandling.oppsummering.antallUtbetalingsdager}*/}
-                {/*        />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('fom')}*/}
-                {/*            value={toDate(personTilBehandling.oppsummering.sykmeldtFraOgMed)}*/}
-                {/*            bold*/}
-                {/*        />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('tom')}*/}
-                {/*            value={toDate(personTilBehandling.oppsummering.sykmeldtTilOgMed)}*/}
-                {/*            bold*/}
-                {/*        />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('sykmeldingsgrad')}*/}
-                {/*            value={`${personTilBehandling.oppsummering.sykmeldingsgrad}%`}*/}
-                {/*            bold*/}
-                {/*        />*/}
-                {/*        <ListSeparator type="dotted" />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('utbetalesFom')}*/}
-                {/*            value={toDate(personTilBehandling.oppsummering.utbetalesFom)}*/}
-                {/*        />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('utbetalesTom')}*/}
-                {/*            value={toDate(personTilBehandling.oppsummering.utbetalesTom)}*/}
-                {/*        />*/}
-                {/*        <ListItem*/}
-                {/*            label={oppsummeringstekster('utbetaling')}*/}
-                {/*            value={`${toKroner(personTilBehandling.oppsummering.utbetaling)} kr`}*/}
-                {/*        />*/}
-                {/*        <ListSeparator type="solid" />*/}
-                {/*    </>*/}
                 <ListSeparator type="solid" />
                 {simuleringContext.error ? (
                     <Normaltekst>{simuleringContext.error}</Normaltekst>
