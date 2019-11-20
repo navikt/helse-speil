4f09bfc6b28a66aa97f190ea2445b648dc290690
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-11-20 09:39:03 +0100

    Fjerne overflødig funksjon

--- src/client/utils/locale/index.js
+++ src/client/utils/locale/index.js
@@ -5,15 +5,13 @@
  * @param: locale (string)   the locale to convert to
  * @returns: (string)        the string representation of the number
  */
-export const toLocaleFixedNumberString = (number, decimals, locale = 'nb-NO') =>
+export const toKronerOgØre = (number, decimals, locale = 'nb-NO') =>
     Number.parseFloat(number).toLocaleString(locale, {
         maximumFractionDigits: decimals,
         minimumFractionDigits: decimals
     });
 
 export const toKroner = number => Number.parseInt(number).toLocaleString('nb-NO');
-
-export const toKronerOgØre = number => toLocaleFixedNumberString(number, 2);
 
 export const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 
