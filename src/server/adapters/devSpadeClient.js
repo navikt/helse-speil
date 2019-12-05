edd11b089de6850c245408625a482780b74c4c04
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-12-05 13:09:07 +0100

    Fjern ubrukt behandlingerForPerson-kall mot Spade

--- src/server/adapters/devSpadeClient.js
+++ src/server/adapters/devSpadeClient.js
@@ -1,13 +1,5 @@
 const fs = require('fs');
 
-const behandlingerForPerson = () => {
-    const fromFile = fs.readFileSync('__mock-data__/tidslinjeperson.json', 'utf-8');
-    const person = JSON.parse(fromFile);
-    return Promise.resolve({
-        statusCode: 200,
-        body: { person }
-    });
-};
 const behandlingerForPeriode = () => {
     const fromFile = fs.readFileSync('__mock-data__/behov.json', 'utf-8');
     const behov = JSON.parse(fromFile);
@@ -17,6 +9,5 @@ const behandlingerForPeriode = () => {
     });
 };
 module.exports = {
-    behandlingerForPerson,
     behandlingerForPeriode
 };
--- src/server/adapters/spadeClient.js
+++ src/server/adapters/spadeClient.js
@@ -1,17 +1,5 @@
 const request = require('request-promise-native');
 
-const behandlingerForPerson = ({ aktørId, accessToken }) => {
-    const options = {
-        uri: `http://spade.default.svc.nais.local/api/behov/${aktørId}`,
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        },
-        json: true,
-        resolveWithFullResponse: true
-    };
-    return request.get(options);
-};
-
 const behandlingerForPeriode = (fom, tom, accessToken) => {
     const options = {
         uri: `http://spade.default.svc.nais.local/api/behov/periode?fom=${fom}&tom=${tom}`,
@@ -25,6 +13,5 @@ const behandlingerForPeriode = (fom, tom, accessToken) => {
 };
 
 module.exports = {
-    behandlingerForPerson,
     behandlingerForPeriode
 };
