7ef674ec26057585b6549cafee0142d4132d8443
Stephen Ramthun <stephenramthun@gmail.com>
2020-01-09 08:51:07 +0100

    Fjern ubrukt test

--- src/client/tekster.test.js
+++
@@ -1,9 +0,0 @@
-import { tekster, oversikttekster } from './tekster';
-
-test('kan slå opp "globale" tekster', () => {
-    expect(tekster('neste')).toEqual('Neste');
-});
-
-test('kan slå opp tekster for sider', () => {
-    expect(oversikttekster('tittel')).toContain('Neste saker');
-});
