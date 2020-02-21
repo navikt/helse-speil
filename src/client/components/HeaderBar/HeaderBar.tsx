79f162cee39b3e8cf444f3297328ec725d31e5a3
Stephen Ramthun <stephenramthun@gmail.com>
2020-02-21 15:40:29 +0100

    Bruk header fra helse-frontend-header

--- src/client/components/HeaderBar/HeaderBar.tsx
+++ src/client/components/HeaderBar/HeaderBar.tsx
@@ -1,30 +1,37 @@
 import React, { useContext } from 'react';
-import Search from '../Search';
 import { AuthContext } from '../../context/AuthContext';
-import './HeaderBar.less';
+import { HeaderEnkel as Header, Søk } from '@navikt/helse-frontend-header';
+import styled from '@emotion/styled';
+import { PersonContext } from '../../context/PersonContext';
+import { useNavigateAfterSearch } from './useNavigateAfterSearch';
+
+const Container = styled.div`
+    flex-shrink: 0;
+    height: max-content;
+`;
 
 const HeaderBar = () => {
     const { authInfo } = useContext(AuthContext);
-    const { name, isLoggedIn } = authInfo;
+    const { hentPerson } = useContext(PersonContext);
+    const { setShouldNavigate } = useNavigateAfterSearch();
+    const { name, ident, isLoggedIn } = authInfo;
 
-    const username = isLoggedIn ? name : 'Ikke pålogget';
+    const brukerinfo = isLoggedIn
+        ? { navn: name, ident: ident ?? '' }
+        : { navn: 'Ikke pålogget', ident: '' };
+
+    const onSøk = (value: string) => {
+        hentPerson(value).then(() => {
+            setShouldNavigate(true);
+        });
+    };
 
     return (
-        <header className="HeaderBar">
-            <a href={'/'} className="title">
-                <h1>NAV Sykepenger</h1>
-            </a>
-            <div className="divider" />
-            <div className="search">
-                <div>
-                    <Search />
-                </div>
-            </div>
-            <div className="divider" />
-            <div id="user" className="user">
-                {username}
-            </div>
-        </header>
+        <Container>
+            <Header tittel="NAV Sykepenger" brukerinfo={brukerinfo}>
+                <Søk onSøk={onSøk} />
+            </Header>
+        </Container>
     );
 };
 
