787750fd652d89d01980410c02f3a5aab67a5bb4
Kevin Sillerud <kevin.sillerud@gmail.com>
2020-05-14 12:59:03 +0200

    Henter client-id og client-secret fra azure integrasjon i vault
    Co-authored-by: Marthe Skogen <Marthe.skogen@nav.no>
    Co-authored-by: Knut Nygaard <knut.nygaard@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- src/server/config.ts
+++ src/server/config.ts
@@ -2,18 +2,30 @@
 
 import { NavConfig, OidcConfig, ServerConfig } from './types';
 import env from 'dotenv';
+import fs from 'fs';
+
+const AZURE_PATH = '/var/run/secrets/nais.io/azure';
+
+const readAzureCredential = (name: string): string => {
+    try {
+        return fs.readFileSync(`${AZURE_PATH}/${name}`, { encoding: 'UTF-8', flag: 'r' });
+    } catch (e) {
+        console.warn(`Fant ikke ${name} i ${AZURE_PATH}, defaulter til 'unknown'`);
+        return 'unknown';
+    }
+};
 
 env.config();
 
 const oidc: OidcConfig = {
     tenantID: process.env.TENANT_ID,
     providerBaseUrl: `https://login.microsoftonline.com/${process.env.TENANT_ID}`,
-    clientID: process.env.CLIENT_ID || 'unknown',
+    clientID: readAzureCredential('client_id') || 'unknown',
     clientIDSpade: process.env.CLIENT_ID_SPADE || 'unknown',
     clientIDSpesialist: process.env.CLIENT_ID_SPESIALIST || 'unknown',
     responseType: ['code'],
     redirectUrl: process.env.REDIRECT_URL || 'http://localhost',
-    clientSecret: process.env.CLIENT_SECRET || 'unknown',
+    clientSecret: readAzureCredential('client_secret') || 'unknown',
     issuer: [`https://login.microsoftonline.com/${process.env.TENANT_ID}/v2.0`],
     scope: `profile openid email ${process.env.CLIENT_ID}/.default`,
     requiredGroup: process.env.REQUIRED_GROUP
