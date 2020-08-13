daf37e9b24595be5b7c46574b240cae31a3810e7
Jonas Hasle <jonas.hasle@nav.no>
2020-08-13 15:13:44 +0200

    Overstyrbare dager synlige i dev men ikke i prod
    Co-authored-by: Martin Solheim <martin.solheim@me.com>
    Co-authored-by: Stephen Ramthun <stephen.ramthun@nav.no>

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -1,1 +1,2 @@
-export const overstyrbareTabellerEnabled = false;
+export const overstyrbareTabellerEnabled =
+    location.hostname === 'speil.nais.preprod.local' || location.hostname === 'localhost';
