60e56aeca5865fef06553eda6a124b7993a26635
Stephen Ramthun <stephenramthun@gmail.com>
2020-08-21 14:33:56 +0200

    Fikser henting av person lokalt
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>

--- src/server/adapters/devSpesialistClient.ts
+++ src/server/adapters/devSpesialistClient.ts
@@ -11,7 +11,7 @@ const behandlingerForPeriode = (_accessToken: string): Promise<Response> => {
 };
 
 const hentPersonByAktørId = async (aktørId: string): Promise<Response> => {
-    const fromFile = fs.readFileSync(`__mock-data__/${filename(aktørId)}`, 'utf-8');
+    const fromFile = fs.readFileSync(`__mock-data__/${filenameForPersonId(aktørId)}`, 'utf-8');
     const person = JSON.parse(fromFile);
     return Promise.resolve({
         status: 200,
@@ -20,7 +20,7 @@ const hentPersonByAktørId = async (aktørId: string): Promise<Response> => {
 };
 
 const hentPersonByFødselsnummer = async (aktørId: string): Promise<Response> => {
-    const fromFile = fs.readFileSync(`__mock-data__/${filename(aktørId)}`, 'utf-8');
+    const fromFile = fs.readFileSync(`__mock-data__/${filenameForPersonId(aktørId)}`, 'utf-8');
     const person = JSON.parse(fromFile);
     return Promise.resolve({
         status: 200,
@@ -28,18 +28,26 @@ const hentPersonByFødselsnummer = async (aktørId: string): Promise<Response> =
     } as Response);
 };
 
-const filename = (aktørId: string) => {
-    switch (aktørId) {
+const filenameForPersonId = (id: string) => {
+    switch (id) {
         case '1000000009871':
+        case '12020069420':
             return 'mock-person_til-godkjenning.json';
         case '87654321962123':
+        case '12020042069':
             return 'mock-person_2perioder.json';
         case '87650000962123':
+        case '21010462423':
             return 'mock-person_infotrygd-direkteovergang.json';
         case '57650000444423':
+        case '22097112413':
             return 'mock-person_infotrygd-forlengelse.json';
         case '1000000000009':
+        case '03120520135':
             return 'mock-person_første-periode-uten-utbetaling.json';
+        case '1234':
+        case '01010025678':
+            return 'mock-person_til-utbetaling.json';
         default:
             return 'mock-person_3perioder_første_periode_kort.json';
     }
