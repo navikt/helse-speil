5fb35f32e8911e48a5f10d4d102caef392f8f08b
Stephen Ramthun <stephenramthun@gmail.com>
2020-08-24 09:53:28 +0200

    Rydder opp i utbetalingsoversikten

--- src/client/routes/Saksbilde/Utbetalingsoversikt.tsx
+++ src/client/routes/Saksbilde/Utbetalingsoversikt.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { ReactNode, useContext } from 'react';
 import Navigasjonsknapper from '../../components/NavigationButtons';
 import { Normaltekst } from 'nav-frontend-typografi';
 import { PersonContext } from '../../context/PersonContext';
@@ -12,6 +12,13 @@ import { Dagtype, Utbetalingsdag } from '../../context/types.internal';
 import Element from 'nav-frontend-typografi/lib/element';
 import Feilikon from '../../components/Ikon/Feilikon';
 import classNames from 'classnames';
+
+type Utbetalingsceller = [ReactNode, ReactNode, ReactNode, ReactNode, ReactNode, ReactNode, ReactNode];
+
+type Utbetalingstabellrad = {
+    celler: Utbetalingsceller;
+    className: string;
+};
 
 const Container = styled.div`
     padding: 1.5rem 2rem;
@@ -30,44 +37,66 @@ const Feilmelding = styled(Normaltekst)`
     margin-left: 1rem;
 `;
 
+const utbetalingsceller = (dag: Utbetalingsdag): Utbetalingsceller => [
+    undefined,
+    dato(dag),
+    ikon(dag),
+    type(dag),
+    gradering(dag),
+    utbetaling(dag),
+    undefined,
+];
+
+const cellerPåMaksdato = (dag: Utbetalingsdag): Utbetalingsceller => [
+    <Feilmeldingsikon height={20} width={20} />,
+    dato(dag),
+    ikon(dag),
+    type(dag),
+    gradering(dag),
+    utbetaling(dag),
+    <Feilmelding>Siste utbetalingsdag for sykepenger</Feilmelding>,
+];
+
+const maksdatorad = (dag: Utbetalingsdag): Utbetalingstabellrad => ({
+    celler: cellerPåMaksdato(dag),
+    className: classNames('error', dag.type === Dagtype.Helg && 'disabled'),
+});
+
+const etterMaksdatoRad = (dag: Utbetalingsdag): Utbetalingstabellrad => ({
+    celler: utbetalingsceller(dag),
+    className: classNames('inactive', dag.type === Dagtype.Helg && 'disabled'),
+});
+
+const utbetalingstabellrad = (dag: Utbetalingsdag): Utbetalingstabellrad => ({
+    celler: utbetalingsceller(dag),
+    className: classNames(dag.type === Dagtype.Helg && 'disabled'),
+});
+
+const utbetalingsheadere = [
+    '',
+    {
+        render: <Element>Sykmeldingsperiode</Element>,
+        kolonner: 3,
+    },
+    <Element>Gradering</Element>,
+    <Element>Utbetaling</Element>,
+    '',
+];
+
 const Utbetalingsoversikt = () => {
     const { aktivVedtaksperiode } = useContext(PersonContext);
     const { maksdato } = useMaksdato();
     const fom = aktivVedtaksperiode?.fom.format(NORSK_DATOFORMAT);
     const tom = aktivVedtaksperiode?.tom.format(NORSK_DATOFORMAT);
-    const tabellbeskrivelse = `Utbetalinger for sykmeldingsperiode fra ${fom} til ${tom}`;
-
-    const headere = [
-        '',
-        {
-            render: <Element>Sykmeldingsperiode</Element>,
-            kolonner: 3,
-        },
-        <Element>Gradering</Element>,
-        <Element>Utbetaling</Element>,
-        '',
-    ];
 
     const erMaksdato = (dag: Utbetalingsdag) => maksdato && dag.dato.isSame(maksdato, 'day');
     const erEtterMaksdato = (dag: Utbetalingsdag) => maksdato && dag.dato.isAfter(maksdato, 'day');
+    const tilUtbetalingsrad = (dag: Utbetalingsdag) =>
+        erMaksdato(dag) ? maksdatorad(dag) : erEtterMaksdato(dag) ? etterMaksdatoRad(dag) : utbetalingstabellrad(dag);
 
-    const rader =
-        aktivVedtaksperiode?.utbetalingstidslinje.map((dag) => ({
-            celler: [
-                erMaksdato(dag) ? <Feilmeldingsikon height={20} width={20} /> : undefined,
-                dato(dag),
-                ikon(dag),
-                type(dag),
-                gradering(dag),
-                utbetaling(dag),
-                erMaksdato(dag) ? <Feilmelding>Siste utbetalingsdag for sykepenger</Feilmelding> : undefined,
-            ],
-            className: classNames(
-                erMaksdato(dag) && 'error',
-                erEtterMaksdato(dag) && 'inactive',
-                dag.type === Dagtype.Helg && 'disabled'
-            ),
-        })) ?? [];
+    const rader = aktivVedtaksperiode?.utbetalingstidslinje.map(tilUtbetalingsrad) ?? [];
+    const headere = utbetalingsheadere;
+    const tabellbeskrivelse = `Utbetalinger for sykmeldingsperiode fra ${fom} til ${tom}`;
 
     return (
         <Container>
