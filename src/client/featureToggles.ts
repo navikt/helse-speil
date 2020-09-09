55e4b2d5203c62c6f067222601d606a63124775d
Stephen Ramthun <stephenramthun@gmail.com>
2020-09-09 11:38:02 +0200

    Disabler spesialisttildeling i prod

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -1,4 +1,4 @@
 const erPreprod = () => location.hostname === 'speil.nais.preprod.local' || location.hostname === 'localhost';
 
 export const overstyrbareTabellerEnabled = erPreprod();
-export const spesialistTildelingEnabled = true;
+export const spesialistTildelingEnabled = erPreprod();
