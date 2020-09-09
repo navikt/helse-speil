d65e36c3088b4ff823a8df11231786829e434643
Stephen Ramthun <stephenramthun@gmail.com>
2020-09-09 11:00:04 +0200

    Bruker tildelinger i spesialist i prod

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -1,6 +1,4 @@
-import { extractIdent } from './utils/cookie';
-
 const erPreprod = () => location.hostname === 'speil.nais.preprod.local' || location.hostname === 'localhost';
 
 export const overstyrbareTabellerEnabled = erPreprod();
-export const spesialistTildelingEnabled = erPreprod() || extractIdent() === 'R154509';
+export const spesialistTildelingEnabled = true;
