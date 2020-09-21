c1cb64cc99c75b04601175ac7fa19aab1688ea4c
Erlend Viddal <eviddal@gmail.com>
2020-09-21 13:54:31 +0200

    Tillater overstyring i produksjon, men bare for Aminet
    Co-authored-by: Martin Solheim <martin.solheim@me.com>
    Co-authored-by: Stephen Ramthun <stephen.ramthun@nav.no>
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -1,4 +1,6 @@
+import { extractIdent } from './utils/cookie';
+
 const erPreprod = () => location.hostname === 'speil.nais.preprod.local' || location.hostname === 'localhost';
 
-export const overstyrbareTabellerEnabled = erPreprod();
+export const overstyrbareTabellerEnabled = erPreprod() || extractIdent() === 'C117102';
 export const spesialistTildelingEnabled = true;
