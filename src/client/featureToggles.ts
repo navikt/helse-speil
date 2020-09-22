17ebbe2b1e0fcd286de9afa34dd240aa82b97b60
Stephen Ramthun <stephenramthun@gmail.com>
2020-09-22 10:50:57 +0200

    Skrur på overstyring i prod
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -1,6 +1,4 @@
-import { extractIdent } from './utils/cookie';
-
 const erPreprod = () => location.hostname === 'speil.nais.preprod.local' || location.hostname === 'localhost';
 
-export const overstyrbareTabellerEnabled = erPreprod() || extractIdent() === 'C117102';
+export const overstyrbareTabellerEnabled = true;
 export const spesialistTildelingEnabled = true;
