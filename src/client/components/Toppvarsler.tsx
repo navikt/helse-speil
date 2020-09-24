cc5f57400ebf4d5224fbfec284112af6394779f7
Anders Østby <anders.ostby@nav.no>
2020-09-24 13:39:44 +0200

    Legger på infomelding hvis perioden er en kandidat for automatisering
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- src/client/components/Toppvarsler.tsx
+++ src/client/components/Toppvarsler.tsx
@@ -1,16 +1,20 @@
 import React, { useContext } from 'react';
 import Varsel, { Varseltype } from '@navikt/helse-frontend-varsel';
 import { PersonContext } from '../context/PersonContext';
+import { Periodetype } from 'internal-types';
 
 const Toppvarsler = () => {
     const { aktivVedtaksperiode } = useContext(PersonContext);
-    const { vilkår, aktivitetslog } = aktivVedtaksperiode!;
+    const { vilkår, aktivitetslog, periodetype } = aktivVedtaksperiode!;
 
     const alleVilkårOppfylt = () =>
         vilkår === undefined || Object.values(vilkår).find((v) => !v?.oppfylt) === undefined;
 
+    const erKandidatForAutomatisering = () => periodetype === Periodetype.Forlengelse && aktivitetslog.length === 0;
+
     return (
         <>
+            {erKandidatForAutomatisering() && <Varsel type={Varseltype.Info}>Kandidat for automatisering</Varsel>}
             {!alleVilkårOppfylt() && <Varsel type={Varseltype.Feil}>Vilkår er ikke oppfylt i deler av perioden</Varsel>}
             {aktivitetslog.length > 0 &&
                 aktivitetslog.map((aktivitet, index) => (
