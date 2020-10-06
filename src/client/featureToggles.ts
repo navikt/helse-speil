55148f32e3ea1055908d8ccc48c46f4e151b338d
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2020-10-06 11:42:08 +0200

    Aktivere paginering i prod
    Bryteren ligger igjen i tilfelle saksbehandlerne hater det.

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -1,9 +1,6 @@
-import { extractIdent } from './utils/cookie';
-
 const erPreprod = () => location.hostname === 'speil.nais.preprod.local' || location.hostname === 'localhost';
-const erDonika = () => extractIdent() === 'D117949';
 
 export const overstyrbareTabellerEnabled = true;
 export const spesialistTildelingEnabled = true;
 export const annulleringerEnabled = erPreprod();
-export const pagineringEnabled = erPreprod() || erDonika();
+export const pagineringEnabled = true;
