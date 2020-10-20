6312ca4f24906c070283ee2e86642818559c0db9
Knut Nygaard <knut.nygaard@nav.no>
2020-10-20 15:30:24 +0200

    Switcher tildeling tilbake bak proxy
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -1,9 +1,6 @@
-import { extractIdent } from './utils/cookie';
-
 const erPreprod = () => location.hostname === 'speil.nais.preprod.local' || location.hostname === 'localhost';
-const erKnutellerJonas = () => extractIdent() === 'N143409' || extractIdent() === 'H159657';
 
 export const overstyrbareTabellerEnabled = true;
 export const annulleringerEnabled = erPreprod();
 export const pagineringEnabled = true;
-export const speilTildeling = erKnutellerJonas();
+export const speilTildeling = true;
