7aa1cacc5e787b223156fbeead465b4d30ec4baa
Simen Ullern <simen.ullern@nav.no>
2020-10-28 15:58:51 +0100

    Flytter sykepengegrunnlag og inntekstopplysninger til redesignet speil
    Co-AUTHORED-by: Martin Solheim <martin.solheim@me.com>

---
+++ src/client/routes/SaksbildeV2/Sykepengegrunnlag/Inntektskilderinnhold.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Element, Normaltekst, Undertekst, Undertittel } from 'nav-frontend-typografi';
+import { toKronerOgØre } from '../../../utils/locale';
+import { Inntektskilde } from 'internal-types';
+import styled from '@emotion/styled';
+import { Grid } from '../../../components/Grid';
+import { Arbeidsgiverikon } from '../../../components/ikoner/Arbeidsgiverikon';
+import { Kilde } from '../../../components/Kilde';
+import { FlexColumn } from '../../../components/Flex';
+
+export interface InntektskilderinnholdProps {
+    inntektskilder: Inntektskilde[];
+}
+
+const Arbeidsgivertittel = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 2rem;
+    > *:not(:last-child) {
+        margin-right: 1rem;
+    }
+`;
+
+const HeaderContainer = styled.div`
+    display: flex;
+    align-items: center;
+
+    > *:not(:last-child) {
+        margin-right: 1rem;
+    }
+`;
+
+const Tittel = styled(Undertittel)`
+    font-size: 18px;
+    color: #3e3832;
+`;
+
+const Tabell = styled.div`
+    display: grid;
+    grid-template-columns: 200px auto;
+    grid-column-gap: 1rem;
+    grid-row-gap: 0.5rem;
+    margin: 0.5rem 0 2rem;
+`;
+
+const Divider = styled.hr`
+    border: none;
+    border-top: 1px solid #e7e9e9;
+    margin: 0 0 2rem 0;
+`;
+
+const Liste = styled.ul`
+    margin: 0.5rem 0 2rem;
+`;
+
+const Innhold = styled(Grid)`
+    margin-top: 2rem;
+    grid-column-gap: 6rem;
+`;
+
+const Kolonnetittel = styled(Undertekst)`
+    color: #3e3832;
+`;
+
+const Inntektskilderinnhold = ({ inntektskilder }: InntektskilderinnholdProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <Innhold kolonner={2}>
+            <FlexColumn>
+                <Arbeidsgivertittel>
+                    <Arbeidsgiverikon />
+                    <Tittel>Arbeidsgiver ({inntektskilder[0].organisasjonsnummer})</Tittel>
+                    <Kilde>Aa</Kilde>
+                </Arbeidsgivertittel>
+                <HeaderContainer>
+                    <Tittel tag="h3">{t('inntektskilder.inntekt')}</Tittel>
+                    <Kilde>IM</Kilde>
+                </HeaderContainer>
+                <Tabell>
+                    <Kolonnetittel>{t('inntektskilder.månedsinntekt')}</Kolonnetittel>
+                    <Kolonnetittel>{t('inntektskilder.årsinntekt')}</Kolonnetittel>
+                    <Element>{`${toKronerOgØre(inntektskilder[0].månedsinntekt!)} kr`}</Element>
+                    <Element>{`${toKronerOgØre(inntektskilder[0].årsinntekt!)} kr`}</Element>
+                </Tabell>
+                <Divider />
+                <HeaderContainer>
+                    <Tittel>{t('inntektskilder.inntektsmeldinger')}</Tittel>
+                </HeaderContainer>
+                <Tabell>
+                    <Normaltekst>{t('inntektskilder.refusjon')}</Normaltekst>
+                    <Normaltekst>{inntektskilder[0].refusjon ? 'Ja' : 'Nei'}</Normaltekst>
+                    <Normaltekst>{t('inntektskilder.arbeidsgiverperiode')}</Normaltekst>
+                    <Normaltekst>{inntektskilder[0].forskuttering ? 'Ja' : 'Nei'}</Normaltekst>
+                    <Normaltekst>{t('inntektskilder.relasjon')}</Normaltekst>
+                    <Normaltekst>Ikke sjekket</Normaltekst>
+                </Tabell>
+            </FlexColumn>
+        </Innhold>
+    );
+};
+
+export default Inntektskilderinnhold;
--- src/client/routes/SaksbildeV2/Sykepengegrunnlag/Sykepengegrunnlag.tsx
+++ src/client/routes/SaksbildeV2/Sykepengegrunnlag/Sykepengegrunnlag.tsx
@@ -1,5 +1,94 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import styled from '@emotion/styled';
+import { PersonContext } from '../../../context/PersonContext';
+import BehandletAvInfotrygd from '@navikt/helse-frontend-behandlet-av-infotrygd';
+import Sykepengegrunnlaginnhold from './Sykepengegrunnlaginnhold';
+import { NORSK_DATOFORMAT } from '../../../utils/date';
+import SykepengegrunnlagInfotrygd from './SykepengegrunnlagInfotrygd';
+import { Periodetype, Vedtaksperiode } from 'internal-types';
+import { førsteVedtaksperiode } from '../../../mapping/selectors';
+import { BehandletVarsel } from '@navikt/helse-frontend-varsel';
+import { AgurkErrorBoundary } from '../../../components/AgurkErrorBoundary';
+import Inntektskilderinnhold from './Inntektskilderinnhold';
+
+const StyledBehandletInnhold = styled(BehandletVarsel)`
+    margin: 2rem 2rem;
+    width: max-content;
+`;
+
+const StyledBehandletAvInfotrygd = styled(BehandletAvInfotrygd)`
+    margin: 2rem 2rem;
+    width: max-content;
+`;
+
+const Sykepengegrunnlagpanel = styled.div`
+    max-width: 1265px;
+    margin: 0 2rem;
+    width: max-content;
+`;
+
+const OversiktContainer = styled.div`
+    display: flex;
+    align-content: space-between;
+`;
+
+const Strek = styled.span`
+    border-right: 2px solid #c6c2bf;
+    height: max-content;
+    display: inline-block;
+    margin-left: 5rem;
+    margin-right: 2rem;
+`;
+
+const Oversikt = ({ aktivVedtaksperiode }: { aktivVedtaksperiode: Vedtaksperiode }) => (
+    <OversiktContainer>
+        <Sykepengegrunnlaginnhold sykepengegrunnlag={aktivVedtaksperiode.sykepengegrunnlag} />
+        <Strek />
+        <Inntektskilderinnhold inntektskilder={aktivVedtaksperiode.inntektskilder} />
+    </OversiktContainer>
+);
 
 export const Sykepengegrunnlag = () => {
-    return <>Sykepengegrunnlag</>;
+    const { aktivVedtaksperiode, personTilBehandling } = useContext(PersonContext);
+
+    if (!aktivVedtaksperiode || !personTilBehandling) return null;
+
+    const førstePeriode = førsteVedtaksperiode(aktivVedtaksperiode, personTilBehandling);
+    const skjæringstidspunkt = aktivVedtaksperiode.vilkår?.dagerIgjen?.skjæringstidspunkt
+        ? aktivVedtaksperiode.vilkår.dagerIgjen.skjæringstidspunkt.format(NORSK_DATOFORMAT)
+        : 'Ukjent dato';
+
+    const { sykepengegrunnlag, periodetype } = aktivVedtaksperiode;
+
+    const Innhold = () =>
+        periodetype === Periodetype.Førstegangsbehandling ? (
+            <Oversikt aktivVedtaksperiode={aktivVedtaksperiode} />
+        ) : periodetype === Periodetype.Infotrygdforlengelse ? (
+            <OversiktContainer>
+                <StyledBehandletAvInfotrygd tittel={`Sykepengegrunnlag satt i Infotrygd`}>
+                    <SykepengegrunnlagInfotrygd
+                        årsinntektFraInntektsmelding={sykepengegrunnlag.årsinntektFraInntektsmelding}
+                    />
+                </StyledBehandletAvInfotrygd>
+                <Strek />
+                <Inntektskilderinnhold inntektskilder={aktivVedtaksperiode.inntektskilder} />
+            </OversiktContainer>
+        ) : (
+            <StyledBehandletInnhold
+                tittel={`Sykepengegrunnlag satt ved skjæringstidspunkt - ${skjæringstidspunkt}`}
+                saksbehandler={førstePeriode?.godkjentAv!}
+                vurderingsdato={førstePeriode?.godkjenttidspunkt?.format(NORSK_DATOFORMAT)}
+                automatiskBehandlet={førstePeriode.automatiskBehandlet}
+            >
+                <Oversikt aktivVedtaksperiode={aktivVedtaksperiode} />
+            </StyledBehandletInnhold>
+        );
+
+    return (
+        <Sykepengegrunnlagpanel>
+            <AgurkErrorBoundary>
+                <Innhold />
+            </AgurkErrorBoundary>
+        </Sykepengegrunnlagpanel>
+    );
 };
