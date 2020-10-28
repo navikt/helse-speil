37e747175740abf59c2fe6d46218379125033fd4
Simen Ullern <simen.ullern@nav.no>
2020-10-28 12:52:02 +0100

    Legger til sykmeldingsperiode i redesignet speil
    Co-AUTHORED-by: Martin Solheim <martin.solheim@me.com>

---
+++ src/client/routes/SaksbildeV2/Sykmeldingsperiode/OverstyrbarSykmeldingsperiodetabell.tsx
@@ -0,0 +1,157 @@
+import React, { useContext, useState } from 'react';
+import { Overstyringsskjema } from '../../../components/tabell/Overstyringsskjema';
+import {
+    dato,
+    ikon,
+    overstyrbarGradering,
+    overstyrbarKilde,
+    overstyrbarType,
+    tomCelle,
+} from '../../../components/tabell/rader';
+import { Dagtype, Sykdomsdag } from 'internal-types';
+import { PersonContext } from '../../../context/PersonContext';
+import { NORSK_DATOFORMAT } from '../../../utils/date';
+import Element from 'nav-frontend-typografi/lib/element';
+import { Overstyringsknapp } from '../../../components/tabell/Overstyringsknapp';
+import styled from '@emotion/styled';
+import { Tabell } from '@navikt/helse-frontend-tabell';
+import { overstyrbareTabellerEnabled } from '../../../featureToggles';
+import { FormProvider, useForm } from 'react-hook-form';
+import { postOverstyring } from '../../../io/http';
+import { useFjernEnToast, useLeggTilEnToast } from '../../../state/toastsState';
+import { kalkulererFerdigToastKey, kalkulererToast, kalkuleringFerdigToast } from './KalkulererOverstyringToast';
+import { OverstyrtDagDTO } from '../../../io/types';
+import { useOverstyrteDager } from './useOverstyrteDager';
+import { pollEtterNyOppgave } from '../../../io/polling';
+import { organisasjonsnummerForPeriode } from '../../../mapping/selectors';
+
+const OverstyrbarTabell = styled(Tabell)`
+    thead,
+    thead tr,
+    thead tr th {
+        vertical-align: bottom;
+    }
+    tbody tr td {
+        height: 48px;
+    }
+`;
+
+const HøyrestiltContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const Feilmelding = styled.p`
+    color: #ba3a26;
+    margin: 1rem 0 0;
+    font-weight: 600;
+`;
+
+const tilOverstyrtDagtype = (type: Dagtype): 'Sykedag' | 'Feriedag' | 'Egenmeldingsdag' | Dagtype => {
+    switch (type) {
+        case Dagtype.Syk:
+            return 'Sykedag';
+        case Dagtype.Ferie:
+            return 'Feriedag';
+        case Dagtype.Egenmelding:
+            return 'Egenmeldingsdag';
+        default:
+            return type;
+    }
+};
+
+const tilOverstyrteDager = (dager: Sykdomsdag[]): OverstyrtDagDTO[] =>
+    dager.map((dag) => ({
+        dato: dag.dato.format('YYYY-MM-DD'),
+        type: tilOverstyrtDagtype(dag.type),
+        grad: dag.gradering,
+    }));
+
+interface OverstyrbarSykmeldingsperiodetabellProps {
+    onOverstyr: () => void;
+    onToggleOverstyring: () => void;
+}
+
+export const OverstyrbarSykmeldingsperiodetabell = ({
+    onOverstyr,
+    onToggleOverstyring,
+}: OverstyrbarSykmeldingsperiodetabellProps) => {
+    const { overstyrteDager, leggTilOverstyrtDag, fjernOverstyrtDag } = useOverstyrteDager();
+    const { aktivVedtaksperiode, personTilBehandling, hentPerson } = useContext(PersonContext);
+    const [overstyringserror, setOverstyringserror] = useState<string>();
+    const leggtilEnToast = useLeggTilEnToast();
+    const fjernToast = useFjernEnToast();
+    const form = useForm({ shouldFocusError: false, mode: 'onBlur' });
+
+    const fom = aktivVedtaksperiode?.fom.format(NORSK_DATOFORMAT);
+    const tom = aktivVedtaksperiode?.tom.format(NORSK_DATOFORMAT);
+    const tabellbeskrivelse = `Overstyrer sykmeldingsperiode fra ${fom} til ${tom}`;
+    const organisasjonsnummer = organisasjonsnummerForPeriode(aktivVedtaksperiode!, personTilBehandling!);
+
+    const headere = [
+        '',
+        { render: <Element>Sykmeldingsperiode</Element>, kolonner: 3 },
+        { render: <Element>Gradering</Element> },
+        overstyrbareTabellerEnabled ? (
+            <HøyrestiltContainer>
+                <Overstyringsknapp overstyrer toggleOverstyring={onToggleOverstyring} />
+            </HøyrestiltContainer>
+        ) : (
+            ''
+        ),
+    ];
+
+    const tilTabellrad = (dag: Sykdomsdag) => {
+        const overstyrtDag = overstyrteDager.find((overstyrtDag) => overstyrtDag.dato.isSame(dag.dato));
+        const erOverstyrt = overstyrtDag !== undefined && JSON.stringify(overstyrtDag) !== JSON.stringify(dag);
+        const dagen = overstyrtDag ?? dag;
+        return {
+            celler: [
+                tomCelle(),
+                dato(dagen),
+                ikon(dagen),
+                overstyrbarType(dagen, leggTilOverstyrtDag, fjernOverstyrtDag),
+                overstyrbarGradering(dagen, leggTilOverstyrtDag, fjernOverstyrtDag),
+                overstyrbarKilde(dagen, erOverstyrt),
+            ],
+            className: dagen.type === Dagtype.Helg ? 'disabled' : undefined,
+        };
+    };
+
+    const rader = aktivVedtaksperiode?.sykdomstidslinje.map(tilTabellrad) ?? [];
+
+    const refetchPerson = () => hentPerson(personTilBehandling!.fødselsnummer);
+
+    const visOverstyringFerdigToast = () =>
+        leggtilEnToast(kalkuleringFerdigToast({ callback: () => fjernToast(kalkulererFerdigToastKey) }));
+
+    const overstyring = () => ({
+        aktørId: personTilBehandling!.aktørId,
+        fødselsnummer: personTilBehandling!.fødselsnummer,
+        organisasjonsnummer: organisasjonsnummer,
+        dager: tilOverstyrteDager(overstyrteDager),
+        begrunnelse: form.getValues().begrunnelse,
+    });
+
+    const sendOverstyring = () => {
+        postOverstyring(overstyring())
+            .then(() => {
+                leggtilEnToast(kalkulererToast({}));
+                onOverstyr();
+                pollEtterNyOppgave(personTilBehandling!.fødselsnummer, aktivVedtaksperiode!.oppgavereferanse)
+                    .then(refetchPerson)
+                    .then(visOverstyringFerdigToast);
+            })
+            .catch(() => setOverstyringserror('Feil under sending av overstyring. Prøv igjen senere.'));
+    };
+
+    return (
+        <FormProvider {...form}>
+            <form onSubmit={form.handleSubmit(sendOverstyring)}>
+                <OverstyrbarTabell beskrivelse={tabellbeskrivelse} headere={headere} rader={rader} />
+                <Overstyringsskjema overstyrteDager={overstyrteDager} avbrytOverstyring={onToggleOverstyring} />
+                {overstyringserror && <Feilmelding role="alert">{overstyringserror}</Feilmelding>}
+            </form>
+        </FormProvider>
+    );
+};
--- src/client/routes/SaksbildeV2/Sykmeldingsperiode/Sykmeldingsperiode.tsx
+++ src/client/routes/SaksbildeV2/Sykmeldingsperiode/Sykmeldingsperiode.tsx
@@ -1,5 +1,74 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import styled from '@emotion/styled';
+import { Navigasjonsknapper } from '../../../components/Navigasjonsknapper';
+import '@navikt/helse-frontend-tabell/lib/main.css';
+import { Sykmeldingsperiodetabell } from './Sykmeldingsperiodetabell';
+import { OverstyrbarSykmeldingsperiodetabell } from './OverstyrbarSykmeldingsperiodetabell';
+import { OverstyringTimeoutModal } from './OverstyringTimeoutModal';
+import { AgurkErrorBoundary } from '../../../components/AgurkErrorBoundary';
+import { Undertittel } from 'nav-frontend-typografi';
+import { PersonContext } from '../../../context/PersonContext';
+import { Dayjs } from 'dayjs';
+import { useMaksdato } from '../../../hooks/useMaksdato';
+import { NORSK_DATOFORMAT } from '../../../utils/date';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 1.5rem 2rem;
+`;
+
+const formaterPeriodeString = (fom: Dayjs, tom: Dayjs) => {
+    const fomString = fom.year() === tom.year() ? fom.format('DD.MM') : fom.format('DD.MM.YYYY');
+    return `${fomString}-${tom.format('DD.MM.YYYY')}`;
+};
+
+const SykmeldingperiodeHeader = styled(Undertittel)`
+    margin-bottom: 2rem;
+`;
 
 export const Sykmeldingsperiode = () => {
-    return <>Sykmeldingsperiode</>;
+    const [overstyrer, setOverstyrer] = useState(false);
+    const [kalkulerer, setKalkulerer] = useState(false);
+    const [overstyringTimedOut, setOverstyringTimedOut] = useState(false);
+    const { aktivVedtaksperiode } = useContext(PersonContext);
+    const { maksdato } = useMaksdato();
+
+    useEffect(() => {
+        let timeoutId: any;
+        if (kalkulerer) {
+            timeoutId = setTimeout(() => setOverstyringTimedOut(true), 10000);
+        }
+        return () => {
+            !!timeoutId && clearTimeout(timeoutId);
+        };
+    }, [kalkulerer]);
+
+    const fom = aktivVedtaksperiode?.fom;
+    const tom = aktivVedtaksperiode?.tom;
+    const periode = fom && tom ? formaterPeriodeString(fom, tom) : 'Ukjent periode';
+    const skjæringstidspunkt = maksdato ? maksdato.format(NORSK_DATOFORMAT) : 'ukjent';
+
+    return (
+        <Container className="Sykmeldingsperiode">
+            <AgurkErrorBoundary>
+                <SykmeldingperiodeHeader>
+                    Sykemeldingsperiode / {periode} / Skjæringstidspunkt {skjæringstidspunkt}
+                </SykmeldingperiodeHeader>
+                {overstyrer ? (
+                    <OverstyrbarSykmeldingsperiodetabell
+                        onOverstyr={() => {
+                            setOverstyrer(false);
+                            setKalkulerer(true);
+                        }}
+                        onToggleOverstyring={() => setOverstyrer((o) => !o)}
+                    />
+                ) : (
+                    <Sykmeldingsperiodetabell toggleOverstyring={() => setOverstyrer((o) => !o)} />
+                )}
+            </AgurkErrorBoundary>
+            {overstyringTimedOut && <OverstyringTimeoutModal onRequestClose={() => setOverstyringTimedOut(false)} />}
+            {!overstyrer && <Navigasjonsknapper />}
+        </Container>
+    );
 };
