05fd011e540a89ab8b2b3e60185ea17811ea3ea2
Martin Solheim <martin.solheim@me.com>
2020-11-03 15:28:12 +0100

    Bruker riktig farge på kildeikon
    Co-authored-by: Stephen Ramthun <stephen.ramthun@nav.no>
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>

--- src/client/components/Kilde.tsx
+++ src/client/components/Kilde.tsx
@@ -4,11 +4,11 @@ import styled from '@emotion/styled';
 export const Kilde = styled.div`
     display: flex;
     justify-content: center;
-    border: 1px solid #0067c5;
+    border: 1px solid #59514b;
     padding: 1px 0.25rem;
     font-size: 14px;
     border-radius: 0.25rem;
-    color: #0067c5;
+    color: #59514b;
     width: 28px;
     box-sizing: border-box;
 `;
