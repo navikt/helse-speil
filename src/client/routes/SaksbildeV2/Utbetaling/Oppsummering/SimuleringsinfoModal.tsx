fb6ff4f341e2da3ee8b5bd89e1ba4004b38515c8
Martin Solheim <martin.solheim@me.com>
2020-11-04 10:11:54 +0100

    Rydder i simuleringsinfomodal
    - Fjerner tilder og dasher som spaceing.
    - Bruker typografi for å indikere hierarki.
    - Legger på mer padding rundt modalinnhold.

--- src/client/routes/SaksbildeV2/Utbetaling/Oppsummering/SimuleringsinfoModal.tsx
+++ src/client/routes/SaksbildeV2/Utbetaling/Oppsummering/SimuleringsinfoModal.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import dayjs from 'dayjs';
 import styled from '@emotion/styled';
 import { Simulering } from 'internal-types';
-import { Normaltekst } from 'nav-frontend-typografi';
-import { Utbetalingsdetalj } from 'external-types';
+import { Element, Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import { Utbetaling, Utbetalingsdetalj } from 'external-types';
 import { NORSK_DATOFORMAT } from '../../../../utils/date';
 import { Grid } from '../../../../components/Grid';
-import { toKronerOgØre } from '../../../../utils/locale';
+import { somPenger, toKronerOgØre } from '../../../../utils/locale';
 import { Modal } from '../../../../components/Modal';
+
+const Modalinnhold = styled.article`
+    padding: 0 4rem 2rem 4rem;
+`;
 
 const Underliste = styled(Grid)`
     &:not(:last-child) {
@@ -15,87 +19,89 @@ const Underliste = styled(Grid)`
     }
 `;
 
+const Luft = styled.div`
+    grid-column-start: 1;
+    grid-column-end: span 2;
+    margin-bottom: 1rem;
+`;
+
 const formaterDato = (forfall: string) => dayjs(forfall).format(NORSK_DATOFORMAT);
 
-interface SimuleringsmodalProps {
+type SimuleringsmodalProps = {
     simulering: Simulering;
     åpenModal: boolean;
     lukkModal: () => void;
-}
-
-export const SimuleringsinfoModal = ({ simulering, åpenModal, lukkModal }: SimuleringsmodalProps) => {
-    return (
-        <Modal isOpen={åpenModal} contentLabel="Simuleringsinfo" onRequestClose={lukkModal}>
-            <Grid gridTemplateColumns="1fr 1fr">
-                <Normaltekst>Totalbeløp simulering</Normaltekst>
-                <Normaltekst>{toKronerOgØre(simulering.totalbeløp)} kr</Normaltekst>
-            </Grid>
-            <>
-                {simulering.perioder.map((periode, index) => (
-                    <Underliste gridTemplateColumns="1fr 1fr" key={`periode-${index}`}>
-                        <Normaltekst>~~~~~~~~~~~~</Normaltekst>
-                        <Normaltekst>~~~~~~~~~~~~</Normaltekst>
-                        <Normaltekst>Periode</Normaltekst>
-                        <Normaltekst>{`${formaterDato(periode.fom)} - ${formaterDato(periode.tom)}`}</Normaltekst>
-                        <>
-                            {periode.utbetalinger.map((utbetaling, index) => (
-                                <React.Fragment key={`utbetaling-${index}`}>
-                                    {index > 0 && (
-                                        <>
-                                            <Normaltekst>-------------------</Normaltekst>
-                                            <Normaltekst>-------------------</Normaltekst>
-                                        </>
-                                    )}
-                                    <Normaltekst>Utbetales til ID</Normaltekst>
-                                    <Normaltekst>{utbetaling.utbetalesTilId}</Normaltekst>
-                                    <Normaltekst>Utbetales til navn</Normaltekst>
-                                    <Normaltekst>{utbetaling.utbetalesTilNavn}</Normaltekst>
-                                    <Normaltekst>Forfall</Normaltekst>
-                                    <Normaltekst>{formaterDato(utbetaling.forfall)}</Normaltekst>
-                                    <Normaltekst>Feilkonto</Normaltekst>
-                                    <Normaltekst>{utbetaling.feilkonto ? 'Ja' : 'Nei'}</Normaltekst>
-                                    {utbetaling.detaljer.map((detalj: Utbetalingsdetalj, index: number) => (
-                                        <React.Fragment key={`detalj-${index}`}>
-                                            {index > 0 && (
-                                                <>
-                                                    <Normaltekst>-------------------</Normaltekst>
-                                                    <Normaltekst>-------------------</Normaltekst>
-                                                </>
-                                            )}
-                                            <Normaltekst>Faktisk fom</Normaltekst>
-                                            <Normaltekst>{formaterDato(detalj.faktiskFom)}</Normaltekst>
-                                            <Normaltekst>Faktisk tom</Normaltekst>
-                                            <Normaltekst>{formaterDato(detalj.faktiskTom)}</Normaltekst>
-                                            <Normaltekst>Sats</Normaltekst>
-                                            <Normaltekst>{toKronerOgØre(detalj.sats)} kr</Normaltekst>
-                                            <Normaltekst>Antall sats</Normaltekst>
-                                            <Normaltekst>{detalj.antallSats}</Normaltekst>
-                                            <Normaltekst>Type sats</Normaltekst>
-                                            <Normaltekst>{detalj.typeSats}</Normaltekst>
-                                            <Normaltekst>Beløp</Normaltekst>
-                                            <Normaltekst>{toKronerOgØre(detalj.belop)} kr</Normaltekst>
-                                            <Normaltekst>Konto</Normaltekst>
-                                            <Normaltekst>{detalj.konto}</Normaltekst>
-                                            <Normaltekst>Klassekode</Normaltekst>
-                                            <Normaltekst>{detalj.klassekode}</Normaltekst>
-                                            <Normaltekst>Klassekodebeskrivelse</Normaltekst>
-                                            <Normaltekst>{detalj.klassekodeBeskrivelse}</Normaltekst>
-                                            <Normaltekst>Uføregrad</Normaltekst>
-                                            <Normaltekst>{detalj.uforegrad}</Normaltekst>
-                                            <Normaltekst>Utbetalingstype</Normaltekst>
-                                            <Normaltekst>{detalj.utbetalingsType}</Normaltekst>
-                                            <Normaltekst>Refunderes orgnummer</Normaltekst>
-                                            <Normaltekst>{detalj.refunderesOrgNr}</Normaltekst>
-                                            <Normaltekst>Tilbakeføring</Normaltekst>
-                                            <Normaltekst>{detalj.tilbakeforing ? 'Ja' : 'Nei'}</Normaltekst>
-                                        </React.Fragment>
-                                    ))}
-                                </React.Fragment>
-                            ))}
-                        </>
-                    </Underliste>
-                ))}
-            </>
-        </Modal>
-    );
 };
+
+export const SimuleringsinfoModal = ({ simulering, åpenModal, lukkModal }: SimuleringsmodalProps) => (
+    <Modal isOpen={åpenModal} contentLabel="Simuleringsinfo" onRequestClose={lukkModal}>
+        <Modalinnhold>
+            <Grid gridTemplateColumns="1fr 1fr">
+                <Undertittel>Totalbeløp simulering</Undertittel>
+                <Undertittel>{toKronerOgØre(simulering.totalbeløp)} kr</Undertittel>
+            </Grid>
+            {simulering.perioder.map((periode, index) => (
+                <Underliste gridTemplateColumns="1fr 1fr" key={`periode-${index}`}>
+                    <Luft />
+                    <Element>Periode</Element>
+                    <Element>{`${formaterDato(periode.fom)} - ${formaterDato(periode.tom)}`}</Element>
+                    <Luft />
+                    {periode.utbetalinger.map((utbetaling, index) => (
+                        <Utbetalingsvisning utbetaling={utbetaling} index={index} />
+                    ))}
+                </Underliste>
+            ))}
+        </Modalinnhold>
+    </Modal>
+);
+
+type UtbetalingsvisningProps = { utbetaling: Utbetaling; index: number };
+const Utbetalingsvisning = ({ utbetaling, index }: UtbetalingsvisningProps) => (
+    <React.Fragment key={`utbetaling-${index}`}>
+        {index > 0 && <Luft />}
+        <Normaltekst>Utbetales til ID</Normaltekst>
+        <Normaltekst>{utbetaling.utbetalesTilId}</Normaltekst>
+        <Normaltekst>Utbetales til navn</Normaltekst>
+        <Normaltekst>{utbetaling.utbetalesTilNavn}</Normaltekst>
+        <Normaltekst>Forfall</Normaltekst>
+        <Normaltekst>{formaterDato(utbetaling.forfall)}</Normaltekst>
+        <Normaltekst>Feilkonto</Normaltekst>
+        <Normaltekst>{utbetaling.feilkonto ? 'Ja' : 'Nei'}</Normaltekst>
+        {utbetaling.detaljer.map((detalj: Utbetalingsdetalj, index: number) => (
+            <Utbetalingsdetaljvisning detalj={detalj} index={index} />
+        ))}
+    </React.Fragment>
+);
+
+type UtbetalingsdetaljvisningProps = { detalj: Utbetalingsdetalj; index: number };
+const Utbetalingsdetaljvisning = ({ detalj, index }: UtbetalingsdetaljvisningProps) => (
+    <React.Fragment key={`detalj-${index}`}>
+        {index > 0 && <Luft />}
+        <Normaltekst>Faktisk fom</Normaltekst>
+        <Normaltekst>{formaterDato(detalj.faktiskFom)}</Normaltekst>
+        <Normaltekst>Faktisk tom</Normaltekst>
+        <Normaltekst>{formaterDato(detalj.faktiskTom)}</Normaltekst>
+        <Normaltekst>Sats</Normaltekst>
+        <Normaltekst>{somPenger(detalj.sats)}</Normaltekst>
+        <Normaltekst>Antall sats</Normaltekst>
+        <Normaltekst>{detalj.antallSats}</Normaltekst>
+        <Normaltekst>Type sats</Normaltekst>
+        <Normaltekst>{detalj.typeSats}</Normaltekst>
+        <Normaltekst>Beløp</Normaltekst>
+        <Normaltekst>{somPenger(detalj.belop)}</Normaltekst>
+        <Normaltekst>Konto</Normaltekst>
+        <Normaltekst>{detalj.konto}</Normaltekst>
+        <Normaltekst>Klassekode</Normaltekst>
+        <Normaltekst>{detalj.klassekode}</Normaltekst>
+        <Normaltekst>Klassekodebeskrivelse</Normaltekst>
+        <Normaltekst>{detalj.klassekodeBeskrivelse}</Normaltekst>
+        <Normaltekst>Uføregrad</Normaltekst>
+        <Normaltekst>{detalj.uforegrad}</Normaltekst>
+        <Normaltekst>Utbetalingstype</Normaltekst>
+        <Normaltekst>{detalj.utbetalingsType}</Normaltekst>
+        <Normaltekst>Refunderes orgnummer</Normaltekst>
+        <Normaltekst>{detalj.refunderesOrgNr}</Normaltekst>
+        <Normaltekst>Tilbakeføring</Normaltekst>
+        <Normaltekst>{detalj.tilbakeforing ? 'Ja' : 'Nei'}</Normaltekst>
+    </React.Fragment>
+);
