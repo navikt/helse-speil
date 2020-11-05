b47527dc871820f0c0acc9b8d33de5d20ed9f4a3
Stephen Ramthun <stephenramthun@gmail.com>
2020-11-05 13:27:59 +0100

    Aktiverer redesign for enkelte saksbehandlere
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -4,7 +4,12 @@ const erLocal = () => location.hostname === 'localhost';
 const erPreprod = () => location.hostname === 'speil.nais.preprod.local';
 const erSupersaksbehandler = () => extractIdent() === 'G103083';
 
+const erUlrik = () => extractIdent() === 'P141762';
+const erDaniel = () => extractIdent() === 'A148751';
+const erMorten = () => extractIdent() === 'N115007';
+const erAminet = () => extractIdent() === 'C117102';
+
 export const overstyrbareTabellerEnabled = true;
 export const annulleringerEnabled = erPreprod() || erLocal() || erSupersaksbehandler();
 export const speilTildeling = true;
-export const speilV2 = erLocal();
+export const speilV2 = erLocal() || erUlrik() || erDaniel() || erMorten() || erAminet();
