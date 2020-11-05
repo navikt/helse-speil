0d6de6e28db2d6a226d1f2489d5083ee6a8e5cfa
Sindre Broch <sindre.broch@nav.no>
2020-11-05 13:20:05 +0100

    Skrur på annullering for supersaksbehandler.
    Co-authored-by: Knut Nygaard<Knut.Nygaard@nav.no>

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -1,7 +1,10 @@
+import { extractIdent } from './utils/cookie';
+
 const erLocal = () => location.hostname === 'localhost';
 const erPreprod = () => location.hostname === 'speil.nais.preprod.local';
+const erSupersaksbehandler = () => extractIdent() === 'G103083';
 
 export const overstyrbareTabellerEnabled = true;
-export const annulleringerEnabled = erPreprod() || erLocal();
+export const annulleringerEnabled = erPreprod() || erLocal() || erSupersaksbehandler();
 export const speilTildeling = true;
 export const speilV2 = erLocal();
