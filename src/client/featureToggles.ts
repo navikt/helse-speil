d21aec11d9813381e18b9ad530565d6db2e6e9d6
Stephen Ramthun <stephenramthun@gmail.com>
2020-11-06 13:43:54 +0100

    Aktiverer redesign for meg selv i prod

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -11,8 +11,9 @@ const erDaniel = () => extractIdent() === 'A148751';
 const erMorten = () => extractIdent() === 'N115007';
 const erAminet = () => extractIdent() === 'C117102';
 const erDigiMort = () => extractIdent() === 'T127350';
+const erStephen = () => extractIdent() === 'R154509';
 
 export const overstyrbareTabellerEnabled = true;
 export const annulleringerEnabled = erPreprod() || erLocal() || erSupersaksbehandler();
 export const speilTildeling = true;
-export const speilV2 = erLocal() || erUlrik() || erDaniel() || erMorten() || erAminet() || erDigiMort();
+export const speilV2 = erLocal() || erUlrik() || erDaniel() || erMorten() || erAminet() || erDigiMort() || erStephen();
