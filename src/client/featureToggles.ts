e0808755447ca5ccbc2b6f5ee4bc2e95582dad7c
Martin Solheim <martin.solheim@me.com>
2020-11-06 09:15:24 +0100

    Gir Morten tilgang til speil v2

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -8,8 +8,9 @@ const erUlrik = () => extractIdent() === 'P141762';
 const erDaniel = () => extractIdent() === 'A148751';
 const erMorten = () => extractIdent() === 'N115007';
 const erAminet = () => extractIdent() === 'C117102';
+const erDigiMort = () => extractIdent() === 'T127350';
 
 export const overstyrbareTabellerEnabled = true;
 export const annulleringerEnabled = erPreprod() || erLocal() || erSupersaksbehandler();
 export const speilTildeling = true;
-export const speilV2 = erLocal() || erUlrik() || erDaniel() || erMorten() || erAminet();
+export const speilV2 = erLocal() || erUlrik() || erDaniel() || erMorten() || erAminet() || erDigiMort();
