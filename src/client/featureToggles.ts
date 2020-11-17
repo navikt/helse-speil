1fcd16c74d16bda78e30b2f3ea7a1c74b145d3b6
Martin Solheim <martin.solheim@me.com>
2020-11-17 09:59:40 +0100

    Skrur på speilv2 for alle
    Co-authored-by: Stephen Ramthun <stephen.ramthun@nav.no>
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -6,27 +6,11 @@ const erSupersaksbehandler = () => erEminem() || erDonika() || erDaniel() || erM
 
 const erEminem = () => extractIdent() === 'G103083';
 const erDonika = () => extractIdent() === 'D117949';
-const erUlrik = () => extractIdent() === 'P141762';
 const erDaniel = () => extractIdent() === 'A148751';
 const erMorten = () => extractIdent() === 'N115007';
 const erAminet = () => extractIdent() === 'C117102';
-const erDigiMort = () => extractIdent() === 'T127350';
-const erStephen = () => extractIdent() === 'R154509';
-const erMartin = () => extractIdent() === 'S149030';
-const erJonas = () => extractIdent() === 'H159657';
-const erOddur = () => extractIdent() === 'S145454';
 
 export const overstyrbareTabellerEnabled = true;
 export const annulleringerEnabled = erPreprod() || erLocal() || erSupersaksbehandler();
 export const speilTildeling = true;
-export const speilV2 =
-    erLocal() ||
-    erUlrik() ||
-    erDaniel() ||
-    erDigiMort() ||
-    erStephen() ||
-    erAminet() ||
-    erMartin() ||
-    erJonas() ||
-    erMorten() ||
-    erOddur();
+export const speilV2 = true;
