fd258ade76d235e24d2457910adc1f24634d15ba
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2020-11-30 13:43:21 +0100

    Annulleringsknapp for supportsaksbehandlere

--- src/client/featureToggles.ts
+++ src/client/featureToggles.ts
@@ -3,17 +3,22 @@ import { extractIdent } from './utils/cookie';
 const erLocal = () => location.hostname === 'localhost';
 const erPreprod = () => location.hostname === 'speil.nais.preprod.local';
 const erSupersaksbehandler = () => erEminem() || erDonika() || erDaniel() || erMorten() || erAminet();
+const erSupportsaksbehandler = () => erEliHåkonsen() || erEirinØdegård();
+
+const erEliHåkonsen = () => extractIdent() === 'H104215';
+const erEirinØdegård = () => extractIdent() === 'O130292';
 
 const erEminem = () => extractIdent() === 'G103083';
 const erDonika = () => extractIdent() === 'D117949';
 const erDaniel = () => extractIdent() === 'A148751';
 const erMorten = () => extractIdent() === 'N115007';
 const erAminet = () => extractIdent() === 'C117102';
+
 const erKnudix = () => extractIdent() === 'N143409';
 const erKevin = () => extractIdent() === 'S151890';
 
 export const overstyrbareTabellerEnabled = true;
-export const annulleringerEnabled = erPreprod() || erLocal() || erSupersaksbehandler();
+export const annulleringerEnabled = erPreprod() || erLocal() || erSupportsaksbehandler() || erSupersaksbehandler();
 export const oppdaterPersondataEnabled = erPreprod() || erLocal() || erSupersaksbehandler() || erKnudix() || erKevin();
 export const speilTildeling = true;
 export const speilV2 = true;
