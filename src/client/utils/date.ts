a74516785dc35547ba2dc39c66b78742389a6b06
Stephen Ramthun <stephenramthun@gmail.com>
2021-01-14 22:19:07 +0100

    Fjerner ubrukt kode

---
+++ src/client/utils/date.ts
@@ -0,0 +1,23 @@
+import dayjs, { Dayjs } from 'dayjs';
+import isoWeek from 'dayjs/plugin/isoWeek';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+
+dayjs.extend(isoWeek);
+dayjs.extend(isSameOrBefore);
+
+export const NORSK_DATOFORMAT = 'DD.MM.YYYY';
+export const NORSK_DATOFORMAT_KORT = 'DD.MM.YY';
+export const ISO_DATOFORMAT = 'YYYY-MM-DD';
+export const ISO_TIDSPUNKTFORMAT = 'YYYY-MM-DDTHH:mm:ss';
+
+export const findLatest = (dates: Dayjs[]): Dayjs => {
+    return Array.from(dates)
+        .sort((a, b) => (b.isAfter(a) ? -1 : a.isAfter(b) ? 1 : 0))
+        .pop()!;
+};
+
+export const findEarliest = (dates: Dayjs[]): Dayjs => {
+    return Array.from(dates)
+        .sort((a, b) => (b.isBefore(a) ? -1 : a.isBefore(b) ? 1 : 0))
+        .pop()!;
+};
