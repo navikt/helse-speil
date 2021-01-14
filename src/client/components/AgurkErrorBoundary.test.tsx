f524158be68934a9662aef5e44e577305c8b75c8
Stephen Ramthun <stephenramthun@gmail.com>
2021-01-14 10:23:44 +0100

    Test AgurkErrorBoundary

---
+++ src/client/components/AgurkErrorBoundary.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { AgurkErrorBoundary } from './AgurkErrorBoundary';
+
+const swallowErrorsInJsDom = () => {
+    window.addEventListener('error', (event: Event) => event.preventDefault());
+};
+
+swallowErrorsInJsDom();
+
+const ThrowsError = ({ errorMessage }: { errorMessage: string }) => {
+    throw Error(errorMessage);
+};
+
+describe('AgurkErrorBoundary', () => {
+    test('rendrer feilmelding', () => {
+        const feilmelding = 'Dette er en feil';
+        render(
+            <AgurkErrorBoundary>
+                <ThrowsError errorMessage={feilmelding} />
+            </AgurkErrorBoundary>
+        );
+        waitFor(() => expect(screen.queryByText(feilmelding)).toBeVisible());
+    });
+});
