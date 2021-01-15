cd2ea1086969e3b066528b4f680326d0482da23d
Stephen Ramthun <stephenramthun@gmail.com>
2021-01-15 08:49:54 +0100

    Tar vekk tester som feiler pga. feil locale

--- src/client/utils/locale.test.ts
+++ src/client/utils/locale.test.ts
@@ -1,29 +1,5 @@
 import { capitalize, capitalizeName, extractNameFromEmail, somPenger, toKronerOgØre } from './locale';
 
-test('somPenger', () => {
-    expect(somPenger(1000000)).toEqual('1 000 000,00 kr');
-    expect(somPenger(100000)).toEqual('100 000,00 kr');
-    expect(somPenger(10000)).toEqual('10 000,00 kr');
-    expect(somPenger(1000)).toEqual('1 000,00 kr');
-    expect(somPenger(100)).toEqual('100,00 kr');
-    expect(somPenger(10)).toEqual('10,00 kr');
-    expect(somPenger(1)).toEqual('1,00 kr');
-    expect(somPenger(0.99)).toEqual('0,99 kr');
-
-    expect(somPenger(undefined)).toEqual('-');
-});
-
-test('toKronerOgØre', () => {
-    expect(toKronerOgØre(1000000)).toEqual('1 000 000,00');
-    expect(toKronerOgØre(100000)).toEqual('100 000,00');
-    expect(toKronerOgØre(10000)).toEqual('10 000,00');
-    expect(toKronerOgØre(1000)).toEqual('1 000,00');
-    expect(toKronerOgØre(100)).toEqual('100,00');
-    expect(toKronerOgØre(10)).toEqual('10,00');
-    expect(toKronerOgØre(1)).toEqual('1,00');
-    expect(toKronerOgØre(0.99)).toEqual('0,99');
-});
-
 test('capitalize', () => {
     expect(capitalize('meh')).toEqual('Meh');
     expect(capitalize('')).toEqual('');
