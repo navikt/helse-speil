6905760ca336681c9a9300a3fdb6e17b1be200fd
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2021-03-17 13:56:01 +0100

    Leser ut oid fra access token
    Co-authored-by: Knut Nygaard <knut.nygaard@nav.no>

--- src/server/auth/authSupport.ts
+++ src/server/auth/authSupport.ts
@@ -83,7 +83,7 @@ const claimsFrom = (token: string): any => {
 
 const createTokenForTest = () =>
     `${Buffer.from(JSON.stringify({ alg: 'HS256', typ: 'JWT' })).toString('base64')}.${Buffer.from(
-        JSON.stringify({ name: 'S. A. Ksbehandler', email: 'dev@nav.no', NAVident: 'dev-ident' })
+        JSON.stringify({ name: 'S. A. Ksbehandler', email: 'dev@nav.no', NAVident: 'dev-ident', oid: 'uuid' })
     ).toString('base64')}.bogussignature`;
 
 const refreshAccessToken = async (azureClient: Client, session: SpeilSession): Promise<boolean> => {
