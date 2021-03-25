1ddf6b4b5af01a6b4b769c542a16923fed61c864
Marthe Skogen <marthe.skogen@nav.no>
2021-03-25 09:06:54 +0100

    Legger til påskeegg i headeren 🐣

--- src/client/EasterEgg.tsx
+++ src/client/EasterEgg.tsx
@@ -1,67 +1,15 @@
 import React from 'react';
-import Snøfnugg from './components/ikoner/Snøfnugg';
-import SkiingCucumber from './components/ikoner/SkiingCucumber';
 import styled from '@emotion/styled';
+import PåskeEgg from './components/ikoner/PåskeEgg';
 
 const Container = styled.div`
     display: flex;
     margin-left: 1rem;
 `;
 
-const SnøfnuggLav = styled.div`
-    margin-top: 1.5rem;
-    margin-right: 0.5rem;
-`;
-
-const SnøfnuggHøy = styled.div`
-    margin-bottom: 0.8rem;
-    margin-right: 0.5rem;
-    padding-top: 0.5rem;
-`;
-
-const Margin = styled.div`
-    margin-right: 0.5rem;
-    display: flex;
-`;
-
 const EasterEgg = () => (
     <Container>
-        <SnøfnuggHøy>
-            <Snøfnugg />
-        </SnøfnuggHøy>
-        <SnøfnuggLav>
-            <Snøfnugg />
-        </SnøfnuggLav>
-        <SnøfnuggHøy>
-            <Snøfnugg />
-        </SnøfnuggHøy>
-        <SnøfnuggLav>
-            <Snøfnugg />
-        </SnøfnuggLav>
-        <SnøfnuggHøy>
-            <Snøfnugg />
-        </SnøfnuggHøy>
-        <SnøfnuggLav>
-            <Snøfnugg />
-        </SnøfnuggLav>
-        <Margin>
-            <SkiingCucumber />
-        </Margin>
-        <SnøfnuggHøy>
-            <Snøfnugg />
-        </SnøfnuggHøy>
-        <SnøfnuggLav>
-            <Snøfnugg />
-        </SnøfnuggLav>
-        <SnøfnuggHøy>
-            <Snøfnugg />
-        </SnøfnuggHøy>
-        <SnøfnuggLav>
-            <Snøfnugg />
-        </SnøfnuggLav>
-        <SnøfnuggHøy>
-            <Snøfnugg />
-        </SnøfnuggHøy>
+        <PåskeEgg />
     </Container>
 );
 
