6d9a154ae56a2d9d3cf7f099f90f8793e5c2d32f
Stephen Ramthun <stephenramthun@gmail.com>
2021-05-27 10:43:35 +0200

    Flytter komponentdeklarasjoner ut av komponentbody

--- src/client/routes/oversikt/behandlingsstatistikk/Statistikkboks.tsx
+++ src/client/routes/oversikt/behandlingsstatistikk/Statistikkboks.tsx
@@ -8,60 +8,63 @@ import { Flex } from '../../../components/Flex';
 
 import { Statistikklinje } from './Statistikklinje';
 
-interface StatistikkboksProps {
+const StyledEkspanderbartPanel = styled(Ekspanderbartpanel)`
+    margin-bottom: 1.75rem;
+    box-shadow: none !important;
+
+    :hover > button:focus {
+        box-shadow: 0 0 0 3px var(--navds-color-blue-80) !important;
+    }
+
+    button {
+        padding: 0.25rem;
+
+        :focus {
+            border-radius: 4px;
+        }
+    }
+`;
+
+const Tittel = styled(Undertekst)`
+    color: var(--navds-color-gray-80);
+    margin-right: 0.5rem;
+`;
+
+const StyledElement = styled(Element)`
+    flex-grow: 1;
+    color: var(--navds-color-gray-80);
+`;
+
+interface HeadingProps {
     tittel: string;
     upperBound: number;
+}
+
+const Heading = ({ tittel, upperBound }: HeadingProps) => (
+    <Flex alignItems={'center'}>
+        <Tittel>{tittel}</Tittel>
+        <StyledElement>{upperBound}</StyledElement>
+    </Flex>
+);
+
+interface StatistikkboksProps extends HeadingProps {
     elementer: { etikett: ReactNode; antall: number }[];
     visesByDefault?: boolean;
 }
 
-export const Statistikkboks = ({ tittel, upperBound, elementer, visesByDefault = false }: StatistikkboksProps) => {
-    const StyledEkspanderbartPanel = styled(Ekspanderbartpanel)`
-        margin-bottom: 1.75rem;
-        box-shadow: none !important;
-
-        :hover > button:focus {
-            box-shadow: 0 0 0 3px var(--navds-color-blue-80) !important;
-        }
-
-        button {
-            padding: 0.25rem;
-
-            :focus {
-                border-radius: 4px;
-            }
-        }
-    `;
-
-    const Tittel = styled(Undertekst)`
-        color: var(--navds-color-gray-80);
-        margin-right: 0.5rem;
-    `;
-
-    const StyledElement = styled(Element)`
-        flex-grow: 1;
-        color: var(--navds-color-gray-80);
-    `;
-
-    const Heading = () => {
-        return (
-            <Flex alignItems={'center'}>
-                <Tittel>{tittel}</Tittel>
-                <StyledElement>{upperBound}</StyledElement>
-            </Flex>
-        );
-    };
-
-    return (
-        <StyledEkspanderbartPanel tittel={<Heading />} border={false} apen={visesByDefault}>
-            {elementer.map((element, index) => (
-                <Statistikklinje
-                    key={index}
-                    etikett={element.etikett}
-                    upperBound={upperBound}
-                    currentValue={element.antall}
-                />
-            ))}
-        </StyledEkspanderbartPanel>
-    );
-};
+export const Statistikkboks = ({ tittel, upperBound, elementer, visesByDefault = false }: StatistikkboksProps) => (
+    <StyledEkspanderbartPanel
+        tittel={<Heading tittel={tittel} upperBound={upperBound} />}
+        border={false}
+        apen={visesByDefault}
+    >
+        {elementer.map((element, index) => (
+            <Statistikklinje
+                key={index}
+                etikett={element.etikett}
+                upperBound={upperBound}
+                currentValue={element.antall}
+            />
+        ))}
+    </StyledEkspanderbartPanel>
+);
--- src/client/routes/oversikt/behandlingsstatistikk/Statistikklinje.tsx
+++ src/client/routes/oversikt/behandlingsstatistikk/Statistikklinje.tsx
@@ -6,12 +6,6 @@ import { Progresjonsbar } from '@navikt/helse-frontend-progresjonsbar';
 import { Flex } from '../../../components/Flex';
 import { TekstMedEllipsis } from '../../../components/TekstMedEllipsis';
 
-interface StatistikklinjeProps {
-    etikett: ReactNode;
-    upperBound: number;
-    currentValue: number;
-}
-
 const StyledProgresjonsbar = styled(Progresjonsbar)`
     height: 0.5rem;
     flex: 1;
@@ -26,12 +20,16 @@ const EtikettContainer = styled.span`
     margin-right: 20px;
 `;
 
-export const Statistikklinje = ({ etikett, upperBound, currentValue }: StatistikklinjeProps) => {
-    return (
-        <Flex alignItems={'center'} style={{ marginBottom: '.5rem' }}>
-            <Antall>{currentValue}</Antall>
-            <EtikettContainer>{etikett}</EtikettContainer>
-            <StyledProgresjonsbar upperBound={upperBound} currentValue={currentValue} />
-        </Flex>
-    );
-};
+interface StatistikklinjeProps {
+    etikett: ReactNode;
+    upperBound: number;
+    currentValue: number;
+}
+
+export const Statistikklinje = ({ etikett, upperBound, currentValue }: StatistikklinjeProps) => (
+    <Flex alignItems={'center'} style={{ marginBottom: '.5rem' }}>
+        <Antall>{currentValue}</Antall>
+        <EtikettContainer>{etikett}</EtikettContainer>
+        <StyledProgresjonsbar upperBound={upperBound} currentValue={currentValue} />
+    </Flex>
+);
