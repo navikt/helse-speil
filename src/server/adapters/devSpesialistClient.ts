1a20918a4cb66bfa368bca41edfa21b5f9c97db2
Truls Jørgensen <trulsjor@gmail.com>
2021-06-16 13:37:42 +0200

    Testdata for revurderering av revurdering
    Co-authored-by: Christian Bulow Skovborg <christian.bulow.skovborg@nav.no>
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Knut Nygaard <knut.nygaard@nav.no>

--- src/server/adapters/devSpesialistClient.ts
+++ src/server/adapters/devSpesialistClient.ts
@@ -127,6 +127,9 @@ const filenameForPersonId = (id: string) => {
         case '19026500128':
         case '2750243667998':
             return 'stinky_stank_førstegangsbehandling.json';
+        case '2750243667999':
+        case '19026500129':
+            return 'teknobasse_revurderer_revurderinger.json';
         default:
             throw new Error('Mangler oppsett i devSpesialistClient.ts');
     }
