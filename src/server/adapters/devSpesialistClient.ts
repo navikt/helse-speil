d0be9816f929aea8222dd41f3d343310c705a29e
Truls Jørgensen <trulsjor@gmail.com>
2021-06-21 10:35:45 +0200

    La til avventer simulering revurdering test case
    Co-authored-by: Knut Nygaard <knut.nygaard@nav.no>

--- src/server/adapters/devSpesialistClient.ts
+++ src/server/adapters/devSpesialistClient.ts
@@ -130,6 +130,9 @@ const filenameForPersonId = (id: string) => {
         case '2750243667999':
         case '19026500129':
             return 'teknobasse_revurderer_revurderinger.json';
+        case '05068821404':
+        case '2446602102798':
+            return 'vakker_ert_avventer_simulering_revurdering.json';
         default:
             throw new Error('Mangler oppsett i devSpesialistClient.ts');
     }
