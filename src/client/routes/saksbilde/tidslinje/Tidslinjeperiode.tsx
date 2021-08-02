62582efd392f9d3cc30a95670376158625ba023f
Stephen Ramthun <stephenramthun@gmail.com>
2021-08-02 15:22:20 +0200

    Fikser overflow av periodeikoner

--- src/client/routes/saksbilde/tidslinje/Tidslinjeperiode.tsx
+++ src/client/routes/saksbilde/tidslinje/Tidslinjeperiode.tsx
@@ -1,120 +1,16 @@
 import styled from '@emotion/styled';
 import classNames from 'classnames';
 import { Infotrygdperiodetilstand, Tidslinjetilstand } from 'internal-types';
-import React, { ReactNode, RefObject, useLayoutEffect, useRef, useState } from 'react';
+import React, { ReactNode, useRef, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import Popover from 'nav-frontend-popover';
 
-import { Period } from '@navikt/helse-frontend-timeline/lib';
 import { PeriodProps } from '@navikt/helse-frontend-timeline/lib/components/Period';
 
-import { TidslinjeperiodeIkon } from '../../../components/ikoner/Tidslinjeperiodeikoner';
 import { prideifisertState } from '../../../components/ikoner/VimpelMedPalmeIkon';
 
-interface StyledPeriodProps {
-    erAktiv?: boolean;
-    ref?: RefObject<HTMLButtonElement>;
-}
-
-export const StyledPeriod = styled(Period)<StyledPeriodProps>`
-    display: flex;
-    align-items: center;
-
-    ${({ erAktiv }) =>
-        erAktiv &&
-        `
-        box-shadow: 0 0 0 2px var(--navds-text-focus);
-        border-color: var(--navds-text-focus);
-        z-index: 20;
-    `}
-    &.foreldet {
-        &.tilUtbetaling,
-        &.tilUtbetalingAutomatisk,
-        &.utbetalt,
-        &.revurdert,
-        &.utbetaltAutomatisk {
-            --period-background-color: var(--nav-lime-gronn-lighten-80);
-            --period-hover-color: #dade99;
-            --period-border-color: var(--nav-lime-gronn-darken-40);
-        }
-
-        &.avslag,
-        &.feilet,
-        &.annullert,
-        &.tilAnnullering,
-        &.annulleringFeilet {
-            --period-background-color: var(--navds-color-red-10);
-            --period-hover-color: var(--navds-color-tag-error-background);
-            --period-border-color: var(--navds-color-tag-error-border);
-        }
-    }
-
-    &.gjeldende {
-        &.revurderes,
-        &.oppgaver {
-            --period-background-color: var(--navds-color-orange-10);
-            --period-hover-color: var(--navds-color-tag-warning-background);
-            --period-border-color: var(--navds-color-tag-warning-border);
-        }
-
-        &.tilUtbetaling,
-        &.tilUtbetalingAutomatisk,
-        &.utbetalt,
-        &.revurdert,
-        &.utbetaltAutomatisk {
-            &.pride {
-                --period-background-color: var(--nav-regnbue-bakgrunn-skew);
-                --period-hover-color: var(--nav-regnbue-bakgrunn-skew);
-                --period-border-color: var(--navds-color-tag-success-border);
-            }
-
-            &:not(.pride) {
-                --period-background-color: var(--navds-color-green-10);
-                --period-hover-color: var(--navds-color-tag-success-background);
-                --period-border-color: var(--navds-color-tag-success-border);
-            }
-        }
-
-        &.avslag,
-        &.feilet,
-        &.annullert,
-        &.tilAnnullering,
-        &.annulleringFeilet {
-            --period-background-color: var(--navds-color-red-10);
-            --period-hover-color: var(--navds-color-tag-error-background);
-            --period-border-color: var(--navds-color-tag-error-border);
-        }
-
-        &.utbetaltIInfotrygd,
-        &.infotrygdferie,
-        &.infotrygdukjent {
-            --period-background-color: var(--nav-infotrygd-bakgrunn-stiplet);
-            --period-hover-color: var(--nav-infotrygd-bakgrunn-stiplet);
-        }
-    }
-`;
-
-const PeriodePin = styled.div`
-    position: absolute;
-    background: #0067c5;
-    height: 6px;
-    width: 2px;
-    top: 0;
-    left: 50%;
-    transform: translate(-1px, -7px);
-
-    &:before {
-        content: '';
-        position: absolute;
-        top: 0;
-        width: 10px;
-        height: 10px;
-        background: #0067c5;
-        transform: translate(-5px, -100%);
-        border-radius: 50%;
-    }
-`;
+import { Periodeknapp, PeriodePin } from './Tidslinjeperiode.styles';
 
 interface TidslinjeperiodeProps extends PeriodProps {
     id: string;
@@ -143,47 +39,26 @@ export const Tidslinjeperiode = ({
     ...props
 }: TidslinjeperiodeProps) => {
     const ref = useRef<HTMLButtonElement>(null);
-    const [erMini, setErMini] = useState(false);
-
     const [anchor, setAnchor] = useState<HTMLButtonElement | undefined>(undefined);
-
-    useLayoutEffect(() => {
-        if (ref.current && ref.current.offsetWidth <= 30.0) {
-            setErMini(true);
-        }
-    }, [ref.current]);
-
     const erPrideifisert = useRecoilValue(prideifisertState);
 
     return (
-        <>
-            <div
-                onMouseOver={(_) => setAnchor(ref.current!)}
-                onMouseOut={() => setAnchor(undefined)}
-                data-testid={`tidslinjeperiode-${props.id}`}
+        <div
+            onMouseOver={(_) => setAnchor(ref.current!)}
+            onMouseOut={() => setAnchor(undefined)}
+            data-testid={`tidslinjeperiode-${props.id}`}
+        >
+            <Periodeknapp
+                erAktiv={erAktiv}
+                ref={ref}
+                className={classNames(tilstand, erForeldet ? 'foreldet' : 'gjeldende', erPrideifisert ? 'pride' : '')}
+                {...props}
             >
-                <StyledPeriod
-                    erAktiv={erAktiv}
-                    ref={ref}
-                    className={classNames(
-                        tilstand,
-                        erForeldet ? 'foreldet' : 'gjeldende',
-                        erPrideifisert ? 'pride' : ''
-                    )}
-                    {...props}
-                >
-                    {!erMini && (
-                        <TidslinjeperiodeIkon
-                            tilstand={tilstand}
-                            styles={{ marginLeft: '0.5rem', pointerEvents: 'none' }}
-                        />
-                    )}
-                    {skalVisePin && <PeriodePin />}
-                </StyledPeriod>
-            </div>
+                {skalVisePin && <PeriodePin />}
+            </Periodeknapp>
             <Tooltip autoFokus={false} ankerEl={anchor}>
                 {hoverLabel}
             </Tooltip>
-        </>
+        </div>
     );
 };
