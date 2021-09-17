235b29e063747718539680718fe8ea905ccc9b50
Stephen Ramthun <stephenramthun@gmail.com>
2021-09-17 13:19:41 +0200

    Visuell state for checkbox hvor bare noen av dagene er markert

--- src/client/routes/saksbilde/utbetaling/utbetalingstabell/MarkerAlleDagerCheckbox.tsx
+++ src/client/routes/saksbilde/utbetaling/utbetalingstabell/MarkerAlleDagerCheckbox.tsx
@@ -1,8 +1,9 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Dagtype } from 'internal-types';
 import React, { Dispatch, SetStateAction, useMemo } from 'react';
 
-import { Checkbox } from '@navikt/ds-react';
+import { Checkbox as NavCheckbox } from '@navikt/ds-react';
 
 import { overstyrPermisjonsdagerEnabled } from '../../../../featureToggles';
 import { UtbetalingstabellDag } from './Utbetalingstabell.types';
@@ -26,6 +27,22 @@ const Container = styled.div`
             max-width: 2rem;
         }
     }
+`;
+
+const Checkbox = styled(NavCheckbox)<{ partial: boolean }>`
+    ${({ partial }) =>
+        partial &&
+        css`
+            > label:before {
+                content: '-';
+                font-size: 1.5rem;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-color: #c4c4c4;
+                border: none;
+            }
+        `}
 `;
 
 interface MarkerAlleDagerCheckboxProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size' | 'value'> {
@@ -61,9 +78,17 @@ export const MarkerAlleDagerCheckbox: React.FC<MarkerAlleDagerCheckboxProps> = (
         }
     };
 
+    const hasSelectedSome = markerteDager.size > 0 && markerteDager.size !== overstyrbareDager.size;
+
     return (
         <Container>
-            <Checkbox onChange={onChange} checked={overstyrbareDager.size === markerteDager.size} {...rest} hideLabel>
+            <Checkbox
+                partial={hasSelectedSome}
+                onChange={onChange}
+                checked={overstyrbareDager.size === markerteDager.size}
+                {...rest}
+                hideLabel
+            >
                 Marker alle dager
             </Checkbox>
         </Container>
