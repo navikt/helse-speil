86270483291b7ec7903ec8b10ca9d8bee7b39e59
Stephen Ramthun <stephenramthun@gmail.com>
2021-09-24 11:48:17 +0200

    Tester at vi viser riktig tekst i overstyringsindikatoren for grad

--- src/client/routes/saksbilde/utbetaling/utbetalingstabell/GradCell.test.tsx
+++ src/client/routes/saksbilde/utbetaling/utbetalingstabell/GradCell.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import dayjs from 'dayjs';
 import React from 'react';
 
@@ -7,15 +8,40 @@ import { GradCell } from './GradCell';
 import { UtbetalingstabellDag } from './Utbetalingstabell.types';
 
 describe('GradCell', () => {
-    const overstyrtTilNullProsentSyk: UtbetalingstabellDag = {
-        type: 'Syk',
-        gradering: 0,
-        dato: dayjs(),
-        isMaksdato: true,
-        sykdomsdag: { type: 'Syk', kilde: 'Sykmelding' },
-    };
-    it('skal tegne en overstyringstrekant med tekst når grad går fra 100 % til 0 %', function () {
+    it('skal tegne en overstyringstrekant med tekst når grad går fra 100 % til 0 %', () => {
+        const overstyrtTilNullProsentSyk: UtbetalingstabellDag = {
+            type: 'Syk',
+            gradering: 0,
+            dato: dayjs(),
+            isMaksdato: true,
+            sykdomsdag: { type: 'Syk', kilde: 'Sykmelding' },
+        };
         render(<GradCell type="Syk" grad={100} overstyrtDag={overstyrtTilNullProsentSyk} />);
-        expect(screen.getByTestId('overstyringsindikator')).toBeVisible();
+        const indikator = screen.getByTestId('overstyringsindikator');
+        expect(indikator).toBeVisible();
+
+        userEvent.hover(indikator);
+        expect(screen.getByText('Endret fra 100 %')).toBeVisible();
+    });
+
+    it('skal tegne en overstyringstrekant med tekst når grad går fra null til 100 %', () => {
+        const overstyrtTilHundreProsentSyk: UtbetalingstabellDag = {
+            type: 'Syk',
+            gradering: 100,
+            dato: dayjs(),
+            isMaksdato: true,
+            sykdomsdag: { type: 'Syk', kilde: 'Sykmelding' },
+        };
+        render(<GradCell type="Egenmelding" overstyrtDag={overstyrtTilHundreProsentSyk} />);
+        const indikator = screen.getByTestId('overstyringsindikator');
+        expect(indikator).toBeVisible();
+
+        userEvent.hover(indikator);
+        expect(screen.getByText('Endret fra dag uten grad')).toBeVisible();
+    });
+
+    it('rendrer ikke overstyringsindikator når vi ikke overstyrer', () => {
+        render(<GradCell type="Egenmelding" grad={100} />);
+        expect(screen.queryByTestId('overstyringsindikator')).toBeNull();
     });
 });
