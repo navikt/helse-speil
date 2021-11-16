6fcd89d2369bc28515ee73b8563a555af1598eb0
Simen Ullern <simen.ullern@nav.no>
2021-11-16 09:42:19 +0100

    Slett varsel om sak allerede tildelt før man legger til nytt

--- src/client/hooks/useVarselOmSakErTildeltAnnenSaksbehandler.ts
+++ src/client/hooks/useVarselOmSakErTildeltAnnenSaksbehandler.ts
@@ -2,21 +2,24 @@ import { useEffect } from 'react';
 
 import { Varseltype } from '@navikt/helse-frontend-varsel';
 
-import { Scopes, useAddVarsel } from '../state/varsler';
+import { Scopes, useAddVarsel, useRemoveVarsel } from '../state/varsler';
 import { capitalizeName } from '../utils/locale';
+
+const erTildeltAnnenSaksbehandler = (saksbehandlerOid: string, personTilBehandling: Person): boolean => {
+    if (!personTilBehandling.tildeling) return false;
+    return personTilBehandling.tildeling && personTilBehandling.tildeling.saksbehandler.oid !== saksbehandlerOid;
+};
 
 export const useVarselOmSakErTildeltAnnenSaksbehandler = (saksbehandlerOid: string, personTilBehandling: Person) => {
     const addVarsel = useAddVarsel();
-
-    const erTildeltAnnenSaksbehandler = (saksbehandlerOid: string, personTilBehandling: Person): boolean => {
-        if (!personTilBehandling.tildeling) return false;
-        return personTilBehandling.tildeling && personTilBehandling.tildeling.saksbehandler.oid !== saksbehandlerOid;
-    };
+    const removeVarsel = useRemoveVarsel();
+    const key = 'sak-allerede-tildelt';
 
     useEffect(() => {
+        removeVarsel(key);
         if (erTildeltAnnenSaksbehandler(saksbehandlerOid, personTilBehandling)) {
             addVarsel({
-                key: saksbehandlerOid + personTilBehandling.aktørId,
+                key: key,
                 type: Varseltype.Info,
                 message: `Saken er allerede tildelt til ${capitalizeName(
                     personTilBehandling.tildeling?.saksbehandler.navn ?? ''
