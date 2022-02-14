59399fcf18eee777c8e35af4a7f79bf8c685d780
Stephen Ramthun <stephenramthun@gmail.com>
2022-02-14 15:31:54 +0100

    Oppdater mock-data

--- packages/backend/spesialistMock/graphql.ts
+++ packages/backend/spesialistMock/graphql.ts
@@ -9,7 +9,7 @@ import { IResolvers } from '@graphql-tools/utils';
 
 import spesialistSchema from '../graphql.schema.json';
 
-const fetchData = (): Record<string, JSON> => {
+const fetchPersondata = (): Record<string, JSON> => {
     const url = path.join(__dirname, '/data');
     const filenames = fs.readdirSync(url);
     const files = filenames.map((filename) => {
@@ -21,16 +21,119 @@ const fetchData = (): Record<string, JSON> => {
         return {
             ...data,
             [person.aktorId]: person,
+            [person.fodselsnummer]: person,
         };
     }, {});
 };
 
-const data = fetchData();
+const persondata = fetchPersondata();
+
+const oppdrag = [
+    {
+        type: 'UTBETALING',
+        status: 'UTBETALT',
+        arbeidsgiveroppdrag: {
+            fagsystemId: 'JEX2GC6ZBRHVFG3DUXIK4CM3P4',
+            linjer: [],
+            organisasjonsnummer: '839942907',
+        },
+        personoppdrag: {
+            fodselsnummer: '20087106951',
+            fagsystemId: 'TMWJ7WRR7FFXJLDW3HYJCPLF5Y',
+            linjer: [
+                {
+                    fom: '2021-12-17',
+                    tom: '2021-12-31',
+                    totalbelop: 15235,
+                },
+            ],
+        },
+        annullering: null,
+        totalbelop: 15235,
+    },
+    {
+        type: 'REVURDERING',
+        status: 'UTBETALT',
+        arbeidsgiveroppdrag: {
+            fagsystemId: 'JEX2GC6ZBRHVFG3DUXIK4CM3P4',
+            linjer: [],
+            organisasjonsnummer: '839942907',
+        },
+        personoppdrag: {
+            fodselsnummer: '20087106951',
+            fagsystemId: 'TMWJ7WRR7FFXJLDW3HYJCPLF5Y',
+            linjer: [
+                {
+                    fom: '2021-12-17',
+                    tom: '2021-12-27',
+                    totalbelop: 9695,
+                },
+                {
+                    fom: '2021-12-28',
+                    tom: '2021-12-30',
+                    totalbelop: 3324,
+                },
+                {
+                    fom: '2021-12-31',
+                    tom: '2021-12-31',
+                    totalbelop: 1385,
+                },
+            ],
+        },
+        annullering: null,
+        totalbelop: 14404,
+    },
+    {
+        type: 'UTBETALING',
+        status: 'UTBETALT',
+        arbeidsgiveroppdrag: {
+            fagsystemId: 'JEX2GC6ZBRHVFG3DUXIK4CM3P4',
+            linjer: [],
+            organisasjonsnummer: '839942907',
+        },
+        personoppdrag: {
+            fodselsnummer: '20087106951',
+            fagsystemId: 'TMWJ7WRR7FFXJLDW3HYJCPLF5Y',
+            linjer: [
+                {
+                    fom: '2021-12-17',
+                    tom: '2021-12-27',
+                    totalbelop: 9695,
+                },
+                {
+                    fom: '2021-12-28',
+                    tom: '2021-12-30',
+                    totalbelop: 3324,
+                },
+                {
+                    fom: '2021-12-31',
+                    tom: '2022-01-05',
+                    totalbelop: 5540,
+                },
+                {
+                    fom: '2022-01-10',
+                    tom: '2022-01-10',
+                    totalbelop: 1108,
+                },
+                {
+                    fom: '2022-01-11',
+                    tom: '2022-01-14',
+                    totalbelop: 5540,
+                },
+            ],
+        },
+        annullering: null,
+        totalbelop: 25207,
+    },
+];
 
 const getResolvers = (): IResolvers => ({
     Query: {
-        person: (_, { fnr }: { fnr: string }) => {
-            return data[fnr];
+        person: (_, { fnr, aktorId }: { fnr?: string; aktorId?: string }) => {
+            return persondata[fnr ?? aktorId ?? ''];
+        },
+        oppdrag: (_, { fnr }: { fnr: string }) => {
+            return oppdrag;
         },
     },
     Periode: {
