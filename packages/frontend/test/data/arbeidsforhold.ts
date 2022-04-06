d7eff75c506c22f5142e3dd6a60a953ae193ec99
Stephen Ramthun <stephenramthun@gmail.com>
2022-04-06 12:51:03 +0200

    Fjern gammel testdata

--- packages/frontend/test/data/arbeidsforhold.ts
+++
@@ -1,7 +0,0 @@
-export const umappetArbeidsforhold: ExternalArbeidsforhold = {
-    organisasjonsnummer: '987654321',
-    stillingstittel: 'UNNASLUNTRER',
-    stillingsprosent: 100,
-    startdato: '1991-01-17',
-    sluttdato: undefined,
-};
