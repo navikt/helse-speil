def6005cc58a9b63631435095436c7f7d3d7bfc8
Simen Ullern <simenullern@gmail.com>
2022-04-08 09:55:20 +0200

    Revert "Fjerner påskeegg"
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>
    
    This reverts commit 74bc508916c4aec7ff9d7e86c0af29c4b1285a45.

--- packages/frontend/EasterEgg.tsx
+++ packages/frontend/EasterEgg.tsx
@@ -1,9 +1,14 @@
 import styled from '@emotion/styled';
 import React from 'react';
+import PåskeEgg from './routes/saksbilde/ikoner/PåskeEgg';
 
 const Container = styled.div`
     display: flex;
     flex-grow: 1;
 `;
 
-export const EasterEgg = () => <Container></Container>;
+export const EasterEgg = () => (
+    <Container>
+        <PåskeEgg />
+    </Container>
+);
