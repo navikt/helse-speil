84ba8a910c72de013062cfbc974797d61587dd7a
Øyvind Hagen <oyvind.hagen@nav.no>
2022-04-28 15:38:05 +0200

    Lagrer filtreringsvalg i oppgavelisten på sessionStorage
    Formålet er at saksbehandler skal beholde filtreringsvalgene gjennom arbeidsdagen.
    
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- packages/frontend/routes/oversikt/table/state/filter.ts
+++ packages/frontend/routes/oversikt/table/state/filter.ts
@@ -4,6 +4,7 @@ import { tabState, TabType } from '../../tabs';
 import { utbetalingTilSykmeldt } from '@utils/featureToggles';
 
 export type Filter<T> = {
+    key: string;
     label: string;
     function: (value: T) => boolean;
     active: boolean;
@@ -16,24 +17,28 @@ type ActiveFiltersPerTab = {
 
 const defaultFilters: Filter<Oppgave>[] = [
     {
+        key: 'UFORDELTE_SAKER',
         label: 'Ufordelte saker',
         active: false,
         function: (oppgave: Oppgave) => !oppgave.tildeling,
         column: 0,
     },
     {
+        key: 'TILDELTE_SAKER',
         label: 'Tildelte saker',
         active: false,
         function: (oppgave: Oppgave) => !!oppgave.tildeling,
         column: 0,
     },
     {
+        key: 'FØRSTEGANG',
         label: 'Førstegang.',
         active: false,
         function: (oppgave: Oppgave) => oppgave.periodetype === 'førstegangsbehandling',
         column: 1,
     },
     {
+        key: 'FORLENGELSE',
         label: 'Forlengelse.',
         active: false,
         function: (oppgave: Oppgave) =>
@@ -41,54 +46,63 @@ const defaultFilters: Filter<Oppgave>[] = [
         column: 1,
     },
     {
+        key: 'FORLENGELSE_IT',
         label: 'Forlengelse - IT',
         active: false,
         function: (oppgave: Oppgave) => oppgave.periodetype === 'overgangFraIt',
         column: 1,
     },
     {
+        key: 'STIKKPRØVER',
         label: 'Stikkprøver',
         active: false,
         function: (oppgave: Oppgave) => oppgave.periodetype === 'stikkprøve',
         column: 1,
     },
     {
+        key: 'RISK_QA',
         label: 'Risk QA',
         active: false,
         function: (oppgave: Oppgave) => oppgave.periodetype === 'riskQa',
         column: 1,
     },
     {
+        key: 'REVURDERING',
         label: 'Revurdering',
         active: false,
         function: (oppgave: Oppgave) => oppgave.periodetype === 'revurdering',
         column: 1,
     },
     {
+        key: 'EN_ARBEIDSGIVER',
         label: 'Én arbeidsgiver',
         active: false,
         function: (oppgave: Oppgave) => oppgave.inntektskilde === 'EN_ARBEIDSGIVER',
         column: 3,
     },
     {
+        key: 'FLERE_ARBEIDSGIVERE',
         label: 'Flere arbeidsgivere',
         active: false,
         function: (oppgave: Oppgave) => oppgave.inntektskilde === 'FLERE_ARBEIDSGIVERE',
         column: 3,
     },
     {
+        key: 'FORTROLIG_ADR',
         label: 'Fortrolig adr.',
         active: false,
         function: (oppgave: Oppgave) => oppgave.periodetype === 'fortroligAdresse',
         column: 1,
     },
     {
+        key: 'UTB_SYKMELDT',
         label: 'Utb. sykmeldt',
         active: false,
         function: (oppgave: Oppgave) => oppgave.periodetype === 'utbetalingTilSykmeldt',
         column: 1,
     },
     {
+        key: 'DELVIS_REFUSJON',
         label: 'Delvis refusjon',
         active: false,
         function: (oppgave: Oppgave) => oppgave.periodetype === 'delvisRefusjon',
@@ -96,15 +110,31 @@ const defaultFilters: Filter<Oppgave>[] = [
     },
 ].filter((item) => utbetalingTilSykmeldt || (item.label != 'Utb. sykmeldt' && item.label != 'Delvis refusjon'));
 
+const storageKeyForFilters = (tab: TabType) => 'filtereForTab_' + tab;
+
+const hentValgteFiltre = (tab: TabType, defaultFilters: Filter<Oppgave>[]) => {
+    const filters = sessionStorage.getItem(storageKeyForFilters(tab));
+    if (filters == null) return defaultFilters;
+
+    const aktiveFiltre = JSON.parse(filters);
+
+    return defaultFilters.map((f) => {
+        return aktiveFiltre.includes(f.key) ? { ...f, active: true } : f;
+    });
+};
+
 const makeFilterActive = (targetFilterLabel: string) => (it: Filter<Oppgave>) =>
     it.label === targetFilterLabel ? { ...it, active: true } : it;
 
-const activeFiltersPerTab = atom<ActiveFiltersPerTab>({
+const allFilters = atom<ActiveFiltersPerTab>({
     key: 'activeFiltersPerTab',
     default: {
-        [TabType.TilGodkjenning]: defaultFilters.map(makeFilterActive('Ufordelte saker')),
-        [TabType.Mine]: defaultFilters,
-        [TabType.Ventende]: defaultFilters,
+        [TabType.TilGodkjenning]: hentValgteFiltre(
+            TabType.TilGodkjenning,
+            defaultFilters.map(makeFilterActive('Ufordelte saker'))
+        ),
+        [TabType.Mine]: hentValgteFiltre(TabType.Mine, defaultFilters),
+        [TabType.Ventende]: hentValgteFiltre(TabType.Ventende, defaultFilters),
     },
 });
 
@@ -112,11 +142,17 @@ const filtersState = selector<Filter<Oppgave>[]>({
     key: 'filtersState',
     get: ({ get }) => {
         const tab = get(tabState);
-        return get(activeFiltersPerTab)[tab];
+        return get(allFilters)[tab];
     },
     set: ({ get, set }, newValue) => {
         const tab = get(tabState);
-        set(activeFiltersPerTab, (filters) => ({ ...filters, [tab]: newValue }));
+
+        if (Array.isArray(newValue)) {
+            const aktiveFiltre = newValue.filter((f) => f.active).map((f) => f.key);
+            sessionStorage.setItem(storageKeyForFilters(tab), JSON.stringify(aktiveFiltre));
+        }
+
+        set(allFilters, (filters) => ({ ...filters, [tab]: newValue }));
     },
 });
 
