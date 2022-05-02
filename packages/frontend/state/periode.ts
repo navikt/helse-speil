11e5635163f095fd68f6dd85bf0e5097ebd9d0b5
Stephen Ramthun <stephenramthun@gmail.com>
2022-05-02 11:01:41 +0200

    Fiks bug hvor default aktiv periode ikke er satt
    Ser at det noen ganger i prod ikke er satt default aktiv periode.
    Klarer ikke gjenskape lokalt, men mistenker at det kan ha sammenheng
    med at recoil holder på activePeriod fra en tidligere person og derfor
    ikke oppdaterer med ny periode når man går inn på en ny person. Prøver
    å tvinge recoil til å oppdatere tilstanden ved å sjekke om den aktive
    perioden faktisk finnes på den aktive personen.

--- packages/frontend/state/periode.ts
+++ packages/frontend/state/periode.ts
@@ -1,37 +1,51 @@
 import { atom, selector, useRecoilValue, useSetRecoilState } from 'recoil';
 
-import type { BeregnetPeriode, GhostPeriode, UberegnetPeriode } from '@io/graphql';
+import type { BeregnetPeriode, GhostPeriode, Person, UberegnetPeriode } from '@io/graphql';
 import { currentPersonState } from '@state/person';
 import { isBeregnetPeriode } from '@utils/typeguards';
 
-const activePeriodState = atom<BeregnetPeriode | UberegnetPeriode | GhostPeriode | null>({
+type ActivePeriod = BeregnetPeriode | UberegnetPeriode | GhostPeriode;
+
+const personHasPeriod = (person: Person, period: ActivePeriod): boolean => {
+    return (
+        person.arbeidsgivere
+            .flatMap((it) => it.generasjoner.flatMap((it) => it.perioder as Array<ActivePeriod>))
+            .find((it) => it.id === period.id) !== undefined ||
+        person.arbeidsgivere.flatMap((it) => it.ghostPerioder).find((it) => it.id === period.id) !== undefined
+    );
+};
+
+const activePeriodState = atom<ActivePeriod | null>({
     key: 'activePeriodState',
     default: null,
 });
 
-const activePeriod = selector<BeregnetPeriode | UberegnetPeriode | GhostPeriode | null>({
+const activePeriod = selector<ActivePeriod | null>({
     key: 'activePeriod',
     get: async ({ get }) => {
-        const activePeriod = get(activePeriodState);
-        if (activePeriod) {
-            return activePeriod;
-        }
-
         const person = await get(currentPersonState);
         if (!person) {
             return null;
         }
 
-        const firstAvailablePeriod = person.arbeidsgivere
-            .flatMap((arbeidsgiver) => arbeidsgiver.generasjoner[0]?.perioder ?? [])
-            .find(
-                (periode) =>
-                    isBeregnetPeriode(periode) &&
-                    periode.behandlingstype === 'BEHANDLET' &&
-                    typeof periode.oppgavereferanse === 'string',
-            );
+        const activePeriod = get(activePeriodState);
+        if (activePeriod && personHasPeriod(person, activePeriod)) {
+            return activePeriod;
+        }
 
-        const periode = firstAvailablePeriod ?? person.arbeidsgivere[0]?.generasjoner[0]?.perioder[0];
+        const allPeriods = person.arbeidsgivere.flatMap((arbeidsgiver) => arbeidsgiver.generasjoner[0]?.perioder ?? []);
+
+        const periodWithOppgave: Maybe<BeregnetPeriode> =
+            allPeriods
+                .filter(isBeregnetPeriode)
+                .find(
+                    (periode) =>
+                        isBeregnetPeriode(periode) &&
+                        periode.behandlingstype === 'BEHANDLET' &&
+                        typeof periode.oppgavereferanse === 'string',
+                ) ?? null;
+
+        const periode = periodWithOppgave ?? person.arbeidsgivere[0]?.generasjoner[0]?.perioder[0];
 
         return isBeregnetPeriode(periode) ? periode : null;
     },
