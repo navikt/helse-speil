698332b48866ab7955ccd71cb6a484eaf0d1c594
Stephen Ramthun <stephenramthun@gmail.com>
2022-05-03 09:49:18 +0200

    Fiks bug hvor tidslinjeperioder legges feil ved søk på ny person
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>

--- packages/frontend/routes/saksbilde/timeline/useTimelineWindow.ts
+++ packages/frontend/routes/saksbilde/timeline/useTimelineWindow.ts
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useLayoutEffect, useMemo, useState } from 'react';
 import dayjs from 'dayjs';
 
 import type { Arbeidsgiver, Infotrygdutbetaling } from '@io/graphql';
@@ -8,11 +8,30 @@ type Periode = {
     tom: DateString;
 };
 
-const getLatestDate = (perioder: Array<Periode>): Dayjs =>
-    perioder.reduce((latest: Dayjs, periode: Periode) => {
+const getLatestDate = (perioder: Array<Periode>): Dayjs => {
+    return perioder.reduce((latest: Dayjs, periode: Periode) => {
         const dato = dayjs(periode.tom);
         return dato.isAfter(latest) ? dato : latest;
     }, dayjs(0));
+};
+
+const getAvailableWindows = (latestDate: Dayjs): Array<TimelineWindow> => [
+    {
+        fom: latestDate.subtract(6, 'month'),
+        tom: latestDate,
+        label: '6 mnd',
+    },
+    {
+        fom: latestDate.subtract(1, 'year'),
+        tom: latestDate,
+        label: '1 år',
+    },
+    {
+        fom: latestDate.subtract(4, 'year'),
+        tom: latestDate,
+        label: '4 år',
+    },
+];
 
 type UseTimelineWindowResult = {
     availableWindows: Array<TimelineWindow>;
@@ -22,38 +41,25 @@ type UseTimelineWindowResult = {
 
 export const useTimelineWindow = (
     arbeidsgivere: Array<Arbeidsgiver>,
-    infotrygdutbetalinger: Array<Infotrygdutbetaling>
+    infotrygdutbetalinger: Array<Infotrygdutbetaling>,
 ): UseTimelineWindowResult => {
     const latestDate = useMemo(() => {
         const perioder = [
-            ...arbeidsgivere.flatMap((arbeidsgiver) => arbeidsgiver.generasjoner[0]?.perioder ?? []),
+            ...arbeidsgivere.flatMap((it) => it.generasjoner[0]?.perioder ?? []),
+            ...arbeidsgivere.flatMap((it) => it.ghostPerioder),
             ...infotrygdutbetalinger,
         ];
         return getLatestDate(perioder);
-    }, []);
+    }, [arbeidsgivere, infotrygdutbetalinger]);
 
-    const availableWindows: Array<TimelineWindow> = [
-        {
-            fom: latestDate.subtract(6, 'month'),
-            tom: latestDate,
-            label: '6 mnd',
-        },
-        {
-            fom: latestDate.subtract(1, 'year'),
-            tom: latestDate,
-            label: '1 år',
-        },
-        {
-            fom: latestDate.subtract(4, 'year'),
-            tom: latestDate,
-            label: '4 år',
-        },
-    ];
+    const [activeWindow, setActiveWindow] = useState<TimelineWindow>(getAvailableWindows(latestDate)[0]);
 
-    const [activeWindow, setActiveWindow] = useState<TimelineWindow>(availableWindows[0]);
+    useLayoutEffect(() => {
+        setActiveWindow(getAvailableWindows(latestDate)[0]);
+    }, [latestDate]);
 
     return {
-        availableWindows: availableWindows,
+        availableWindows: getAvailableWindows(latestDate),
         activeWindow,
         setActiveWindow,
     };
