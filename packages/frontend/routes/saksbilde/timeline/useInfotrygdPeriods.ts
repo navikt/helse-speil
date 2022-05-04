cb5dff7b2cb948c8df9b8e7c233e3c65bc022476
Stephen Ramthun <stephenramthun@gmail.com>
2022-05-04 10:34:16 +0200

    Vis ikke tilbakeføringer fra Infotrygd i tidslinjen

--- packages/frontend/routes/saksbilde/timeline/useInfotrygdPeriods.ts
+++ packages/frontend/routes/saksbilde/timeline/useInfotrygdPeriods.ts
@@ -2,12 +2,14 @@ import { useMemo } from 'react';
 import { Infotrygdutbetaling } from '@io/graphql';
 
 export const useInfotrygdPeriods = (
-    infotrygdutbetalinger: Array<Infotrygdutbetaling>
+    infotrygdutbetalinger: Array<Infotrygdutbetaling>,
 ): Map<string, Array<InfotrygdPeriod>> =>
     useMemo(() => {
         const map = new Map<string, Array<InfotrygdPeriod>>();
 
-        for (const utbetaling of infotrygdutbetalinger) {
+        const utbetalinger = infotrygdutbetalinger.filter((it) => it.typetekst !== 'Tilbakeført');
+
+        for (const utbetaling of utbetalinger) {
             if (!map.has(utbetaling.organisasjonsnummer)) {
                 map.set(utbetaling.organisasjonsnummer, []);
             }
