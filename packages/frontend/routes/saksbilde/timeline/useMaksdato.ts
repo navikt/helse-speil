201e14f27bc91c426e596f1e937e9ac5a0b3fbb5
Stephen Ramthun <stephenramthun@gmail.com>
2022-05-06 14:00:45 +0200

    Filtrér vekk ugyldige maksdatoer før visning i tidslinja

--- packages/frontend/routes/saksbilde/timeline/useMaksdato.ts
+++ packages/frontend/routes/saksbilde/timeline/useMaksdato.ts
@@ -1,9 +1,21 @@
-import { Arbeidsgiver } from '@io/graphql';
+import { Arbeidsgiver, Periode } from '@io/graphql';
 import { isBeregnetPeriode } from '@utils/typeguards';
+
+const isValidDate = (date?: Maybe<DateString>): boolean => {
+    return typeof date === 'string' && !isNaN(new Date(date).getTime());
+};
+
+const hasValidMaksdato = (period: BeregnetPeriode): boolean => {
+    return isValidDate(period.maksdato);
+};
+
+const byFomDescending = (a: Periode, b: Periode): number => {
+    return new Date(b.fom).getTime() - new Date(a.fom).getTime();
+};
 
 export const useMaksdato = (arbeidsgivere: Array<Arbeidsgiver>): DateString | undefined =>
     arbeidsgivere
         .flatMap((it) => it.generasjoner[0]?.perioder)
         .filter(isBeregnetPeriode)
-        .sort((a, b) => new Date(b.fom).getTime() - new Date(a.fom).getTime())
-        .find((it) => it.maksdato)?.maksdato;
+        .sort(byFomDescending)
+        .find(hasValidMaksdato)?.maksdato;
