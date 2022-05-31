6082289c6aaffdb9ec4e3ca8fe54c6686b2481ac
Sindre Broch <sindre.broch@nav.no>
2022-05-31 16:27:38 +0200

    Fikser lokalt kall til leggPåVent

--- packages/backend/leggpåvent/devLeggPåVentClient.ts
+++ packages/backend/leggpåvent/devLeggPåVentClient.ts
@@ -2,8 +2,15 @@ import request from 'request-promise-native';
 import { NotatDTO } from '../types';
 
 export default {
-    leggPåVent: async (speilToken: string, oppgavereferanse: string, notat: NotatDTO): Promise<any> =>
-        request.post(`http://localhost:9001/api/leggpaavent/${oppgavereferanse}`, { body: notat }),
+    leggPåVent: async (speilToken: string, oppgavereferanse: string, notat: NotatDTO): Promise<any> => {
+        const options = {
+            uri: `http://localhost:9001/api/leggpaavent/${oppgavereferanse}`,
+            body: notat,
+            resolveWithFullResponse: true,
+            json: true,
+        };
+        return request.post(options);
+    },
 
     fjernPåVent: async (speilToken: string, oppgavereferanse: string): Promise<any> =>
         request.delete(`http://localhost:9001/api/leggpaavent/${oppgavereferanse}`),
