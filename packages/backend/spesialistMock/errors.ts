95540f89a3f701f02368750bd1d8710cb0ee2c23
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-06-13 17:36:06 +0200

    Likt format som i spesialist i mocken

--- packages/backend/spesialistMock/errors.ts
+++ packages/backend/spesialistMock/errors.ts
@@ -1,10 +1,10 @@
 import { Extension } from './types';
 
 export class NotFoundError extends Error {
-    extensions: Array<Extension>;
+    extensions: Extension;
 
     constructor(message: string, field: string) {
         super(message);
-        this.extensions = [{ code: 404, field }];
+        this.extensions = { code: 404, field };
     }
 }
