49d3d78bd6f5b3e63bd0a9b41d27adae41ebf89d
Stephen Ramthun <stephenramthun@gmail.com>
2022-08-15 12:44:55 +0200

    Fiks bug hvor emotionjs prøver å legge en ref på en funksjonskomponent som ikke tar ref.
    * Vi bør se på muligheten for å skrive oss vekk fra emotion og heller bruke ren css med css-modules. Da beholder vi namespacing av css-klasser og slipper bugs som det her samt overheaden det er å ha emotions runtime kjørende i nettleseren.

--- packages/frontend/components/ikoner/Ikon.tsx
+++ packages/frontend/components/ikoner/Ikon.tsx
@@ -4,16 +4,19 @@ export interface IkonProps extends React.SVGAttributes<SVGElement> {
     alt?: string;
 }
 
-export const Ikon: React.FC<IkonProps> = ({
-    children,
-    width = 16,
-    height = 16,
-    viewBox = '0 0 24 24',
-    alt,
-    ...rest
-}: IkonProps) => (
-    <svg pointerEvents="none" aria-labelledby="title" width={width} height={height} viewBox={viewBox} {...rest}>
-        <title id="title">{alt}</title>
-        {children}
-    </svg>
+export const Ikon = React.forwardRef<SVGSVGElement, IkonProps>(
+    ({ children, width = 16, height = 16, viewBox = '0 0 24 24', alt, ...rest }: IkonProps, ref) => (
+        <svg
+            pointerEvents="none"
+            aria-labelledby="title"
+            width={width}
+            height={height}
+            viewBox={viewBox}
+            {...rest}
+            ref={ref}
+        >
+            <title id="title">{alt}</title>
+            {children}
+        </svg>
+    ),
 );
