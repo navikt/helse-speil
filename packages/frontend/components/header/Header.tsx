65bd5638da4b43359e35eb2fb6da9a9db12f5c85
Stephen Ramthun <stephenramthun@gmail.com>
2022-08-17 09:15:05 +0200

    Henter person før redirect etter søk

--- packages/frontend/components/header/Header.tsx
+++ packages/frontend/components/header/Header.tsx
@@ -1,18 +1,12 @@
-import React, { FormEvent, useEffect, useRef } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import { Search } from '@navikt/ds-react';
 import { Header as InternalHeader } from '@navikt/ds-react-internal';
 
 import { authState } from '@state/authentication';
-import { useToggleEasterEgg } from '@state/easterEgg';
-import { useAddVarsel, useRemoveVarsel } from '@state/varsler';
-import { useFetchPerson, usePersonLoadable } from '@state/person';
-import { erGyldigPersonId } from '@hooks/useRefreshPersonVedUrlEndring';
 import { SystemMenu } from '@components/SystemMenu';
+import { Personsøk } from '@components/header/Personsøk';
 import { UserMenu } from '@components/UserMenu';
-import { isPerson } from '@utils/typeguards';
-import { SpeilError } from '@utils/error';
 import { graphqlplayground, toggleMeny } from '@utils/featureToggles';
 
 import { ToggleMenyButton } from './ToggleMeny/ToggleMenyButton';
@@ -20,59 +14,13 @@ import { EasterEgg } from '../../EasterEgg';
 
 import styles from './Header.module.css';
 
-const useNavigateOnFetch = () => {
-    const person = usePersonLoadable();
-    const history = useHistory();
-    const hasFetched = useRef(false);
-
-    const hasFetchedSuccessfully = () => {
-        return hasFetched.current && person.state === 'hasValue' && isPerson(person.contents);
-    };
-
-    useEffect(() => {
-        if (hasFetchedSuccessfully()) {
-            hasFetched.current = false;
-            history.push(`/person/${person.contents.aktorId}/utbetaling`);
-        }
-    }, [person, hasFetched]);
-
-    return hasFetched;
+const useBrukerinfo = () => {
+    const { name, ident, isLoggedIn } = useRecoilValue(authState);
+    return isLoggedIn ? { navn: name, ident: ident ?? '' } : { navn: 'Ikke pålogget', ident: '' };
 };
 
 export const Header = () => {
-    const fetchPerson = useFetchPerson();
-    const removeVarsel = useRemoveVarsel();
-    const addVarsel = useAddVarsel();
-    const hasFetched = useNavigateOnFetch();
-
-    const toggleEasterEgg = useToggleEasterEgg();
-
-    const { name, ident, isLoggedIn } = useRecoilValue(authState);
-    const searchRef = useRef<HTMLInputElement>(null);
-
-    const brukerinfo = isLoggedIn ? { navn: name, ident: ident ?? '' } : { navn: 'Ikke pålogget', ident: '' };
-
-    const onSøk = (event: FormEvent) => {
-        event.preventDefault();
-        removeVarsel('ugyldig-søk');
-        const personId = searchRef.current?.value;
-
-        if (!personId) {
-            return;
-        }
-
-        if (personId.toLowerCase() === 'agurk') {
-            toggleEasterEgg('Agurk');
-            return;
-        }
-
-        if (!erGyldigPersonId(personId)) {
-            addVarsel(new SpeilError(`"${personId}" er ikke en gyldig aktør-ID/fødselsnummer.`));
-        } else {
-            hasFetched.current = true;
-            fetchPerson(personId);
-        }
-    };
+    const brukerinfo = useBrukerinfo();
 
     return (
         <InternalHeader className={styles.Header}>
@@ -81,9 +29,7 @@ export const Header = () => {
                     NAV Sykepenger
                 </Link>
             </InternalHeader.Title>
-            <form className={styles.SearchForm} onSubmit={onSøk}>
-                <Search label="Søk" size="small" variant="secondary" placeholder="Søk" ref={searchRef} />
-            </form>
+            <Personsøk />
             <EasterEgg />
             {toggleMeny && <ToggleMenyButton />}
             {graphqlplayground && (
---
+++ packages/frontend/components/header/Personsøk.tsx
@@ -0,0 +1,61 @@
+import React, { FormEvent, useRef, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Search } from '@navikt/ds-react';
+
+import { useFetchPerson } from '@state/person';
+import { useToggleEasterEgg } from '@state/easterEgg';
+import { useAddVarsel, useRemoveVarsel } from '@state/varsler';
+import { erGyldigPersonId } from '@hooks/useRefreshPersonVedUrlEndring';
+import { SpeilError } from '@utils/error';
+
+import styles from '@components/header/Header.module.css';
+import { useLoadingToast } from '@hooks/useLoadingToast';
+
+export const Personsøk: React.FC = () => {
+    const fetchPerson = useFetchPerson();
+    const removeVarsel = useRemoveVarsel();
+    const addVarsel = useAddVarsel();
+    const history = useHistory();
+    const [isFetching, setIsFetching] = useState(false);
+    const toggleEasterEgg = useToggleEasterEgg();
+
+    useLoadingToast({ isLoading: isFetching, message: 'Henter person' });
+
+    const searchRef = useRef<HTMLInputElement>(null);
+
+    const søkOppPerson = (event: FormEvent) => {
+        event.preventDefault();
+        removeVarsel('ugyldig-søk');
+        const personId = searchRef.current?.value;
+
+        if (!personId || isFetching) {
+            return;
+        }
+
+        if (personId.toLowerCase() === 'agurk') {
+            toggleEasterEgg('Agurk');
+            return;
+        }
+
+        if (!erGyldigPersonId(personId)) {
+            addVarsel(new SpeilError(`"${personId}" er ikke en gyldig aktør-ID/fødselsnummer.`));
+        } else {
+            setIsFetching(true);
+            fetchPerson(personId)
+                .then((personState) => {
+                    if (personState.person) {
+                        history.push(`/person/${personState.person.aktorId}/utbetaling`);
+                    }
+                })
+                .finally(() => {
+                    setIsFetching(false);
+                });
+        }
+    };
+
+    return (
+        <form className={styles.SearchForm} onSubmit={søkOppPerson}>
+            <Search label="Søk" size="small" variant="secondary" placeholder="Søk" ref={searchRef} />
+        </form>
+    );
+};
