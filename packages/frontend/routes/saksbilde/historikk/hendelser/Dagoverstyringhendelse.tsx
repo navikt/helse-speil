af16aba06f11eed5850f87d84cf23dfb40bc5244
Stephen Ramthun <stephenramthun@gmail.com>
2022-08-30 15:48:26 +0200

    Gruppér dager på grad og type i dagoverstyringshendelser i høyreloggen

--- packages/frontend/routes/saksbilde/historikk/hendelser/Dagoverstyringhendelse.tsx
+++ packages/frontend/routes/saksbilde/historikk/hendelser/Dagoverstyringhendelse.tsx
@@ -8,6 +8,40 @@ import { ExpandableHistorikkContent } from './ExpandableHistorikkContent';
 
 import styles from './Dagoverstyringhendelse.module.css';
 
+type Endring = Pick<OverstyrtDag, 'grad' | 'type'>;
+
+type GroupedDays = {
+    start: DateString;
+    end: DateString;
+} & Endring;
+
+const areSimilar = (a: Endring, b: Endring): boolean => {
+    return a.grad === b.grad && a.type === b.type;
+};
+
+const byDate = (a: OverstyrtDag, b: OverstyrtDag): number => {
+    return new Date(a.dato).getTime() - new Date(b.dato).getTime();
+};
+
+const groupSimilarDays = (days: Array<OverstyrtDag>): Array<GroupedDays> => {
+    return [...days].sort(byDate).reduce((groups: Array<GroupedDays>, day: OverstyrtDag) => {
+        const group = groups[groups.length - 1];
+        if (!group || !areSimilar(group, day)) {
+            return [
+                ...groups,
+                {
+                    start: day.dato,
+                    end: day.dato,
+                    grad: day.grad,
+                    type: day.type,
+                },
+            ];
+        }
+        groups[groups.length - 1].end = day.dato;
+        return groups;
+    }, []);
+};
+
 interface DagoverstyringhendelseProps extends Omit<DagoverstyringhendelseObject, 'type' | 'id'> {}
 
 export const Dagoverstyringhendelse: React.FC<DagoverstyringhendelseProps> = ({
@@ -27,14 +61,16 @@ export const Dagoverstyringhendelse: React.FC<DagoverstyringhendelseProps> = ({
             <ExpandableHistorikkContent>
                 <BodyShort size="small">Begrunnelse: {begrunnelse}</BodyShort>
                 <div className={styles.Content}>
-                    {dager.map((dag, i) => (
+                    {groupSimilarDays(dager).map((group, i) => (
                         <div key={i} className={styles.Grid}>
-                            <BodyShort>Dato:</BodyShort>
-                            <BodyShort>{getFormattedDateString(dag.dato)}</BodyShort>
+                            <BodyShort>Fra:</BodyShort>
+                            <BodyShort>{getFormattedDateString(group.start)}</BodyShort>
+                            <BodyShort>Til:</BodyShort>
+                            <BodyShort>{getFormattedDateString(group.end)}</BodyShort>
                             <BodyShort>Grad:</BodyShort>
-                            <BodyShort>{dag.grad} %</BodyShort>
+                            <BodyShort>{group.grad} %</BodyShort>
                             <BodyShort>Type:</BodyShort>
-                            <BodyShort>{dag.type}</BodyShort>
+                            <BodyShort>{group.type}</BodyShort>
                         </div>
                     ))}
                 </div>
