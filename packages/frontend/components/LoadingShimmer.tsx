f1d912b510af53b6c43d34dd12665675fa1aa314
Stephen Ramthun <stephenramthun@gmail.com>
2022-09-07 13:13:09 +0200

    Tilpass loader-komponent

--- packages/frontend/components/LoadingShimmer.tsx
+++ packages/frontend/components/LoadingShimmer.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import styles from './LoadingShimmer.module.css';
 
 export const LoadingShimmer: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ ...divProps }) => {
-    return <div className={styles.LoadingShimmer} {...divProps} />;
+    return <span className={styles.LoadingShimmer} {...divProps} />;
 };
