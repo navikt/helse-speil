9004e54f437f97f7281e13ec1c4dd42a98e647a0
Stephen Ramthun <stephenramthun@gmail.com>
2022-09-20 08:33:58 +0200

    Legg til ident i useInnloggetSaksbehandler

--- packages/frontend/state/authentication.ts
+++ packages/frontend/state/authentication.ts
@@ -30,6 +30,7 @@ export const useInnloggetSaksbehandler = (): Saksbehandler => {
         oid: authInfo.oid,
         navn: authInfo.name,
         epost: authInfo.email,
+        ident: authInfo.ident,
     };
 };
 
