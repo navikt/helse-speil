4364fbd8a6dc5a77a03461664c16e8b572eff9e1
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-09-22 10:31:56 +0200

    Mer rydding i tilganger

--- packages/frontend/utils/featureToggles.ts
+++ packages/frontend/utils/featureToggles.ts
@@ -2,39 +2,17 @@ import { extractGroups, extractIdent } from '@utils/cookie';
 
 const groupIdForTbd = 'f787f900-6697-440d-a086-d5bb56e26a9c';
 const groupIdForBesluttere = '59f26eef-0a4f-4038-bf46-3a5b2f252155';
-const eminem = 'G103083';
-const supersaksbehandlere = [eminem, 'D117949', 'A148751', 'N115007', 'C117102', 'S145454', 'E148846'];
-const fagkoordinatorer = [
-    'J150708',
-    'S108267',
-    'S109074',
-    'K104950',
-    'O107213',
-    'K105348',
-    'T143450',
-    'B137568',
-    'H126784',
-    'A151722',
-    'M136300',
-    'F131883',
-    'K104953',
-    'A147735',
-    'A160730',
-];
-const enhetsledere = ['B138607'];
-const faktiskSupportsaksbehandlere = ['H104215', 'O130292', 'F111930'];
 
-const tilgangStikkprøver = [
-    'F140836',
-    'H104215',
-    'S109031',
-    'O123659',
-    'S160466',
-    'K104953',
-    'J153777',
-    'V149621',
-    'D163344',
-];
+const eminem = 'G103083';
+const supersaksbehandlere = [eminem, 'A148751', 'N115007', 'C117102'];
+
+const fagkoordinatorer = ['M136300', 'S108267'];
+
+const enhetsledere = ['B138607'];
+
+const coaches = ['J153777', 'F131883', 'K104953', 'S109031', 'V149621', 'S160466', 'O123659', 'A160730'];
+
+const tilgangStikkprøver = ['F140836', 'S109031', 'O123659', 'S160466', 'K104953', 'J153777', 'V149621'];
 
 const kunLesetilgang: string[] = [];
 
@@ -43,9 +21,8 @@ export const erDev = () => location.hostname === 'speil.dev.intern.nav.no';
 
 const harKunLesetilgang = () => kunLesetilgang.includes(extractIdent());
 const harTilgangTilAlt = () => [...supersaksbehandlere, ...fagkoordinatorer, ...enhetsledere].includes(extractIdent());
-const erFaktiskSupportsaksbehandler = () => faktiskSupportsaksbehandlere.includes(extractIdent()); // ref @support på Slack
+const erCoach = () => coaches.includes(extractIdent());
 const harTilgangStikkprøver = () => tilgangStikkprøver.includes(extractIdent());
-const harTilgangTilUtbetalingTilSykmeldt = () => ['J153777'].includes(extractIdent());
 
 const erPåTeamBømlo = () => extractGroups().includes(groupIdForTbd);
 
@@ -57,13 +34,7 @@ export const overstyrPermisjonsdagerEnabled = true;
 export const stikkprøve = harTilgangStikkprøver() || harTilgangTilAlt() || erLocal() || erDev();
 export const flereArbeidsgivere = true;
 
-export const utbetalingTilSykmeldt =
-    erLocal() ||
-    erDev() ||
-    erPåTeamBømlo() ||
-    harTilgangTilAlt() ||
-    erFaktiskSupportsaksbehandler() ||
-    harTilgangTilUtbetalingTilSykmeldt();
+export const utbetalingTilSykmeldt = erLocal() || erDev() || erPåTeamBømlo() || harTilgangTilAlt() || erCoach();
 
 export const kanFrigiAndresOppgaver = harTilgangTilAlt() || erLocal() || erDev();
 
