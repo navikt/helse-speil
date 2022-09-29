b69299229a7014b66af6375885c8cc44242f8df3
Elias A. Thøgersen <elias3004@msn.com>
2022-09-29 17:08:49 +0200

    Oppdaterer prettier-plugin-sort-imports og oppdaterer regler i config

--- packages/frontend/prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    singleQuote: true,
-    tabWidth: 4,
-    printWidth: 120,
-    importOrder: ['^@navikt/(.*)$', '/(components|hooks|io|mapping|modell|routes|state|utils|assets)/', '^[./]'],
-    importOrderSeparation: true,
-    trailingComma: 'all',
-};
