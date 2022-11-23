43604aeb90a08dd9d985d1d11ada4d2ea37b39e2
Stephen Ramthun <stephenramthun@gmail.com>
2022-11-23 11:26:28 +0100

    Legg til tester for IkkeLoggetInn

--- packages/frontend/routes/IkkeLoggetInn.test.tsx
+++ packages/frontend/routes/IkkeLoggetInn.test.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { PageNotFound } from './PageNotFound';
+import { IkkeLoggetInn } from './IkkeLoggetInn';
 
-describe('PageNotFound', () => {
+describe('IkkeLoggetInn', () => {
     it('rendrer uten violations', async () => {
-        const { container } = render(<PageNotFound />);
+        const { container } = render(<IkkeLoggetInn />);
 
         const result = await axe(container);
 
--- packages/frontend/routes/IkkeLoggetInn.tsx
+++ packages/frontend/routes/IkkeLoggetInn.tsx
@@ -1,26 +1,12 @@
-import styled from '@emotion/styled';
 import React from 'react';
 
 import { BodyShort, Link } from '@navikt/ds-react';
 
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: calc(100% - 3em);
-
-    p {
-        font-size: 1.5rem;
-        padding: 0.5rem;
-    }
-`;
+import styles from './IkkeLoggetInn.module.css';
 
 export const IkkeLoggetInn = () => (
-    <Container>
-        <BodyShort as="p">Du må logge inn for å få tilgang til systemet</BodyShort>
-        <BodyShort as="p">
-            <Link href="/">Gå til innloggingssiden</Link>
-        </BodyShort>
-    </Container>
+    <main className={styles.IkkeLoggetInn}>
+        <BodyShort>Du må logge inn for å få tilgang til systemet</BodyShort>
+        <Link href="/">Gå til innloggingssiden</Link>
+    </main>
 );
