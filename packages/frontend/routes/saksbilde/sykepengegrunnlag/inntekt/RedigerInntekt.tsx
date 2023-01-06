ce89b65ab39dfc45f1c03db46cd81f9d3a33d4b2
Jonas Hasle <jonas.hasle@nav.no>
2023-01-06 10:59:58 +0100

    Stilendringer i RedigerInntekt
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- packages/frontend/routes/saksbilde/sykepengegrunnlag/inntekt/RedigerInntekt.tsx
+++ packages/frontend/routes/saksbilde/sykepengegrunnlag/inntekt/RedigerInntekt.tsx
@@ -81,7 +81,7 @@ export const RedigerInntekt = ({ setEditing, editing, erRevurdering, vilkårsgru
             closedText={erRevurdering ? 'Revurder' : 'Endre'}
             onOpen={() => setEditing(true)}
             onClose={() => setEditing(false)}
-            style={{ justifySelf: 'flex-end', alignSelf: 'baseline' }}
+            style={{ placeSelf: 'center flex-end' }}
         />
     ) : (
         <PopoverHjelpetekst ikon={<SortInfoikon />}>
--- packages/frontend/routes/saksbilde/sykepengegrunnlag/refusjon/Refusjonsoversikt.tsx
+++ packages/frontend/routes/saksbilde/sykepengegrunnlag/refusjon/Refusjonsoversikt.tsx
@@ -1,39 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { BodyShort } from '@navikt/ds-react';
 
 import { Bold } from '@components/Bold';
-import { EditButton } from '@components/EditButton';
 import { Refusjonsopplysning } from '@io/http';
 
 import { Refusjonslinje } from './Refusjonslinje';
 
 import styles from './Refusjonsoversikt.module.css';
 
-const canEditRefusjon = false;
-
 interface RefusjonProps {
     refusjon: Refusjonsopplysning[];
 }
 
 export const Refusjonsoversikt: React.FC<RefusjonProps> = ({ refusjon }) => {
-    const [isEditing, setIsEditing] = useState(false);
-
     return (
         <div className={styles.Refusjonsoversikt}>
             <div className={styles.Header}>
                 <div className={styles.Tittel}>
                     <Bold>Refusjon</Bold>
                 </div>
-                {canEditRefusjon && (
-                    <EditButton
-                        isOpen={isEditing}
-                        openText="Avbryt"
-                        closedText="Revurder"
-                        onOpen={() => setIsEditing(true)}
-                        onClose={() => setIsEditing(false)}
-                    />
-                )}
             </div>
             <table className={styles.Table}>
                 <thead>
