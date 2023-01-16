37b362f2e2522b6383bcae7934480bbbe2ed7f74
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-01-16 19:48:10 +0100

    Setter callId og logger tidsbruk på graphql-kall til spesialist

--- packages/backend/graphql/graphQLClient.ts
+++ packages/backend/graphql/graphQLClient.ts
@@ -1,6 +1,8 @@
 import request from 'request-promise-native';
+import { v4 as uuidv4 } from 'uuid';
 
 import config from '../config';
+import logger from '../logging';
 import { OidcConfig, OnBehalfOf } from '../types';
 
 const spesialistBaseUrl = config.server.spesialistBaseUrl;
@@ -13,16 +15,23 @@ export interface GraphQLClient {
 
 export default (oidcConfig: OidcConfig, onBehalfOf: OnBehalfOf): GraphQLClient => ({
     postGraphQLQuery: async (speilToken: string, data: string): Promise<request.FullResponse> => {
+        const callId = uuidv4();
         const onBehalfOfToken = await onBehalfOf.hentFor(oidcConfig.clientIDSpesialist, speilToken);
         const options = {
             uri: `${baseUrl}/graphql`,
             headers: {
                 Authorization: `Bearer ${onBehalfOfToken}`,
+                'X-Request-Id': callId,
             },
             resolveWithFullResponse: true,
             json: true,
             body: data,
         };
-        return request.post(options);
+        const start = Date.now();
+        return request.post(options).then((response): Promise<request.FullResponse> => {
+            const tidBrukt = Date.now() - start;
+            logger.info(`GraphQL-kall til ${baseUrl} med X-Request-Id: ${callId} ferdig etter ${tidBrukt} ms`);
+            return response;
+        });
     },
 });
