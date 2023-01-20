82e597964112060a1ea6a5dbe6dd92aa112057c9
Elias A. Thøgersen <elias3004@msn.com>
2023-01-20 12:40:21 +0100

    Fikser bug hvor revurdering/endre-knappen ble borte
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- packages/frontend/state/selectors/arbeidsgiver.ts
+++ packages/frontend/state/selectors/arbeidsgiver.ts
@@ -4,5 +4,5 @@ import { Arbeidsgiver } from '@io/graphql';
  Finner arbeidsgiver HVIS perioden du sender inn er i nyeste generasjon.
  */
 export const getArbeidsgiverWithPeriod = (person: FetchedPerson, period: ActivePeriod): Arbeidsgiver | null => {
-    return person.arbeidsgivere.find((it) => it.generasjoner[0]?.perioder.find((it) => it === period)) ?? null;
+    return person.arbeidsgivere.find((it) => it.generasjoner[0]?.perioder.find((it) => it.id === period.id)) ?? null;
 };
