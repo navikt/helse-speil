fb0ffd94059cbc5c897e9d1004f74266f7de163c
Øyvind Hagen <oyvind.hagen@nav.no>
2023-03-14 12:56:10 +0100

    :loud_sound: Forsøker å rekjøre graphql-spørring dersom den feiler
    Logger feil som info ved første forsøk, error (som før) ved feil på andre gangs forsøk.
    
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- packages/backend/graphql/graphQLRoutes.ts
+++ packages/backend/graphql/graphQLRoutes.ts
@@ -12,16 +12,25 @@ export default ({ graphQLClient }: SetupOptions) => {
     const router = Router();
 
     router.post('/', (req: SpeilRequest, res: Response) => {
-        graphQLClient
-            .postGraphQLQuery(req.session!.speilToken, req.body)
-            .then((it) => {
-                res.status(200).send(it.body);
-            })
-            .catch(({ error }) => {
-                logger.error(`Feil ved GraphQL-sending`, error);
-                res.status(200).send(error);
-            });
+        postSpørring(graphQLClient, req, res);
     });
 
     return router;
+};
+
+const postSpørring = (graphQLClient: GraphQLClient, req: SpeilRequest, res: Response, førsteForsøk: boolean = true) => {
+    graphQLClient
+        .postGraphQLQuery(req.session!.speilToken, req.body)
+        .then((it) => {
+            res.status(200).send(it.body);
+        })
+        .catch(({ error }) => {
+            if (førsteForsøk) {
+                logger.info(`Feil ved GraphQL-sending, prøver en gang til`, error);
+                postSpørring(graphQLClient, req, res, false);
+            } else {
+                logger.error(`Feil ved GraphQL-sending`, error);
+                res.status(200).send(error);
+            }
+        });
 };
