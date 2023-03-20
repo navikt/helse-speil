414d36b49991d90e7b61fe6e19347c519b780f0b
Øyvind Hagen <oyvind.hagen@nav.no>
2023-03-20 15:46:30 +0100

    🚸 Forhindrer at lovme-varsel sender sak til totrinns
    Koden som nå ikke vil kjøres er ikke ryddet vekk ettersom det vil komme et nytt varsel som skal bruke dette.
    
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- packages/frontend/hooks/useHarVurderLovvalgOgMedlemskapVarsel.ts
+++ packages/frontend/hooks/useHarVurderLovvalgOgMedlemskapVarsel.ts
@@ -7,14 +7,18 @@ export const useHarVurderLovvalgOgMedlemskapVarsel = (): boolean => {
     const periode = useActivePeriod();
     const arbeidsgiver = useCurrentArbeidsgiver();
 
+    // TODO: Dette skal endres så snart nytt uavklart "lovvalg og medlemskap"-varsel med annen tekst er klart i spleis
+    // Dette er gjort slik for at det gamle varselet ikke skal trigge totrinns og notat-modal.
+    const varselTekst = 'ikke-eksisterende varseltekst for å unngå videre eksekvering av koden';
+    if (varselTekst === 'ikke-eksisterende varseltekst for å unngå videre eksekvering av koden') {
+        return false;
+    }
+
     if (!isBeregnetPeriode(periode) || !arbeidsgiver) {
         return false;
     }
 
     const vedtaksperiodeHarIkkeBlittUtbetaltFør = !harBlittUtbetaltTidligere(periode, arbeidsgiver);
 
-    return (
-        vedtaksperiodeHarIkkeBlittUtbetaltFør &&
-        periode.varsler.some((varsel) => varsel === 'Vurder lovvalg og medlemskap')
-    );
+    return vedtaksperiodeHarIkkeBlittUtbetaltFør && periode.varsler.some((varsel) => varsel === varselTekst);
 };
