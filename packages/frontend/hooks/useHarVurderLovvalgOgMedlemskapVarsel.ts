25f49a6e5b93c21eff14ba3ee569365e0349b71e
Elias A. Thøgersen <elias3004@msn.com>
2023-03-22 12:45:06 +0100

    ♻️ bruk varslerForGenerasjon i stedet for varsler
    * I useHarVurderLovvalgOgMedlemskapVarsel-hooken
    
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
    Co-authored-by: Øydis Kind Refsum <oydis.kind.refsum@nav.no>

--- packages/frontend/hooks/useHarVurderLovvalgOgMedlemskapVarsel.ts
+++ packages/frontend/hooks/useHarVurderLovvalgOgMedlemskapVarsel.ts
@@ -1,24 +1,20 @@
-import { useCurrentArbeidsgiver } from '@state/arbeidsgiver';
-import { useActivePeriod } from '@state/periode';
-import { harBlittUtbetaltTidligere } from '@state/selectors/period';
-import { isBeregnetPeriode } from '@utils/typeguards';
-
 export const useHarVurderLovvalgOgMedlemskapVarsel = (): boolean => {
-    const periode = useActivePeriod();
-    const arbeidsgiver = useCurrentArbeidsgiver();
+    return false;
 
-    // TODO: Dette skal endres så snart nytt uavklart "lovvalg og medlemskap"-varsel med annen tekst er klart i spleis
-    // Dette er gjort slik for at det gamle varselet ikke skal trigge totrinns og notat-modal.
-    const varselTekst = 'ikke-eksisterende varseltekst for å unngå videre eksekvering av koden';
-    if (varselTekst === 'ikke-eksisterende varseltekst for å unngå videre eksekvering av koden') {
-        return false;
-    }
+    /* TODO: Koden under skal kommenteres inn hvis man blir enige om at RV_MV_3 skal medføre totrinn.
+        Hvis det blir bestemt at den ikke skal medføre totrinn kan hooken og bruk av den fjernes. */
 
-    if (!isBeregnetPeriode(periode) || !arbeidsgiver) {
-        return false;
-    }
-
-    const vedtaksperiodeHarIkkeBlittUtbetaltFør = !harBlittUtbetaltTidligere(periode, arbeidsgiver);
-
-    return vedtaksperiodeHarIkkeBlittUtbetaltFør && periode.varsler.some((varsel) => varsel === varselTekst);
+    // const periode = useActivePeriod();
+    // const arbeidsgiver = useCurrentArbeidsgiver();
+    //
+    // if (!isBeregnetPeriode(periode) || !arbeidsgiver) {
+    //     return false;
+    // }
+    //
+    // const vedtaksperiodeHarIkkeBlittUtbetaltFør = !harBlittUtbetaltTidligere(periode, arbeidsgiver);
+    //
+    // return (
+    //     vedtaksperiodeHarIkkeBlittUtbetaltFør &&
+    //     periode.varslerForGenerasjon.some((varsel) => varsel.kode === 'RV_MV_3')
+    // );
 };
