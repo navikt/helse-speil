60e927a76d78c4fbdfb01a828f88de56749ac70e
Martin Solheim <martin.solheim@me.com>
2023-05-15 14:22:37 +0200

    💄 Fjerner styled components i Vilkårsgrupperad
    - Fikser only-export-components lint feil ved å fjerne emotion
    - Bruker css modules i stedet

--- packages/frontend/routes/saksbilde/vilkår/vilkårsgrupper/Vilkårsgrupperad.tsx
+++ packages/frontend/routes/saksbilde/vilkår/vilkårsgrupper/Vilkårsgrupperad.tsx
@@ -1,28 +1,19 @@
-import styled from '@emotion/styled';
 import React, { ReactNode } from 'react';
 
 import { BodyShort } from '@navikt/ds-react';
+
+import styles from './Vilkårsgrupperad.module.css';
 
 interface Props {
     label: string;
     children: ReactNode | ReactNode[];
 }
 
-const Navn = styled(BodyShort)`
-    min-width: 15rem;
-`;
-
-const Verdi = styled.div`
-    font-family: 'Source Sans Pro', Arial, Helvetica, sans-serif;
-    font-size: 1rem;
-    color: var(--a-text-default);
-    text-align: left;
-    min-width: 100px;
-`;
-
 export const Vilkårsgrupperad = ({ label, children }: Props) => (
     <>
-        <Navn as="p">{label}</Navn>
-        <Verdi>{children}</Verdi>
+        <BodyShort className={styles.Navn} as="p">
+            {label}
+        </BodyShort>
+        <div className={styles.Verdi}>{children}</div>
     </>
 );
