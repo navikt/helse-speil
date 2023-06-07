b5868cf88b705f1d06598020c23d0339b1109ea8
Martin Solheim <martin.solheim@me.com>
2023-06-07 12:15:43 +0200

    ♻️ Bytter ut hjemmesnekra løsning for navigasjon i SaksbildeMenu
    - Bruker NavLink fra react-router for å slippe å regne ut og matche path for å finne aktiv tab
    - Styler med css modules

--- packages/frontend/routes/saksbilde/saksbildeMenu/SaksbildeMenu.tsx
+++ packages/frontend/routes/saksbilde/saksbildeMenu/SaksbildeMenu.tsx
@@ -1,17 +1,16 @@
 import classNames from 'classnames';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { BodyShort } from '@navikt/ds-react';
 
 import { ErrorBoundary } from '@components/ErrorBoundary';
 import { LoadingShimmer } from '@components/LoadingShimmer';
-import { Location, useNavigation } from '@hooks/useNavigation';
 import { useActivePeriod } from '@state/periode';
 import { useIsFetchingPerson } from '@state/person';
 import { onLazyLoadFail } from '@utils/error';
 import { isBeregnetPeriode, isGhostPeriode } from '@utils/typeguards';
 
-import { TabLink } from '../TabLink';
 import { DropdownMenu } from './dropdown/DropdownMenu';
 
 import styles from './SaksbildeMenu.module.css';
@@ -19,74 +18,58 @@ import styles from './SaksbildeMenu.module.css';
 // @ts-ignore
 const HistorikkHeader = React.lazy(() => import('../historikk').catch(onLazyLoadFail));
 
-const SaksbildeMenuGhostPeriode: React.FC = () => {
-    const { pathForLocation } = useNavigation();
-
-    return (
-        <div className={styles.SaksbildeMenu}>
-            <div>
-                <span className={styles.TabList} role="tablist">
-                    <TabLink to={pathForLocation(Location.Sykepengegrunnlag)} title="Sykepengegrunnlag">
-                        Sykepengegrunnlag
-                    </TabLink>
-                </span>
-                <DropdownMenu />
-            </div>
-            <HistorikkHeader />
+const SaksbildeMenuGhostPeriode: React.FC = () => (
+    <div className={styles.SaksbildeMenu}>
+        <div>
+            <nav className={styles.TabList} role="tablist">
+                <NavLenke to="sykepengegrunnlag" tittel="Sykepengegrunnlag" />
+            </nav>
+            <DropdownMenu />
         </div>
-    );
-};
+        <HistorikkHeader />
+    </div>
+);
 
 interface SaksbildeMenuBeregnetPeriodeProps {
     activePeriod: FetchedBeregnetPeriode;
 }
 
-const SaksbildeMenuBeregnetPeriode = ({ activePeriod }: SaksbildeMenuBeregnetPeriodeProps) => {
-    const { pathForLocation } = useNavigation();
-
-    return (
-        <div className={styles.SaksbildeMenu}>
-            <div>
-                <span className={styles.TabList} role="tablist">
-                    <TabLink to={pathForLocation(Location.Utbetaling)} title="Utbetaling">
-                        Utbetaling
-                    </TabLink>
-                    <TabLink to={pathForLocation(Location.Inngangsvilkår)} title="Inngangsvilkår">
-                        Inngangsvilkår
-                    </TabLink>
-                    <TabLink to={pathForLocation(Location.Sykepengegrunnlag)} title="Sykepengegrunnlag">
-                        Sykepengegrunnlag
-                    </TabLink>
-                    {activePeriod.risikovurdering && (
-                        <TabLink to={pathForLocation(Location.Faresignaler)} title="Faresignaler">
-                            Faresignaler
-                        </TabLink>
-                    )}
-                </span>
-                <DropdownMenu />
-            </div>
-            <HistorikkHeader />
+const SaksbildeMenuBeregnetPeriode = ({ activePeriod }: SaksbildeMenuBeregnetPeriodeProps) => (
+    <div className={styles.SaksbildeMenu}>
+        <div>
+            <nav className={styles.TabList} role="tablist">
+                <NavLenke to="utbetaling" tittel="Utbetaling" />
+                <NavLenke to="inngangsvilkår" tittel="Inngangsvilkår" />
+                <NavLenke to="sykepengegrunnlag" tittel="Sykepengegrunnlag" />
+                {activePeriod.risikovurdering && <NavLenke to="faresignaler" tittel="Faresignaler" />}
+            </nav>
+            <DropdownMenu />
         </div>
-    );
-};
+        <HistorikkHeader />
+    </div>
+);
 
-const SaksbildeMenuUberegnetPeriode: React.FC = () => {
-    const { pathForLocation } = useNavigation();
-
-    return (
-        <div className={styles.SaksbildeMenu}>
-            <div>
-                <span className={styles.TabList} role="tablist">
-                    <TabLink to={pathForLocation(Location.Utbetaling)} title="Utbetaling">
-                        Utbetaling
-                    </TabLink>
-                </span>
-                <DropdownMenu />
-            </div>
-            <HistorikkHeader />
+const SaksbildeMenuUberegnetPeriode: React.FC = () => (
+    <div className={styles.SaksbildeMenu}>
+        <div>
+            <nav className={styles.TabList} role="tablist">
+                <NavLenke to="utbetaling" tittel="Utbetaling" />
+            </nav>
+            <DropdownMenu />
         </div>
-    );
-};
+        <HistorikkHeader />
+    </div>
+);
+
+const NavLenke = ({ tittel, to }: { tittel: string; to: string }) => (
+    <NavLink
+        className={({ isActive }) => classNames([styles.NavLink], { [styles.ActiveLink]: isActive })}
+        to={to}
+        title={tittel}
+    >
+        {tittel}
+    </NavLink>
+);
 
 const SaksbildeMenuContainer: React.FC = () => {
     const activePeriod = useActivePeriod();
