8ec97d0dfbc90829a1b84fc45849e0ce068a9234
Martin Solheim <martin.solheim@me.com>
2023-06-22 11:11:11 +0200

    ✅ Jest magi for å få mocking av funksjon til å virke
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- packages/frontend/__mocks__/@state/oppgaver.js
+++ packages/frontend/__mocks__/@state/oppgaver.js
@@ -1,9 +1,5 @@
 module.exports = {
     useOppgaver: jest.fn(),
     useMineOppgaver: jest.fn(),
-    useOppgaverLoadable: jest.fn(),
-    useRefetchOppgaver: jest.fn().mockReturnValue(() => null),
-    useFerdigstilteOppgaver: jest.fn().mockReturnValue(() => null),
-    useRefetchFerdigstilteOppgaver: jest.fn().mockReturnValue(() => null),
-    useTildelOppgave: jest.fn().mockReturnValue(() => null),
+    useQueryOppgaver: jest.fn(),
 };
