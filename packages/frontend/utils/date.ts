c844a53fe7ac78c9ba5acc8bbba46bf0ecd29fc9
Martin Solheim <martin.solheim@me.com>
2023-06-22 12:02:59 +0200

    🔥 Fjerner funksjoner som bare var testet

--- packages/frontend/utils/date.ts
+++ packages/frontend/utils/date.ts
@@ -1,27 +1,21 @@
-import dayjs, { Dayjs, max, min } from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import isoWeek from 'dayjs/plugin/isoWeek';
+import minMax from 'dayjs/plugin/minMax';
 
 import { Maybe } from '@io/graphql';
 
 dayjs.extend(isoWeek);
 dayjs.extend(isSameOrBefore);
 dayjs.extend(isSameOrAfter);
+dayjs.extend(minMax);
 
 export const NORSK_DATOFORMAT_MED_KLOKKESLETT = 'DD.MM.YY kl. HH.mm';
 export const NORSK_DATOFORMAT = 'DD.MM.YYYY';
 export const NORSK_DATOFORMAT_KORT = 'DD.MM.YY';
 export const ISO_DATOFORMAT = 'YYYY-MM-DD';
 export const ISO_TIDSPUNKTFORMAT = 'YYYY-MM-DDTHH:mm:ss';
-
-export const findLatest = (dates: Dayjs[]): Dayjs => {
-    return max(dates);
-};
-
-export const findEarliest = (dates: Dayjs[]): Dayjs => {
-    return min(dates);
-};
 
 export const getFormattedDateString = (dateString?: Maybe<DateString>): string =>
     typeof dateString === 'string' ? dayjs(dateString).format(NORSK_DATOFORMAT) : '';
