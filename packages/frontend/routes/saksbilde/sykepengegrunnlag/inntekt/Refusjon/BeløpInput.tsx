1c26a416f486a90b4ca1d2bd96ca961751628a1a
Martin Solheim <martin.solheim@me.com>
2023-08-10 16:19:18 +0200

    🚨 Fikser ts feil

--- packages/frontend/routes/saksbilde/sykepengegrunnlag/inntekt/Refusjon/BeløpInput.tsx
+++ packages/frontend/routes/saksbilde/sykepengegrunnlag/inntekt/Refusjon/BeløpInput.tsx
@@ -1,34 +1,10 @@
 import classNames from 'classnames';
 import React from 'react';
-import { Control, FieldError, useController } from 'react-hook-form';
+import { Control, useController } from 'react-hook-form';
 
 import { RefusjonFormValues } from './useRefusjonFormField';
 
 import styles from './Refusjon.module.css';
-
-interface BeløpInputProps {
-    beløp: number;
-    updateBeløp: (beløp: number) => void;
-    clearError: () => void;
-    error?: FieldError;
-}
-
-const BeløpInput = ({ beløp, updateBeløp, clearError, error }: BeløpInputProps) => (
-    <input
-        className={classNames({
-            [styles.BeløpInput]: true,
-            [styles.InputError]: error?.message,
-        })}
-        type="number"
-        onBlur={(event) => {
-            const nyttBeløp = Number(event.target.value);
-            if (nyttBeløp === beløp) return;
-            clearError();
-            updateBeløp(Number(event.target.value));
-        }}
-        defaultValue={beløp && Math.round((beløp + Number.EPSILON) * 100) / 100}
-    />
-);
 
 interface ControlledBeløpInputProps {
     beløp: number;
@@ -38,7 +14,7 @@ interface ControlledBeløpInputProps {
 
 export const ControlledBeløpInput = ({ beløp, index, control }: ControlledBeløpInputProps) => {
     const isNumeric = (input: string) => /^\d+(\.\d{1,2})?$/.test(input);
-    const { field, fieldState, formState } = useController({
+    const { field, fieldState } = useController({
         name: `refusjonsopplysninger.${index}.beløp`,
         control: control,
         rules: {
