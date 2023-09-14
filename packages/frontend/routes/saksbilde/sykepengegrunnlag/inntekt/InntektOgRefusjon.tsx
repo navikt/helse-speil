c6f1cc42890f0d0e165a1bdae542d83d1beb27d6
oydisrefsum <oydis.kind.refsum@nav.no>
2023-09-14 09:24:07 +0200

    ♻️ Tar ut inntekt og refusjon hjelpefunksjoner til egen utils fil
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>

--- packages/frontend/routes/saksbilde/sykepengegrunnlag/inntekt/InntektOgRefusjon.tsx
+++ packages/frontend/routes/saksbilde/sykepengegrunnlag/inntekt/InntektOgRefusjon.tsx
@@ -1,6 +1,5 @@
 import { SisteTreMånedersInntekt } from './SisteTreMånedersInntekt';
 import classNames from 'classnames';
-import dayjs from 'dayjs';
 import React, { useEffect, useState } from 'react';
 
 import { Bold } from '@components/Bold';
@@ -10,187 +9,40 @@ import { PopoverHjelpetekst } from '@components/PopoverHjelpetekst';
 import { AnonymizableContainer } from '@components/anonymizable/AnonymizableContainer';
 import { Clipboard } from '@components/clipboard';
 import { SortInfoikon } from '@components/ikoner/SortInfoikon';
-import { useIsReadOnlyOppgave } from '@hooks/useIsReadOnlyOppgave';
 import {
     Arbeidsgiver,
-    BeregnetPeriode,
     InntektFraAOrdningen,
     Inntektskilde,
     Inntektstype,
     Maybe,
     OmregnetArsinntekt,
-    Periodetilstand,
 } from '@io/graphql';
 import { Refusjonsopplysning } from '@io/http';
 import {
-    useArbeidsgiver,
     useEndringerForPeriode,
     useErAktivPeriodeLikEllerFørPeriodeTilGodkjenning,
     useLokaleRefusjonsopplysninger,
     useLokaltMånedsbeløp,
-    usePeriodForSkjæringstidspunkt,
-    usePeriodForSkjæringstidspunktForArbeidsgiver,
 } from '@state/arbeidsgiver';
 import { useCurrentPerson } from '@state/person';
-import { isForkastet } from '@state/selectors/period';
-import { overstyrInntektEnabled } from '@utils/featureToggles';
-import { isBeregnetPeriode, isGhostPeriode } from '@utils/typeguards';
 
 import { Arbeidsgivernavn } from '../Arbeidsgivernavn';
 import { OverstyrArbeidsforholdUtenSykdom } from '../overstyring/OverstyrArbeidsforholdUtenSykdom';
-import { BegrunnelseForOverstyring } from '../overstyring/overstyring.types';
 import { Refusjonsoversikt } from '../refusjon/Refusjonsoversikt';
 import { EditableInntekt } from './EditableInntekt/EditableInntekt';
 import { ReadOnlyInntekt } from './ReadOnlyInntekt';
 import { RedigerGhostInntekt } from './RedigerGhostInntekt';
 import { RedigerInntektOgRefusjon } from './RedigerInntektOgRefusjon/RedigerInntektOgRefusjon';
-import { harIngenUtbetaltePerioderFor, harPeriodeTilBeslutterFor } from './inntektOgRefusjonUtils';
+import {
+    endreInntektMedSykefraværBegrunnelser,
+    endreInntektUtenSykefraværBegrunnelser,
+    maybePeriodeTilGodkjenning,
+    useArbeidsforholdKanOverstyres,
+    useGhostInntektKanOverstyres,
+    useInntektKanRevurderes,
+} from './inntektOgRefusjonUtils';
 
 import styles from './Inntekt.module.css';
-
-const maybePeriodeTilGodkjenning = (person: FetchedPerson, skjæringstidspunkt: DateString): Maybe<BeregnetPeriode> => {
-    return (
-        (
-            person?.arbeidsgivere
-                .flatMap((it) => it.generasjoner[0]?.perioder)
-                .filter(isBeregnetPeriode) as unknown as Array<BeregnetPeriode>
-        ).find(
-            (it) =>
-                it.periodetilstand === Periodetilstand.TilGodkjenning && it.skjaeringstidspunkt === skjæringstidspunkt,
-        ) ?? null
-    );
-};
-
-const maybePeriodeForSkjæringstidspunkt = (
-    person: FetchedPerson,
-    skjæringstidspunkt: DateString,
-): Maybe<BeregnetPeriode> => {
-    return (
-        (
-            person?.arbeidsgivere
-                .flatMap((it) => it.generasjoner[0]?.perioder)
-                .filter(isBeregnetPeriode) as unknown as Array<BeregnetPeriode>
-        ).find((it) => it.skjaeringstidspunkt === skjæringstidspunkt) ?? null
-    );
-};
-
-const harIngenBeregnedePerioder = (arbeidsgiver: Arbeidsgiver, skjæringstidspunkt: DateString): boolean =>
-    (
-        arbeidsgiver?.generasjoner[0]?.perioder.filter(
-            (it) => it.skjaeringstidspunkt === skjæringstidspunkt && isBeregnetPeriode(it),
-        ) ?? []
-    ).length === 0;
-
-const harIngenEtterfølgendePerioder = (
-    arbeidsgiver: Arbeidsgiver,
-    skjæringstidspunkt: DateString,
-    fom: DateString,
-): boolean =>
-    (
-        arbeidsgiver?.generasjoner[0]?.perioder.filter(
-            (it) => it.skjaeringstidspunkt === skjæringstidspunkt && dayjs(it.fom).isSameOrAfter(fom),
-        ) ?? []
-    ).length === 0;
-
-const useInntektKanRevurderes = (skjæringstidspunkt: DateString): boolean => {
-    const person = useCurrentPerson();
-    const periodeVedSkjæringstidspunkt = usePeriodForSkjæringstidspunkt(skjæringstidspunkt);
-    const isReadOnlyOppgave = useIsReadOnlyOppgave();
-    const erAktivPeriodeLikEllerFørPeriodeTilGodkjenning = useErAktivPeriodeLikEllerFørPeriodeTilGodkjenning();
-
-    if (!person) return false;
-
-    const harPeriodeTilBeslutter = harPeriodeTilBeslutterFor(person, skjæringstidspunkt);
-
-    return (
-        overstyrInntektEnabled &&
-        !isForkastet(periodeVedSkjæringstidspunkt) &&
-        !isReadOnlyOppgave &&
-        !harPeriodeTilBeslutter &&
-        erAktivPeriodeLikEllerFørPeriodeTilGodkjenning
-    );
-};
-
-const useArbeidsforholdKanOverstyres = (skjæringstidspunkt: DateString, organisasjonsnummer: string): boolean => {
-    const person = useCurrentPerson();
-    const period = usePeriodForSkjæringstidspunktForArbeidsgiver(skjæringstidspunkt, organisasjonsnummer);
-    const erAktivPeriodeLikEllerFørPeriodeTilGodkjenning = useErAktivPeriodeLikEllerFørPeriodeTilGodkjenning();
-    const arbeidsgiver = useArbeidsgiver(organisasjonsnummer);
-
-    if (!isGhostPeriode(period) || !person || !arbeidsgiver) {
-        return false;
-    }
-
-    const periodeForSkjæringstidspunkt = maybePeriodeForSkjæringstidspunkt(person, period.skjaeringstidspunkt);
-    const harPeriodeTilBeslutter = harPeriodeTilBeslutterFor(person, period.skjaeringstidspunkt);
-    const arbeidsgiverHarIngenBeregnedePerioder = harIngenBeregnedePerioder(arbeidsgiver, skjæringstidspunkt);
-    const arbeidsgiverHarIngenEtterfølgendePerioder = harIngenEtterfølgendePerioder(
-        arbeidsgiver,
-        skjæringstidspunkt,
-        period.fom,
-    );
-
-    return (
-        arbeidsgiverHarIngenBeregnedePerioder &&
-        arbeidsgiverHarIngenEtterfølgendePerioder &&
-        !harPeriodeTilBeslutter &&
-        periodeForSkjæringstidspunkt !== null &&
-        erAktivPeriodeLikEllerFørPeriodeTilGodkjenning
-    );
-};
-
-const useGhostInntektKanOverstyres = (skjæringstidspunkt: DateString, organisasjonsnummer: string): boolean => {
-    const person = useCurrentPerson();
-    const period = usePeriodForSkjæringstidspunktForArbeidsgiver(skjæringstidspunkt, organisasjonsnummer);
-    const erAktivPeriodeLikEllerFørPeriodeTilGodkjenning = useErAktivPeriodeLikEllerFørPeriodeTilGodkjenning();
-
-    if (!isGhostPeriode(period) || !person) {
-        return false;
-    }
-
-    const periodeTilGodkjenning = maybePeriodeTilGodkjenning(person, period.skjaeringstidspunkt);
-
-    const harUtbetaltePerioder = !harIngenUtbetaltePerioderFor(person, period.skjaeringstidspunkt);
-
-    const harPeriodeTilBeslutter = harPeriodeTilBeslutterFor(person, period.skjaeringstidspunkt);
-
-    return (
-        (harUtbetaltePerioder || periodeTilGodkjenning !== null) &&
-        !harPeriodeTilBeslutter &&
-        erAktivPeriodeLikEllerFørPeriodeTilGodkjenning
-    );
-};
-
-const endreInntektUtenSykefraværBegrunnelser: BegrunnelseForOverstyring[] = [
-    {
-        id: '0',
-        forklaring: 'Arbeidsforhold har vart kortere enn 3 måneder',
-        subsumsjon: { paragraf: '8-28', ledd: '3', bokstav: 'b' },
-    },
-    {
-        id: '1',
-        forklaring: 'Varig lønnsendring',
-        subsumsjon: { paragraf: '8-28', ledd: '3', bokstav: 'c' },
-    },
-    {
-        id: '2',
-        forklaring: 'Innrapportert feil inntekt til A-ordningen',
-        subsumsjon: { paragraf: '8-28', ledd: '5' },
-    },
-    {
-        id: '3',
-        forklaring: 'Annen kilde til endring',
-        subsumsjon: { paragraf: '8-28' },
-    },
-];
-
-const endreInntektMedSykefraværBegrunnelser: BegrunnelseForOverstyring[] = [
-    { id: '0', forklaring: 'Korrigert inntekt i inntektsmelding' },
-    { id: '1', forklaring: 'Tariffendring i inntektsmelding' },
-    { id: '2', forklaring: 'Innrapportert feil inntekt til A-ordningen' },
-    { id: '3', forklaring: 'Endring/opphør av refusjon' },
-    { id: '4', forklaring: 'Annen kilde til endring' },
-];
 
 interface InntektUtenSykefraværProps {
     organisasjonsnummer: string;
--- packages/frontend/routes/saksbilde/sykepengegrunnlag/inntekt/inntektOgRefusjonUtils.ts
+++ packages/frontend/routes/saksbilde/sykepengegrunnlag/inntekt/inntektOgRefusjonUtils.ts
@@ -1,5 +1,19 @@
-import { BeregnetPeriode, Periodetilstand } from '@io/graphql';
-import { isBeregnetPeriode } from '@utils/typeguards';
+import dayjs from 'dayjs';
+
+import { useIsReadOnlyOppgave } from '@hooks/useIsReadOnlyOppgave';
+import { Arbeidsgiver, BeregnetPeriode, Maybe, Periodetilstand } from '@io/graphql';
+import {
+    useArbeidsgiver,
+    useErAktivPeriodeLikEllerFørPeriodeTilGodkjenning,
+    usePeriodForSkjæringstidspunkt,
+    usePeriodForSkjæringstidspunktForArbeidsgiver,
+} from '@state/arbeidsgiver';
+import { useCurrentPerson } from '@state/person';
+import { isForkastet } from '@state/selectors/period';
+import { overstyrInntektEnabled } from '@utils/featureToggles';
+import { isBeregnetPeriode, isGhostPeriode } from '@utils/typeguards';
+
+import { BegrunnelseForOverstyring } from '../overstyring/overstyring.types';
 
 export const harIngenUtbetaltePerioderFor = (person: FetchedPerson, skjæringstidspunkt: DateString): boolean => {
     return (
@@ -28,3 +42,153 @@ export const harPeriodeTilBeslutterFor = (person: FetchedPerson, skjæringstidsp
         ).some((it) => it.totrinnsvurdering?.erBeslutteroppgave) ?? false
     );
 };
+
+export const useGhostInntektKanOverstyres = (skjæringstidspunkt: DateString, organisasjonsnummer: string): boolean => {
+    const person = useCurrentPerson();
+    const period = usePeriodForSkjæringstidspunktForArbeidsgiver(skjæringstidspunkt, organisasjonsnummer);
+    const erAktivPeriodeLikEllerFørPeriodeTilGodkjenning = useErAktivPeriodeLikEllerFørPeriodeTilGodkjenning();
+
+    if (!isGhostPeriode(period) || !person) {
+        return false;
+    }
+
+    const periodeTilGodkjenning = maybePeriodeTilGodkjenning(person, period.skjaeringstidspunkt);
+
+    const harUtbetaltePerioder = !harIngenUtbetaltePerioderFor(person, period.skjaeringstidspunkt);
+
+    const harPeriodeTilBeslutter = harPeriodeTilBeslutterFor(person, period.skjaeringstidspunkt);
+
+    return (
+        (harUtbetaltePerioder || periodeTilGodkjenning !== null) &&
+        !harPeriodeTilBeslutter &&
+        erAktivPeriodeLikEllerFørPeriodeTilGodkjenning
+    );
+};
+
+export const maybePeriodeTilGodkjenning = (
+    person: FetchedPerson,
+    skjæringstidspunkt: DateString,
+): Maybe<BeregnetPeriode> => {
+    return (
+        (
+            person?.arbeidsgivere
+                .flatMap((it) => it.generasjoner[0]?.perioder)
+                .filter(isBeregnetPeriode) as unknown as Array<BeregnetPeriode>
+        ).find(
+            (it) =>
+                it.periodetilstand === Periodetilstand.TilGodkjenning && it.skjaeringstidspunkt === skjæringstidspunkt,
+        ) ?? null
+    );
+};
+
+export const useArbeidsforholdKanOverstyres = (
+    skjæringstidspunkt: DateString,
+    organisasjonsnummer: string,
+): boolean => {
+    const person = useCurrentPerson();
+    const period = usePeriodForSkjæringstidspunktForArbeidsgiver(skjæringstidspunkt, organisasjonsnummer);
+    const erAktivPeriodeLikEllerFørPeriodeTilGodkjenning = useErAktivPeriodeLikEllerFørPeriodeTilGodkjenning();
+    const arbeidsgiver = useArbeidsgiver(organisasjonsnummer);
+
+    if (!isGhostPeriode(period) || !person || !arbeidsgiver) {
+        return false;
+    }
+
+    const periodeForSkjæringstidspunkt = maybePeriodeForSkjæringstidspunkt(person, period.skjaeringstidspunkt);
+    const harPeriodeTilBeslutter = harPeriodeTilBeslutterFor(person, period.skjaeringstidspunkt);
+    const arbeidsgiverHarIngenBeregnedePerioder = harIngenBeregnedePerioder(arbeidsgiver, skjæringstidspunkt);
+    const arbeidsgiverHarIngenEtterfølgendePerioder = harIngenEtterfølgendePerioder(
+        arbeidsgiver,
+        skjæringstidspunkt,
+        period.fom,
+    );
+
+    return (
+        arbeidsgiverHarIngenBeregnedePerioder &&
+        arbeidsgiverHarIngenEtterfølgendePerioder &&
+        !harPeriodeTilBeslutter &&
+        periodeForSkjæringstidspunkt !== null &&
+        erAktivPeriodeLikEllerFørPeriodeTilGodkjenning
+    );
+};
+
+const maybePeriodeForSkjæringstidspunkt = (
+    person: FetchedPerson,
+    skjæringstidspunkt: DateString,
+): Maybe<BeregnetPeriode> => {
+    return (
+        (
+            person?.arbeidsgivere
+                .flatMap((it) => it.generasjoner[0]?.perioder)
+                .filter(isBeregnetPeriode) as unknown as Array<BeregnetPeriode>
+        ).find((it) => it.skjaeringstidspunkt === skjæringstidspunkt) ?? null
+    );
+};
+
+const harIngenBeregnedePerioder = (arbeidsgiver: Arbeidsgiver, skjæringstidspunkt: DateString): boolean =>
+    (
+        arbeidsgiver?.generasjoner[0]?.perioder.filter(
+            (it) => it.skjaeringstidspunkt === skjæringstidspunkt && isBeregnetPeriode(it),
+        ) ?? []
+    ).length === 0;
+
+const harIngenEtterfølgendePerioder = (
+    arbeidsgiver: Arbeidsgiver,
+    skjæringstidspunkt: DateString,
+    fom: DateString,
+): boolean =>
+    (
+        arbeidsgiver?.generasjoner[0]?.perioder.filter(
+            (it) => it.skjaeringstidspunkt === skjæringstidspunkt && dayjs(it.fom).isSameOrAfter(fom),
+        ) ?? []
+    ).length === 0;
+
+export const useInntektKanRevurderes = (skjæringstidspunkt: DateString): boolean => {
+    const person = useCurrentPerson();
+    const periodeVedSkjæringstidspunkt = usePeriodForSkjæringstidspunkt(skjæringstidspunkt);
+    const isReadOnlyOppgave = useIsReadOnlyOppgave();
+    const erAktivPeriodeLikEllerFørPeriodeTilGodkjenning = useErAktivPeriodeLikEllerFørPeriodeTilGodkjenning();
+
+    if (!person) return false;
+
+    const harPeriodeTilBeslutter = harPeriodeTilBeslutterFor(person, skjæringstidspunkt);
+
+    return (
+        overstyrInntektEnabled &&
+        !isForkastet(periodeVedSkjæringstidspunkt) &&
+        !isReadOnlyOppgave &&
+        !harPeriodeTilBeslutter &&
+        erAktivPeriodeLikEllerFørPeriodeTilGodkjenning
+    );
+};
+
+export const endreInntektUtenSykefraværBegrunnelser: BegrunnelseForOverstyring[] = [
+    {
+        id: '0',
+        forklaring: 'Arbeidsforhold har vart kortere enn 3 måneder',
+        subsumsjon: { paragraf: '8-28', ledd: '3', bokstav: 'b' },
+    },
+    {
+        id: '1',
+        forklaring: 'Varig lønnsendring',
+        subsumsjon: { paragraf: '8-28', ledd: '3', bokstav: 'c' },
+    },
+    {
+        id: '2',
+        forklaring: 'Innrapportert feil inntekt til A-ordningen',
+        subsumsjon: { paragraf: '8-28', ledd: '5' },
+    },
+    {
+        id: '3',
+        forklaring: 'Annen kilde til endring',
+        subsumsjon: { paragraf: '8-28' },
+    },
+];
+
+export const endreInntektMedSykefraværBegrunnelser: BegrunnelseForOverstyring[] = [
+    { id: '0', forklaring: 'Korrigert inntekt i inntektsmelding' },
+    { id: '1', forklaring: 'Tariffendring i inntektsmelding' },
+    { id: '2', forklaring: 'Innrapportert feil inntekt til A-ordningen' },
+    { id: '3', forklaring: 'Endring/opphør av refusjon' },
+    { id: '4', forklaring: 'Annen kilde til endring' },
+];
