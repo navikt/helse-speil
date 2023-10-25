ea607c4f9111ebad9a1f419a6148e4d456986409
Elias Andreassen Thøgersen <elias3004@msn.com>
2023-10-25 15:24:32 +0200

    Revert "🐛 Gjør så useRefreshPersonVedOpptegnelse ikke henter tidligere personer"
    This reverts commit 047126d1619418056f22c3a1a6f6a09894ae3dcd.

--- packages/frontend/hooks/useRefreshPersonVedOpptegnelse.ts
+++ packages/frontend/hooks/useRefreshPersonVedOpptegnelse.ts
@@ -1,19 +1,22 @@
 import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 
+import { FetchPersonDocument } from '@io/graphql';
 import { nyesteOpptegnelserState } from '@state/opptegnelser';
 import { useFetchPersonQuery } from '@state/person';
+
+import { client } from '../routes/apolloClient';
 
 const personHarFåttOpptegnelse = (opptegnelser: Opptegnelse[], valgtAktørId: string): boolean =>
     opptegnelser.some((opptegnelse) => opptegnelse.aktørId.toString() === valgtAktørId);
 
 export const useRefreshPersonVedOpptegnelse = () => {
     const opptegnelser = useRecoilValue(nyesteOpptegnelserState);
-    const { data, refetch } = useFetchPersonQuery();
+    const { data } = useFetchPersonQuery();
 
     useEffect(() => {
         if (data !== undefined && data.person?.aktorId && personHarFåttOpptegnelse(opptegnelser, data.person.aktorId)) {
-            void refetch();
+            client.refetchQueries({ include: [FetchPersonDocument] });
         }
     }, [opptegnelser]);
 };
