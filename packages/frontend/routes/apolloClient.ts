9ef1420794768ba7a8acd482e3032815d432b4dd
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-10-31 22:46:53 +0100

    ➖ Vekk med cross-fetch
    Den trengs bare for eldre browsere eller når man kjører på serveren.
    
    Fjernet den opprinnelig fordi jeg trodde den forårsaket warning om "the
    `punycode` module is deprecated" - det viste seg at "løsningen" var å
    downgrade node til v20, men jeg ser ikke noen grunn til ikke å fjerne en
    unødvendig dependency 😄

--- packages/frontend/routes/apolloClient.ts
+++ packages/frontend/routes/apolloClient.ts
@@ -1,12 +1,10 @@
-import fetch from 'cross-fetch';
-
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import possibletypes from '@io/graphql/generated/possibletypes';
 
 const baseUrlGraphQL = (process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '') + '/graphql';
 
 export const client = new ApolloClient({
-    link: new HttpLink({ uri: baseUrlGraphQL, fetch }),
+    link: new HttpLink({ uri: baseUrlGraphQL }),
     cache: new InMemoryCache({
         dataIdFromObject: () => undefined,
         possibleTypes: possibletypes.possibleTypes,
