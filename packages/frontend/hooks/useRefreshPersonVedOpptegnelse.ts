a939946cb538daacbadb4c5de979e656c13973fd
Martin Solheim <martin.solheim@me.com>
2023-11-09 16:10:18 +0100

    🐛 Forsøker å være enda litt rundere i vilkårsgrunnlagkantene
    Ikke krev at det finnes et vilkårsgrunnlag i VenstremenyBeregnetPeriode heller. Her er det ogsåhåndtering av undefined/null fra før.

--- packages/frontend/hooks/useRefreshPersonVedOpptegnelse.ts
+++ packages/frontend/hooks/useRefreshPersonVedOpptegnelse.ts
@@ -1,10 +1,16 @@
+import { NetworkStatus } from '@apollo/client';
 import { useHåndterOpptegnelser } from '@state/opptegnelser';
 import { useFetchPersonQuery } from '@state/person';
 
 export const useRefreshPersonVedOpptegnelse = () => {
-    const { data, refetch } = useFetchPersonQuery();
+    const { data, networkStatus, refetch } = useFetchPersonQuery();
     useHåndterOpptegnelser((opptegnelse) => {
-        if (data !== undefined && data.person?.aktorId && opptegnelse.aktørId.toString() === data.person.aktorId) {
+        if (
+            data !== undefined &&
+            data.person?.aktorId &&
+            opptegnelse.aktørId.toString() === data.person.aktorId &&
+            !(networkStatus in [NetworkStatus.loading, NetworkStatus.refetch])
+        ) {
             void refetch();
         }
     });
