b7c79996797aaecf514d750c3b841463269151ae
Øyvind Hagen <oyvind.hagen@nav.no>
2023-11-13 16:00:25 +0100

    🐛 Fjerner duplikat varsel
    Det som skal vises kommer fra Saksbildevarsler.
    
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- packages/frontend/routes/saksbilde/saksbilder/UberegnetPeriodeView.tsx
+++ packages/frontend/routes/saksbilde/saksbilder/UberegnetPeriodeView.tsx
@@ -1,10 +1,9 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { Alert, Loader } from '@navikt/ds-react';
+import { Loader } from '@navikt/ds-react';
 
-import { Dag, Sykdomsdagtype, UberegnetPeriode } from '@io/graphql';
-import { isNotReady } from '@state/selectors/period';
+import { UberegnetPeriode } from '@io/graphql';
 import { onLazyLoadFail } from '@utils/error';
 import { getPeriodState } from '@utils/mapping';
 
@@ -19,55 +18,6 @@ const Utbetaling = React.lazy(() =>
     import('../utbetaling/Utbetaling.js').then((res) => ({ default: res.Utbetaling })).catch(onLazyLoadFail),
 );
 
-const containsOnly = (days: Array<Dag>, ...dayTypes: Array<Sykdomsdagtype>): boolean => {
-    const weekends = [Sykdomsdagtype.SykHelgedag, Sykdomsdagtype.FriskHelgedag];
-    return days.every((dag) => [...dayTypes, ...weekends].includes(dag.sykdomsdagtype));
-};
-
-const containsPayment = (days: Array<Dag>): boolean => {
-    return days.some((day) => typeof day.utbetalingsinfo?.utbetaling === 'number');
-};
-
-const getErrorMessage = (period: UberegnetPeriode): ReactNode => {
-    if (isNotReady(period)) {
-        return (
-            <Alert className={styles.Varsel} variant="info">
-                Perioden har ingen utbetaling og er ikke beregnet.
-            </Alert>
-        );
-    }
-
-    if (containsOnly(period.tidslinje, Sykdomsdagtype.Feriedag)) {
-        return (
-            <Alert className={styles.Varsel} variant="info">
-                Perioden inneholder kun ferie og er ikke beregnet.
-            </Alert>
-        );
-    }
-
-    if (containsOnly(period.tidslinje, Sykdomsdagtype.Permisjonsdag)) {
-        return (
-            <Alert className={styles.Varsel} variant="info">
-                Perioden inneholder kun permisjon og er ikke beregnet.
-            </Alert>
-        );
-    }
-
-    if (!containsPayment(period.tidslinje)) {
-        return (
-            <Alert className={styles.Varsel} variant="info">
-                Perioden har ingen utbetaling og er ikke beregnet.
-            </Alert>
-        );
-    }
-
-    return (
-        <Alert className={styles.Varsel} variant="info">
-            Perioden er ikke beregnet.
-        </Alert>
-    );
-};
-
 const UberegnetPeriodeViewLoader: React.FC = () => {
     return (
         <div className={styles.Skeleton}>
@@ -80,27 +30,22 @@ interface UberegnetPeriodeViewProps {
     activePeriod: UberegnetPeriode;
 }
 
-export const UberegnetPeriodeView = ({ activePeriod }: UberegnetPeriodeViewProps) => {
-    const errorMelding = getErrorMessage(activePeriod);
-
-    return (
-        <>
-            <Venstremeny />
-            <div className={styles.Content}>
-                <Saksbildevarsler periodState={getPeriodState(activePeriod)} varsler={activePeriod.varsler} />
-                <SaksbildeMenu />
-                {errorMelding}
-                <div className={styles.RouteContainer}>
-                    <React.Suspense fallback={<UberegnetPeriodeViewLoader />}>
-                        <Routes>
-                            <Route path="dagoversikt" element={<Utbetaling />} />
-                        </Routes>
-                    </React.Suspense>
-                </div>
+export const UberegnetPeriodeView = ({ activePeriod }: UberegnetPeriodeViewProps) => (
+    <>
+        <Venstremeny />
+        <div className={styles.Content}>
+            <Saksbildevarsler periodState={getPeriodState(activePeriod)} varsler={activePeriod.varsler} />
+            <SaksbildeMenu />
+            <div className={styles.RouteContainer}>
+                <React.Suspense fallback={<UberegnetPeriodeViewLoader />}>
+                    <Routes>
+                        <Route path="dagoversikt" element={<Utbetaling />} />
+                    </Routes>
+                </React.Suspense>
             </div>
-            <Historikk />
-        </>
-    );
-};
+        </div>
+        <Historikk />
+    </>
+);
 
 export default UberegnetPeriodeView;
