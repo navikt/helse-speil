b54bebc617666e16380463e01319cead35c5c366
Martin Solheim <martin.solheim@me.com>
2023-11-13 08:35:54 +0100

    🐛 Polling kjører ikke onCompleted for hver response
    Håndterer nye opptegnelser i useEffect

--- packages/frontend/io/http/polling.ts
+++ packages/frontend/io/http/polling.ts
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { useQuery } from '@apollo/client';
 import { OpptegnelserDocument, Opptegnelsetype } from '@io/graphql';
 import { useMottaOpptegnelser, useNyesteOpptegnelseSekvens, useOpptegnelserPollingRate } from '@state/opptegnelser';
@@ -6,25 +8,28 @@ export const usePollEtterOpptegnelser = () => {
     const mottaOpptegnelser = useMottaOpptegnelser();
     const sekvensId = useNyesteOpptegnelseSekvens();
     const pollInterval = useOpptegnelserPollingRate();
-    useQuery(OpptegnelserDocument, {
+    const { data } = useQuery(OpptegnelserDocument, {
         variables: {
             sekvensId,
         },
         pollInterval,
-        onCompleted: (data) => {
-            mottaOpptegnelser(
-                data.opptegnelser.map((opptegnelse) => ({
-                    sekvensnummer: opptegnelse.sekvensnummer,
-                    type: tilOpptegnelsetype(opptegnelse.type),
-                    aktørId: Number.parseInt(opptegnelse.aktorId),
-                    payload: opptegnelse.payload,
-                })),
-            );
-        },
         onError: (error) => {
             console.error(error);
         },
     });
+
+    useEffect(() => {
+        const opptegnelser = data?.opptegnelser ?? [];
+        if (opptegnelser.length === 0) return;
+        mottaOpptegnelser(
+            opptegnelser.map((opptegnelse) => ({
+                sekvensnummer: opptegnelse.sekvensnummer,
+                type: tilOpptegnelsetype(opptegnelse.type),
+                aktørId: Number.parseInt(opptegnelse.aktorId),
+                payload: opptegnelse.payload,
+            })),
+        );
+    }, [data]);
 };
 
 const tilOpptegnelsetype = (opptegnelsetype: Opptegnelsetype): OpptegnelseType => {
