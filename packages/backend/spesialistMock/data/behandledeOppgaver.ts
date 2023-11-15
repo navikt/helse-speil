2a0dc9b0f61eca9abf22ac2768ec5048aa018055
Elias Andreassen Thøgersen <elias3004@msn.com>
2023-11-15 17:03:11 +0100

    🤡 Legger tilbake behandlede oppgaver i mock som ble fjernet ved en feil

--- packages/backend/spesialistMock/data/behandledeOppgaver.ts
+++ packages/backend/spesialistMock/data/behandledeOppgaver.ts
@@ -1,3 +1,24 @@
-import { BehandletOppgave } from '../schemaTypes';
+import { AntallArbeidsforhold, BehandletOppgave, Oppgavetype, Periodetype } from '../schemaTypes';
 
-export const behandledeOppgaver: BehandletOppgave[] = [];
+export const behandledeOppgaver: BehandletOppgave[] = [
+    {
+        id: '4347',
+        aktorId: '2564094783926',
+        ferdigstiltAv: 'Utvikler, Lokal',
+        ferdigstiltTidspunkt: '2022-09-12T08:54:18.301013',
+        antallArbeidsforhold: AntallArbeidsforhold.FlereArbeidsforhold,
+        periodetype: Periodetype.Forstegangsbehandling,
+        oppgavetype: Oppgavetype.Soknad,
+        personnavn: { fornavn: 'Aggressiv', mellomnavn: null, etternavn: 'Agurk' },
+    },
+    {
+        id: '4348',
+        aktorId: '2564094783926',
+        ferdigstiltAv: 'Saksbehandler, Annen',
+        ferdigstiltTidspunkt: '2022-09-12T09:41:55.590219',
+        antallArbeidsforhold: AntallArbeidsforhold.EtArbeidsforhold,
+        periodetype: Periodetype.Forstegangsbehandling,
+        oppgavetype: Oppgavetype.Revurdering,
+        personnavn: { fornavn: 'Passiv', mellomnavn: null, etternavn: 'Aubergine' },
+    },
+];
