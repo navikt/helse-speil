63534eac6c71875bc3d7167eb7060bafb6d4cf51
Jonas Hasle <hei@jonashasle.no>
2024-02-08 09:43:46 +0100

    ♻️ Bruker CSS-modules i Varselseksjon
    - fjerner styled components

--- packages/frontend/routes/saksbilde/varsler/Varselseksjon.tsx
+++ packages/frontend/routes/saksbilde/varsler/Varselseksjon.tsx
@@ -1,32 +1,18 @@
-import styled from '@emotion/styled';
 import React, { ReactNode } from 'react';
 
 import { BodyLong } from '@navikt/ds-react';
 
-import { FlexColumn } from '@components/Flex';
-
-const Container = styled(FlexColumn)`
-    padding: 0.5rem 0;
-    max-width: 120ch;
-
-    &:not(:last-of-type) {
-        margin-bottom: 1rem;
-    }
-
-    white-space: pre-wrap;
-`;
-
-const Tittel = styled(BodyLong)`
-    font-weight: 600;
-`;
+import styles from './Varselseksjon.module.css';
 
 interface VarselseksjonProps extends ChildrenProps {
     tittel: ReactNode;
 }
 
 export const Varselseksjon: React.FC<VarselseksjonProps> = ({ tittel, children }) => (
-    <Container>
-        <Tittel as="p">{tittel}</Tittel>
+    <div className={styles.container}>
+        <BodyLong as="p" className={styles.tittel}>
+            {tittel}
+        </BodyLong>
         <BodyLong>{children}</BodyLong>
-    </Container>
+    </div>
 );
