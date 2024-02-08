a81fb5e0872fa204a02c47d486cb853334e44969
Jonas Hasle <hei@jonashasle.no>
2024-02-08 13:57:06 +0100

    ♻️ Bruker CSS-modules for komponent DateCell

--- packages/frontend/routes/saksbilde/utbetaling/utbetalingstabell/DateCell.tsx
+++ packages/frontend/routes/saksbilde/utbetaling/utbetalingstabell/DateCell.tsx
@@ -1,20 +1,8 @@
-import styled from '@emotion/styled';
 import React from 'react';
 
 import { getFormattedDateString } from '@utils/date';
 
-const Cell = styled.td`
-    &:after {
-        content: '';
-        position: absolute;
-        background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='8' cy='8' r='6.1' stroke='%233E3832' stroke-width='3' stroke-dasharray='1.6'/%3E%3C/svg%3E%0A");
-        height: 16px;
-        width: 16px;
-        left: 8px;
-        top: 50%;
-        transform: translateY(-50%);
-    }
-`;
+import styles from './DateCell.module.css';
 
 interface DateCellProps {
     date: DateString;
@@ -22,4 +10,8 @@ interface DateCellProps {
 }
 
 export const DateCell = ({ date, erOverstyrt }: DateCellProps) =>
-    erOverstyrt ? <Cell>{getFormattedDateString(date)}</Cell> : <td>{getFormattedDateString(date)}</td>;
+    erOverstyrt ? (
+        <td className={styles.datecell}>{getFormattedDateString(date)}</td>
+    ) : (
+        <td>{getFormattedDateString(date)}</td>
+    );
