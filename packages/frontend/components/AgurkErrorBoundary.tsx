19fcb6236369fcc7d8c10df55e2837fe0d1ff19b
Jonas Hasle <hei@jonashasle.no>
2024-02-09 09:22:25 +0100

    ♻️ Bruker CSS-modules i AgurkErrorBoundary

--- packages/frontend/components/AgurkErrorBoundary.tsx
+++ packages/frontend/components/AgurkErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled';
+import classNames from 'classnames';
 import React, { useRef, useState } from 'react';
 
 import { BodyShort } from '@navikt/ds-react';
@@ -6,40 +6,7 @@ import { BodyShort } from '@navikt/ds-react';
 import { ErrorBoundary } from './ErrorBoundary';
 import { Clipboard } from './clipboard';
 
-const Container = styled.div`
-    margin: 2rem;
-
-    > *:not(:last-child) {
-        margin-bottom: 2.5rem;
-    }
-`;
-const Agurktekst = styled(BodyShort)`
-    display: flex;
-    align-items: center;
-
-    &:before {
-        content: '🥒';
-        font-size: 30px;
-        margin-right: 0.5rem;
-    }
-`;
-const Utviklermelding = styled.span`
-    display: flex;
-    align-items: center;
-    color: var(--a-text-default);
-
-    :hover .Clipboard__children {
-        border-bottom: none;
-    }
-`;
-const Feilmelding = styled.p`
-    display: block;
-    white-space: pre;
-    margin: 1em 0;
-    background-color: var(--a-surface-danger-subtle);
-    color: var(--a-text-danger);
-    padding: 1rem;
-`;
+import styles from './AgurkErrorBoundary.module.css';
 
 interface ErrorInnholdProps {
     errormelding?: string;
@@ -49,20 +16,20 @@ interface ErrorInnholdProps {
 const ErrorInnhold: React.FC<ErrorInnholdProps> = ({ sidenavn, errormelding }) => {
     const ref = useRef<HTMLParagraphElement>(null);
     return (
-        <Container>
-            <Agurktekst as="p">
+        <div className={styles.container}>
+            <BodyShort as="p" className={styles.agurktekst}>
                 Det har dessverre oppstått en feil, og for denne perioden kan ikke {sidenavn ?? 'siden'} vises.
-            </Agurktekst>
+            </BodyShort>
             <div>
-                <Utviklermelding>
+                <span className={styles.utviklermelding}>
                     Feilmelding til utviklere (<Clipboard copySource={ref}>trykk på ikonet for å kopiere:</Clipboard>
                     ):
-                </Utviklermelding>
-                <Feilmelding className="typo-undertekst" ref={ref}>
+                </span>
+                <p className={classNames(styles.feilmelding, 'typo-undertekst')} ref={ref}>
                     {errormelding}
-                </Feilmelding>
+                </p>
             </div>
-        </Container>
+        </div>
     );
 };
 
