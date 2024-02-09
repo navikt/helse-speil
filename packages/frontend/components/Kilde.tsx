e74fda673a1e6da3561cfc46002e6fcae6e0189b
Jonas Hasle <hei@jonashasle.no>
2024-02-09 13:17:53 +0100

    ♻️ Bruker SCSS-modules i Kilde

--- packages/frontend/components/Kilde.tsx
+++ packages/frontend/components/Kilde.tsx
@@ -1,5 +1,5 @@
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
+import styles from './Kilde.module.scss';
+import classNames from 'classnames';
 import React from 'react';
 
 import { Tooltip } from '@navikt/ds-react';
@@ -14,52 +14,25 @@ interface KildeProps {
 
 type KildeikonType = Kildetype | Inntektskilde | 'AINNTEKT' | undefined;
 
-const ainntektStyle = (props: KildeProps) =>
-    props.type === 'AINNTEKT' &&
-    css`
-        background-color: var(--a-deepblue-100);
-        border-color: var(--a-deepblue-500);
-    `;
-
-const aordningenStyle = (props: KildeProps) =>
-    props.type === Inntektskilde.Aordningen &&
-    css`
-        background-color: var(--a-green-100);
-        border-color: var(--a-green-500);
-    `;
-
-const sykmeldingStyle = (props: KildeProps) =>
-    props.type === Kildetype.Sykmelding &&
-    css`
-        background-color: var(--a-lightblue-100);
-        border-color: var(--a-blue-500);
-    `;
-
-const søknadStyle = (props: KildeProps) =>
-    props.type === Kildetype.Soknad &&
-    css`
-        background-color: var(--a-purple-100);
-        border-color: var(--a-purple-400);
-    `;
-
-const inntektsmeldingStyle = (props: KildeProps) =>
-    props.type === Kildetype.Inntektsmelding &&
-    css`
-        background-color: var(--a-limegreen-100);
-        border-color: var(--a-orange-600);
-    `;
-
-const saksbehandlerStyle = (props: KildeProps) =>
-    (props.type === Kildetype.Saksbehandler || props.type === Inntektskilde.SkjonnsmessigFastsatt) &&
-    css`
-        background-color: var(--a-gray-100);
-        color: var(--a-text-default);
-        border-color: var(--a-gray-700);
-        svg {
-            width: 12px;
-            height: 12px;
-        }
-    `;
+const finnCSSklasse = (type: KildeikonType) => {
+    switch (type) {
+        case 'AINNTEKT':
+            return 'ainntekt';
+        case Inntektskilde.Aordningen:
+            return 'aordningen';
+        case Kildetype.Sykmelding:
+            return 'sykmelding';
+        case Kildetype.Soknad:
+            return 'søknad';
+        case Kildetype.Inntektsmelding:
+            return 'inntektsmelding';
+        case Kildetype.Saksbehandler:
+        case Inntektskilde.SkjonnsmessigFastsatt:
+            return 'saksbehandler';
+        default:
+            return '';
+    }
+};
 
 const getKildeTypeTooltip = (kilde: KildeikonType): string => {
     switch (kilde) {
@@ -87,32 +60,6 @@ const getKildeTypeTooltip = (kilde: KildeikonType): string => {
     }
 };
 
-const Kildeikon = styled.div<KildeProps>`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 12px;
-    border-radius: 3px;
-    border-style: solid;
-    border-width: 1px;
-    background-color: transparent;
-    color: var(--a-text-default);
-    width: 1.25rem;
-    height: 1rem;
-    cursor: default;
-
-    .Tekst {
-        letter-spacing: 0.5px;
-    }
-
-    ${ainntektStyle};
-    ${aordningenStyle};
-    ${sykmeldingStyle};
-    ${søknadStyle};
-    ${inntektsmeldingStyle};
-    ${saksbehandlerStyle};
-`;
-
 const erTekst = (kilde: KildeikonType): boolean =>
     kilde !== Kildetype.Saksbehandler &&
     kilde !== Inntektskilde.Saksbehandler &&
@@ -121,9 +68,9 @@ const erTekst = (kilde: KildeikonType): boolean =>
 export const Kilde = ({ type, children, className }: KildeProps) => {
     return (
         <Tooltip content={getKildeTypeTooltip(type)}>
-            <Kildeikon className={className} type={type}>
-                {erTekst(type) ? <div className="Tekst">{children}</div> : children}
-            </Kildeikon>
+            <div className={classNames(styles.kildeikon, styles[`kildeikon__${finnCSSklasse(type)}`], className)}>
+                {erTekst(type) ? <div className={styles.tekst}>{children}</div> : children}
+            </div>
         </Tooltip>
     );
 };
