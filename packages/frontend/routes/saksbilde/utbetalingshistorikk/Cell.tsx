66f04038ef6475ac983748e6296b64d2771bed99
Jonas Hasle <hei@jonashasle.no>
2024-02-09 15:08:44 +0100

    ⚰️ Cell

--- packages/frontend/routes/saksbilde/utbetalingshistorikk/Cell.tsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-import React from 'react';
-
-const Container = styled.td`
-    padding: 0.5rem 0.75rem;
-`;
-
-export const Cell: React.FC<React.HTMLAttributes<HTMLTableCellElement>> = (props) => <Container {...props} />;
