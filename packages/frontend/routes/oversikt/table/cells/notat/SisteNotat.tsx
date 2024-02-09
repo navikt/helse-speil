2473f933a9cb1c58951303f199f085b2bbf1b59e
Jonas Hasle <hei@jonashasle.no>
2024-02-09 09:33:05 +0100

    ♻️ Bruker CSS-modules i SisteNotat

--- packages/frontend/routes/oversikt/table/cells/notat/SisteNotat.tsx
+++ packages/frontend/routes/oversikt/table/cells/notat/SisteNotat.tsx
@@ -1,22 +1,17 @@
-import styled from '@emotion/styled';
 import React from 'react';
 
 import { BodyShort } from '@navikt/ds-react';
 
 import { NORSK_DATOFORMAT_MED_KLOKKESLETT } from '@utils/date';
 
-const NotatTekst = styled(BodyShort)`
-    font-size: 14px;
-    color: var(--a-gray-800);
-    white-space: pre-wrap;
-`;
+import styles from './SisteNotat.module.css';
 
 interface SisteNotatProps {
     notat: Notat;
 }
 
 export const SisteNotat: React.FC<SisteNotatProps> = ({ notat }) => (
-    <NotatTekst as="p">
+    <BodyShort as="p" className={styles.siste}>
         {notat.opprettet.format(NORSK_DATOFORMAT_MED_KLOKKESLETT)} : {notat.tekst}
-    </NotatTekst>
+    </BodyShort>
 );
