436ebc0d5f12c9ee97463becee667e2400d838a2
Jonas Hasle <hei@jonashasle.no>
2024-02-09 13:30:48 +0100

    ♻️ Bruker SCSS-modules i PopoverHjelpetekst

--- packages/frontend/components/PopoverHjelpetekst.tsx
+++ packages/frontend/components/PopoverHjelpetekst.tsx
@@ -1,25 +1,7 @@
-import styled from '@emotion/styled';
+import styles from './PopoverHjelpetekst.module.scss';
 import React, { ReactNode, useRef, useState } from 'react';
 
 import { Popover, PopoverProps } from '@navikt/ds-react';
-
-const StyledPopover = styled(Popover)`
-    padding: 1rem 0.5rem;
-    border: 1px solid #6a6a6a;
-    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.25);
-    border-radius: 2px;
-    width: max-content;
-
-    .popover__pil {
-        border-right: 1px solid #6a6a6a;
-        border-bottom: 1px solid #6a6a6a;
-        box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
-    }
-`;
-
-const Ikon = styled.div`
-    cursor: pointer;
-`;
 
 interface PopoverHjelpetekstProps extends Pick<PopoverProps, 'offset'>, React.HTMLAttributes<HTMLDivElement> {
     ikon: ReactNode;
@@ -33,17 +15,25 @@ export const PopoverHjelpetekst: React.FC<PopoverHjelpetekstProps> = ({ children
 
     return (
         <div {...divProps}>
-            <Ikon
+            <div
                 ref={ref}
                 onClick={() => {
                     anchor ? setAnchor(null) : setAnchor(ref.current);
                 }}
+                className={styles.ikon}
             >
                 {ikon}
-            </Ikon>
-            <StyledPopover open={anchor !== null} anchorEl={anchor} offset={offset} onClose={close} placement="top">
+            </div>
+            <Popover
+                className={styles.popover}
+                open={anchor !== null}
+                anchorEl={anchor}
+                offset={offset}
+                onClose={close}
+                placement="top"
+            >
                 {children}
-            </StyledPopover>
+            </Popover>
         </div>
     );
 };
