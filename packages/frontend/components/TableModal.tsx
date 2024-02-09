bc675624c2e635aab404684034be1f316feb0f50
Jonas Hasle <hei@jonashasle.no>
2024-02-09 12:59:09 +0100

    ♻️ Bruker SCSS-modules i TableModal

--- packages/frontend/components/TableModal.tsx
+++ packages/frontend/components/TableModal.tsx
@@ -1,45 +1,12 @@
-import styled from '@emotion/styled';
+import styles from './TableModal.module.scss';
 import React from 'react';
 
-import { ModalProps, Modal as SpeilModal } from './Modal';
-
-const Table = styled.table`
-    th,
-    td {
-        padding: 0.5rem;
-
-        &:not(:last-of-type) {
-            padding-right: 1.5rem;
-        }
-    }
-
-    th {
-        text-align: left;
-        font-weight: 400;
-        font-size: 14px;
-        color: var(--a-gray-800);
-    }
-
-    tbody > tr:nth-of-type(2n-1) > td {
-        background-color: var(--a-gray-100);
-    }
-`;
-
-const Modal = styled(SpeilModal)`
-    width: max-content;
-    padding: 18px 24px 24px;
-
-    header {
-        font-size: 16px;
-        font-weight: 600;
-        margin-bottom: 24px;
-    }
-`;
+import { Modal, ModalProps } from './Modal';
 
 type TableModalProps = ModalProps;
 
 export const TableModal: React.FC<TableModalProps> = ({ children, ...modalProps }) => (
-    <Modal {...modalProps}>
-        <Table>{children}</Table>
+    <Modal className={styles.tablemodal} {...modalProps}>
+        <table className={styles.table}>{children}</table>
     </Modal>
 );
