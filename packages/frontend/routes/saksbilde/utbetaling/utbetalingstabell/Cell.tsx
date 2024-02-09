7be492ef90131c70c5a11a288331a7db8ccb2c98
Jonas Hasle <hei@jonashasle.no>
2024-02-09 15:53:51 +0100

    🐛 Fikser så overstyrte felt blir italic i utbetalingstabellen

--- packages/frontend/routes/saksbilde/utbetaling/utbetalingstabell/Cell.tsx
+++ packages/frontend/routes/saksbilde/utbetaling/utbetalingstabell/Cell.tsx
@@ -4,10 +4,9 @@ import React from 'react';
 import styles from './Cell.module.css';
 
 interface CellProps extends React.HTMLAttributes<HTMLTableCellElement> {
-    children: React.ReactNode;
     italic?: boolean;
 }
 
-export const Cell: React.FC<CellProps> = (props, italic = false) => (
-    <td {...props} className={classNames(props.className, italic && styles.italic)} />
+export const Cell: React.FC<CellProps> = (props) => (
+    <td className={classNames(props.className, props.italic && styles.italic)}>{props.children}</td>
 );
