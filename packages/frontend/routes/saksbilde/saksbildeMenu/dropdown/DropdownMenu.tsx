fdbb341290b1d9c6b731a2831463515f2c3d3144
Jonas Hasle <hei@jonashasle.no>
2024-02-14 13:08:18 +0100

    ♻️ Bruker SCSS-components i DropdownMenu

--- packages/frontend/routes/saksbilde/saksbildeMenu/dropdown/DropdownMenu.tsx
+++ packages/frontend/routes/saksbilde/saksbildeMenu/dropdown/DropdownMenu.tsx
@@ -1,10 +1,10 @@
+import styles from './DropdownMenu.module.scss';
 import React, { useRef, useState } from 'react';
 
 import { Collapse, Expand } from '@navikt/ds-icons';
 import { Dropdown } from '@navikt/ds-react-internal';
 
 import { LoadingShimmer } from '@components/LoadingShimmer';
-import { TabButton } from '@components/TabButton';
 import { useInteractOutside } from '@hooks/useInteractOutside';
 import { useIsReadOnlyOppgave } from '@hooks/useIsReadOnlyOppgave';
 import { useCurrentArbeidsgiver } from '@state/arbeidsgiver';
@@ -17,8 +17,6 @@ import { AnnullerButton } from './AnnullerButton';
 import { OppdaterPersondataButton } from './OppdaterPersondataButton';
 import { PåVentButton } from './PåVentButton';
 import { TildelingDropdownMenuButton } from './TildelingDropdownMenuButton';
-
-import styles from './DropdownMenu.module.css';
 
 const DropdownMenuContentSkeleton: React.FC = () => {
     return (
@@ -51,7 +49,7 @@ const DropdownMenuContent: React.FC = () => {
     const personIsAssignedUser = (person?.tildeling && person?.tildeling?.oid === user.oid) ?? false;
 
     return (
-        <Dropdown.Menu placement="bottom-start" className={styles.DropdownMenu}>
+        <Dropdown.Menu placement="bottom-start" className={styles.dropdown}>
             {isBeregnetPeriode(period) && period.oppgave?.id && !readOnly && (
                 <>
                     <Dropdown.Menu.List>
@@ -98,9 +96,9 @@ export const DropdownMenu: React.FC = () => {
     return (
         <span ref={content}>
             <Dropdown onSelect={closeDropdown}>
-                <TabButton as={Dropdown.Toggle} className={styles.MenuButton} onClick={toggleDropdown}>
+                <Dropdown.Toggle className={styles.menu} onClick={toggleDropdown}>
                     Meny {open ? <Collapse title="collapse" /> : <Expand title="expand" />}
-                </TabButton>
+                </Dropdown.Toggle>
                 <React.Suspense fallback={<DropdownMenuContentSkeleton />}>
                     <DropdownMenuContent />
                 </React.Suspense>
