0dc747624ca0d22c31b17c6cd31e10d567d5645d
Jonas Hasle <hei@jonashasle.no>
2024-02-14 13:19:28 +0100

    💬 Oppdaterer begrunnelsemalene for skjønnsfastsetting

--- packages/frontend/routes/saksbilde/sykepengegrunnlag/skjønnsfastsetting/skjønnsfastsetting.ts
+++ packages/frontend/routes/saksbilde/sykepengegrunnlag/skjønnsfastsetting/skjønnsfastsetting.ts
@@ -75,21 +75,21 @@ export const skjønnsfastsettelseBegrunnelser = (
 ];
 
 const malEnArbeidsgiver = (omregnetÅrsinntekt = 0, sammenligningsgrunnlag = 0) => {
-    return `Månedsinntekten som er oppgitt av arbeidsgiveren din på ${toKronerOgØre(
+    return `Månedsinntekten som er beregnet for arbeidsforholdet ditt er totalt ${toKronerOgØre(
         omregnetÅrsinntekt / 12,
-    )} kroner utgjør ${toKronerOgØre(
+    )} kroner. Regnet om til årsinntekt blir det ${toKronerOgØre(
         omregnetÅrsinntekt,
-    )} kroner i årsinntekt. Denne årsinntekten avviker med mer enn 25 prosent fra inntekten som er rapportert til Skatteetaten på ${toKronerOgØre(
+    )} kroner. Denne årsinntekten avviker med mer enn 25 prosent fra inntekten som er rapportert til Skatteetaten på ${toKronerOgØre(
         sammenligningsgrunnlag,
     )} kroner de siste tolv månedene før måneden du ble syk.\n\nNår årsinntekten avviker med mer enn 25 prosent fra rapportert inntekt, skal sykepengegrunnlaget fastsettes ved skjønn ut fra den årsinntekten som kan sannsynliggjøres på det tidspunktet du ble syk. Se folketrygdloven § 8-30 andre avsnitt.\n\nNår vi fastsetter sykepengegrunnlaget ditt ved skjønn, legger vi vekt på om avviket skyldes endringer i arbeidssituasjonen din. Målet med den skjønnsmessige vurderingen er å komme frem til inntekten du ville hatt om du ikke hadde blitt syk.`;
 };
 
 const malFlereArbeidsgivere = (omregnetÅrsinntekt = 0, sammenligningsgrunnlag = 0) => {
-    return `Månedsinntekten som er oppgitt av dine arbeidsgivere på ${toKronerOgØre(
+    return `Månedsinntekten som er beregnet for arbeidsforholdene dine er totalt ${toKronerOgØre(
         omregnetÅrsinntekt / 12,
-    )} kroner utgjør ${toKronerOgØre(
+    )} kroner. Regnet om til årsinntekt blir det ${toKronerOgØre(
         omregnetÅrsinntekt,
-    )} kroner i årsinntekt. Denne årsinntekten avviker med mer enn 25 prosent fra inntekten som er rapportert til Skatteetaten på ${toKronerOgØre(
+    )} kroner. Denne årsinntekten avviker med mer enn 25 prosent fra inntekten som er rapportert til Skatteetaten på ${toKronerOgØre(
         sammenligningsgrunnlag,
     )} kroner de siste tolv månedene før måneden du ble syk.\n\nNår årsinntekten avviker med mer enn 25 prosent fra rapportert inntekt, skal sykepengegrunnlaget fastsettes ved skjønn ut fra den årsinntekten som kan sannsynliggjøres på det tidspunktet du ble syk. Se folketrygdloven § 8-30 andre avsnitt.\n\nNår vi fastsetter sykepengegrunnlaget ditt ved skjønn, legger vi vekt på om avviket skyldes endringer i arbeidssituasjonen din. Målet med den skjønnsmessige vurderingen er å komme frem til inntekten du ville hatt om du ikke hadde blitt syk.`;
 };
