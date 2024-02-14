472cb75aacb1d82e2018dc0da79c2bb4bf8d8029
Jonas Hasle <hei@jonashasle.no>
2024-02-14 10:35:44 +0100

    ♻️ Bruker SCSS-components i Row (utbetalingstabell)

--- packages/frontend/routes/saksbilde/table/Row.tsx
+++ packages/frontend/routes/saksbilde/table/Row.tsx
@@ -1,9 +1,10 @@
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
+import styles from './Row.module.scss';
+import classNames from 'classnames';
+import React from 'react';
 
 import { helgetyper } from '../utbetaling/utbetalingstabell/helgUtils';
 
-interface RowProps {
+interface RowProps extends React.HTMLAttributes<HTMLTableRowElement> {
     erAvvist?: boolean;
     erAGP?: boolean;
     type?: Utbetalingstabelldagtype;
@@ -12,73 +13,30 @@ interface RowProps {
     erHelg?: boolean;
 }
 
-const leftAlignedLine = (color: string) => css`
-    > td:first-of-type {
-        position: relative;
-    }
-
-    > td:first-of-type:before {
-        content: '';
-        position: absolute;
-        left: 0;
-        top: 0;
-        background-color: ${color};
-        width: 3px;
-        height: 2rem;
-    }
-`;
-
-const helgStyle = (props: RowProps) =>
-    ((props.type && [...helgetyper, 'Helg'].includes(props.type)) || props.erHelg) &&
-    css`
-        background: repeating-linear-gradient(
-            123deg,
-            var(--speil-light-hover),
-            var(--speil-light-hover) 1px,
-            transparent 1px,
-            transparent 9px
-        );
-    `;
-
-const avvistStyle = (props: RowProps) =>
-    props.erAvvist &&
-    css`
-        background-color: #f9d2cc;
-        ${leftAlignedLine('var(--a-border-danger)')}
-    `;
-
-const arbeidsgiverperiodeStyle = (props: RowProps) =>
-    props.erAGP &&
-    css`
-        background-color: #f8f8f8;
-        ${leftAlignedLine('var(--a-text-subtle)')}
-    `;
-
-const nydagStyle = (props: RowProps) =>
-    props.nyDag &&
-    css`
-        background-color: #fff;
-    `;
-
-export const Row = styled.tr<RowProps>`
-    position: relative;
-
-    > td {
-        position: relative;
-        height: 2rem;
-        padding: 0 1rem;
-        vertical-align: middle;
-        box-sizing: border-box;
-        border-bottom: 1px solid #c6c2bf;
-        background: ${(RowProps) => (RowProps.markertDag ? '#f1f1f1' : 'transparent')};
-
-        &:not(:first-of-type):not(:last-of-type) {
-            padding-right: 1rem;
-        }
-    }
-
-    ${helgStyle};
-    ${avvistStyle};
-    ${arbeidsgiverperiodeStyle};
-    ${nydagStyle};
-`;
+export const Row = ({
+    children,
+    erAvvist = false,
+    erAGP = false,
+    type,
+    markertDag,
+    nyDag = false,
+    erHelg = false,
+    className = '',
+}: RowProps) => {
+    const viseHelgStil = (type && [...helgetyper, 'Helg'].includes(type)) || erHelg;
+    return (
+        <tr
+            className={classNames(
+                styles.row,
+                markertDag && styles.markert,
+                viseHelgStil && styles.helg,
+                erAvvist && styles.avvist,
+                erAGP && styles.agp,
+                nyDag && styles.nydag,
+                className,
+            )}
+        >
+            {children}
+        </tr>
+    );
+};
