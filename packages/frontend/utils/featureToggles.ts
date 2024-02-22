084f31bab2151f57e183bf9baeba360ca4163ddd
Jonas Hasle <hei@jonashasle.no>
2024-02-22 10:00:23 +0100

    🚩 Legger til sanityMaler
    Co-authored-by: Trine Linderud <trine.linderud@nav.no>

--- packages/frontend/utils/featureToggles.ts
+++ packages/frontend/utils/featureToggles.ts
@@ -66,3 +66,5 @@ export const harBeslutterrolle: boolean = extractGroups().includes(groupIdForBes
 export const harSpesialsaktilgang: boolean = extractGroups().includes(groupIdSpesialsaker) || erUtvikling();
 
 export const toggleMeny: boolean = erLocal() || erDev();
+
+export const sanityMaler: boolean = erLocal() || erDev();
