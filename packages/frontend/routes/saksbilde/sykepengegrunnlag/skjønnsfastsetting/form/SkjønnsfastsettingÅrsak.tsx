787eeb4d9090492e1a4b28834dbbd6d6468ff239
Trine Linderud <trinelinderuds@gmail.com>
2024-02-22 10:44:23 +0100

    ✨ Oppdaterer skjønnsfastsettingÅrsak til å hente fra sanity i dev
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>

--- packages/frontend/routes/saksbilde/sykepengegrunnlag/skjønnsfastsetting/form/SkjønnsfastsettingÅrsak.tsx
+++ packages/frontend/routes/saksbilde/sykepengegrunnlag/skjønnsfastsetting/form/SkjønnsfastsettingÅrsak.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
+import { useRecoilValue } from 'recoil';
 
 import { Radio, RadioGroup } from '@navikt/ds-react';
+
+import { sanityMaler } from '@utils/featureToggles';
+
+import { skjønnsfastsettingMaler } from '../state';
 
 import styles from './SkjønnsfastsettingBegrunnelse.module.css';
 
 export const SkjønnsfastsettingÅrsak = () => {
     const { formState, register } = useFormContext();
+    const årsakerFraSanity = useRecoilValue(skjønnsfastsettingMaler).flatMap((it) => it.arsak);
     const { ref, ...årsakValidation } = register('årsak', { required: 'Du må velge en årsak' });
+
+    const årsaker = sanityMaler ? årsakerFraSanity : årsakerGammel;
 
     return (
         <RadioGroup
@@ -25,4 +33,4 @@ export const SkjønnsfastsettingÅrsak = () => {
     );
 };
 
-const årsaker = ['Skjønnsfastsettelse ved mer enn 25 % avvik (§ 8-30, andre avsnitt)'];
+const årsakerGammel = ['Skjønnsfastsettelse ved mer enn 25 % avvik (§ 8-30, andre avsnitt)'];
