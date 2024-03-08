f39954fa6189d3f2e98dbf21807017bbda73bc9b
Trine Linderud <trinelinderuds@gmail.com>
2024-03-08 12:56:21 +0100

    ✨ Bruker Widget og Tilbakemelding for flexjar i saksbildet i dev
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>

--- packages/frontend/routes/saksbilde/Saksbilde.tsx
+++ packages/frontend/routes/saksbilde/Saksbilde.tsx
@@ -4,13 +4,17 @@ import { Route, Routes } from 'react-router-dom';
 import { Alert } from '@navikt/ds-react';
 
 import { ErrorBoundary } from '@components/ErrorBoundary';
+import { Tilbakemelding } from '@components/flexjar/Tilbakemelding';
+import { Widget } from '@components/flexjar/Widget';
 import { useFjernPersonFraApolloCache } from '@hooks/useFjernPersonFraApolloCache';
 import { useRefreshPersonVedOpptegnelse } from '@hooks/useRefreshPersonVedOpptegnelse';
 import { useVarselOmSakErTildeltAnnenSaksbehandler } from '@hooks/useVarselOmSakErTildeltAnnenSaksbehandler';
 import { AmplitudeProvider } from '@io/amplitude';
 import { usePollEtterOpptegnelser } from '@io/http';
 import { useSelectPeriodOnOppgaveChanged } from '@state/periode';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { onLazyLoadFail } from '@utils/error';
+import { flexjar } from '@utils/featureToggles';
 
 import { VenterPåEndringProvider } from './VenterPåEndringContext';
 import { PersonHeader } from './personHeader';
@@ -48,6 +52,7 @@ const SaksbildeContent = () => {
     useSelectPeriodOnOppgaveChanged();
     useVarselOmSakErTildeltAnnenSaksbehandler();
     useKeyboardShortcuts();
+    const queryClient = new QueryClient();
 
     return (
         <div className={styles.Saksbilde}>
@@ -59,6 +64,17 @@ const SaksbildeContent = () => {
                         <Route path="utbetalingshistorikk" element={<Utbetalingshistorikk />} />
                         <Route path="/*" element={<PeriodeView />} />
                     </Routes>
+                    {flexjar && (
+                        <QueryClientProvider client={queryClient}>
+                            <Widget>
+                                <Tilbakemelding
+                                    feedbackId="test"
+                                    tittel="Hjelp oss med å gjøre siden bedre"
+                                    sporsmal="Har du noen forslag til forbedringer?"
+                                />
+                            </Widget>
+                        </QueryClientProvider>
+                    )}
                 </VenterPåEndringProvider>
             </AmplitudeProvider>
         </div>
