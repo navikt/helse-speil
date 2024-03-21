e4e8bf942b7e07b925c51f8a951dc735e260fd8c
Elias Andreassen Thøgersen <elias3004@msn.com>
2024-03-21 09:55:17 +0100

    🥚 Legger til topplinjebilder og skrur på påskegurken

--- packages/frontend/components/header/EasterEgg.tsx
+++ packages/frontend/components/header/EasterEgg.tsx
@@ -1,5 +1,14 @@
+// import julegurken from '../../assets/topplinjebilder/julegurken.svg';
+import påskegurken from '../../assets/topplinjebilder/påskegurken.svg';
+// import sommergurken from '../../assets/topplinjebilder/sommergurken.svg';
 import React from 'react';
 
 import styles from './EasterEgg.module.css';
 
-export const EasterEgg = () => <div className={styles.container}>{/* Putt easter eggs her */}</div>;
+export const EasterEgg = () => (
+    <div className={styles.container}>
+        {/*<img style={{ marginLeft: '1.5rem' }} alt="Julepynt" src={julegurken} />*/}
+        <img style={{ margin: '-4px 0 -5px 1.5rem' }} alt="Påskepynt" src={påskegurken} />
+        {/*<img style={{ marginLeft: '1.5rem' }} alt="Sommerpynt" src={sommergurken} />*/}
+    </div>
+);
