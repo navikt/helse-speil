49cd744891f7aac902aaebf28ec07f47b1e0c97e
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-04-02 09:03:59 +0200

    🥚 Takk for i år, påske!

--- packages/frontend/components/header/EasterEgg.tsx
+++ packages/frontend/components/header/EasterEgg.tsx
@@ -1,5 +1,5 @@
 // import julegurken from '../../assets/topplinjebilder/julegurken.svg';
-import påskegurken from '../../assets/topplinjebilder/påskegurken.svg';
+// import påskegurken from '../../assets/topplinjebilder/påskegurken.svg';
 // import sommergurken from '../../assets/topplinjebilder/sommergurken.svg';
 import React from 'react';
 
@@ -8,7 +8,7 @@ import styles from './EasterEgg.module.css';
 export const EasterEgg = () => (
     <div className={styles.container}>
         {/*<img style={{ marginLeft: '1.5rem' }} alt="Julepynt" src={julegurken} />*/}
-        <img style={{ margin: '-4px 0 -5px 1.5rem' }} alt="Påskepynt" src={påskegurken} />
+        {/*<img style={{ margin: '-4px 0 -5px 1.5rem' }} alt="Påskepynt" src={påskegurken} />*/}
         {/*<img style={{ marginLeft: '1.5rem' }} alt="Sommerpynt" src={sommergurken} />*/}
     </div>
 );
