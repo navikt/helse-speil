f28cc384138392dbabb1d721df9d87534322e26c
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-04-02 09:36:11 +0200

    🥚 Datosjekker for å fjerne easter eggs
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
    Co-authored-by: Solveig Grieg <solveig.grieg@nav.no>
    Co-authored-by: Elias Andreassen Thøgersen <elias.andreassen.thogersen@nav.no>
    Co-authored-by: Øydis Kind Refsum <oydis.kind.refsum@nav.no>
    Co-authored-by: Trine Linderud <trine.linderud@nav.no>
    Co-authored-by: Jørgen Engesvold <jorgen.engesvold@nav.no>

--- packages/frontend/components/header/EasterEgg.tsx
+++ packages/frontend/components/header/EasterEgg.tsx
@@ -1,14 +1,28 @@
-// import julegurken from '../../assets/topplinjebilder/julegurken.svg';
-// import påskegurken from '../../assets/topplinjebilder/påskegurken.svg';
-// import sommergurken from '../../assets/topplinjebilder/sommergurken.svg';
+import julegurken from '../../assets/topplinjebilder/julegurken.svg';
+import påskegurken from '../../assets/topplinjebilder/påskegurken.svg';
+import sommergurken from '../../assets/topplinjebilder/sommergurken.svg';
+import dayjs from 'dayjs';
 import React from 'react';
+
+import { ISO_DATOFORMAT } from '@utils/date';
 
 import styles from './EasterEgg.module.css';
 
 export const EasterEgg = () => (
     <div className={styles.container}>
-        {/*<img style={{ marginLeft: '1.5rem' }} alt="Julepynt" src={julegurken} />*/}
-        {/*<img style={{ margin: '-4px 0 -5px 1.5rem' }} alt="Påskepynt" src={påskegurken} />*/}
-        {/*<img style={{ marginLeft: '1.5rem' }} alt="Sommerpynt" src={sommergurken} />*/}
+        <Julepynt />
+        <Påskepynt />
+        <Sommergurken />
     </div>
 );
+
+const Påskepynt = () =>
+    dayjs() < dayjs('2024-04-02', ISO_DATOFORMAT) ? (
+        <img style={{ margin: '-4px 0 -5px 1.5rem' }} alt="Påskepynt" src={påskegurken} />
+    ) : null;
+
+const Julepynt = () =>
+    dayjs().get('month') == 11 ? <img style={{ marginLeft: '1.5rem' }} alt="Julepynt" src={julegurken} /> : null;
+
+const Sommergurken = () =>
+    dayjs().get('month') == 6 ? <img style={{ marginLeft: '1.5rem' }} alt="Sommerpynt" src={sommergurken} /> : null;
