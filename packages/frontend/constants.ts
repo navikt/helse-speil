5201fbf75561117e57e2fe431a290f0231226a4a
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-05-04 17:19:15 +0200

    🚧 Må bruke wss:// når siden er lastet med https://

--- packages/frontend/constants.ts
+++ packages/frontend/constants.ts
@@ -1,5 +1,4 @@
 const baseUrl: string = import.meta.env.DEV ? 'http://localhost:3000' : '';
-const websocketsUrl: string = import.meta.env.DEV ? 'ws://localhost:3000' : `ws://${location.host}`;
 
 export const BASE_URL = baseUrl;
-export const WEBSOCKETS_URL = websocketsUrl;
+export const WEBSOCKETS_URL = baseUrl.replace(/http/, 'ws');
