f5e3ac80fa15667445a717e219a89ecf2f03620c
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-05-04 13:47:53 +0200

    🚧 Port 3000 gjelder jo bare lokalt og internt i containeren 🤦

--- packages/frontend/constants.ts
+++ packages/frontend/constants.ts
@@ -1,5 +1,5 @@
 const baseUrl: string = import.meta.env.DEV ? 'http://localhost:3000' : '';
-const websocketsUrl: string = import.meta.env.DEV ? 'ws://localhost:3000' : `ws://${window.location.host}:3000`;
+const websocketsUrl: string = import.meta.env.DEV ? 'ws://localhost:3000' : `ws://${location.host}`;
 
 export const BASE_URL = baseUrl;
 export const WEBSOCKETS_URL = websocketsUrl;
