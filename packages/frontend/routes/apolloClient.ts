4594a01d9acaffab35ec022aa3a5cb7b808a6e7c
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-05-23 14:49:04 +0200

    ⚡️ Normalisering av underliggende felter i persondataene
    Det dukket opp en person i prod med over 150 vedtaksperioder, som Speil
    brukte over ti minutter på å rendre, pga. at Apollo Client jobbet
    iherdig med å holde dataene i cachen sin i sync.
    
    Vi tester dette i dev først, for å se om vi har bommet på noe av cache-
    oppsettet.
    
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>
    Co-authored-by: Elias Andreassen Thøgersen <elias.andreassen.thogersen@nav.no>

--- packages/frontend/routes/apolloClient.ts
+++ packages/frontend/routes/apolloClient.ts
@@ -1,47 +1,66 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, TypePolicies } from '@apollo/client';
 import possibletypes from '@io/graphql/generated/possibletypes';
+import { erUtvikling } from '@utils/featureToggles';
 
 import { BASE_URL } from '../constants';
+
+const getTypePolicies = (): TypePolicies => {
+    const prodPolicies: TypePolicies = {
+        Query: {
+            fields: {
+                oppgaveFeed: {
+                    keyArgs: ['filtrering', 'sortering'],
+                    merge(_, incoming) {
+                        const incomingOppgaver = incoming.oppgaver;
+                        return {
+                            oppgaver: incomingOppgaver,
+                            totaltAntallOppgaver: incoming.totaltAntallOppgaver,
+                        };
+                    },
+                },
+                behandledeOppgaverFeed: {
+                    keyArgs: [],
+                    merge(_, incoming) {
+                        const incomingOppgaver = incoming.oppgaver;
+                        return {
+                            oppgaver: incomingOppgaver,
+                            totaltAntallOppgaver: incoming.totaltAntallOppgaver,
+                        };
+                    },
+                },
+            },
+        },
+        OppgaveTilBehandling: { keyFields: ['id'] },
+        BehandletOppgave: { keyFields: ['id'] },
+        Notater: { keyFields: ['id'] },
+        Notat: { keyFields: ['id'] },
+        Kommentar: { keyFields: ['id'] },
+        Periode: { keyFields: ['id'] },
+        Kommentarer: { keyFields: ['id'] },
+        Person: { keyFields: ['fodselsnummer'] },
+        VarselDTO: { keyFields: ['generasjonId', 'kode'] },
+    };
+
+    if (!erUtvikling()) {
+        return prodPolicies;
+    }
+    return {
+        ...prodPolicies,
+        SoknadArbeidsgiver: { keyFields: ['id'] },
+        SoknadNav: { keyFields: ['id'] },
+        Inntektsmelding: { keyFields: ['id'] },
+        Utbetaling: { keyFields: ['id'] },
+        UberegnetPeriode: { keyFields: ['vedtaksperiodeId', 'periodetilstand'] },
+        BeregnetPeriode: { keyFields: ['vedtaksperiodeId', 'beregningId'] },
+        PeriodeHistorikkElement: { keyFields: ['timestamp'] },
+    };
+};
 
 export const client = new ApolloClient({
     link: new HttpLink({ uri: `${BASE_URL}/graphql` }),
     cache: new InMemoryCache({
         dataIdFromObject: () => undefined,
         possibleTypes: possibletypes.possibleTypes,
-        typePolicies: {
-            Query: {
-                fields: {
-                    oppgaveFeed: {
-                        keyArgs: ['filtrering', 'sortering'],
-                        merge(_, incoming) {
-                            const incomingOppgaver = incoming.oppgaver;
-                            return {
-                                oppgaver: incomingOppgaver,
-                                totaltAntallOppgaver: incoming.totaltAntallOppgaver,
-                            };
-                        },
-                    },
-                    behandledeOppgaverFeed: {
-                        keyArgs: [],
-                        merge(_, incoming) {
-                            const incomingOppgaver = incoming.oppgaver;
-                            return {
-                                oppgaver: incomingOppgaver,
-                                totaltAntallOppgaver: incoming.totaltAntallOppgaver,
-                            };
-                        },
-                    },
-                },
-            },
-            OppgaveTilBehandling: { keyFields: ['id'] },
-            BehandletOppgave: { keyFields: ['id'] },
-            Notater: { keyFields: ['id'] },
-            Notat: { keyFields: ['id'] },
-            Kommentar: { keyFields: ['id'] },
-            Periode: { keyFields: ['id'] },
-            Kommentarer: { keyFields: ['id'] },
-            Person: { keyFields: ['fodselsnummer'] },
-            VarselDTO: { keyFields: ['generasjonId', 'kode'] },
-        },
+        typePolicies: getTypePolicies(),
     }),
 });
