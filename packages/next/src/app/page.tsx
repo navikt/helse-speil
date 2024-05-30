60e113482baf6a811719e821d0e17a3c1da70bc8
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2024-05-30 10:26:18 +0200

    🚧 Legger til next skjelett
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>
    Co-authored-by: Trine Linderud <trine.linderud@nav.no>
    Co-authored-by: Karl J. Overå <k@rl.run>

---
+++ packages/next/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@navikt/ds-react';
+
+export default function Home() {
+    return (
+        <div>
+            hei
+            <Button>hade</Button>
+        </div>
+    );
+}
