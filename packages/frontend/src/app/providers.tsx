731f8ceba4d1b04804bb41b67d47135de92291bd
Jonas Hasle <hei@jonashasle.no>
2024-05-30 15:51:17 +0200

    🚧 Next next
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
    Co-authored-by: Trine Linderud <trine.linderud@nav.no>
    Co-authored-by: Karl J. Overå <k@rl.run>

--- packages/frontend/src/app/providers.tsx
+++ packages/frontend/src/app/providers.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import dayjs from 'dayjs';
 import 'dayjs/locale/nb';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
@@ -5,6 +7,11 @@ import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import minMax from 'dayjs/plugin/minMax';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import { PropsWithChildren, ReactElement, useState } from 'react';
+
+import { createApolloClient } from '@/app/apollo/apolloClient';
+import { ApolloProvider } from '@apollo/client';
+
 
 dayjs.extend(relativeTime);
 dayjs.extend(minMax);
@@ -12,3 +19,9 @@ dayjs.extend(isSameOrAfter);
 dayjs.extend(isSameOrBefore);
 dayjs.extend(customParseFormat);
 dayjs.locale('nb');
+
+export const Providers = ({ children }: PropsWithChildren): ReactElement => {
+    const [apolloClient] = useState(() => createApolloClient());
+
+    return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
+};
