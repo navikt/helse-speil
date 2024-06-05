b6ea842baea3e6972515d4388dee57e34ec6ccae
Karl J. Overå <k@rl.run>
2024-06-05 16:02:52 +0200

    🛂 Litt strengere validering av wonderwall og parsing av token
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>

--- src/auth/token.ts
+++ src/auth/token.ts
@@ -1,12 +1,23 @@
 import { IncomingMessage } from 'http';
-import { decodeJwt } from 'jose';
 import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { ZodError, z } from 'zod';
 
-import { getToken, parseAzureUserToken, requestAzureOboToken } from '@navikt/oasis';
+import { getToken, requestAzureOboToken, validateAzureToken } from '@navikt/oasis';
 
 import { erLokal } from '@/env';
+import logger from '@/logger';
 
-export function getTokenPayload() {
+type TokenPayload = z.infer<typeof tokenPayloadSchema>;
+const tokenPayloadSchema = z.object({
+    oid: z.string(),
+    preferred_username: z.string(),
+    name: z.string(),
+    NAVident: z.string(),
+    groups: z.array(z.string()),
+});
+
+export async function getTokenPayload(): Promise<TokenPayload> {
     if (erLokal) {
         return {
             oid: 'local-oid',
@@ -18,12 +29,47 @@ export function getTokenPayload() {
     }
 
     const token = hentWonderwallToken(headers());
-    if (!token) throw new Error('TODO skrive bedre feilmelding');
-    const payload = parseAzureUserToken(token);
-    if (!payload.ok) throw new Error('TODO skrive bedre feilmelding');
-    // TODO: Få oasis til å støtte andre ting i payload, så vi slipper jose direkte
-    const josePayload = decodeJwt<{ oid: string; groups: string[] }>(token);
-    return { ...payload, oid: josePayload.oid, groups: josePayload.groups };
+    if (!token) {
+        // Wonderwall sin autoLogin: true gjør denne casen egentlig umulig
+        redirect('/oauth2/login');
+    }
+
+    const validationResult = await validateAzureToken(token);
+    if (!validationResult.ok) {
+        if (validationResult.errorType === 'token expired') {
+            redirect('/oauth2/login');
+        }
+
+        throw new Error(`JWT invalid, validation failed: ${validationResult.error.message}`, {
+            cause: validationResult.error,
+        });
+    }
+
+    try {
+        return tokenPayloadSchema.parse({
+            oid: validationResult.payload.oid,
+            preferred_username: validationResult.payload.preferred_username,
+            name: validationResult.payload.name,
+            NAVident: validationResult.payload.NAVident,
+            groups: validationResult.payload.groups,
+        } satisfies Record<keyof TokenPayload, unknown>);
+    } catch (e) {
+        if (e instanceof ZodError) {
+            logger.sikker.error(
+                `Klarte ikke å parse payloaden fra tokenet til ${validationResult.payload.preferred_username}\n\n${`The following envs are missing: ${
+                    e.errors
+                        .filter((it) => it.message === 'Required')
+                        .map((it) => it.path.join('.'))
+                        .join(', ') || 'None are missing, but zod is not happy. Look at cause'
+                }`}`,
+            );
+            logger.error('Failed to parse token payload', e);
+        } else {
+            logger.error('Unknown error: Failed to parse token payload', e);
+        }
+
+        throw e;
+    }
 }
 
 export async function byttTilOboToken(token: string, scope: string): Promise<ReturnType<typeof requestAzureOboToken>> {
