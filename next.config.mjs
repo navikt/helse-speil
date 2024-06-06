8e0b65db41d045e942e6ec6842b88a675d36f528
Karl J. Overå <k@rl.run>
2024-06-06 13:23:55 +0200

    📈 Grafana Faro i Browser

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
     experimental: {
         optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons', '@navikt/ds-icons'],
-        serverComponentsExternalPackages: ['graphql-yoga', 'graphql'],
+        serverComponentsExternalPackages: ['graphql-yoga', 'graphql', '@grafana/faro-web-tracing'],
     },
     eslint: {
         ignoreDuringBuilds: true,
--- src/app/api/flexjar/route.ts
+++ src/app/api/flexjar/route.ts
@@ -1,6 +1,7 @@
 import { postOpprett } from '@/app/api/flexjar/flexjar';
 
-export const dynamic = 'force-dynamic'; // defaults to auto
+export const dynamic = 'force-dynamic';
+
 export async function POST(request: Request) {
     return postOpprett(request);
 }
