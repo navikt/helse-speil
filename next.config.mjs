8c28127cf5a5d35ec08841cf3a0b282bd60e4f36
Trine Linderud <trinelinderuds@gmail.com>
2024-07-24 15:27:43 +0200

    🔥 Fjerner @navikt/ds-icons i package.json

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     output: 'standalone',
     assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
     experimental: {
-        optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons', '@navikt/ds-icons'],
+        optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
         serverComponentsExternalPackages: ['graphql-yoga', 'graphql', '@grafana/faro-web-tracing'],
     },
     eslint: {
