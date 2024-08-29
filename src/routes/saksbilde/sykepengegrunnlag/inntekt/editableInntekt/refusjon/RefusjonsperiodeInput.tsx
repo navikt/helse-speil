f3da3cf1a67afe3234c186ecdfa96a23452b6dcc
Martin Remøy Solheim <martin.solheim@me.com>
2024-08-29 14:36:31 +0200

    🚚 Flytter useFomField hook til egen fil

--- src/routes/saksbilde/sykepengegrunnlag/inntekt/editableInntekt/refusjon/RefusjonsperiodeInput.tsx
+++ src/routes/saksbilde/sykepengegrunnlag/inntekt/editableInntekt/refusjon/RefusjonsperiodeInput.tsx
@@ -5,6 +5,7 @@ import { useFormContext } from 'react-hook-form';
 import { DatePicker, HStack, useDatepicker } from '@navikt/ds-react';
 
 import { Maybe } from '@io/graphql';
+import { useFomField } from '@saksbilde/sykepengegrunnlag/inntekt/editableInntekt/refusjon/useFomField';
 import {
     RefusjonFormFields,
     RefusjonFormValues,
@@ -91,58 +92,6 @@ export const RefusjonsperiodeInput = ({
             </DatePicker>
         </HStack>
     );
-};
-
-const useFomField = (fom: string, tom: string | undefined, index: number) => {
-    const { register, setValue } = useFormContext<RefusjonFormValues>();
-    const [fomValue, setFomValue] = useState<string>(somNorskDato(fom) ?? fom);
-
-    const setFomField = useCallback(
-        (nyFom: string) => {
-            setValue(`refusjonsopplysninger.${index}.fom`, nyFom);
-        },
-        [index, setValue],
-    );
-
-    const updateFom = useCallback(
-        (date: Date | undefined) => {
-            const isoDate = dayjs(date).format(ISO_DATOFORMAT);
-            setFomField(isoDate);
-            setFomValue(dayjs(date).format(NORSK_DATOFORMAT));
-        },
-        [setFomField],
-    );
-
-    const onChangeFom = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const date = dayjs(event.target.value, NORSK_DATOFORMAT, true).format(ISO_DATOFORMAT);
-        const validDate = date !== 'Invalid Date';
-
-        setFomValue(event.target.value);
-        validDate && setFomField(date);
-        event.currentTarget.focus();
-    };
-
-    const fomField = register(`refusjonsopplysninger.${index}.fom`, {
-        required: false,
-        validate: {
-            måHaGyldigFormat: (value) =>
-                value == undefined || dayjs(value, ISO_DATOFORMAT).isValid() || 'Datoen må ha format dd.mm.åååå',
-            fomKanIkkeværeEtterTom: (value) =>
-                tom == undefined || dayjs(value).isSameOrBefore(tom) || 'Fom kan ikke være etter tom',
-        },
-    });
-
-    const fomDatePicker = useDatepicker({
-        defaultSelected: somDate(fom),
-        defaultMonth: somDate(fom),
-        onDateChange: updateFom,
-    });
-
-    return {
-        fomField: { ...fomField, onChange: onChangeFom, value: fomValue },
-        fomDatePicker,
-        setFomField,
-    };
 };
 
 const useTomField = (
---
+++ src/routes/saksbilde/sykepengegrunnlag/inntekt/editableInntekt/refusjon/useFomField.tsx
@@ -0,0 +1,60 @@
+import dayjs from 'dayjs';
+import React, { useCallback, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+import { useDatepicker } from '@navikt/ds-react';
+
+import { RefusjonFormValues } from '@saksbilde/sykepengegrunnlag/inntekt/editableInntekt/refusjon/useRefusjonFormField';
+import { ISO_DATOFORMAT, NORSK_DATOFORMAT, somDate, somNorskDato } from '@utils/date';
+
+export const useFomField = (fom: string, tom: string | undefined, index: number) => {
+    const { register, setValue } = useFormContext<RefusjonFormValues>();
+    const [fomValue, setFomValue] = useState<string>(somNorskDato(fom) ?? fom);
+
+    const setFomField = useCallback(
+        (nyFom: string) => {
+            setValue(`refusjonsopplysninger.${index}.fom`, nyFom);
+        },
+        [index, setValue],
+    );
+
+    const updateFom = useCallback(
+        (date: Date | undefined) => {
+            const isoDate = dayjs(date).format(ISO_DATOFORMAT);
+            setFomField(isoDate);
+            setFomValue(dayjs(date).format(NORSK_DATOFORMAT));
+        },
+        [setFomField],
+    );
+
+    const onChangeFom = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const date = dayjs(event.target.value, NORSK_DATOFORMAT, true).format(ISO_DATOFORMAT);
+        const validDate = date !== 'Invalid Date';
+
+        setFomValue(event.target.value);
+        validDate && setFomField(date);
+        event.currentTarget.focus();
+    };
+
+    const fomField = register(`refusjonsopplysninger.${index}.fom`, {
+        required: false,
+        validate: {
+            måHaGyldigFormat: (value) =>
+                value == undefined || dayjs(value, ISO_DATOFORMAT).isValid() || 'Datoen må ha format dd.mm.åååå',
+            fomKanIkkeværeEtterTom: (value) =>
+                tom == undefined || dayjs(value).isSameOrBefore(tom) || 'Fom kan ikke være etter tom',
+        },
+    });
+
+    const fomDatePicker = useDatepicker({
+        defaultSelected: somDate(fom),
+        defaultMonth: somDate(fom),
+        onDateChange: updateFom,
+    });
+
+    return {
+        fomField: { ...fomField, onChange: onChangeFom, value: fomValue },
+        fomDatePicker,
+        setFomField,
+    };
+};
