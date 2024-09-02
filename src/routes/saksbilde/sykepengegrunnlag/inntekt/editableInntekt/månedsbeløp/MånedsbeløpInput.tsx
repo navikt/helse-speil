b912ba7a5f282bcc7abcf5789d04b66c805ba4fe
Martin Remøy Solheim <martin.solheim@me.com>
2024-09-02 08:48:14 +0200

    ♻️ Kontrollerer visningen av månedsbeløp
    Dette gjør at man kan vise norsk formattert beløp men representere det som standard us beløp i datamodellen. Det lar også bruker inputte norske beløp uten rar oppførsel.

--- src/routes/saksbilde/sykepengegrunnlag/inntekt/editableInntekt/månedsbeløp/MånedsbeløpInput.tsx
+++ src/routes/saksbilde/sykepengegrunnlag/inntekt/editableInntekt/månedsbeløp/MånedsbeløpInput.tsx
@@ -1,11 +1,12 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 import { PopoverHjelpetekst } from '@components/PopoverHjelpetekst';
 import { SortInfoikon } from '@components/ikoner/SortInfoikon';
 import { Maybe } from '@io/graphql';
 import { InntektFormFields } from '@saksbilde/sykepengegrunnlag/inntekt/editableInntekt/EditableInntekt';
+import { toKronerOgØre } from '@utils/locale';
 import { avrundetToDesimaler, isNumeric } from '@utils/tall';
 
 import styles from './ManedsbeløpInput.module.css';
@@ -29,6 +30,10 @@ export const MånedsbeløpInput = ({
         trigger,
     } = useFormContext<InntektFormFields>();
 
+    const defaultValue = lokaltMånedsbeløp || (initialMånedsbeløp && avrundetToDesimaler(initialMånedsbeløp));
+
+    const [visningsverdi, setVisningsverdi] = useState<string>(toKronerOgØre(defaultValue ?? 0));
+
     const { ref, onBlur, ...inputValidation } = register('manedsbelop', {
         disabled: skalDeaktiveres,
         required: 'Månedsbeløp mangler',
@@ -38,24 +43,37 @@ export const MånedsbeløpInput = ({
             måVæreMindreEnn: (value) =>
                 Number(value) < 10000000 || 'Systemet håndterer ikke månedsbeløp over 10 millioner',
         },
-        setValueAs: (value) => value.replaceAll(' ', '').replaceAll(',', '.'),
+        setValueAs: (value) => value.replaceAll(' ', '').replaceAll(',', '.').replaceAll(String.fromCharCode(160), ''),
     });
 
     return (
         <>
             <div className={styles.column}>
                 <input
+                    {...inputValidation}
                     className={classNames([styles.Input], {
                         [styles.InputError]: !!manedsbelop?.message,
                     })}
                     id="manedsbelop"
+                    value={visningsverdi}
+                    onChange={(event) => {
+                        setVisningsverdi(event.target.value);
+                    }}
                     ref={ref}
-                    defaultValue={lokaltMånedsbeløp || (initialMånedsbeløp && avrundetToDesimaler(initialMånedsbeløp))}
                     onBlur={(event) => {
+                        const nyttBeløp = Number(
+                            event.target.value
+                                .replaceAll(' ', '')
+                                .replaceAll(',', '.')
+                                // Når tallet blir formattert av toKronerOgØre får det non braking space i stedet for ' '
+                                .replaceAll(String.fromCharCode(160), ''),
+                        );
+
+                        setVisningsverdi(Number.isNaN(nyttBeløp) ? event.target.value : toKronerOgØre(nyttBeløp));
+
                         void onBlur(event);
                         void trigger('manedsbelop');
                     }}
-                    {...inputValidation}
                 />
                 {manedsbelop && (
                     <label className={styles.Feilmelding} htmlFor="manedsbelop">
