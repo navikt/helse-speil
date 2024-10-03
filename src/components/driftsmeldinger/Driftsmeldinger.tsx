b0c2e7e5d665641093a47291160c0cafc0c7c4d4
Trine Linderud <trinelinderuds@gmail.com>
2024-10-03 15:10:25 +0200

    ✨ Mulig å lukke driftsmelding med nivå info og success

--- src/components/driftsmeldinger/Driftsmeldinger.tsx
+++ src/components/driftsmeldinger/Driftsmeldinger.tsx
@@ -14,31 +14,34 @@ import styles from './Driftsmeldinger.module.scss';
 export const Driftsmeldinger = () => {
     const { driftsmeldinger, loading, error } = useDriftsmelding();
 
-    const nivåTilVariant = (level: 'info' | 'warning' | 'error' | 'success') => {
-        switch (level) {
-            case 'info':
-                return 'info';
-            case 'warning':
-                return 'warning';
-            case 'error':
-                return 'error';
-            case 'success':
-                return 'success';
-        }
+    const Driftsmelding = ({
+        level,
+        children,
+    }: {
+        children?: React.ReactNode;
+        level: 'info' | 'warning' | 'error' | 'success';
+    }) => {
+        const [show, setShow] = React.useState(true);
+
+        return show ? (
+            <Alert variant={level} closeButton={level === 'info' || level === 'success'} onClose={() => setShow(false)}>
+                {children || 'Content'}
+            </Alert>
+        ) : null;
     };
 
     return R.sortBy(driftsmeldinger, [R.prop('opprettet'), 'desc']).map((it, index) => {
         const harGått30min = dayjs(it.opprettet).add(30, 'minutes').isBefore(dayjs());
-
         if (harGått30min && it.level === 'success') return null;
+
         return (
-            <Alert key={index} variant={nivåTilVariant(it.level)} className={styles.driftsmelding}>
+            <Driftsmelding key={index} level={it.level}>
                 <BodyShort className={styles.tittel} weight="semibold">
                     {it.level === 'success' ? `[Løst] ${it.tittel}` : it.tittel}
                 </BodyShort>
                 <BodyShort>{it.melding}</BodyShort>
                 <BodyShort className={styles.dato}>{getFormattedDatetimeString(it.opprettet.toString())}</BodyShort>
-            </Alert>
+            </Driftsmelding>
         );
     });
 };
