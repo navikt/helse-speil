ba94c46f97c765b83bfde60c9bcd527d255dcc15
Trine Linderud <trinelinderuds@gmail.com>
2024-10-08 14:17:45 +0200

    ✨ Mulig å åpne/lukke driftsmeldinger
    Co-authored-by: Elias Andreassen Thøgersen <elias.andreassen.thogersen@nav.no>

--- src/components/driftsmeldinger/Driftsmeldinger.tsx
+++ src/components/driftsmeldinger/Driftsmeldinger.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import classNames from 'classnames';
 import dayjs from 'dayjs';
-import React from 'react';
+import React, { ReactElement, useState } from 'react';
 import * as R from 'remeda';
 
-import { Alert, BodyShort } from '@navikt/ds-react';
+import { ChevronDownIcon } from '@navikt/aksel-icons';
+import { Alert, BodyLong, BodyShort, HStack } from '@navikt/ds-react';
 
-import { useDriftsmelding } from '@external/sanity';
+import { Driftsmelding as DriftsmeldingType, useDriftsmelding } from '@external/sanity';
+import { Maybe } from '@io/graphql';
 import { getFormattedDatetimeString } from '@utils/date';
 
 import styles from './Driftsmeldinger.module.scss';
@@ -14,34 +17,46 @@ import styles from './Driftsmeldinger.module.scss';
 export const Driftsmeldinger = () => {
     const { driftsmeldinger, loading, error } = useDriftsmelding();
 
-    const Driftsmelding = ({
-        level,
-        children,
-    }: {
-        children?: React.ReactNode;
-        level: 'info' | 'warning' | 'error' | 'success';
-    }) => {
-        const [show, setShow] = React.useState(true);
+    return R.sortBy(driftsmeldinger, [R.prop('opprettet'), 'desc']).map((driftsmelding, index) => {
+        const harGått30min = dayjs(driftsmelding.opprettet).add(30, 'minutes').isBefore(dayjs());
+        if (harGått30min && driftsmelding.level === 'success') return null;
 
-        return show ? (
-            <Alert variant={level} closeButton={level === 'info' || level === 'success'} onClose={() => setShow(false)}>
-                {children || 'Content'}
-            </Alert>
-        ) : null;
-    };
-
-    return R.sortBy(driftsmeldinger, [R.prop('opprettet'), 'desc']).map((it, index) => {
-        const harGått30min = dayjs(it.opprettet).add(30, 'minutes').isBefore(dayjs());
-        if (harGått30min && it.level === 'success') return null;
-
-        return (
-            <Driftsmelding key={index} level={it.level}>
-                <BodyShort className={styles.tittel} weight="semibold">
-                    {it.level === 'success' ? `[Løst] ${it.tittel}` : it.tittel}
-                </BodyShort>
-                <BodyShort>{it.melding}</BodyShort>
-                <BodyShort className={styles.dato}>{getFormattedDatetimeString(it.opprettet.toString())}</BodyShort>
-            </Driftsmelding>
-        );
+        return <Driftsmelding key={index} driftsmelding={driftsmelding} />;
     });
+};
+
+interface DriftsmeldingProps {
+    driftsmelding: DriftsmeldingType;
+}
+
+const Driftsmelding = ({ driftsmelding }: DriftsmeldingProps): Maybe<ReactElement> => {
+    const [show, setShow] = useState(true);
+    const [åpneDriftsmelding, setÅpneDriftsmelding] = useState(false);
+
+    return show ? (
+        <Alert
+            variant={driftsmelding.level}
+            closeButton={driftsmelding.level === 'info' || driftsmelding.level === 'success'}
+            onClose={() => setShow(false)}
+            onClick={() => setÅpneDriftsmelding(!åpneDriftsmelding)}
+            className={styles.driftsmelding}
+        >
+            <HStack gap="2">
+                <BodyShort className={styles.tittel}>
+                    {driftsmelding.level === 'success' ? `[Løst] ${driftsmelding.tittel}` : driftsmelding.tittel}
+                </BodyShort>
+                <BodyShort className={styles.dato}>
+                    ({getFormattedDatetimeString(driftsmelding.opprettet.toString())})
+                </BodyShort>
+                <span className={styles.button}>
+                    <ChevronDownIcon
+                        title="Vis mer"
+                        fontSize="1.5rem"
+                        className={classNames(styles.chevron, åpneDriftsmelding && styles.chevronrotated)}
+                    />
+                </span>
+            </HStack>
+            {åpneDriftsmelding && <BodyLong className={styles.melding}>{driftsmelding.melding}</BodyLong>}
+        </Alert>
+    ) : null;
 };
