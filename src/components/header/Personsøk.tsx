4ffabf29718552b77acca08cbacb4cd2db668fe5
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2024-10-09 18:00:26 +0200

    ♻️ Opprett abonnement når vi venter på at person blir klar til visning

--- src/components/header/Personsøk.tsx
+++ src/components/header/Personsøk.tsx
@@ -4,9 +4,9 @@ import React, { FormEvent, ReactElement, useRef } from 'react';
 import { Search } from '@navikt/ds-react';
 
 import { erUtvikling } from '@/env';
-import { ApolloError, useLazyQuery } from '@apollo/client';
+import { ApolloError, useLazyQuery, useMutation } from '@apollo/client';
 import { useLoadingToast } from '@hooks/useLoadingToast';
-import { FetchPersonDocument } from '@io/graphql';
+import { FetchPersonDocument, OpprettAbonnementDocument } from '@io/graphql';
 import { validFødselsnummer } from '@io/graphql/common';
 import { useAddVarsel, useRapporterGraphQLErrors } from '@state/varsler';
 import { SpeilError, apolloExtensionValue } from '@utils/error';
@@ -20,6 +20,7 @@ export const Personsøk = (): ReactElement => {
     const router = useRouter();
     const rapporterError = useRapporterGraphQLErrors();
     const [hentPerson, { loading }] = useLazyQuery(FetchPersonDocument);
+    const [opprettAbonnement] = useMutation(OpprettAbonnementDocument);
 
     useLoadingToast({ isLoading: loading, message: 'Henter person' });
 
@@ -49,7 +50,10 @@ export const Personsøk = (): ReactElement => {
                     if (error?.graphQLErrors) {
                         if (erUtvikling && personKlargjøres(error)) {
                             const aktørId = apolloExtensionValue(error, 'persondata_hentes_for');
-                            router.push(`/person/${aktørId}/dagoversikt`);
+                            if (aktørId) {
+                                opprettAbonnement({ variables: { personidentifikator: aktørId } });
+                                router.push(`/person/${aktørId}/dagoversikt`);
+                            }
                         } else {
                             rapporterError(error?.graphQLErrors, personId);
                         }
