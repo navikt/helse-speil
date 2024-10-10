e8a328e74039a60c13da4026b3f4f2e86fcf4141
Martin Remøy Solheim <martin.solheim@me.com>
2024-10-10 15:27:35 +0200

    ♻️ Ikke bruk css hvis man kan style med akselkomponenter
    Co-authored-by: Jonas Hasle <hei@jonashasle.no>

--- src/routes/saksbilde/sykepengegrunnlag/inntekt/inntektOgRefusjon/InntektOgRefusjonHeader.tsx
+++ src/routes/saksbilde/sykepengegrunnlag/inntekt/inntektOgRefusjon/InntektOgRefusjonHeader.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import { HStack } from '@navikt/ds-react';
+import { BodyShort, HStack } from '@navikt/ds-react';
 
 import { Kilde } from '@components/Kilde';
-import { AnonymizableContainer } from '@components/anonymizable/AnonymizableContainer';
+import { AnonymizableText } from '@components/anonymizable/AnonymizableText';
 import { Clipboard } from '@components/clipboard';
 import { Inntektskilde } from '@io/graphql';
 import { Arbeidsgivernavn } from '@saksbilde/sykepengegrunnlag/Arbeidsgivernavn';
-import styles from '@saksbilde/sykepengegrunnlag/inntekt/Inntekt.module.css';
 
 interface InntektOgRefusjonHeaderProps {
     arbeidsgivernavn: string;
@@ -20,24 +19,23 @@ export const InntektOgRefusjonHeader = ({
     organisasjonsnummer,
     kilde,
 }: InntektOgRefusjonHeaderProps) => (
-    <HStack marginBlock="0 6" justify="space-between">
-        <div className={styles.ArbeidsgiverHeader}>
-            <Arbeidsgivernavn className={styles.Arbeidsgivernavn} arbeidsgivernavn={arbeidsgivernavn} />
-            <div className={styles.Organisasjonsnummer}>
+    <HStack marginBlock="5 6" gap="3" align="center">
+        <Arbeidsgivernavn arbeidsgivernavn={arbeidsgivernavn} />
+        <HStack>
+            <BodyShort weight="semibold" size="large">
                 (
-                <Clipboard
-                    copyMessage="Organisasjonsnummer er kopiert"
-                    tooltip={{ content: 'Kopier organisasjonsnummer' }}
-                >
-                    <AnonymizableContainer>{organisasjonsnummer}</AnonymizableContainer>
-                </Clipboard>
+            </BodyShort>
+            <Clipboard copyMessage="Organisasjonsnummer er kopiert" tooltip={{ content: 'Kopier organisasjonsnummer' }}>
+                <AnonymizableText weight="semibold">{organisasjonsnummer}</AnonymizableText>
+            </Clipboard>
+            <BodyShort weight="semibold" size="large">
                 )
-            </div>
-            {kilde === Inntektskilde.Aordningen ? (
-                <Kilde type={Inntektskilde.Aordningen}>AO</Kilde>
-            ) : (
-                <Kilde type="AINNTEKT">AA</Kilde>
-            )}
-        </div>
+            </BodyShort>
+        </HStack>
+        {kilde === Inntektskilde.Aordningen ? (
+            <Kilde type={Inntektskilde.Aordningen}>AO</Kilde>
+        ) : (
+            <Kilde type="AINNTEKT">AA</Kilde>
+        )}
     </HStack>
 );
