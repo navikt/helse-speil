23407a6d133e4a737830e6f53dde3e6e5f8dd0fb
Martin Remøy Solheim <martin.solheim@me.com>
2024-11-04 10:28:44 +0100

    🚨 Navngir default exports

--- src/nais/nais.ts
+++ src/nais/nais.ts
@@ -1,7 +1,9 @@
-export default {
+const naisApp = {
     telemetryCollectorURL: 'http://localhost:9001/collect',
     app: {
         name: 'speil',
         version: 'local',
     },
 };
+
+export default naisApp;
