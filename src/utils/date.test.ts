700bd37310e9174e0276a81ad9db1cd187b0c545
Martin Remøy Solheim <martin.solheim@me.com>
2024-12-19 15:04:16 +0100

    ✅ Tester date util

---
+++ src/utils/date.test.ts
@@ -0,0 +1,29 @@
+import { getFormattedDateString, getFormattedDatetimeString, somDate, somDato, somNorskDato } from '@utils/date';
+
+describe('date', () => {
+    it('skal formattere dato til norsk format', () => {
+        expect(getFormattedDateString()).toBe('');
+        expect(getFormattedDateString(undefined)).toBe('');
+        expect(getFormattedDateString(null)).toBe('');
+        expect(getFormattedDateString('2020-01-01')).toBe('01.01.2020');
+    });
+    it('skal formattere dato til norsk format eller undefined', () => {
+        expect(somNorskDato(undefined)).toBe(undefined);
+        expect(somNorskDato('2020-01-01')).toBe('01.01.2020');
+    });
+    it('skal formattere dato med klokkeslett til norsk format', () => {
+        expect(getFormattedDatetimeString()).toBe('');
+        expect(getFormattedDatetimeString(undefined)).toBe('');
+        expect(getFormattedDatetimeString(null)).toBe('');
+        expect(getFormattedDatetimeString('2020-01-01T13:37:00')).toBe('01.01.2020 kl. 13.37');
+    });
+    it('skal gjøre string til dayjs', () => {
+        expect(somDato('2020-01-01').format()).toEqual('2020-01-01T00:00:00+01:00');
+    });
+    it('skal gjøre string til Date', () => {
+        expect(somDate()).toBeUndefined();
+        expect(somDate('hei')).toBeUndefined();
+        expect(somDate('2020-20-20')).toBeUndefined();
+        expect(somDate('2020-01-01')).toEqual(new Date('2019-12-31T23:00:00.000Z'));
+    });
+});
