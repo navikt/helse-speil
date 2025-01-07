6c6acf40559fe663ef5ae07ba1b33ff04a1a693c
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-01-07 16:12:12 +0100

    ♻️ Bruk Historikkhendelse for TotrinnsvurderingReturHendelse

--- src/routes/saksbilde/historikk/hendelser/TotrinnsvurderingReturHendelse.tsx
+++ src/routes/saksbilde/historikk/hendelser/TotrinnsvurderingReturHendelse.tsx
@@ -1,19 +1,14 @@
-import classNames from 'classnames';
-import { AnimatePresence, motion } from 'framer-motion';
-import React, { PropsWithChildren, ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 
-import { ChevronDownIcon, ChevronUpIcon } from '@navikt/aksel-icons';
+import { BodyLong, VStack } from '@navikt/ds-react';
 
 import { PeriodehistorikkType } from '@io/graphql';
+import { Expandable } from '@saksbilde/historikk/hendelser/Expandable';
 import { TotrinnsvurderingReturIkon } from '@saksbilde/historikk/hendelser/HendelseIkon';
 import { KommentarSeksjon } from '@saksbilde/historikk/komponenter/kommentarer/KommentarSeksjon';
 import { HistorikkhendelseObject } from '@typer/historikk';
 
-import { Hendelse } from './Hendelse';
-import { HendelseDate } from './HendelseDate';
-import { MAX_TEXT_LENGTH_BEFORE_TRUNCATION } from './notat/constants';
-
-import notatStyles from './TotrinnsvurderingReturHendelse.module.css';
+import { Historikkhendelse } from './Historikkhendelse';
 
 type TotrinnsvurderingReturHendelseProps = Omit<HistorikkhendelseObject, 'type' | 'id'>;
 
@@ -25,92 +20,33 @@ export const TotrinnsvurderingReturHendelse = ({
     historikkinnslagId,
     kommentarer,
 }: TotrinnsvurderingReturHendelseProps): ReactElement => {
-    const [expanded, setExpanded] = useState(false);
-    const isExpandable = () =>
-        (notattekst && notattekst.length > MAX_TEXT_LENGTH_BEFORE_TRUNCATION) ||
-        (notattekst && notattekst.split('\n').length > 2) ||
-        false;
-
-    const toggleNotat = (event: React.KeyboardEvent) => {
-        if (event.code === 'Enter' || event.code === 'Space') {
-            setExpanded(isExpandable() && !expanded);
-        }
-    };
+    const førsteTekstlinje = notattekst?.split(/\r?\n/, 1)[0];
+    const øvrigeTekstlinjer = notattekst?.slice(førsteTekstlinje!.length)?.trim();
 
     return (
-        <Hendelse title="Returnert" icon={<TotrinnsvurderingReturIkon />}>
-            <HendelseDate timestamp={timestamp} ident={saksbehandler ?? 'Automatisk'} />
-            <div
-                role="button"
-                tabIndex={0}
-                onKeyDown={toggleNotat}
-                onClick={() => {
-                    // ikke minimer når man markerer tekst
-                    if (window.getSelection()?.type !== 'Range') setExpanded(isExpandable() && !expanded);
-                }}
-                className={classNames(notatStyles.NotatTextWrapper, isExpandable() && notatStyles.cursorpointer)}
-            >
-                <UtvidbartInnhold expanded={expanded}>{notattekst}</UtvidbartInnhold>
-                {isExpandable() && <ExpandButton expanded={expanded} />}
-            </div>
+        <Historikkhendelse
+            icon={<TotrinnsvurderingReturIkon />}
+            title="Returnert"
+            timestamp={timestamp}
+            saksbehandler={saksbehandler ?? 'Automatisk'}
+            aktiv={true}
+        >
+            {notattekst && (
+                <VStack gap="0">
+                    {førsteTekstlinje}
+                    {øvrigeTekstlinjer !== '' && (
+                        <Expandable>
+                            <BodyLong style={{ whiteSpace: 'pre-wrap' }}>{øvrigeTekstlinjer}</BodyLong>
+                        </Expandable>
+                    )}
+                </VStack>
+            )}
             <KommentarSeksjon
                 kommentarer={kommentarer}
                 dialogRef={dialogRef ?? undefined}
                 historikkinnslagId={historikkinnslagId}
                 historikktype={PeriodehistorikkType.TotrinnsvurderingRetur}
             />
-        </Hendelse>
+        </Historikkhendelse>
     );
 };
-
-interface UtvidbartInnholdProps {
-    expanded: boolean;
-    erPåvent?: boolean;
-}
-
-export const UtvidbartInnhold = ({
-    expanded,
-    erPåvent = false,
-    children,
-}: PropsWithChildren<UtvidbartInnholdProps>): ReactElement => (
-    <AnimatePresence mode="wait">
-        {expanded ? (
-            <motion.div
-                key="div"
-                className={notatStyles.Notat}
-                initial={{ height: 40 }}
-                exit={{ height: 40 }}
-                animate={{ height: 'auto' }}
-                transition={{
-                    type: 'tween',
-                    duration: 0.2,
-                    ease: 'easeInOut',
-                }}
-            >
-                {erPåvent ? children : <pre className={notatStyles.Notat}>{children}</pre>}
-            </motion.div>
-        ) : (
-            <motion.p key="p" className={notatStyles.NotatTruncated}>
-                {children}
-            </motion.p>
-        )}
-    </AnimatePresence>
-);
-
-interface ExpandButtonProps {
-    expanded: boolean;
-}
-
-const ExpandButton = ({ expanded }: ExpandButtonProps): ReactElement => (
-    <span className={notatStyles.lesmer}>
-        {expanded ? (
-            <>
-                Vis mindre <ChevronUpIcon title="Vis mindre av teksten" fontSize="1.5rem" />
-            </>
-        ) : (
-            <>
-                Vis mer <ChevronDownIcon title="Vis mer av teksten" fontSize="1.5rem" />
-            </>
-        )}
-    </span>
-);
--- src/routes/saksbilde/historikk/hendelser/notat/constants.ts
+++
@@ -1,1 +0,0 @@
-export const MAX_TEXT_LENGTH_BEFORE_TRUNCATION = 45;
