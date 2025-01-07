1adb75419a7855aad6a98b58f20315d0ff34bbdd
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-01-07 15:41:11 +0100

    ♻️ Bruk Historikkhendelse for Notathendelse

--- src/routes/saksbilde/historikk/hendelser/notat/Notathendelse.tsx
+++ src/routes/saksbilde/historikk/hendelser/notat/Notathendelse.tsx
@@ -1,29 +1,19 @@
 import classNames from 'classnames';
-import { AnimatePresence, motion } from 'framer-motion';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 
-import {
-    ChatIcon,
-    CheckmarkCircleIcon,
-    ChevronDownIcon,
-    ChevronUpIcon,
-    PaperplaneIcon,
-    TimerPauseIcon,
-} from '@navikt/aksel-icons';
-import { ErrorMessage } from '@navikt/ds-react';
+import { ChatIcon, CheckmarkCircleIcon, PaperplaneIcon, TimerPauseIcon } from '@navikt/aksel-icons';
+import { BodyLong, ErrorMessage, VStack } from '@navikt/ds-react';
 
 import { useMutation } from '@apollo/client';
 import { FeilregistrerNotatMutationDocument } from '@io/graphql';
-import notatStyles from '@saksbilde/historikk/hendelser/notat/Notathendelse.module.css';
+import { Expandable } from '@saksbilde/historikk/hendelser/Expandable';
+import { Historikkhendelse } from '@saksbilde/historikk/hendelser/Historikkhendelse';
 import { KommentarSeksjon } from '@saksbilde/historikk/komponenter/kommentarer/KommentarSeksjon';
 import { useInnloggetSaksbehandler } from '@state/authentication';
 import { NotathendelseObject } from '@typer/historikk';
 import { NotatType } from '@typer/notat';
 
-import { Hendelse } from '../Hendelse';
-import { HendelseDate } from '../HendelseDate';
 import { HendelseDropdownMenu } from './HendelseDropdownMenu';
-import { MAX_TEXT_LENGTH_BEFORE_TRUNCATION } from './constants';
 
 import styles from './Notathendelse.module.css';
 
@@ -39,8 +29,6 @@ export const Notathendelse = ({
     feilregistrert,
     kommentarer,
 }: NotathendelseProps): ReactElement => {
-    const [expanded, setExpanded] = useState(false);
-
     const innloggetSaksbehandler = useInnloggetSaksbehandler();
 
     const [feilregistrerNotat, { loading, error }] = useMutation(FeilregistrerNotatMutationDocument, {
@@ -60,93 +48,51 @@ export const Notathendelse = ({
         },
     });
 
-    const isExpandable = () => tekst.length > MAX_TEXT_LENGTH_BEFORE_TRUNCATION || tekst.split('\n').length > 2;
-    const toggleNotat = (event: React.KeyboardEvent) => {
-        if (event.code === 'Enter' || event.code === 'Space') {
-            setExpanded(isExpandable() && !expanded);
-        }
-    };
-
+    const førsteTekstlinje = tekst.split(/\r?\n/, 1)[0]!;
+    const øvrigeTekstlinjer = tekst.slice(førsteTekstlinje.length).trim();
     return (
-        <Hendelse
-            title={<NotatTittel feilregistrert={feilregistrert} notattype={notattype} />}
+        <Historikkhendelse
             icon={<NotatIkon notattype={notattype} />}
+            title={toNotatTittel(notattype) + (feilregistrert ? ' (feilregistrert)' : '')}
+            kontekstknapp={
+                !feilregistrert && innloggetSaksbehandler.ident === saksbehandler ? (
+                    <HendelseDropdownMenu feilregistrerAction={feilregistrerNotat} isFetching={loading} />
+                ) : undefined
+            }
+            timestamp={timestamp}
+            saksbehandler={saksbehandler}
+            aktiv={true}
         >
-            {!feilregistrert && innloggetSaksbehandler.ident === saksbehandler && (
-                <HendelseDropdownMenu feilregistrerAction={feilregistrerNotat} isFetching={loading} />
-            )}
-            <HendelseDate timestamp={timestamp} ident={saksbehandler} />
-            <div
-                role="button"
-                tabIndex={0}
-                onKeyDown={toggleNotat}
-                onClick={() => {
-                    // ikke minimer når man markerer tekst
-                    if (window.getSelection()?.type !== 'Range') setExpanded(isExpandable() && !expanded);
-                }}
-                className={classNames(notatStyles.NotatTextWrapper, isExpandable() && notatStyles.cursorpointer)}
-            >
-                <AnimatePresence mode="wait">
-                    {expanded ? (
-                        <motion.pre
-                            key="pre"
-                            className={styles.Notat}
-                            initial={{ height: 40 }}
-                            exit={{ height: 40 }}
-                            animate={{ height: 'auto' }}
-                            transition={{
-                                type: 'tween',
-                                duration: 0.2,
-                                ease: 'easeInOut',
-                            }}
-                        >
-                            {tekst}
-                        </motion.pre>
-                    ) : (
-                        <motion.p key="p" className={styles.NotatTruncated}>
-                            {tekst}
-                        </motion.p>
-                    )}
-                </AnimatePresence>
-                {isExpandable() && (
-                    <span className={styles.lesmer}>
-                        {expanded ? (
-                            <>
-                                Vis mindre <ChevronUpIcon title="Vis mer av notatet" fontSize="1.5rem" />
-                            </>
-                        ) : (
-                            <>
-                                Vis mer
-                                <ChevronDownIcon title="Vis mindre av notatet" fontSize="1.5rem" />
-                            </>
-                        )}
-                    </span>
-                )}
-            </div>
             {error && <ErrorMessage>Kunne ikke feilregistrere notat. Prøv igjen senere.</ErrorMessage>}
+            <VStack gap="0">
+                {førsteTekstlinje}
+                {øvrigeTekstlinjer !== '' && (
+                    <Expandable>
+                        <BodyLong style={{ whiteSpace: 'pre-wrap' }}>{øvrigeTekstlinjer}</BodyLong>
+                    </Expandable>
+                )}
+            </VStack>
             <KommentarSeksjon
                 kommentarer={kommentarer}
                 dialogRef={dialogRef}
                 historikkinnslagId={Number.parseInt(id)}
             />
-        </Hendelse>
+        </Historikkhendelse>
     );
 };
 
-interface NotatTittelProps {
-    feilregistrert: boolean;
-    notattype: NotatType;
-}
-
-const NotatTittel = ({ feilregistrert, notattype }: NotatTittelProps): ReactElement => (
-    <span className={classNames(feilregistrert)}>
-        {notattype === 'OpphevStans' && 'Stans opphevet'}
-        {notattype === 'PaaVent' && 'Lagt på vent'}
-        {notattype === 'Retur' && 'Returnert'}
-        {notattype === 'Generelt' && 'Notat'}
-        {feilregistrert && ' (feilregistrert)'}
-    </span>
-);
+const toNotatTittel = (notattype: NotatType): string => {
+    switch (notattype) {
+        case 'OpphevStans':
+            return 'Stans opphevet';
+        case 'PaaVent':
+            return 'Lagt på vent';
+        case 'Retur':
+            return 'Returnert';
+        default:
+            return 'Notat';
+    }
+};
 
 interface NotatIkonProps {
     notattype: NotatType;
