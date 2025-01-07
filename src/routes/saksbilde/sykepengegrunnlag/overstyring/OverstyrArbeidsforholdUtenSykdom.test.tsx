4828abe395bdc900c945bdbc758d85c8cc2f0892
Trine Linderud <trinelinderuds@gmail.com>
2025-01-07 14:54:11 +0100

    ✅ Skip ustabil test
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- src/routes/saksbilde/sykepengegrunnlag/overstyring/OverstyrArbeidsforholdUtenSykdom.test.tsx
+++ src/routes/saksbilde/sykepengegrunnlag/overstyring/OverstyrArbeidsforholdUtenSykdom.test.tsx
@@ -36,110 +36,107 @@ describe('OverstyrArbeidsforholdUtenSykdom Tests', () => {
     // Forventer at <OverstyrArbeidsforholdUtenSykdon /> skal forsvinne etter man har sendt inn overstyring,
     // men det er noe feil som gjør at komponentet ikke forstår at overstyring er sendt og resultat mottat og dermed
     // skulle vært skjult.
-    it.failing(
-        'skal ikke vise ikke bruk arbeidsforholdet knap om arbeidsforholdet ikke er deaktivert og arbeidsforholdet har blitt markert som ikke i bruk',
-        async () => {
-            const periode = enBeregnetPeriode();
-            const arbeidsgiver = enArbeidsgiver().medPerioder([periode]);
-            const person = enPerson().medArbeidsgivere([arbeidsgiver]);
+    it.skip('skal ikke vise ikke bruk arbeidsforholdet knap om arbeidsforholdet ikke er deaktivert og arbeidsforholdet har blitt markert som ikke i bruk', async () => {
+        const periode = enBeregnetPeriode();
+        const arbeidsgiver = enArbeidsgiver().medPerioder([periode]);
+        const person = enPerson().medArbeidsgivere([arbeidsgiver]);
 
-            const mocks = [
-                overstyrArbeidsforholdMutationMock(periode.skjaeringstidspunkt, periode.vedtaksperiodeId, {
-                    begrunnelse: 'Avbrudd mer enn 14 dager (generell)',
-                    deaktivert: true,
-                    forklaring: 'En begrunnelse',
-                    orgnummer: '987654321',
-                    lovhjemmel: { paragraf: '8-15', lovverk: 'folketrygdloven', lovverksversjon: '1998-12-18' },
-                }),
-                opprettAbonnementMock(person.aktorId),
-                createMock({
-                    request: { query: OpptegnelserDocument, variables: { sekvensId: undefined } },
-                    result: {
-                        data: {
-                            __typename: 'Query',
-                            opptegnelser: [],
-                        },
+        const mocks = [
+            overstyrArbeidsforholdMutationMock(periode.skjaeringstidspunkt, periode.vedtaksperiodeId, {
+                begrunnelse: 'Avbrudd mer enn 14 dager (generell)',
+                deaktivert: true,
+                forklaring: 'En begrunnelse',
+                orgnummer: '987654321',
+                lovhjemmel: { paragraf: '8-15', lovverk: 'folketrygdloven', lovverksversjon: '1998-12-18' },
+            }),
+            opprettAbonnementMock(person.aktorId),
+            createMock({
+                request: { query: OpptegnelserDocument, variables: { sekvensId: undefined } },
+                result: {
+                    data: {
+                        __typename: 'Query',
+                        opptegnelser: [],
                     },
-                }),
-                createMock({
-                    request: { query: OpptegnelserDocument, variables: { sekvensId: undefined } },
-                    result: {
-                        data: {
-                            __typename: 'Query',
-                            opptegnelser: [
-                                {
-                                    aktorId: person.aktorId,
-                                    type: 'UTBETALING_ANNULLERING_OK',
-                                    sekvensnummer: 1,
-                                    payload: 'payload1',
-                                    __typename: 'Opptegnelse',
-                                },
-                                {
-                                    aktorId: person.aktorId,
-                                    type: 'PERSONDATA_OPPDATERT',
-                                    sekvensnummer: 2,
-                                    payload: 'payload2',
-                                    __typename: 'Opptegnelse',
-                                },
-                            ],
-                        },
+                },
+            }),
+            createMock({
+                request: { query: OpptegnelserDocument, variables: { sekvensId: undefined } },
+                result: {
+                    data: {
+                        __typename: 'Query',
+                        opptegnelser: [
+                            {
+                                aktorId: person.aktorId,
+                                type: 'UTBETALING_ANNULLERING_OK',
+                                sekvensnummer: 1,
+                                payload: 'payload1',
+                                __typename: 'Opptegnelse',
+                            },
+                            {
+                                aktorId: person.aktorId,
+                                type: 'PERSONDATA_OPPDATERT',
+                                sekvensnummer: 2,
+                                payload: 'payload2',
+                                __typename: 'Opptegnelse',
+                            },
+                        ],
                     },
-                }),
-                createMock({
-                    request: { query: OpptegnelserDocument, variables: { sekvensId: 2 } },
-                    result: {
-                        data: {
-                            __typename: 'Query',
-                            opptegnelser: [
-                                {
-                                    aktorId: person.aktorId,
-                                    type: 'NY_SAKSBEHANDLEROPPGAVE',
-                                    sekvensnummer: 3,
-                                    payload: 'payload3',
-                                    __typename: 'Opptegnelse',
-                                },
-                            ],
-                        },
+                },
+            }),
+            createMock({
+                request: { query: OpptegnelserDocument, variables: { sekvensId: 2 } },
+                result: {
+                    data: {
+                        __typename: 'Query',
+                        opptegnelser: [
+                            {
+                                aktorId: person.aktorId,
+                                type: 'NY_SAKSBEHANDLEROPPGAVE',
+                                sekvensnummer: 3,
+                                payload: 'payload3',
+                                __typename: 'Opptegnelse',
+                            },
+                        ],
                     },
-                }),
-                createMock({
-                    request: { query: OpptegnelserDocument, variables: { sekvensId: 3 } },
-                    result: {
-                        data: {
-                            __typename: 'Query',
-                            opptegnelser: [],
-                        },
+                },
+            }),
+            createMock({
+                request: { query: OpptegnelserDocument, variables: { sekvensId: 3 } },
+                result: {
+                    data: {
+                        __typename: 'Query',
+                        opptegnelser: [],
                     },
-                }),
-            ];
-            const PollingWrapper = ({ children }: PropsWithChildren) => {
-                usePollEtterOpptegnelser();
-                return <>{children}</>;
-            };
+                },
+            }),
+        ];
+        const PollingWrapper = ({ children }: PropsWithChildren) => {
+            usePollEtterOpptegnelser();
+            return <>{children}</>;
+        };
 
-            render(
-                <PollingWrapper>
-                    <VenterPåEndringProvider>
-                        <OverstyrArbeidsforholdUtenSykdom
-                            organisasjonsnummerAktivPeriode={arbeidsgiver.organisasjonsnummer}
-                            skjæringstidspunkt={periode.skjaeringstidspunkt}
-                            arbeidsforholdErDeaktivert={false}
-                            person={person}
-                        />
-                    </VenterPåEndringProvider>
-                </PollingWrapper>,
-                { mocks },
-            );
-            await userEvent.click(screen.getByRole('button'));
-            await userEvent.click(screen.getAllByRole('radio')[0]!);
-            await userEvent.type(screen.getByRole('textbox'), 'En begrunnelse');
-            await userEvent.click(screen.getByRole('button', { name: 'Ferdig' }));
-            await waitForElementToBeRemoved(screen.queryByRole('button', { name: 'Ferdig' }));
-            expect(
-                screen.queryByRole('button', { name: 'Ikke bruk arbeidsforholdet i beregningen' }),
-            ).not.toBeInTheDocument();
-        },
-    );
+        render(
+            <PollingWrapper>
+                <VenterPåEndringProvider>
+                    <OverstyrArbeidsforholdUtenSykdom
+                        organisasjonsnummerAktivPeriode={arbeidsgiver.organisasjonsnummer}
+                        skjæringstidspunkt={periode.skjaeringstidspunkt}
+                        arbeidsforholdErDeaktivert={false}
+                        person={person}
+                    />
+                </VenterPåEndringProvider>
+            </PollingWrapper>,
+            { mocks },
+        );
+        await userEvent.click(screen.getByRole('button'));
+        await userEvent.click(screen.getAllByRole('radio')[0]!);
+        await userEvent.type(screen.getByRole('textbox'), 'En begrunnelse');
+        await userEvent.click(screen.getByRole('button', { name: 'Ferdig' }));
+        await waitForElementToBeRemoved(screen.queryByRole('button', { name: 'Ferdig' }));
+        expect(
+            screen.queryByRole('button', { name: 'Ikke bruk arbeidsforholdet i beregningen' }),
+        ).not.toBeInTheDocument();
+    });
 
     it('skal vise angreknap om arbeidsforholdet er deaktivert og knappen ikke er trykket', () => {
         const periode = enBeregnetPeriode();
