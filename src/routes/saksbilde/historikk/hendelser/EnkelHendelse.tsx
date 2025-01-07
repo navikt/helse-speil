5fc98f7fa677855ef8c9e19134ca7d6df1877a13
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-01-07 13:20:48 +0100

    ♻️ Kortslutt EnkelHendelse til å bruke Historikkhendelse

--- src/routes/saksbilde/historikk/hendelser/EnkelHendelse.tsx
+++ src/routes/saksbilde/historikk/hendelser/EnkelHendelse.tsx
@@ -1,28 +1,15 @@
-import classNames from 'classnames';
-import React, { ReactElement, ReactNode } from 'react';
+import React, { ReactElement } from 'react';
 
-import { BodyShort, HStack } from '@navikt/ds-react';
-
-import { HendelseDate } from '@saksbilde/historikk/hendelser/HendelseDate';
+import { Historikkhendelse } from '@saksbilde/historikk/hendelser/Historikkhendelse';
 import { DateString } from '@typer/shared';
 
-import styles from './EnkelHendelse.module.scss';
-
 interface EnkelHendelseProps {
-    icon: ReactNode;
-    title: ReactNode;
+    icon: ReactElement;
+    title: string;
     timestamp: DateString;
     saksbehandler?: string;
 }
 
 export const EnkelHendelse = ({ icon, title, timestamp, saksbehandler }: EnkelHendelseProps): ReactElement => (
-    <li tabIndex={0} className={classNames(styles.fokusområde, styles.hendelse)}>
-        <div className={styles.iconContainer}>{icon}</div>
-        <div className={styles.content}>
-            <HStack gap="1">
-                <BodyShort weight="semibold">{title}</BodyShort>
-            </HStack>
-            <HendelseDate timestamp={timestamp} ident={saksbehandler} />
-        </div>
-    </li>
+    <Historikkhendelse icon={icon} title={title} timestamp={timestamp} saksbehandler={saksbehandler} />
 );
--- src/routes/saksbilde/historikk/hendelser/dokument/Dokumenthendelse.tsx
+++ src/routes/saksbilde/historikk/hendelser/dokument/Dokumenthendelse.tsx
@@ -6,8 +6,6 @@ import { DateString } from '@typer/shared';
 
 import { getKildetekst, getKildetype } from './dokument';
 
-import styles from './Dokumenthendelse.module.scss';
-
 type DokumenthendelseProps = {
     dokumenttype: 'Sykmelding' | 'InntektHentetFraAordningen';
     timestamp: DateString;
@@ -16,13 +14,7 @@ type DokumenthendelseProps = {
 export const Dokumenthendelse = ({ dokumenttype, timestamp }: DokumenthendelseProps): ReactElement => (
     <EnkelHendelse
         title={
-            <span className={styles.header}>
-                <span>
-                    {dokumenttype === 'InntektHentetFraAordningen'
-                        ? 'Inntekt hentet fra A-ordningen'
-                        : dokumenttype + ' mottatt'}
-                </span>
-            </span>
+            dokumenttype === 'InntektHentetFraAordningen' ? 'Inntekt hentet fra A-ordningen' : dokumenttype + ' mottatt'
         }
         icon={<Kilde type={getKildetype(dokumenttype)}>{getKildetekst(dokumenttype)}</Kilde>}
         timestamp={timestamp}
