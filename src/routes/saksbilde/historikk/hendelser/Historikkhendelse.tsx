7a518373dbcaaa69f3a7f1ec2680f0aed849e41f
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-01-07 13:16:40 +0100

    ♻️ Inline alle varianter i Historikkhendelse, gjør alle innslag tab-bare

--- src/routes/saksbilde/historikk/hendelser/Historikkhendelse.tsx
+++ src/routes/saksbilde/historikk/hendelser/Historikkhendelse.tsx
@@ -1,12 +1,14 @@
-import React, { PropsWithChildren, ReactElement } from 'react';
+import classNames from 'classnames';
+import React, { PropsWithChildren, ReactElement, useRef, useState } from 'react';
 
-import { VStack } from '@navikt/ds-react';
+import { ChevronDownIcon, ChevronUpIcon } from '@navikt/aksel-icons';
+import { BodyShort, HStack, Spacer, VStack } from '@navikt/ds-react';
 
-import { EnkelHendelse } from '@saksbilde/historikk/hendelser/EnkelHendelse';
-import { ExpandableHendelse } from '@saksbilde/historikk/hendelser/ExpandableHendelse';
-import { Hendelse } from '@saksbilde/historikk/hendelser/Hendelse';
+import { AnimatedExpandableDiv } from '@components/AnimatedExpandableDiv';
 import { HendelseDate } from '@saksbilde/historikk/hendelser/HendelseDate';
 import { DateString } from '@typer/shared';
+
+import styles from './Historikkhendelse.module.scss';
 
 type HistorikkhendelseProps = {
     icon?: ReactElement;
@@ -26,24 +28,66 @@ export const Historikkhendelse = ({
     aktiv = true,
     children,
 }: PropsWithChildren<HistorikkhendelseProps>): ReactElement => {
-    return children ? (
-        aktiv ? (
-            <Hendelse title={title} icon={icon}>
+    const [expanded, setExpanded] = useState(false);
+    const ref = useRef(null);
+
+    function toggleExpanded() {
+        setExpanded(!expanded);
+    }
+
+    return aktiv || !children ? (
+        <li tabIndex={0} className={classNames(styles.fokusområde, styles.hendelse)}>
+            <div className={styles.iconContainer}>{icon}</div>
+            <div className={styles.content}>
+                <BodyShort weight="semibold">{title}</BodyShort>
                 {kontekstknapp}
                 <HendelseDate timestamp={timestamp} ident={saksbehandler} />
-                <VStack gap="2">{children}</VStack>
-            </Hendelse>
-        ) : (
-            <ExpandableHendelse
-                tittel={title}
-                ikon={icon}
-                tidsstempel={timestamp}
-                saksbehandler={saksbehandler ?? undefined}
-            >
-                <VStack gap="2">{children}</VStack>
-            </ExpandableHendelse>
-        )
+                {children && <VStack gap="2">{children}</VStack>}
+            </div>
+        </li>
     ) : (
-        <EnkelHendelse title={title} icon={icon} timestamp={timestamp} saksbehandler={saksbehandler} />
+        <li
+            role="button"
+            tabIndex={0}
+            onClick={(event: React.MouseEvent) => {
+                // Ikke minimer når man markerer tekst
+                if (window.getSelection()?.type !== 'Range') {
+                    toggleExpanded();
+                    event.stopPropagation();
+                }
+            }}
+            onKeyDown={(event: React.KeyboardEvent) => {
+                // Reager kun hvis akkurat dette elementet er markert med tab
+                if (event.target === ref.current) {
+                    if (event.key === 'Enter' || event.key === ' ') {
+                        toggleExpanded();
+                        event.preventDefault();
+                        event.stopPropagation(); // Forhindre urelatert scrolling
+                    }
+                }
+            }}
+            className={classNames(styles.fokusområde, styles.klikkbar, styles.hendelse)}
+            ref={ref}
+        >
+            <div className={styles.iconContainer}>{icon}</div>
+            <div className={styles.content}>
+                <HStack gap="1" wrap={false}>
+                    <HStack>
+                        <BodyShort weight="semibold">{title}</BodyShort>
+                        {expanded ? (
+                            <ChevronUpIcon title="Vis mindre" fontSize="1.5rem" />
+                        ) : (
+                            <ChevronDownIcon title="Vis mer" fontSize="1.5rem" />
+                        )}
+                    </HStack>
+                    {kontekstknapp && <Spacer />}
+                    {kontekstknapp}
+                </HStack>
+                <HendelseDate timestamp={timestamp} ident={saksbehandler} />
+                <AnimatedExpandableDiv expanded={expanded}>
+                    <VStack gap="2">{children}</VStack>
+                </AnimatedExpandableDiv>
+            </div>
+        </li>
     );
 };
