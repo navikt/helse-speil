2117747c9ed832487009287938b3d95879efa776
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-01-08 15:33:31 +0100

    🐛 Vis "Sykmeldt" i stedet for ekte navn ved anonymisering

--- src/routes/oversikt/table/cells/SøkerCell.tsx
+++ src/routes/oversikt/table/cells/SøkerCell.tsx
@@ -4,6 +4,7 @@ import { Table, Tooltip } from '@navikt/ds-react';
 
 import { AnonymizableTextWithEllipsis } from '@components/anonymizable/AnonymizableText';
 import { Personinfo } from '@io/graphql';
+import { useIsAnonymous } from '@state/anonymization';
 import { capitalizeName } from '@utils/locale';
 
 type Name = Pick<Personinfo, 'fornavn' | 'etternavn' | 'mellomnavn'>;
@@ -18,11 +19,12 @@ interface SøkerProps {
 }
 
 export const SøkerCell = ({ name }: SøkerProps): ReactElement => {
+    const isAnonymous = useIsAnonymous();
     const formatertNavn = getFormattedName(name);
 
     return (
         <Table.DataCell style={{ width: 180 }}>
-            <Tooltip content={formatertNavn}>
+            <Tooltip content={isAnonymous ? 'Sykmeldt' : formatertNavn}>
                 <span>
                     <AnonymizableTextWithEllipsis style={{ width: 180 }}>{formatertNavn}</AnonymizableTextWithEllipsis>
                 </span>
