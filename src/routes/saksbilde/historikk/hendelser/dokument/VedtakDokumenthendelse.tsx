310dc1bf102aa48aa04177514b5247247f85e221
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-01-09 12:57:52 +0100

    ♻️ VedtakDokumenthendelse tar i bruk Historikkhendelse
    Co-authored-by: Svein Elgstøen <189128153+navelgen@users.noreply.github.com>

--- src/routes/saksbilde/historikk/hendelser/dokument/VedtakDokumenthendelse.tsx
+++ src/routes/saksbilde/historikk/hendelser/dokument/VedtakDokumenthendelse.tsx
@@ -1,16 +1,12 @@
 import React, { ReactElement } from 'react';
 
 import { ExternalLinkIcon } from '@navikt/aksel-icons';
-import { Link } from '@navikt/ds-react';
+import { Button } from '@navikt/ds-react';
 
 import { Kilde } from '@components/Kilde';
 import { hoppTilModia } from '@components/SystemMenu';
+import { Historikkhendelse } from '@saksbilde/historikk/hendelser/Historikkhendelse';
 import { DateString } from '@typer/shared';
-
-import { Hendelse } from '../Hendelse';
-import { HendelseDate } from '../HendelseDate';
-
-import styles from './VedtakDokumentHendelse.module.scss';
 
 type VedtakDokumentHendelseProps = {
     dokumentId?: string;
@@ -23,26 +19,24 @@ export const VedtakDokumentHendelse = ({
     fødselsnummer,
     timestamp,
 }: VedtakDokumentHendelseProps): ReactElement => (
-    <Hendelse
-        title={
-            <span className={styles.header}>
-                <span>Melding om vedtak</span>
-            </span>
-        }
+    <Historikkhendelse
         icon={<Kilde type="VEDTAK">MV</Kilde>}
-    >
-        <Link
-            onClick={() =>
-                hoppTilModia(
-                    `https://spinnsyn-frontend-interne.intern.nav.no/syk/sykepenger?id=${dokumentId}`,
-                    fødselsnummer,
-                )
-            }
-            className={styles['åpne-vedtak']}
-        >
-            Åpne vedtak i ny fane
-            <ExternalLinkIcon className={styles.eksternlenke} />
-        </Link>
-        <HendelseDate timestamp={timestamp} />
-    </Hendelse>
+        title="Melding om vedtak"
+        kontekstknapp={
+            <Button
+                icon={<ExternalLinkIcon />}
+                title={'Åpne vedtak i ny fane'}
+                variant="tertiary"
+                size="xsmall"
+                onClick={() =>
+                    hoppTilModia(
+                        `https://spinnsyn-frontend-interne.intern.nav.no/syk/sykepenger?id=${dokumentId}`,
+                        fødselsnummer,
+                    )
+                }
+            />
+        }
+        timestamp={timestamp}
+        aktiv={false}
+    />
 );
