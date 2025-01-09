eaa26bbe40bce54f232928e02e2329f04a09fb59
Trine Linderud <trinelinderuds@gmail.com>
2025-01-09 13:41:19 +0100

    🐛 Perioder med overstyringer vises kun til beslutter
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- src/routes/saksbilde/venstremeny/HarBeslutteroppgaver.tsx
+++ src/routes/saksbilde/venstremeny/HarBeslutteroppgaver.tsx
@@ -4,6 +4,7 @@ import { Maybe, PersonFragment } from '@io/graphql';
 import { Periodeinformasjon } from '@saksbilde/venstremeny/Periodeinformasjon';
 import { usePeriodeTilGodkjenning } from '@state/arbeidsgiver';
 import { useSetActivePeriodId } from '@state/periode';
+import { isBeregnetPeriode } from '@utils/typeguards';
 
 interface HarBeslutteroppgaverProps {
     person: PersonFragment;
@@ -14,6 +15,13 @@ export const HarBeslutteroppgaver = ({ person }: HarBeslutteroppgaverProps): May
     const harPeriodeTilGodkjenning = usePeriodeTilGodkjenning(person);
 
     if (!harPeriodeTilGodkjenning) return null;
+
+    const harBeslutteroppgave = person.arbeidsgivere
+        .flatMap((arbeidsgiver) => arbeidsgiver.generasjoner)
+        .flatMap((generasjon) => generasjon.perioder)
+        .some((periode) => isBeregnetPeriode(periode) && periode.totrinnsvurdering?.erBeslutteroppgave);
+
+    if (!harBeslutteroppgave) return null;
 
     const overstyringer = person.arbeidsgivere
         .map(
@@ -27,7 +35,11 @@ export const HarBeslutteroppgaver = ({ person }: HarBeslutteroppgaverProps): May
                                 .some((overstyring) => overstyring.vedtaksperiodeId === periode.vedtaksperiodeId),
                         ),
                     )
-                    .map((periode) => ({ id: periode.id, fom: periode.fom, tom: periode.tom })),
+                    .map((periode) => ({
+                        id: periode.id,
+                        fom: periode.fom,
+                        tom: periode.tom,
+                    })),
             }),
         )
         .filter((overstyring) => overstyring.perioder.length > 0);
