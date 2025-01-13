ff8f780b957cbe487f9fbe246bb85409368e9878
Trine Linderud <trinelinderuds@gmail.com>
2025-01-13 12:52:18 +0100

    ⚡️ Driftsmeldinger blir hentet kun ved navigering, ikke rerender
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- src/hooks/useRefetchDriftsmeldinger.tsx
+++ src/hooks/useRefetchDriftsmeldinger.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
+
 import { useApolloClient } from '@apollo/client';
 
 export const useRefetchDriftsmeldinger = () => {
     const apolloClient = useApolloClient();
-    apolloClient.refetchQueries({
-        include: ['Driftsmelding'],
-    });
+
+    useEffect(() => {
+        const handleRouteChange = () => {
+            apolloClient.refetchQueries({
+                include: ['Driftsmelding'],
+            });
+        };
+
+        handleRouteChange();
+    }, [apolloClient]);
 };
