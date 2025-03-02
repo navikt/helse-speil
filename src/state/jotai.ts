3ccc2fb3815f86c6025e3d06903bd7c83388ab7b
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-03-02 14:42:38 +0100

    🐛 Fiks atomWithSessionStorage

--- src/state/jotai.ts
+++ src/state/jotai.ts
@@ -1,14 +1,11 @@
-import { atomWithStorage } from 'jotai/utils';
+import { atomWithStorage, createJSONStorage } from 'jotai/utils';
 
 // Jotai's atomWithStorage defaulter til localStorage. Dette er en hjelpefunksjon man kan bruke når man i stedet vil
 // lagre i sessionStorage
 export function atomWithSessionStorage<T>(key: string, initialValue: T) {
-    return atomWithStorage<T>(key, initialValue, {
-        getItem: (key) => {
-            const storedValue = sessionStorage.getItem(key);
-            return storedValue ? (JSON.parse(storedValue) as T) : initialValue;
-        },
-        setItem: (key, value) => sessionStorage.setItem(key, JSON.stringify(value)),
-        removeItem: (key) => sessionStorage.removeItem(key),
-    });
+    return atomWithStorage<T>(
+        key,
+        initialValue,
+        createJSONStorage(() => (typeof window !== 'undefined' ? sessionStorage : (undefined as unknown as Storage))),
+    );
 }
