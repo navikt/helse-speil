551d21f27473d6fb4705b61eb7613e8851a6c945
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-04-08 18:32:35 +0200

    ✨ Vis toast etter "ikke-forespurt" reberegning
    Hvis spleis reberegner perioden av andre årsaker enn at saksbehandler
    har gjort noe i speil (for eksempel hvis det kommer inn nye dokumenter
    eller det skjer en endring i Infotrygd), bør det være litt mer synlig
    for saksbehandler enn at det bare dukker opp et nytt innslag i
    historikken.

--- src/hooks/useRefreshPersonVedOpptegnelse.ts
+++ src/hooks/useRefreshPersonVedOpptegnelse.ts
@@ -2,10 +2,13 @@ import { NetworkStatus } from '@apollo/client';
 import { erOpptegnelseForNyOppgave, useHåndterOpptegnelser } from '@state/opptegnelser';
 import { useSelectPeriod } from '@state/periode';
 import { useFetchPersonQuery } from '@state/person';
+import { useAddToast, useToasts } from '@state/toasts';
 
 export const useRefreshPersonVedOpptegnelse = () => {
     const { data, networkStatus, refetch } = useFetchPersonQuery();
     const selectPeriod = useSelectPeriod();
+    const addToast = useAddToast();
+    const toasts = useToasts();
 
     useHåndterOpptegnelser(async (opptegnelse) => {
         if (
@@ -17,6 +20,13 @@ export const useRefreshPersonVedOpptegnelse = () => {
             const result = await refetch();
             if (erOpptegnelseForNyOppgave(opptegnelse)) {
                 if (result.data.person) selectPeriod(result.data.person);
+                if (toasts.length === 0) {
+                    addToast({
+                        key: 'VedtaksperiodeReberegnetToastKey',
+                        message: 'Perioden er reberegnet',
+                        timeToLiveMs: 3000,
+                    });
+                }
             }
         }
     });
