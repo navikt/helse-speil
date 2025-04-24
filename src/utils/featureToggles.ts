fdcb215fb49b5e5207b20c183d08a269d21acfd0
Trine Linderud <trine.linderud@nav.no>
2025-04-24 13:21:20 +0200

    🚩 Fjerner noen identer fra feature toggles

--- src/utils/featureToggles.ts
+++ src/utils/featureToggles.ts
@@ -3,19 +3,16 @@ import { erLokal, erUtvikling } from '@/env';
 const groupIdForTbd = 'f787f900-6697-440d-a086-d5bb56e26a9c';
 const groupIdForBesluttere = '59f26eef-0a4f-4038-bf46-3a5b2f252155';
 
-const supersaksbehandlere = ['N115007', 'C117102', 'K164523', 'A148751', 'S161635'];
+const supersaksbehandlere = ['N115007', 'K164523', 'A148751', 'S161635'];
 
 const fagkoordinatorer = ['M136300', 'S108267', 'K123956', 'G157538'];
 
 const enhetsledere = ['K105348', 'L105454'];
 
 const coaches = [
-    'J153777',
     'F131883',
-    'K104953',
     'V149621',
     'S160466',
-    'O123659',
     'B164848',
     'K162139',
     'F160464',
