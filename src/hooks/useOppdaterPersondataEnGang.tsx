0fb933c47dc6211564fc6f2ba4c07cae478e8f2c
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-05-30 10:16:36 +0200

    🚩 Skrur på automatisk oppdatering av Infotrygd-historikk for alle

--- src/hooks/useOppdaterPersondataEnGang.tsx
+++ src/hooks/useOppdaterPersondataEnGang.tsx
@@ -1,22 +1,19 @@
 import { useEffect } from 'react';
 
 import { useFetchPersonQuery } from '@/state/person';
-import { useBrukerGrupper } from '@auth/brukerContext';
 import { useOppdaterPersondata } from '@saksbilde/saksbildeMenu/dropdown/useOppdaterPersondata';
-import { erUtviklingEllerErPåTeamBømlo } from '@utils/featureToggles';
 
 export const useOppdaterPersondataEnGang = () => {
     const { data } = useFetchPersonQuery();
     const fødselsnummer = data?.person?.fodselsnummer;
     const [forespørPersonoppdatering] = useOppdaterPersondata();
-    const brukergrupper = useBrukerGrupper();
 
     useEffect(() => {
-        if (fødselsnummer != undefined && erUtviklingEllerErPåTeamBømlo(brukergrupper)) {
+        if (fødselsnummer != undefined) {
             void forespørPersonoppdatering(fødselsnummer);
         }
         /* eslint-disable-next-line react-hooks/exhaustive-deps --
-          Kan ikke bruke forespørPersonoppdatering som dependency, da blir det dobble kall
+          Kan ikke oppgi forespørPersonoppdatering som avhengighet, da blir det doble kall
          */
     }, [fødselsnummer]);
 };
