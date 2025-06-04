3af1be53653b5fade060fdbc80762a229381cf32
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-06-04 19:04:48 +0200

    💄 Litt finere "personen kunne ikke hentes"-side

--- src/routes/saksbilde/saksbilder/PeriodeViewError.tsx
+++ src/routes/saksbilde/saksbilder/PeriodeViewError.tsx
@@ -4,8 +4,8 @@ import React, { ReactElement } from 'react';
 import styles from './SharedViews.module.css';
 
 export const PeriodeViewError = (): ReactElement => (
-    <div className={styles.Content}>
-        Klarte ikke å laste personen.
+    <div className={styles.Error}>
+        <span>Personen kunne ikke hentes.</span>
         <Link href="/">Klikk her for å gå tilbake til oppgaveoversikten</Link>
     </div>
 );
