55b400ab7c30a101c36fd8a7467499c276a94522
Håvard Eggen <11397871+gammalost@users.noreply.github.com>
2025-09-23 12:01:13 +0200

    🐛 Viser riktig tidspunkt i endringslogg for dager
    Co-authored-by: Trine Linderud <trine.linderud@nav.no>

--- src/components/endringslogg/EndringsloggDager.tsx
+++ src/components/endringslogg/EndringsloggDager.tsx
@@ -25,12 +25,12 @@ export const EndringsloggDager = ({ endringer, closeModal, showModal }: Endrings
             <Table zebraStripes>
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell>Dato og tidspunkt</Table.HeaderCell>
+                        <Table.HeaderCell>Dato</Table.HeaderCell>
                         <Table.HeaderCell>Dagtype</Table.HeaderCell>
                         <Table.HeaderCell>Grad</Table.HeaderCell>
                         <Table.HeaderCell>Begrunnelse</Table.HeaderCell>
                         <Table.HeaderCell>Kilde</Table.HeaderCell>
-                        <Table.HeaderCell>Endret dato</Table.HeaderCell>
+                        <Table.HeaderCell>Endret dato og tidspunkt</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
@@ -38,7 +38,7 @@ export const EndringsloggDager = ({ endringer, closeModal, showModal }: Endrings
                         .sort((a, b) => sortTimestampDesc(a.timestamp, b.timestamp))
                         .map(({ begrunnelse, saksbehandler, timestamp, grad, fraGrad, dag, dato }, i) => (
                             <Table.Row key={i}>
-                                <Table.DataCell>{getFormattedDatetimeString(dato)}</Table.DataCell>
+                                <Table.DataCell>{getFormattedDateString(dato)}</Table.DataCell>
                                 <Table.DataCell>{dag.speilDagtype}</Table.DataCell>
                                 <Table.DataCell>
                                     <span className={styles.PreviousValue}>
@@ -50,7 +50,7 @@ export const EndringsloggDager = ({ endringer, closeModal, showModal }: Endrings
                                     <BodyShort className={styles.Begrunnelse}>{begrunnelse}</BodyShort>
                                 </Table.DataCell>
                                 <Table.DataCell>{saksbehandler.ident ?? saksbehandler.navn}</Table.DataCell>
-                                <Table.DataCell>{getFormattedDateString(timestamp)}</Table.DataCell>
+                                <Table.DataCell>{getFormattedDatetimeString(timestamp)}</Table.DataCell>
                             </Table.Row>
                         ))}
                 </Table.Body>
