2c38a9a130dafdd7d789051f09cd2a4f7d347ae6
Martin Remøy Solheim <3852471+MartinMekk@users.noreply.github.com>
2025-11-07 11:26:32 +0100

    ♻️ Rydd opp i kreativ bruk av react-hook-form i LeggTilDagerForm
    Co-authored-by: Trine Linderud <trine.linderud@nav.no>

--- src/types/utbetalingstabell.ts
+++ src/types/utbetalingstabell.ts
@@ -3,27 +3,30 @@ import { Speildag } from '@saksbilde/utbetaling/utbetalingstabell/utbetalingstab
 
 import { DateString } from './shared';
 
-export type Utbetalingstabelldagtype =
-    | 'Syk'
-    | 'Ferie'
-    | 'FriskHelg'
-    | 'Feriehelg'
-    | 'SykHelg'
-    | 'SykNav'
-    | 'Helg'
-    | 'Ukjent'
-    | 'Egenmelding'
-    | 'Permisjon'
-    | 'Arbeid'
-    | 'Avslått'
-    | 'ArbeidIkkeGjenopptatt'
-    | 'Foreldrepenger'
-    | 'AAP'
-    | 'Dagpenger'
-    | 'Svangerskapspenger'
-    | 'Pleiepenger'
-    | 'Omsorgspenger'
-    | 'Opplæringspenger';
+export const utbetalingstabelldagtypeValues = [
+    'Syk',
+    'Ferie',
+    'FriskHelg',
+    'Feriehelg',
+    'SykHelg',
+    'SykNav',
+    'Helg',
+    'Ukjent',
+    'Egenmelding',
+    'Permisjon',
+    'Arbeid',
+    'Avslått',
+    'ArbeidIkkeGjenopptatt',
+    'Foreldrepenger',
+    'AAP',
+    'Dagpenger',
+    'Svangerskapspenger',
+    'Pleiepenger',
+    'Omsorgspenger',
+    'Opplæringspenger',
+] as const;
+
+export type Utbetalingstabelldagtype = (typeof utbetalingstabelldagtypeValues)[number];
 
 export type OverstyringerPrDag = {
     vedtaksperiodeId: string;
