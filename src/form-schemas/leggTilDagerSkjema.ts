b411fe723479c26b7b23ff7388357d2d948b74cf
Martin Remøy Solheim <3852471+MartinMekk@users.noreply.github.com>
2025-11-07 15:45:30 +0100

    🦺 Riktig validering for legg til dager skjema

--- src/form-schemas/leggTilDagerSkjema.ts
+++ src/form-schemas/leggTilDagerSkjema.ts
@@ -4,38 +4,36 @@ import { kanVelgeGrad } from '@saksbilde/utbetaling/utbetalingstabell/endringFor
 import { utbetalingstabelldagtypeValues } from '@typer/utbetalingstabell';
 import { somDato } from '@utils/date';
 
-export type LeggTilDagerFormFields = z.infer<ReturnType<typeof lagLeggTilDagerSchema>>;
+export type LeggTilDagerFormFields = z.infer<typeof LeggTilDagerSchema>;
 
-export const lagLeggTilDagerSchema = () => {
-    return z
-        .object({
-            dag: z.enum(utbetalingstabelldagtypeValues),
-            fom: z.iso.date(),
-            tom: z.iso.date(),
-            grad: z.optional(
-                z
-                    .number({
-                        error: (issue) =>
-                            issue.input == null || issue.input === '' ? 'Velg grad' : 'Grad må være et tall',
-                    })
-                    .min(0, 'Grad må være over 0')
-                    .max(100, 'Grad må være 100 eller lavere'),
-            ),
-        })
-        .superRefine((val, ctx) => {
-            if (kanVelgeGrad(val.dag) && !(Number.isNaN(val.grad) || val.grad == null)) {
-                ctx.addIssue({
-                    code: 'custom',
-                    error: 'Velg grad',
-                    input: val.grad,
-                });
-            }
-            if (Math.abs(somDato(val.fom).diff(somDato(val.tom), 'days')) <= 16) {
-                ctx.addIssue({
-                    code: 'custom',
-                    error: 'Kan ikke legge til dager tidligere enn 16 i forkant av sykmelding',
-                    input: val.fom,
-                });
-            }
-        });
-};
+export const LeggTilDagerSchema = z
+    .object({
+        dag: z.enum(utbetalingstabelldagtypeValues),
+        fom: z.iso.date(),
+        tom: z.iso.date(),
+        grad: z.optional(
+            z
+                .number({
+                    error: (issue) =>
+                        issue.input == null || issue.input === '' ? 'Velg grad' : 'Grad må være et tall',
+                })
+                .min(0, 'Grad må være over 0')
+                .max(100, 'Grad må være 100 eller lavere'),
+        ),
+    })
+    .superRefine((val, ctx) => {
+        if (!kanVelgeGrad(val.dag) && (Number.isNaN(val.grad) || val.grad == null)) {
+            ctx.addIssue({
+                code: 'custom',
+                message: 'Velg grad',
+                input: val.grad,
+            });
+        }
+        if (Math.abs(somDato(val.fom).diff(somDato(val.tom), 'days')) > 16) {
+            ctx.addIssue({
+                code: 'custom',
+                message: 'Kan ikke legge til dager tidligere enn 16 i forkant av sykmelding',
+                input: val.fom,
+            });
+        }
+    });
