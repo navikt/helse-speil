300a770f4af3b59b4863e7812e7a9a92dd2db08d
Martin Remøy Solheim <3852471+MartinMekk@users.noreply.github.com>
2025-12-04 08:56:27 +0100

    ♻️ Fjern useMemo på useVisiblePeriods

--- src/routes/saksbilde/timeline/hooks/useVisiblePeriods.ts
+++ src/routes/saksbilde/timeline/hooks/useVisiblePeriods.ts
@@ -1,10 +1,7 @@
 import { Dayjs } from 'dayjs';
-import { useMemo } from 'react';
 
 import { DatePeriod } from '@typer/shared';
 
-export const useVisiblePeriods = <T extends DatePeriod>(end: Dayjs, start: Dayjs, periods: T[]): T[] =>
-    useMemo(
-        () => periods.filter((it) => end.isSameOrAfter(it.fom) && start.isSameOrBefore(it.tom)),
-        [end, start, periods],
-    );
+export function useVisiblePeriods<T extends DatePeriod>(end: Dayjs, start: Dayjs, periods: T[]): T[] {
+    return periods.filter((it) => end.isSameOrAfter(it.fom) && start.isSameOrBefore(it.tom));
+}
