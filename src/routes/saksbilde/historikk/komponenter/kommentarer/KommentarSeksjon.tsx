7baa8586d3147d66f45df92133418b39d0c40414
Trine Linderud <trine.linderud@nav.no>
2025-12-08 11:12:54 +0100

    🐛 Kommentarer åpner/lukker seg bare hvis man trykker på header

--- src/routes/saksbilde/historikk/komponenter/kommentarer/KommentarSeksjon.tsx
+++ src/routes/saksbilde/historikk/komponenter/kommentarer/KommentarSeksjon.tsx
@@ -1,7 +1,8 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
+
+import { ReadMore } from '@navikt/ds-react';
 
 import { Kommentar, PeriodehistorikkType } from '@io/graphql';
-import { Expandable } from '@saksbilde/historikk/komponenter/Expandable';
 import { Kommentarer } from '@saksbilde/historikk/komponenter/kommentarer/Kommentarer';
 import { LeggTilNyKommentar } from '@saksbilde/historikk/komponenter/kommentarer/LeggTilNyKommentar';
 
@@ -17,19 +18,27 @@ export const KommentarSeksjon = ({
     dialogRef,
     historikkinnslagId,
     historikktype,
-}: KommentarSeksjonProps): ReactElement => (
-    <>
-        {kommentarer?.length > 0 && (
-            <Expandable expandText={`Kommentarer (${kommentarer?.length})`} collapseText="Lukk kommentarer">
-                <Kommentarer kommentarer={kommentarer} />
-            </Expandable>
-        )}
-        {dialogRef && (
-            <LeggTilNyKommentar
-                historikktype={historikktype}
-                dialogRef={dialogRef}
-                historikkinnslagId={historikkinnslagId}
-            />
-        )}
-    </>
-);
+}: KommentarSeksjonProps): ReactElement => {
+    const [open, setOpen] = useState(false);
+    const antall = kommentarer?.length ?? 0;
+    return (
+        <>
+            {antall > 0 && (
+                <ReadMore
+                    onClick={() => setOpen(!open)}
+                    size="small"
+                    header={open ? `Kommentarer (${kommentarer?.length})` : 'Lukk kommentarer'}
+                >
+                    <Kommentarer kommentarer={kommentarer} />
+                </ReadMore>
+            )}
+            {dialogRef && (
+                <LeggTilNyKommentar
+                    historikktype={historikktype}
+                    dialogRef={dialogRef}
+                    historikkinnslagId={historikkinnslagId}
+                />
+            )}
+        </>
+    );
+};
