e19cc8432d4fb57aedd1732af1e32f3cf3c04d0d
Håvard Eggen <11397871+gammalost@users.noreply.github.com>
2025-12-16 09:50:00 +0100

    💄 Legg til nyttårsgurk

--- src/components/header/EasterEgg.tsx
+++ src/components/header/EasterEgg.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import React, { ReactElement } from 'react';
 
 import julegurken from '@assets/topplinjebilder/julegurken.svg';
+import nyttårsgurken from '@assets/topplinjebilder/nyttårsgurken.svg';
 import påskegurken from '@assets/topplinjebilder/påskegurken.svg';
 import sommergurken from '@assets/topplinjebilder/sommergurken.svg';
 import { ISO_DATOFORMAT } from '@utils/date';
@@ -12,6 +13,7 @@ import styles from './EasterEgg.module.css';
 export const EasterEgg = (): ReactElement => (
     <div className={styles.container}>
         <Julepynt />
+        <Nyttårspynt />
         <Påskepynt />
         <Sommergurken />
     </div>
@@ -23,8 +25,14 @@ const Påskepynt = (): ReactElement | null =>
     ) : null;
 
 const Julepynt = (): ReactElement | null =>
-    dayjs().get('month') == 11 ? (
+    dayjs().get('month') == 11 && dayjs().get('date') <= 30 ? (
         <Image style={{ marginLeft: '1.5rem' }} priority={true} alt="Julepynt" src={julegurken} />
+    ) : null;
+
+const Nyttårspynt = (): ReactElement | null =>
+    (dayjs().get('month') === 11 && dayjs().get('date') === 31) ||
+    (dayjs().get('month') === 0 && dayjs().isoWeek() <= 2) ? (
+        <Image style={{ marginLeft: '1.5rem' }} priority={true} alt="Nyttårspynt" src={nyttårsgurken} />
     ) : null;
 
 const Sommergurken = (): ReactElement | null =>
