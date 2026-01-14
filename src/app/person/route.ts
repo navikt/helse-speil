81ce5e66f776d93bb3bac98795db6e13ecb85a22
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-01-14 16:38:38 +0100

    🐛 Korriger feil URL mot personsøk i Speil
    Co-authored-by: Håvard Eggen <11397871+gammalost@users.noreply.github.com>

--- src/app/person/route.ts
+++ src/app/person/route.ts
@@ -32,7 +32,7 @@ export async function POST(request: NextRequest) {
 
         personPseudoId = (
             await (
-                await fetch(`${getServerEnv().SPESIALIST_BASEURL}/api/spesialist/person/sok`, {
+                await fetch(`${getServerEnv().SPESIALIST_BASEURL}/api/spesialist/personer/sok`, {
                     method: 'POST',
                     headers: headers,
                     body: JSON.stringify({
