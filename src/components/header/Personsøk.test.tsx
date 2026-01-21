f89bbed0a3bbf952a62b229d0f27739f5c9992a3
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-01-21 14:56:44 +0100

    ✅ Bruk riktige vitest-funksjoner for mocking av Promise-svar

--- src/components/header/Personsøk.test.tsx
+++ src/components/header/Personsøk.test.tsx
@@ -2,20 +2,20 @@ import mockRouter from 'next-router-mock';
 import { Mock } from 'vitest';
 
 import { Varsler } from '@components/Varsler';
-import { FetchPersonDocument } from '@io/graphql';
 import { postPersonSok } from '@io/rest/generated/personsøk/personsøk';
-import { enPerson } from '@test-data/person';
-import { createInitialQuery, render, screen } from '@test-utils';
+import { useFetchPersonQuery } from '@state/person';
+import { render, screen } from '@test-utils';
 import userEvent from '@testing-library/user-event';
 
 import { Personsøk } from './Personsøk';
 
 vi.mock('@io/rest/generated/personsøk/personsøk');
+vi.mock('@state/person');
 
 describe('Personsøk', () => {
-    it('should fetch a person and redirect to dagoversikt', async () => {
+    it('finds a personPseudoId and redirects to dagoversikt', async () => {
         const personPseudoId = 'fb8c1382-bf1e-4027-9ab8-37a2c7772231';
-        (postPersonSok as Mock).mockReturnValueOnce({
+        (postPersonSok as Mock).mockResolvedValueOnce({
             data: { personPseudoId: personPseudoId, klarForVisning: true },
         });
 
@@ -27,41 +27,20 @@ describe('Personsøk', () => {
         expect(mockRouter.pathname).toEqual('/person/[personPseudoId]/dagoversikt');
     });
 
-    it('when it errors it should redirect to root and display varsel', async () => {
-        mockRouter.setCurrentUrl('/person/fb8c1382-bf1e-4027-9ab8-37a2c7772231/dagoversikt');
-
-        (postPersonSok as Mock).mockReturnValueOnce({
-            data: undefined,
-            status: 404,
-        });
+    it('displays varsel when person is not found', async () => {
+        (postPersonSok as Mock).mockRejectedValueOnce({ response: { status: 404 } });
+        (useFetchPersonQuery as Mock).mockReturnValue({});
 
         render(
             <div>
                 <Varsler />
                 <Personsøk />
             </div>,
-            {
-                initialQueries: [
-                    // Bruker på routen lastes først pga <Varsler />
-                    createInitialQuery(
-                        FetchPersonDocument,
-                        {
-                            __typename: 'Query',
-                            person: enPerson({
-                                aktorId: '1111111111111',
-                                fodselsnummer: '12345678910',
-                            }),
-                        },
-                        { personPseudoId: '11111111-1111-1111-1111-111111111111' },
-                    ),
-                ],
-            },
         );
 
         await userEvent.type(screen.getByRole('searchbox', { name: 'Søk' }), '1234567891000');
         await userEvent.click(screen.getByRole('button', { name: 'Søk' }));
 
-        expect(mockRouter.pathname).toEqual('/');
         expect(screen.getByText('Personen er ikke i Speil'));
     });
 });
