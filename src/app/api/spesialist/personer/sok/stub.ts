6b3e6ce293cc5e5b942d364e3fdb36d9113c1e44
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-01-21 16:03:42 +0100

    🤡 Fiks 404 når person ikke finnes i lokal devserver

--- src/app/api/spesialist/personer/sok/stub.ts
+++ src/app/api/spesialist/personer/sok/stub.ts
@@ -9,7 +9,7 @@ export const stub = async (_request: Request) => {
     const person = fetchPersondata()[identitetsnummer ?? aktørId ?? ''];
 
     const personPseudoId = person?.personPseudoId;
-    if (personPseudoId == undefined) return Response.json({ status: 404 });
+    if (personPseudoId == undefined) return Response.json({}, { status: 404 });
     const klarForVisning = person?.personinfo != null;
     return Response.json({ personPseudoId: personPseudoId, klarForVisning: klarForVisning });
 };
