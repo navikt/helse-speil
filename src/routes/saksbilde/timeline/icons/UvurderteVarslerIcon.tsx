3e571a49e122a2f6e59985dcc57e660d56f846ca
Elias Andreassen Thøgersen <elias3004@msn.com>
2026-01-29 15:42:50 +0100

    💄 fikser på periodestyling for darkmode

--- src/routes/saksbilde/timeline/icons/UvurderteVarslerIcon.tsx
+++ src/routes/saksbilde/timeline/icons/UvurderteVarslerIcon.tsx
@@ -26,7 +26,7 @@ export const UvurderteVarslerIcon = ({
             <title id={titleId}>{alt}</title>
             <path
                 d="M6.49135 0.328848C6.70292 -0.104036 7.28641 -0.11103 7.50721 0.316672L13.9315 13.1013C14.1405 13.5063 13.8631 14 13.4266 14H0.575864C0.145067 14 -0.132785 13.518 0.0648783 13.1135L6.49135 0.328848Z"
-                fill="#D47B00"
+                fill="var(--ax-bg-warning-strong)"
             />
         </svg>
     );
