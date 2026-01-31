67e282ce179dafef202078c2662042ae27e40d8b
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2026-01-31 13:25:32 +0100

    WIP - placeholder-filer

---
+++ src/app/api/spesialist/vedtaksperioder/[vedtaksperiodeId]/notater/[notatId]/feilregistrer/route.ts
@@ -0,0 +1,5 @@
+import { stubEllerVideresendTilSpesialist } from '@app/api/spesialist/common';
+import { stub } from '@app/api/spesialist/vedtaksperioder/[vedtaksperiodeId]/notater/stub';
+
+// Bytt ut med noe fornuftig
+export const POST = stubEllerVideresendTilSpesialist(stub);
---
+++ src/app/api/spesialist/vedtaksperioder/[vedtaksperiodeId]/notater/[notatId]/kommentarer/stub.ts
@@ -0,0 +1,11 @@
+/*import { NextResponse } from 'next/server';
+
+import { DialogMock } from '@spesialist-mock/storage/dialog';
+import { NotatMock } from '@spesialist-mock/storage/notat';
+import { VarselMock } from '@spesialist-mock/storage/varsel';
+
+export const stub = async (_request: Request, params: Promise<{ kommentarId: string }>) => {
+    const { kommentarId } = await params;
+    const kommentar = DialogMock.addKommentar();
+    return NextResponse.json(kommentar, { status: 200 });
+};*/
