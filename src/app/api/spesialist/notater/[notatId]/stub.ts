42e94a632d93a5096978847aa77845a46302e0f1
Martin Remøy Solheim <3852471+MartinMekk@users.noreply.github.com>
2026-02-02 15:03:14 +0100

    🐛 Fiks feil i typing av stub

--- src/app/api/spesialist/notater/[notatId]/stub.ts
+++ src/app/api/spesialist/notater/[notatId]/stub.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 import { NotatMock } from '@spesialist-mock/storage/notat';
 
-export const stub = async (_request: Request, params: Promise<{ vedtaksperiodeId: string; notatId: string }>) => {
+export const stub = async (_request: Request, params: Promise<{ notatId: string }>) => {
     const { notatId } = await params;
     const notat = NotatMock.getNotat(Number(notatId));
     return NextResponse.json(notat, { status: 200 });
