497308840066ebadfd59c4292ed6347655eb1590
Elias Andreassen Thøgersen <elias3004@msn.com>
2026-02-03 15:06:04 +0100

    💄 Rydder mer i farge-tokens

--- src/components/ikoner/Maksdatoikon.tsx
+++ src/components/ikoner/Maksdatoikon.tsx
@@ -9,7 +9,7 @@ export const Maksdatoikon = ({ width = 16, height = 16, className, alt }: IkonPr
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z"
-                fill="#BA3A26"
+                fill="var(--ax-bg-danger-strong)"
             />
             <circle cx="7.99958" cy="7.99946" r="2.28571" fill="var(--ax-text-neutral-contrast)" />
         </g>
