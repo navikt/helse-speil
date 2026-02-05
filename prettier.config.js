2fcfba0f0b4237ef2e6e148457a21f6647be5e69
Elias Andreassen Thøgersen <elias3004@msn.com>
2026-02-05 11:03:07 +0100

    🔧 fjerner .prettierrc - har prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,33 @@ import baseConfig from '@navikt/tsm-prettier';
 
 const config = {
     ...baseConfig,
-    plugins: [...(baseConfig.plugins || []), 'prettier-plugin-tailwindcss'],
-    tailwindStylesheet: './src/styles/globals.css',
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    printWidth: 120,
+    plugins: [...(baseConfig.plugins || []), '@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+    importOrder: [
+        '^./globals.css$',
+        '<THIRD_PARTY_MODULES>',
+        '^@navikt/(.*)$',
+        '^(@(.*)|@(.*)/(.*))',
+        '^([.]{1,2}/)+[æøåÆØÅa-zA-Z]+((/[æøåÆØÅa-zA-Z]+)+)?$',
+        '^.*([.]css)|.*([.]scss)$',
+    ],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    trailingComma: 'all',
+    tailwindStylesheet: './src/app/globals.css',
     tailwindFunctions: ['classNames', 'cn'],
+    overrides: [
+        {
+            files: ['*.yml', '*.yaml'],
+            options: {
+                bracketSpacing: false,
+                tabWidth: 2,
+            },
+        },
+    ],
 };
 
 export default config;
