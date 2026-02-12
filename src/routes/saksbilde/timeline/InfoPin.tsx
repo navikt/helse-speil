bc3e6e201568fec3c48807d9c9c39560f8c4a880
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-02-12 16:36:11 +0100

    ♻️ Hent notater via REST-operasjon også for InfoPin

--- src/routes/saksbilde/timeline/InfoPin.tsx
+++ src/routes/saksbilde/timeline/InfoPin.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 
 import { Sykdomsdagtype, Utbetalingsdagtype } from '@io/graphql';
+import { useGetNotaterForVedtaksperiode } from '@io/rest/generated/notater/notater';
 import { DatePeriod } from '@typer/shared';
 import { cn } from '@utils/tw';
 import { isBeregnetPeriode, isUberegnetPeriode } from '@utils/typeguards';
@@ -18,19 +19,25 @@ const shouldShowInfoPin = (period: DatePeriod): boolean => {
     );
 };
 
-const shouldShowNotatPin = (period: DatePeriod): boolean => {
-    if (!isBeregnetPeriode(period) && !isUberegnetPeriode(period)) return false;
-
-    return period.notater.some((notat) => notat.type === 'Generelt');
-};
-
 interface InfoPinProps {
     period: DatePeriod;
 }
 
+const useHarGenereltNotat = (period: DatePeriod) => {
+    const vedtaksperiodeId =
+        isBeregnetPeriode(period) || isUberegnetPeriode(period) ? period.vedtaksperiodeId : undefined;
+    const { data } = useGetNotaterForVedtaksperiode(vedtaksperiodeId!, {
+        query: {
+            enabled: vedtaksperiodeId !== undefined,
+        },
+    });
+
+    return data?.some((notat) => notat.type === 'Generelt') || false;
+};
+
 export const InfoPin = ({ period }: InfoPinProps): ReactElement | null => {
     const showInfoPin = shouldShowInfoPin(period);
-    const showNotatPin = shouldShowNotatPin(period);
+    const showNotatPin = useHarGenereltNotat(period);
 
     if (!showInfoPin && !showNotatPin) {
         return null;
