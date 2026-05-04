d0874c35ab2225e1b83fe4cee519be245c6f1fdd
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2026-05-04 14:36:14 +0200

    ✨  Bare vis forkast-knapp for visse uberegnede perioder
    Den skal ikke vises for revurderinger eller forkastede perioder.
    
    Co-authored-by: Eirik Gallefoss <eirik.gallefoss@nav.no>
    Co-authored-by: Sivert Lunsæter <sivert.frang.lunseter@nav.no>

--- src/routes/saksbilde/venstremeny/VenstremenyUberegnetPeriode.tsx
+++ src/routes/saksbilde/venstremeny/VenstremenyUberegnetPeriode.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 
 import { useHarBetabrukerrolle, useHarSkrivetilgang } from '@hooks/brukerrolleHooks';
-import { PersonFragment, UberegnetPeriodeFragment } from '@io/graphql';
+import { Periodetilstand, PersonFragment, UberegnetPeriodeFragment } from '@io/graphql';
 import { HarBeslutteroppgaver } from '@saksbilde/venstremeny/HarBeslutteroppgaver';
 import { HarVurderbareVarsler } from '@saksbilde/venstremeny/HarVurderbareVarsler';
 import { ForkastFraUberegnetButton } from '@saksbilde/venstremeny/utbetaling/ForkastFraUberegnetButton';
@@ -24,12 +24,21 @@ export const VenstremenyUberegnetPeriode = ({
 }: VenstremenyUberegnetPeriodeProps): ReactElement => {
     const harBetabrukerrolle = useHarBetabrukerrolle();
     const harSkrivetilgang = useHarSkrivetilgang();
+    const skalViseForkastknapp =
+        harBetabrukerrolle &&
+        harSkrivetilgang &&
+        [
+            Periodetilstand.AvventerInntektsopplysninger,
+            Periodetilstand.ForberederGodkjenning,
+            Periodetilstand.ManglerInformasjon,
+            Periodetilstand.VenterPaEnAnnenPeriode,
+        ].includes(activePeriod.periodetilstand);
     return (
         <section className={styles.Venstremeny}>
             <PeriodeCard.Uberegnet periode={activePeriod} inntektsforhold={inntektsforhold} />
             <HarVurderbareVarsler person={currentPerson} />
             <HarBeslutteroppgaver person={currentPerson} />
-            {harBetabrukerrolle && harSkrivetilgang && <ForkastFraUberegnetButton activePeriod={activePeriod} />}
+            {skalViseForkastknapp && <ForkastFraUberegnetButton activePeriod={activePeriod} />}
         </section>
     );
 };
