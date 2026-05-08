4c9b613d03d221233ec2f2d0c9670efac6d53754
Elias Andreassen Thøgersen <elias3004@msn.com>
2026-05-08 10:29:50 +0200

    💄 pirk i høyremeny i dialogmelding

--- src/routes/saksbilde/dialogmelding/dokumentvisning/Dokumentvisning.tsx
+++ src/routes/saksbilde/dialogmelding/dokumentvisning/Dokumentvisning.tsx
@@ -4,7 +4,12 @@ import { Heading, VStack } from '@navikt/ds-react';
 
 export function Dokumentvisning(): ReactElement {
     return (
-        <VStack as="section" className="w-91.5 border-l border-l-ax-border-neutral-subtle p-4 [grid-area:høyremeny]">
+        <VStack
+            as="section"
+            gap="space-8"
+            className="w-91.5 border-l border-l-ax-border-neutral-subtle [grid-area:høyremeny]"
+            padding="space-16"
+        >
             <Heading level="3" size="xsmall">
                 Dokumentvisning
             </Heading>
