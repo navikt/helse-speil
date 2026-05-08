021a09c197653918b9c90763ae3e0bc0b1689ffc
Elias Andreassen Thøgersen <elias3004@msn.com>
2026-05-08 09:56:50 +0200

    ♻️ Viser skeleton mens liste av dialoger hentes

--- src/app/person/[personPseudoId]/(dialogmelding)/dialogmelding/page.tsx
+++ src/app/person/[personPseudoId]/(dialogmelding)/dialogmelding/page.tsx
@@ -1,18 +1,20 @@
 'use client';
 
-import { redirect, useParams } from 'next/navigation';
+import { redirect } from 'next/navigation';
+import React, { ReactElement, use } from 'react';
 
-import { BodyShort, Box } from '@navikt/ds-react';
+import { BodyShort, Box, Heading, VStack } from '@navikt/ds-react';
 
 import { useGetDialogmeldinger } from '@io/rest/generated/default/default';
-import { finnNyesteDialog } from '@saksbilde/dialogmelding/testdata';
+import { ApiBehandlerMedDialoger } from '@io/rest/generated/sporhund.schemas';
+import { DialogmeldingContentSkeleton } from '@saksbilde/dialogmelding/content/DialogmeldingContentSkeleton';
 
-export default function Page() {
-    const { personPseudoId } = useParams<{ personPseudoId: string }>();
+export default function Page({ params }: { params: Promise<{ personPseudoId: string }> }): ReactElement {
+    const { personPseudoId } = use(params);
     const { data, isPending } = useGetDialogmeldinger(personPseudoId);
 
     if (isPending || data === undefined) {
-        return <div>skeleton</div>;
+        return <DialogmeldingContentSkeleton />;
     }
 
     const nyesteDialog = finnNyesteDialog(data);
@@ -23,7 +25,16 @@ export default function Page() {
 
     return (
         <Box as="section" padding="space-16" className="[grid-area:content]">
-            <BodyShort className="text-(--ax-text-subtle)">Ingen dialoger funnet.</BodyShort>
+            <VStack gap="space-8">
+                <Heading level="2" size="medium">
+                    Dialogmelding
+                </Heading>
+                <BodyShort>Ingen dialoger funnet.</BodyShort>
+            </VStack>
         </Box>
     );
+}
+
+function finnNyesteDialog(behandlerDialoger: ApiBehandlerMedDialoger[]) {
+    return behandlerDialoger.flatMap((b) => b.dialoger).sort((a, b) => b.tid.localeCompare(a.tid))[0] ?? null;
 }
--- src/routes/saksbilde/dialogmelding/testdata.ts
+++ src/routes/saksbilde/dialogmelding/testdata.ts
@@ -1,11 +1,5 @@
-import { ApiBehandlerMedDialoger } from '@io/rest/generated/sporhund.schemas';
-
 export const testBehandlere = [
     { behandlernavn: 'Linus Lege', behandlerId: 'behandlerId-1' },
     { behandlernavn: 'Solveig Lege', behandlerId: 'behandlerId-2' },
     { behandlernavn: 'Christian Lege', behandlerId: 'behandlerId-3' },
 ];
-
-export function finnNyesteDialog(behandlerDialoger: ApiBehandlerMedDialoger[]) {
-    return behandlerDialoger.flatMap((b) => b.dialoger).sort((a, b) => b.tid.localeCompare(a.tid))[0] ?? null;
-}
