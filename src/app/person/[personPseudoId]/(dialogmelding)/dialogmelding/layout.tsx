734c0f8d6602e1aa2280c4b1498f8bb110198541
Elias Andreassen Thøgersen <elias3004@msn.com>
2026-06-08 15:27:51 +0200

    🐛 Fikser hydreringsfeil i dokumentvisning

--- src/app/person/[personPseudoId]/(dialogmelding)/dialogmelding/layout.tsx
+++ src/app/person/[personPseudoId]/(dialogmelding)/dialogmelding/layout.tsx
@@ -1,11 +1,18 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { usePathname } from 'next/navigation';
 import React, { PropsWithChildren, ReactElement } from 'react';
 
 import { DialogmeldingHeader } from '@saksbilde/dialogmelding/DialogmeldingHeader';
-import { Dokumentvisning } from '@saksbilde/dialogmelding/dokumentvisning/Dokumentvisning';
 import { VenstremenyDialogmelding } from '@saksbilde/dialogmelding/venstremeny/VenstremenyDialogmelding';
+
+// Lastes kun på klienten: bredden leses fra localStorage, så server-rendering ville gitt
+// hydration-mismatch / glimt av standardbredde ved refresh.
+const Dokumentvisning = dynamic(
+    () => import('@saksbilde/dialogmelding/dokumentvisning/Dokumentvisning').then((mod) => mod.Dokumentvisning),
+    { ssr: false },
+);
 
 export default function Layout({ children }: PropsWithChildren): ReactElement {
     const pathname = usePathname();
