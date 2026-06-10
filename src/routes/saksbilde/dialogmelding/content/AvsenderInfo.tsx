c4e1f22541d9147becc62b06b159f293b7cf3f91
Linus Johansson <linus.123@hotmail.com>
2026-06-10 09:43:52 +0200

    Her skulle vært til når det var system og ikke fra

--- src/routes/saksbilde/dialogmelding/content/AvsenderInfo.tsx
+++ src/routes/saksbilde/dialogmelding/content/AvsenderInfo.tsx
@@ -22,6 +22,12 @@ export function AvsenderInfo({ avsender, brukerNavn, behandlerNavn }: Props): Re
                 </BodyShort>
             </HStack>
         );
+    } else if (avsender === ApiDialogmeldingAvsender.SYSTEM) {
+        return (
+            <BodyShort className="text-ax-text-neutral-subtle">
+                <span className="font-bold">Til:</span> {behandlerNavn}
+            </BodyShort>
+        );
     } else {
         return (
             <BodyShort className="text-ax-text-neutral-subtle">
