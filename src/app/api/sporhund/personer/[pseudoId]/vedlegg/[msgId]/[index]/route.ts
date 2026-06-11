f1348024772ff7ad9d1170ec41a2baab10602ec8
Elias Andreassen Thøgersen <elias3004@msn.com>
2026-06-11 15:44:45 +0200

    🤡 Tekst inne i mocka vedlegg

--- src/app/api/sporhund/personer/[pseudoId]/vedlegg/[msgId]/[index]/route.ts
+++ src/app/api/sporhund/personer/[pseudoId]/vedlegg/[msgId]/[index]/route.ts
@@ -5,16 +5,24 @@ import { stubEllerVideresendTilSporhund } from '@app/api/sporhund/common';
 const mockPdfContent = `%PDF-1.4
 1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj
 2 0 obj<</Type/Pages/Kids[3 0 R]/Count 1>>endobj
-3 0 obj<</Type/Page/MediaBox[0 0 612 792]/Parent 2 0 R>>endobj
+3 0 obj<</Type/Page/MediaBox[0 0 612 792]/Parent 2 0 R/Contents 5 0 R/Resources<</Font<</F1 4 0 R>>>>>>endobj
+4 0 obj<</Type/Font/Subtype/Type1/BaseFont/Helvetica>>endobj
+5 0 obj<</Length 44>>
+stream
+BT /F1 24 Tf 100 700 Td (Mocked vedlegg) Tj ET
+endstream
+endobj
 xref
-0 4
+0 6
 0000000000 65535 f 
 0000000009 00000 n 
 0000000058 00000 n 
 0000000115 00000 n 
-trailer<</Size 4/Root 1 0 R>>
+0000000266 00000 n 
+0000000340 00000 n 
+trailer<</Size 6/Root 1 0 R>>
 startxref
-190
+440
 %%EOF`;
 
 async function stub(
