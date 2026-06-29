75c88f3b0da9426598afbfc1cf089faeb374c18d
Trine Linderud <trine.linderud@nav.no>
2026-06-29 10:50:01 +0200

    🐛 Splitter opp LeggTilDagerForm i arbeidstaker og selvstendig
    Co-authored-by: Martin Remøy Solheim <3852471+martinmekk@users.noreply.github.com>

--- src/form-schemas/leggTilDagerSkjema.ts
+++ src/form-schemas/leggTilDagerSkjema.ts
@@ -1,56 +1,58 @@
 import { z } from 'zod/v4';
 
 import { kanVelgeGrad } from '@saksbilde/utbetaling/utbetalingstabell/endringForm/kanVelgeGrad';
-import { utbetalingstabelldagtypeValues } from '@typer/utbetalingstabell';
+import { utbetalingstabelldagtypeSelvstendigValues, utbetalingstabelldagtypeValues } from '@typer/utbetalingstabell';
 import { somDato } from '@utils/date';
 
-export type LeggTilDagerFormFields = z.infer<ReturnType<typeof lagLeggTilDagerSchema>>;
+const lagGradValidering = (grad: number | null, ctx: z.RefinementCtx) => {
+    if (grad == null || (grad as unknown) === '') {
+        ctx.addIssue({
+            code: 'custom',
+            message: 'Velg grad',
+            input: grad,
+            path: ['grad'],
+        });
+    } else if (Number.isNaN(grad)) {
+        ctx.addIssue({
+            code: 'custom',
+            message: 'Grad må være et tall',
+            input: grad,
+            path: ['grad'],
+        });
+    } else if (grad < 0) {
+        ctx.addIssue({
+            code: 'too_small',
+            message: `Grad må være minst 0`,
+            minimum: 0,
+            origin: 'number',
+            input: grad,
+            path: ['grad'],
+        });
+    } else if (grad > 100) {
+        ctx.addIssue({
+            code: 'too_big',
+            message: 'Grad må være 100 eller lavere',
+            maximum: 100,
+            origin: 'number',
+            input: grad,
+            path: ['grad'],
+        });
+    }
+};
 
-export const lagLeggTilDagerSchema = (erSelvstendig: boolean) =>
+export const lagLeggTilDagerArbeidstakerSchema = () =>
     z
         .object({
             dagtype: z.enum(utbetalingstabelldagtypeValues),
             fom: z.iso.date(),
             tom: z.iso.date(),
-            grad: z.nullable(z.number()),
+            grad: z.number(),
         })
-        .superRefine(({ fom, tom, dagtype, grad }, ctx) => {
+        .superRefine(({ dagtype, grad }, ctx) => {
             if (kanVelgeGrad(dagtype)) {
-                if (grad == null || (grad as unknown) === '') {
-                    ctx.addIssue({
-                        code: 'custom',
-                        message: 'Velg grad',
-                        input: grad,
-                        path: ['grad'],
-                    });
-                } else if (Number.isNaN(grad)) {
-                    ctx.addIssue({
-                        code: 'custom',
-                        message: 'Grad må være et tall',
-                        input: grad,
-                        path: ['grad'],
-                    });
-                } else if (grad < 0) {
-                    ctx.addIssue({
-                        code: 'too_small',
-                        message: `Grad må være minst 0`,
-                        minimum: 0,
-                        origin: 'number',
-                        input: grad,
-                        path: ['grad'],
-                    });
-                } else if (grad > 100) {
-                    ctx.addIssue({
-                        code: 'too_big',
-                        message: 'Grad må være 100 eller lavere',
-                        maximum: 100,
-                        origin: 'number',
-                        input: grad,
-                        path: ['grad'],
-                    });
-                }
+                lagGradValidering(grad, ctx);
             }
-            if (!erSelvstendig && (dagtype === 'Syk' || dagtype === 'SykNav')) {
+            if (dagtype === 'Syk' || dagtype === 'SykNav') {
                 ctx.addIssue({
                     code: 'custom',
                     message: `Du kan ikke legge til ${dagtype === 'Syk' ? 'Syk' : 'Syk (Nav)'} i forkant av sykmelding`,
@@ -58,10 +60,20 @@ export const lagLeggTilDagerSchema = (erSelvstendig: boolean) =>
                     path: ['dagtype'],
                 });
             }
+        });
+
+export const lagLeggTilDagerSelvstendigSchema = () =>
+    z
+        .object({
+            dagtype: z.enum(utbetalingstabelldagtypeSelvstendigValues),
+            fom: z.iso.date(),
+            tom: z.iso.date(),
+        })
+        .superRefine(({ fom, tom, dagtype }, ctx) => {
             const antallDager = Math.abs(somDato(fom).diff(somDato(tom), 'days'));
             const maksDagerForDagtype = dagtype === 'MeldingTilNav' ? 16 : 18;
 
-            if (erSelvstendig && antallDager > maksDagerForDagtype) {
+            if (antallDager > maksDagerForDagtype) {
                 ctx.addIssue({
                     code: 'custom',
                     message: `Maks ${dagtype === 'MeldingTilNav' ? '16 "Melding til Nav-dager"' : '18 dager'} kan legges inn før sykmeldingen`,
@@ -70,3 +82,6 @@ export const lagLeggTilDagerSchema = (erSelvstendig: boolean) =>
                 });
             }
         });
+
+export type LeggTilDagerArbeidstakerFormFields = z.infer<ReturnType<typeof lagLeggTilDagerArbeidstakerSchema>>;
+export type LeggTilDagerSelvstendigFormFields = z.infer<ReturnType<typeof lagLeggTilDagerSelvstendigSchema>>;
--- src/routes/saksbilde/utbetaling/utbetalingstabell/LeggTilDagerForm.tsx
+++ src/routes/saksbilde/utbetaling/utbetalingstabell/LeggTilDagerForm.tsx
@@ -4,7 +4,12 @@ import { FormProvider, useController, useForm, useWatch } from 'react-hook-form'
 
 import { Button, DatePicker, ErrorMessage, HStack, VStack, useDatepicker } from '@navikt/ds-react';
 
-import { LeggTilDagerFormFields, lagLeggTilDagerSchema } from '@/form-schemas/leggTilDagerSkjema';
+import {
+    LeggTilDagerArbeidstakerFormFields,
+    LeggTilDagerSelvstendigFormFields,
+    lagLeggTilDagerArbeidstakerSchema,
+    lagLeggTilDagerSelvstendigSchema,
+} from '@/form-schemas/leggTilDagerSkjema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Kilde, Kildetype } from '@io/graphql';
 import { GradField } from '@saksbilde/utbetaling/utbetalingstabell/GradField';
@@ -27,94 +32,189 @@ interface LeggTilDagerFormProps {
     erSelvstendig: boolean;
 }
 
-export const LeggTilDagerForm = React.memo(
-    ({ periodeFom, onSubmitPølsestrekk, erSelvstendig }: LeggTilDagerFormProps): ReactElement => {
-        const periodeFomMinusEnDag = dayjs(periodeFom, ISO_DATOFORMAT).subtract(1, 'day');
+export const LeggTilDagerForm = ({ periodeFom, onSubmitPølsestrekk, erSelvstendig }: LeggTilDagerFormProps) => {
+    return erSelvstendig ? (
+        <LeggTilDagerSelvstendigForm periodeFom={periodeFom} onSubmitPølsestrekk={onSubmitPølsestrekk} />
+    ) : (
+        <LeggTilDagerArbeidstakerForm periodeFom={periodeFom} onSubmitPølsestrekk={onSubmitPølsestrekk} />
+    );
+};
 
-        const form = useForm<LeggTilDagerFormFields>({
-            resolver: zodResolver(lagLeggTilDagerSchema(erSelvstendig)),
-            reValidateMode: 'onBlur',
-            shouldUnregister: false,
-            defaultValues: {
-                dagtype: 'Syk',
-                fom: periodeFomMinusEnDag.format(ISO_DATOFORMAT),
-                tom: periodeFom,
-                grad: 100,
-            },
-        });
+interface LeggTilDagerArbeidstakerFormProps {
+    periodeFom: DateString;
+    onSubmitPølsestrekk: (nyeDager: Map<string, Utbetalingstabelldag>) => void;
+}
 
-        const errors = form.formState.errors;
-        const fomError = errors.fom?.message;
-        const gradError = errors.grad?.message;
-        const dagtypeError = errors.dagtype?.message;
-        const watchDag = useWatch({ name: 'dagtype', control: form.control });
+const LeggTilDagerArbeidstakerForm = ({
+    periodeFom,
+    onSubmitPølsestrekk,
+}: LeggTilDagerArbeidstakerFormProps): ReactElement => {
+    const periodeFomMinusEnDag = dayjs(periodeFom, ISO_DATOFORMAT).subtract(1, 'day');
 
-        const overstyringsdagtyper = erSelvstendig
-            ? overstyringsdagtyperSelvstendig.filter((dag) => dag !== AvslattMeldingTilNavdag)
-            : overstyringsdagtyperArbeidstaker;
+    const form = useForm<LeggTilDagerArbeidstakerFormFields>({
+        resolver: zodResolver(lagLeggTilDagerArbeidstakerSchema()),
+        reValidateMode: 'onBlur',
+        shouldUnregister: false,
+        defaultValues: {
+            dagtype: 'Syk',
+            fom: periodeFomMinusEnDag.format(ISO_DATOFORMAT),
+            tom: periodeFom,
+            grad: 100,
+        },
+    });
 
-        const handleSubmit = (values: LeggTilDagerFormFields) => {
-            const nyeDagerMap = new Map<string, Utbetalingstabelldag>();
+    const errors = form.formState.errors;
+    const fomError = errors.fom?.message;
+    const gradError = errors.grad?.message;
+    const dagtypeError = errors.dagtype?.message;
+    const watchDag = useWatch({ name: 'dagtype', control: form.control });
 
-            let endringFom = dayjs(values.fom, ISO_DATOFORMAT);
-            while (endringFom.isBefore(dayjs(periodeFom, ISO_DATOFORMAT))) {
-                nyeDagerMap.set(endringFom.format(ISO_DATOFORMAT), {
-                    dato: endringFom.format(ISO_DATOFORMAT),
-                    kilde: { type: Kildetype.Saksbehandler } as Kilde,
-                    dag: alleTypeendringer.find((dag) => dag.speilDagtype === values.dagtype)!,
-                    erAGP: false,
-                    erVentetid: false,
-                    erAvvist: false,
-                    erForeldet: false,
-                    erMaksdato: false,
-                    grad: values?.grad,
-                    erNyDag: true,
-                });
-                endringFom = endringFom.add(1, 'day');
-            }
+    const handleSubmit = (values: LeggTilDagerArbeidstakerFormFields) => {
+        const nyeDagerMap = new Map<string, Utbetalingstabelldag>();
 
-            onSubmitPølsestrekk(nyeDagerMap);
-        };
+        let endringFom = dayjs(values.fom, ISO_DATOFORMAT);
+        while (endringFom.isBefore(dayjs(periodeFom, ISO_DATOFORMAT))) {
+            nyeDagerMap.set(endringFom.format(ISO_DATOFORMAT), {
+                dato: endringFom.format(ISO_DATOFORMAT),
+                kilde: { type: Kildetype.Saksbehandler } as Kilde,
+                dag: alleTypeendringer.find((dag) => dag.speilDagtype === values.dagtype)!,
+                erAGP: false,
+                erVentetid: false,
+                erAvvist: false,
+                erForeldet: false,
+                erMaksdato: false,
+                grad: values?.grad,
+                erNyDag: true,
+            });
+            endringFom = endringFom.add(1, 'day');
+        }
 
-        return (
-            <FormProvider {...form}>
-                <form onSubmit={form.handleSubmit(handleSubmit)} autoComplete="off">
-                    <HStack gap="space-8" align="end" paddingBlock="space-0 space-8">
-                        <DateField name="fom" label="Dato f.o.m." defaultMonth={periodeFomMinusEnDag.toDate()} />
-                        <DateField name="tom" label="Dato t.o.m." disabled />
-                        <DagtypeSelect
-                            name="dagtype"
-                            erSelvstendig={erSelvstendig}
-                            overstyringsdagtyper={overstyringsdagtyper}
-                            hideError
-                        />
-                        <GradField name="grad" kanIkkeVelgeDagtype={!kanVelgeGrad(watchDag)} hideError />
-                        <Button size="small" type="submit" variant="secondary" data-testid="legg-til">
-                            Legg til
-                        </Button>
-                    </HStack>
-                    <VStack>
-                        {fomError && (
-                            <ErrorMessage size="small" showIcon>
-                                Dato f.o.m: {fomError}
-                            </ErrorMessage>
+        onSubmitPølsestrekk(nyeDagerMap);
+    };
+
+    return (
+        <FormProvider {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)} autoComplete="off">
+                <HStack gap="space-8" align="end" paddingBlock="space-0 space-8">
+                    <DateField name="fom" label="Dato f.o.m." defaultMonth={periodeFomMinusEnDag.toDate()} />
+                    <DateField name="tom" label="Dato t.o.m." disabled />
+                    <DagtypeSelect
+                        name="dagtype"
+                        erSelvstendig={false}
+                        overstyringsdagtyper={overstyringsdagtyperArbeidstaker}
+                        hideError
+                    />
+                    <GradField name="grad" kanIkkeVelgeDagtype={!kanVelgeGrad(watchDag)} hideError />
+                    <Button size="small" type="submit" variant="secondary" data-testid="legg-til">
+                        Legg til
+                    </Button>
+                </HStack>
+                <VStack>
+                    {fomError && (
+                        <ErrorMessage size="small" showIcon>
+                            Dato f.o.m: {fomError}
+                        </ErrorMessage>
+                    )}
+                    {dagtypeError && (
+                        <ErrorMessage size="small" showIcon>
+                            Dagtype: {dagtypeError}
+                        </ErrorMessage>
+                    )}
+                    {gradError && (
+                        <ErrorMessage size="small" showIcon>
+                            Grad: {gradError}
+                        </ErrorMessage>
+                    )}
+                </VStack>
+            </form>
+        </FormProvider>
+    );
+};
+
+interface LeggTilDagerSelvstendigFormProps {
+    periodeFom: DateString;
+    onSubmitPølsestrekk: (nyeDager: Map<string, Utbetalingstabelldag>) => void;
+}
+
+const LeggTilDagerSelvstendigForm = ({
+    periodeFom,
+    onSubmitPølsestrekk,
+}: LeggTilDagerSelvstendigFormProps): ReactElement => {
+    const periodeFomMinusEnDag = dayjs(periodeFom, ISO_DATOFORMAT).subtract(1, 'day');
+
+    const form = useForm<LeggTilDagerSelvstendigFormFields>({
+        resolver: zodResolver(lagLeggTilDagerSelvstendigSchema()),
+        reValidateMode: 'onBlur',
+        shouldUnregister: false,
+        defaultValues: {
+            dagtype: 'MeldingTilNav',
+            fom: periodeFomMinusEnDag.format(ISO_DATOFORMAT),
+            tom: periodeFom,
+        },
+    });
+
+    const errors = form.formState.errors;
+    const fomError = errors.fom?.message;
+    const dagtypeError = errors.dagtype?.message;
+    const watchDag = useWatch({ name: 'dagtype', control: form.control });
+
+    const handleSubmit = (values: LeggTilDagerSelvstendigFormFields) => {
+        const nyeDagerMap = new Map<string, Utbetalingstabelldag>();
+
+        let endringFom = dayjs(values.fom, ISO_DATOFORMAT);
+        while (endringFom.isBefore(dayjs(periodeFom, ISO_DATOFORMAT))) {
+            nyeDagerMap.set(endringFom.format(ISO_DATOFORMAT), {
+                dato: endringFom.format(ISO_DATOFORMAT),
+                kilde: { type: Kildetype.Saksbehandler } as Kilde,
+                dag: alleTypeendringer.find((dag) => dag.speilDagtype === values.dagtype)!,
+                erAGP: false,
+                erVentetid: false,
+                erAvvist: false,
+                erForeldet: false,
+                erMaksdato: false,
+                erNyDag: true,
+            });
+            endringFom = endringFom.add(1, 'day');
+        }
+
+        onSubmitPølsestrekk(nyeDagerMap);
+    };
+
+    return (
+        <FormProvider {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)} autoComplete="off">
+                <HStack gap="space-8" align="end" paddingBlock="space-0 space-8">
+                    <DateField name="fom" label="Dato f.o.m." defaultMonth={periodeFomMinusEnDag.toDate()} />
+                    <DateField name="tom" label="Dato t.o.m." disabled />
+                    <DagtypeSelect
+                        name="dagtype"
+                        erSelvstendig={true}
+                        overstyringsdagtyper={overstyringsdagtyperSelvstendig.filter(
+                            (dag) => dag !== AvslattMeldingTilNavdag,
                         )}
-                        {dagtypeError && (
-                            <ErrorMessage size="small" showIcon>
-                                Dagtype: {dagtypeError}
-                            </ErrorMessage>
-                        )}
-                        {gradError && (
-                            <ErrorMessage size="small" showIcon>
-                                Grad: {gradError}
-                            </ErrorMessage>
-                        )}
-                    </VStack>
-                </form>
-            </FormProvider>
-        );
-    },
-);
+                        hideError
+                    />
+                    <GradField name="grad" kanIkkeVelgeDagtype={!kanVelgeGrad(watchDag)} hideError />
+                    <Button size="small" type="submit" variant="secondary" data-testid="legg-til">
+                        Legg til
+                    </Button>
+                </HStack>
+                <VStack>
+                    {fomError && (
+                        <ErrorMessage size="small" showIcon>
+                            Dato f.o.m: {fomError}
+                        </ErrorMessage>
+                    )}
+                    {dagtypeError && (
+                        <ErrorMessage size="small" showIcon>
+                            Dagtype: {dagtypeError}
+                        </ErrorMessage>
+                    )}
+                </VStack>
+            </form>
+        </FormProvider>
+    );
+};
 
 interface DateFieldProps {
     name: string;
@@ -154,5 +254,3 @@ function DateField({ name, label, defaultMonth, disabled, className }: DateField
         </DatePicker>
     );
 }
-
-LeggTilDagerForm.displayName = 'LeggTilDagerForm';
