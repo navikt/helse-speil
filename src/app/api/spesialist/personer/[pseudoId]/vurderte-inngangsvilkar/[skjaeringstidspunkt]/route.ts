bdbb44ab1d9b255778a6a97f22d9722afb9aa9dd
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-08-13 09:21:39 +0200

    🔥 Slett resten av koden relatert til inngangsvilkår
    Operasjonene er borte, og koden kompilerer ikke

--- src/app/api/spesialist/personer/[pseudoId]/vurderte-inngangsvilkar/[skjaeringstidspunkt]/route.ts
+++
@@ -1,8 +0,0 @@
-import { stubEllerVideresendTilSpesialist } from '@app/api/spesialist/common';
-
-import { stub } from './stub';
-
-type Params = { pseudoId: string; skjaeringstidspunkt: string };
-
-export const GET = stubEllerVideresendTilSpesialist<Params>(stub);
-export const POST = stubEllerVideresendTilSpesialist<Params>(stub);
